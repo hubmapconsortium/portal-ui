08a7499ea96fd7c08275102296e25931ea5fbd83
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2019-10-08 12:36:27 -0400

    Stub out all the pages of the sitemap

--- portal/main.py
+++ portal/main.py
@@ -3,10 +3,22 @@ from jinja2 import TemplateNotFound
 
 blueprint = Blueprint('main', __name__, template_folder='templates')
 
-@blueprint.route('/', defaults={'page': 'index'})
-@blueprint.route('/<page>')
-def show(page):
-    try:
-        return render_template('pages/%s.html' % page)
-    except TemplateNotFound:
-        abort(404)
+
+@blueprint.route('/')
+def index():
+    return render_template('pages/index.html')
+
+
+@blueprint.route('/browse/<type>')
+def browse(type):
+    return render_template('pages/browse.html')
+
+
+@blueprint.route('/browse/<type>/<id>')
+def details(type, id):
+    return render_template('pages/details.html')
+
+
+@blueprint.route('/help')
+def help():
+    return render_template('pages/help.html')
