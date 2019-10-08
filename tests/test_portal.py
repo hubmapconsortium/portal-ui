262a4062041143c0729dce2d861a1390fa3dff38
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2019-10-08 15:11:53 -0400

    Test HTTP response codes

--- tests/test_portal.py
+++ tests/test_portal.py
@@ -12,11 +12,30 @@ def client():
     with app.test_client() as client:
         with app.app_context():
             pass
-            # TODO: Any necessary initialization.
-            # flaskr.init_db()
+            # Do any necessary initializations here.
         yield client
 
 
 def test_home(client):
     response = client.get('/')
     assert response.status == '200 OK'
+
+def test_browse(client):
+    response = client.get('/browse/dataset')
+    assert response.status == '200 OK'
+
+def test_details(client):
+    response = client.get('/browse/dataset/1')
+    assert response.status == '200 OK'
+
+def test_help(client):
+    response = client.get('/help')
+    assert response.status == '200 OK'
+
+def test_404(client):
+    response = client.get('/no-page-here')
+    assert response.status == '404 NOT FOUND'
+
+def test_404_browse(client):
+    response = client.get('/browse/no-such-type')
+    assert response.status == '404 NOT FOUND'
