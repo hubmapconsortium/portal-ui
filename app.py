89b5f82a5e7e45bca79edd96fe8b44ed521fa69a
mccalluc <mccalluc@users.noreply.github.com>
2019-11-12 13:34:17 -0500

    Move auth routes to separate file; Remove vestigial config

--- app.py
+++ app.py
@@ -2,18 +2,21 @@
 
 from flask import Flask, session
 
-from portal import routes
+from portal import routes, routes_auth
 
 
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_pyfile('app.conf')
 app.register_blueprint(routes.blueprint)
+app.register_blueprint(routes_auth.blueprint)
+
 
 @app.context_processor
 def inject_template_globals():
     return {
         'is_authenticated': session.get('is_authenticated')
     }
+
 
 # actually run the app if this is called as a script
 if __name__ == '__main__':
--- portal/__init__.py
+++ portal/__init__.py
@@ -1,14 +1,1 @@
-from flask import Flask
-import globus_sdk
 
-from . import routes
-
-
-def create_app(test_config=None):
-    # Create and configure the app:
-    app = Flask(__name__, instance_relative_config=True)
-    app.config.from_pyfile('app.conf')
-
-    app.register_blueprint(routes.blueprint)
-
-    return app
