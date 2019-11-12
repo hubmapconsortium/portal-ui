f66234adfd805627e15fd50a8d0ca7e5b4fded22
mccalluc <mccalluc@users.noreply.github.com>
2019-11-12 13:16:11 -0500

    Toggles between login/logout

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from flask import Flask
+from flask import Flask, session
 
 from portal import routes
 
@@ -8,6 +8,12 @@ from portal import routes
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_pyfile('app.conf')
 app.register_blueprint(routes.blueprint)
+
+@app.context_processor
+def inject_template_globals():
+    return {
+        'is_authenticated': session.get('is_authenticated')
+    }
 
 # actually run the app if this is called as a script
 if __name__ == '__main__':
