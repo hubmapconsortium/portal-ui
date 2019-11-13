9eb4502664ed519953e81eeccf965c36769c8fa3
mccalluc <mccalluc@users.noreply.github.com>
2019-11-13 14:05:39 -0500

    Fix constructor signature

--- portal/api/test_client.py
+++ portal/api/test_client.py
@@ -5,6 +5,6 @@ from portal.api.client import ApiClient
 
 def test_api_get_entity_types(mocker):
     mocker.patch('requests.get')
-    client = ApiClient('http://FAKE')
+    client = ApiClient('http://FAKE', 'FAKE-TOKEN')
     client.get_entity_types()
     requests.get.assert_called_once_with('http://FAKE/entities')
