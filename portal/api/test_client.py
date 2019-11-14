d054966f9285d73e625a0ecf0f1a20bfcb55f66d
mccalluc <mccalluc@users.noreply.github.com>
2019-11-14 11:03:05 -0500

    whitespace

--- portal/api/test_client.py
+++ portal/api/test_client.py
@@ -9,6 +9,7 @@ def test_api_get_entity_types(mocker):
     client.get_entity_types()
     requests.get.assert_called_once_with('http://FAKE/entities')
 
+
 def test_format_timestamp():
     assert(_format_timestamp(1000) == '1970-01-01 00:00:01')
     assert(_format_timestamp(1000000000000) == '2001-09-09 01:46:40')
