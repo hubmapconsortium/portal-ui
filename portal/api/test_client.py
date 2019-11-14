53fd6b4476e0ca5d9b9d35d1a1f656bd8153c0d5
mccalluc <mccalluc@users.noreply.github.com>
2019-11-14 11:02:04 -0500

    test timestamp formatting

--- portal/api/test_client.py
+++ portal/api/test_client.py
@@ -1,6 +1,6 @@
 import requests
 
-from portal.api.client import ApiClient
+from portal.api.client import ApiClient, _format_timestamp
 
 
 def test_api_get_entity_types(mocker):
@@ -8,3 +8,7 @@ def test_api_get_entity_types(mocker):
     client = ApiClient('http://FAKE', 'FAKE-TOKEN')
     client.get_entity_types()
     requests.get.assert_called_once_with('http://FAKE/entities')
+
+def test_format_timestamp():
+    assert(_format_timestamp(1000) == '1970-01-01 00:00:01')
+    assert(_format_timestamp(1000000000000) == '2001-09-09 01:46:40')
