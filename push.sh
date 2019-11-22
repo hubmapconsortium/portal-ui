6607411e99f568ccf7d9d7478e205357463e35d5
mccalluc <mccalluc@users.noreply.github.com>
2019-11-22 14:04:08 -0500

    bash syntax

--- push.sh
+++ push.sh
@@ -5,7 +5,7 @@ die() { set +v; echo "$*" 1>&2 ; exit 1; }
 git diff --quiet || die 'Uncommitted changes: Stash or commit before push.'
 
 VERSION=`cat VERSION`
-IMAGE_NAME = hubmap/portal-ui:$VERSION
+IMAGE_NAME=hubmap/portal-ui:$VERSION
 
 git tag $VERSION
 git push origin --tags
