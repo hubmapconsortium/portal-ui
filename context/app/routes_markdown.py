bae1606d6ca3c3fe28b25ec959050385c4daf31e
mccalluc <mccalluc@users.noreply.github.com>
2019-12-04 14:12:18 -0500

    Read from file

--- context/app/routes_markdown.py
+++ context/app/routes_markdown.py
@@ -1,6 +1,8 @@
 import re
+from glob import glob
+from os.path import realpath, dirname
 
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, current_app
 import mistune
 
 from .config import types
@@ -10,9 +12,13 @@ blueprint = Blueprint('routes_markdown', __name__, template_folder='templates')
 def title_from_md(md):
     return re.search(r'^#+\s+(\S.*)', md, re.MULTILINE)[1]
 
+# glob(f'{dirname(__file__)}/markdown/**/*.md', recursive=True)
+
+
 @blueprint.route('/fake')
 def fake():
-    content_md = '# Howdy!\n*Content!*'
+    with open(f'{dirname(__file__)}/markdown/fake.md') as md_file:
+        content_md = md_file.read()
     title = title_from_md(content_md)
     content_html = mistune.markdown(content_md)
 
