22362d6a740b0c87a9965600f11b580b6f92a1cd
mccalluc <mccalluc@users.noreply.github.com>
2019-12-04 11:34:10 -0500

    Parse out title from markdown

--- context/app/routes_markdown.py
+++ context/app/routes_markdown.py
@@ -1,3 +1,5 @@
+import re
+
 from flask import Blueprint, render_template
 import mistune
 
@@ -5,10 +7,13 @@ from .config import types
 
 blueprint = Blueprint('routes_markdown', __name__, template_folder='templates')
 
+def title_from_md(md):
+    return re.search(r'^#+\s+(\S.*)', md, re.MULTILINE)[1]
 
 @blueprint.route('/fake')
 def fake():
-    content_md = '*Content!*'
+    content_md = '# Howdy!\n*Content!*'
+    title = title_from_md(content_md)
     content_html = mistune.markdown(content_md)
 
-    return render_template('pages/markdown.html', types=types, title='title!!!', content_html=content_html)
+    return render_template('pages/markdown.html', types=types, title=title, content_html=content_html)
