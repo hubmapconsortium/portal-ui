ffa1583e4d779228e73ac9a1a6201ad4f610a3fd
mccalluc <mccalluc@users.noreply.github.com>
2019-12-04 15:02:27 -0500

    Load md matching request path

--- context/app/markdown/fake.md
+++ context/app/markdown/fake.md
@@ -1,3 +1,3 @@
 # Howdy!
 
-*Content!!!*
+*Content!*
--- context/app/routes_markdown.py
+++ context/app/routes_markdown.py
@@ -2,7 +2,7 @@ import re
 from glob import glob
 from os.path import realpath, dirname
 
-from flask import Blueprint, render_template, current_app
+from flask import Blueprint, render_template, request
 import mistune
 
 from .config import types
@@ -12,14 +12,16 @@ blueprint = Blueprint('routes_markdown', __name__, template_folder='templates')
 def title_from_md(md):
     return re.search(r'^#+\s+(\S.*)', md, re.MULTILINE)[1]
 
-# glob(f'{dirname(__file__)}/markdown/**/*.md', recursive=True)
 
-
-@blueprint.route('/fake')
-def fake():
-    with open(f'{dirname(__file__)}/markdown/fake.md') as md_file:
+def markdown_view():
+    with open(dirname(__file__) + '/markdown/' + request.path + '.md') as md_file:
         content_md = md_file.read()
     title = title_from_md(content_md)
     content_html = mistune.markdown(content_md)
-
     return render_template('pages/markdown.html', types=types, title=title, content_html=content_html)
+
+
+app_dir = dirname(__file__)
+for f in glob(app_dir + '/**/*.md', recursive=True):
+    route = f.replace(app_dir + '/markdown', '').replace('.md', '')
+    markdown_route = blueprint.route(route)(markdown_view)
