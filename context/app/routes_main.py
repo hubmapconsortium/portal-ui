44aa17820aa633548bef7d2b21df8ebfb59f20bd
mccalluc <mccalluc@users.noreply.github.com>
2019-12-17 14:42:29 -0500

    No link if no _TODO

--- context/app/routes_main.py
+++ context/app/routes_main.py
@@ -50,7 +50,11 @@ def details(type, uuid):
         path = list(error.schema_path)
         path[-1] = path[-1] + '_TODO'
         for path_component in path:
-            schema_cursor = schema_cursor[path_component]
+            if path_component in schema_cursor:
+                schema_cursor = schema_cursor[path_component]
+            else:
+                schema_cursor = None
+                break
         error.issue_url = schema_cursor
         flash(error)
 
