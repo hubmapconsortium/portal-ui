a51c6be839904763a8c0e27357305636b71daf4b
mccalluc <mccalluc@users.noreply.github.com>
2019-12-17 16:22:05 -0500

    Add tests

--- context/app/test_validation_utils.py
+++ context/app/test_validation_utils.py
@@ -14,3 +14,59 @@ def test_multiple_errors():
         callback
     )
     assert callback.call_count == 2
+
+
+def test_single_error():
+    callback = MagicMock()
+    for_each_validation_error(
+        {'required_1': 'value'},
+        {'required': ['required_1', 'required_2']},
+        callback
+    )
+    assert callback.call_count == 1
+
+
+def test_called_without_url():
+    def callback(error):
+        assert error.message == "'required_1' is a required property"
+        assert error.issue_url == None
+    for_each_validation_error(
+        {},
+        {'required': ['required_1']},
+        callback
+    )
+
+
+def test_called_with_url():
+    def callback(error):
+        assert error.message == "'required_1' is a required property"
+        assert error.issue_url == 'github url'
+    for_each_validation_error(
+        {},
+        {
+            'required_TODO': 'github url',
+            'required': ['required_1']
+        },
+        callback
+    )
+
+
+def test_deep():
+    def callback(error):
+        assert error.issue_url == 'github url'
+    for_each_validation_error(
+        {},
+        {
+            'properties': {
+                'outer': {
+                    'properties': {
+                        'inner': {
+                            'required_TODO': 'github url',
+                            'required': ['deep_prop']
+                        }
+                    }
+                }
+            }
+        },
+        callback
+    )
