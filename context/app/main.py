7f2c9cbaa975dfe799b7b99015565f41b7ec5d49
mccalluc <mccalluc@users.noreply.github.com>
2019-12-19 19:03:55 -0500

    implement friendly error pages

--- context/app/main.py
+++ context/app/main.py
@@ -1,14 +1,25 @@
-from flask import Flask, session
+from flask import Flask, session, render_template
 
 from . import routes_main, routes_auth, routes_markdown
+
+def not_found(e):
+    return render_template('errors/404.html', types={}), 404
+
+
+def access_denied(e):
+    return render_template('errors/403.html', types={}), 403
 
 
 def create_app():
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_pyfile('app.conf')
+
     app.register_blueprint(routes_main.blueprint)
     app.register_blueprint(routes_auth.blueprint)
     app.register_blueprint(routes_markdown.blueprint)
+
+    app.register_error_handler(404, not_found)
+    app.register_error_handler(403, access_denied)
 
     @app.context_processor
     def inject_template_globals():
