4e72659b8e96cd39d17d6ca294c5d4c08093efc4
mccalluc <mccalluc@users.noreply.github.com>
2019-12-23 23:17:40 -0500

    Test 400

--- context/app/test_routes_main.py
+++ context/app/test_routes_main.py
@@ -4,6 +4,7 @@ from xml.etree.ElementTree import ParseError
 import json
 
 import pytest
+import requests
 
 from .main import create_app
 from .config import types
@@ -113,6 +114,24 @@ def test_403_html_page(client_not_logged_in):
     assert response.status == '403 FORBIDDEN'
     assert_is_valid_html(response)
     assert '403: Access Denied' in response.data.decode('utf8')
+
+
+def mock_get_400(path, **kwargs):
+    class MockResponse():
+        def __init__(self):
+            self.status_code = 400
+            self.text = 'Logger call requires this'
+        def raise_for_status(self):
+            raise requests.exceptions.HTTPError(response=self)
+    return MockResponse()
+
+
+def test_400_html_page(client, mocker):
+    mocker.patch('requests.get', side_effect=mock_get_400)
+    response = client.get('/browse/donor')
+    assert response.status == '400 BAD REQUEST'
+    assert_is_valid_html(response)
+    assert '400: Bad Request' in response.data.decode('utf8')
 
 
 def test_login(client):
