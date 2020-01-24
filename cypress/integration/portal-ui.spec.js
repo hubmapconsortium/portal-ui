3a82f8e4d5c815c013a1579cf8fb3398610155d8
mccalluc <mccalluc@users.noreply.github.com>
2020-01-24 11:39:14 -0500

    Test more navigation

--- cypress/integration/portal-ui.spec.js
+++ cypress/integration/portal-ui.spec.js
@@ -6,7 +6,39 @@ describe('portal-ui', () => {
   });
 
   it('works', () => {
+    // Homepage
     cy.visit('/');
     cy.contains('Welcome to HuBMAP');
+    cy.contains('Human BioMolecular Atlas Program: Supported by the NIH Common Fund.');
+
+    // Donors browse
+    cy.contains('Donors').click();
+    cy.contains('Browse donor')
+
+    // Donors details
+    cy.contains('TODO: name').click();
+    cy.contains('Warning!');
+    cy.contains('Mock Entity');
+    // Provenance
+    cy.contains('undefined - Input');
+    // Vitessce
+    cy.contains('Scatterplot (t-SNE)')
+    cy.contains('4839 cells');
+
+    // Samples browse
+    cy.contains('Samples').click();
+    cy.contains('Browse sample');
+
+    // Sample details
+    cy.contains('TODO: name').click();
+    cy.contains('Warning!');
+    cy.contains('Mock Entity');
+
+    // Help
+    cy.contains('Help').click();
+    cy.contains('TODO: Say something helpful here!');
+
+    // Login
+    // cy.contains('Login').click();
   });
 });
