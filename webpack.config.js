f9b36a2400b490ec538bf8649fcc7eb0ff97d358
tsliaw <16737715+tsliaw@users.noreply.github.com>
2020-04-24 16:50:10 -0700

    Added favicon with webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const webpack = require('webpack');
 const resolve = require('path').resolve;
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin')
 
 const config = {
   entry: './context/app/static/js/index.jsx',
@@ -57,6 +58,9 @@ const config = {
         use: { loader: "@svgr/webpack" },
       },
     ]
-  }
+  },
+  plugins: [
+    new FaviconsWebpackPlugin('./context/app/static/favicon.ico')
+  ]
 };
 module.exports = config;
