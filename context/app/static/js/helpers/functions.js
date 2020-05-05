27ab2e3eb6c8a02416ae86d00e0786431a046a88
John Conroy <62477388+john-conroy@users.noreply.github.com>
2020-05-05 09:31:38 -0400

    Refactor isEmptyArrayOrObject helper func
    Co-authored-by: Chuck McCallum <mccalluc@users.noreply.github.com>

--- context/app/static/js/helpers/functions.js
+++ context/app/static/js/helpers/functions.js
@@ -1,9 +1,9 @@
 export function isEmptyArrayOrObject(val) {
-  let ret = false;
   if (val.constructor.name === 'Object') {
-    ret = Object.keys(val).length === 0;
-  } else if (val.constructor.name === 'Array') {
-    ret = val.length === 0;
+    return Object.keys(val).length === 0;
   }
-  return ret;
+  if (val.constructor.name === 'Array') {
+    return val.length === 0;
+  }
+  return false;
 }
