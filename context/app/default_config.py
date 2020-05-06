a141cfb0dc9a85dce21a4a3eac507839732148c8
mccalluc <mccalluc@users.noreply.github.com>
2020-05-06 12:14:20 -0400

    Update DefaultConfig to be consistent and add explanatory note

--- context/app/default_config.py
+++ context/app/default_config.py
@@ -1,10 +1,16 @@
 class DefaultConfig(object):
+    # This should be updated when app.conf is updated:
+    # Test runs will see this config rather than app.conf.
+    # Tests should not make API calls...
+    # but they may expect certain keys to be present.
+
     ENTITY_API_TIMEOUT = 5
     ENTITY_API_BASE = 'should-be-overridden'
 
     GROUP_ID = 'should-be-overridden'
 
     ELASTICSEARCH_ENDPOINT = 'should-be-overridden'
+    ASSETS_ENDPOINT = 'should-be-overridden'
 
     SECRET_KEY = 'should-be-overridden'
     APP_CLIENT_ID = 'should-be-overridden'
