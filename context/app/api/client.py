cbc1585f2c6a8ddc964c1a9fcea388d10ac9deb4
ilan-gold <ilanbassgold@gmail.com>
2020-05-12 18:46:16 -0400

    some formatting

--- context/app/api/client.py
+++ context/app/api/client.py
@@ -46,20 +46,20 @@ IMAGING = {
     "name": 'NAME',
     "layers": [],
     "staticLayout": [
-      { "component": 'layerController', "x": 0, "y": 0, "w": 4, "h": 6 },
-      {
-        "component": 'spatial',
-        "props": {
-          "view": {
-            "zoom": -1,
-            "target": [512, 512, 0],
-          },
+        {"component": 'layerController', "x": 0, "y": 0, "w": 4, "h": 6},
+        {
+            "component": 'spatial',
+            "props": {
+                "view": {
+                    "zoom": -1,
+                    "target": [512, 512, 0],
+                },
+            },
+            "x": 4,
+            "y": 0,
+            "w": 8,
+            "h": 6,
         },
-        "x": 4,
-        "y": 0,
-        "w": 8,
-        "h": 6,
-      },
     ],
 }
 
@@ -68,7 +68,7 @@ ASSAY_CONF_LOOKUP = {
     "salmon_rnaseq_10x": {
         "base_conf": SCATTERPLOT,
         "files_conf": [
-            { "rel_path": "dim_reduced_clustered/dim_reduced_clustered.json", "type": "CELLS" }, 
+            {"rel_path": "dim_reduced_clustered/dim_reduced_clustered.json", "type": "CELLS"},
             # { "rel_path": "cluster-marker-genes/cluster_marker_genes.json", "type": "FACTORS" }
         ]
     },
@@ -86,6 +86,7 @@ IMAGE_ASSAYS = ["codex_cytokit"]
 
 def _format_timestamp(ts):
     return datetime.utcfromtimestamp(int(ts) / 1000).strftime('%Y-%m-%d %H:%M:%S')
+
 
 class ApiClient():
     def __init__(self, url_base=None, nexus_token=None, is_mock=False):
@@ -247,41 +248,41 @@ class ApiClient():
             )
             token = 'fake-token'
             return {
-            "description": "DEMO",
-            "layers": [
-                {
-                    "name": "cells",
-                    "type": "CELLS",
-                    "url": cellsUri,
-                    "requestInit": {
-                        "headers": {
-                            'Authorization': 'Bearer ' + token
-                        }
-                    }
-                },
-            ],
-            "name": "Linnarsson",
-            "staticLayout": [
-                {
-                    "component": "scatterplot",
-                    "props": {
-                        "mapping": "UMAP",
-                        "view": {
-                            "zoom": 4,
-                            "target": [0, 0, 0]
+                "description": "DEMO",
+                "layers": [
+                    {
+                        "name": "cells",
+                        "type": "CELLS",
+                        "url": cellsUri,
+                        "requestInit": {
+                            "headers": {
+                                'Authorization': 'Bearer ' + token
+                            }
                         }
                     },
-                    "x": 0, "y": 0, "w": 12, "h": 2
-                },
-            ]
-        }
+                ],
+                "name": "Linnarsson",
+                "staticLayout": [
+                    {
+                        "component": "scatterplot",
+                        "props": {
+                            "mapping": "UMAP",
+                            "view": {
+                                "zoom": 4,
+                                "target": [0, 0, 0]
+                            }
+                        },
+                        "x": 0, "y": 0, "w": 12, "h": 2
+                    },
+                ]
+            }
         else:
             return self._build_vitessce_conf(entity)
-    
+
     def _build_assets_url(self, rel_path, uuid):
         base_url = urllib.parse.urljoin(
-            current_app.config['ASSETS_ENDPOINT'], 
-            str(Path(uuid) / Path(rel_path)) 
+            current_app.config['ASSETS_ENDPOINT'],
+            str(Path(uuid) / Path(rel_path))
         )
         token_param = urllib.parse.urlencode({"token": self.nexus_token})
         return base_url + '?' + token_param
@@ -292,27 +293,29 @@ class ApiClient():
         schema['type'] = 'ome-tiff'
         schema['url'] = self._build_assets_url(rel_path, uuid)
         schema['metadata'] = {}
-        offsets_path = Path(CODEX_OFFSETS_PATH) / Path(Path(rel_path).name.replace('ome.tiff', 'offsets.json'))
+        offsets_path = Path(CODEX_OFFSETS_PATH) / \
+            Path(Path(rel_path).name.replace('ome.tiff', 'offsets.json'))
         schema['metadata']['omeTiffOffsetsUrl'] = self._build_assets_url(str(offsets_path), uuid)
         return schema
-    
+
     def _build_image_layer_datauri(self, rel_path, uuid):
         image_layer = {}
         image_paths = [
             str(rel_path).replace('#CODEX_TILE#', f'R{str(r).zfill(3)}_X{str(x).zfill(3)}_Y{str(y).zfill(3)}') for (r, x, y) in itertools.product(*[R_VALS, X_VALS, Y_VALS])
         ]
-        image_layer['images'] = [self._build_image_schema(image_path, uuid) for image_path in image_paths]
+        image_layer['images'] = [self._build_image_schema(
+            image_path, uuid) for image_path in image_paths]
         image_layer['schema_version'] = '0.0.1'
         return DataURI.make(
-            'text/plain', charset='us-ascii', base64=True, data= json.dumps(image_layer)
+            'text/plain', charset='us-ascii', base64=True, data=json.dumps(image_layer)
         )
 
     def _build_layer_conf(self, file, uuid, assay_type):
         return {
             "type": file["type"],
-            "url": self._build_assets_url(file["rel_path"], uuid) 
-                if assay_type not in IMAGE_ASSAYS 
-                else self._build_image_layer_datauri(file["rel_path"], uuid),
+            "url": self._build_assets_url(file["rel_path"], uuid)
+            if assay_type not in IMAGE_ASSAYS
+            else self._build_image_layer_datauri(file["rel_path"], uuid),
             "name": file["type"].lower(),
         }
 
