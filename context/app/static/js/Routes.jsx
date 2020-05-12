78109b5bd421a339e762174808750a4da415f1cf
John Conroy <john-conroy@users.noreply.github.com>
2020-05-12 17:25:05 -0400

    Pass elasticsearch endpoint from flask to Search

--- context/app/static/js/Routes.jsx
+++ context/app/static/js/Routes.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable camelcase */
 import React from 'react';
+import PropTypes from 'prop-types';
 import NoticeAlert from './components/NoticeAlert';
 import Details from './components/Details';
 import Home from './components/Home';
@@ -8,14 +9,15 @@ import Search from './components/Search/Search';
 function Routes(props) {
   const { flaskData } = props;
   const {
-    flashed_messages, entity, provenance, vitessce_conf,
+    flashed_messages, entity, provenance, vitessce_conf, endpoints,
   } = flaskData;
   const urlPath = window.location.pathname;
 
-  if (urlPath.indexOf('/browse') > -1) {
+  if (urlPath.startsWith('/browse')) {
     return (
       <>
-        {flashed_messages && flashed_messages.length && <NoticeAlert errors={flashed_messages} />}
+        {flashed_messages && flashed_messages.length
+          ? <NoticeAlert errors={flashed_messages} /> : null}
         <Details
           assayMetaData={entity}
           provData={provenance}
@@ -31,11 +33,25 @@ function Routes(props) {
     );
   }
 
-  if (urlPath.indexOf('/search') > -1) {
+  if (urlPath.startsWith('/search')) {
     return (
-      <Search />
+      <Search esEndpoint={endpoints.esEndpoint} />
     );
   }
 }
+
+Routes.propTypes = {
+  flaskData: PropTypes.exact({
+    entity: PropTypes.object,
+    flashed_messages: PropTypes.array,
+    provenance: PropTypes.object,
+    vitessce_conf: PropTypes.object,
+    endpoints: PropTypes.object,
+  }),
+};
+
+Routes.defaultProps = {
+  flaskData: {},
+};
 
 export default Routes;
