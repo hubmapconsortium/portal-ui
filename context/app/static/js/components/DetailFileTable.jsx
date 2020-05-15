62c6b3f2d36c4e834ab20cdc2593caf8e3184d82
John Conroy <john-conroy@users.noreply.github.com>
2020-05-15 14:37:33 -0400

    Add DetailFileTable component

---
+++ context/app/static/js/components/DetailFileTable.jsx
@@ -0,0 +1,72 @@
+import React from 'react';
+import styled from 'styled-components';
+import Paper from '@material-ui/core/Paper';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Link from '@material-ui/core/Link';
+import SectionHeader from './SectionHeader';
+import { readCookie } from '../helpers/functions';
+
+const StyledTableContainer = styled(TableContainer)`
+  max-height: 231px;
+`;
+
+const StyledLink = styled(Link)`
+  color: #3781D1;
+`;
+
+const columns = [
+  { id: 'rel_path', label: 'File' },
+  { id: 'size', label: 'File Size' },
+  { id: 'type', label: 'Type' }];
+
+
+function DetailFileTable(props) {
+  const { files: rows, assetsEndpoint, uuid } = props;
+  const cook = readCookie('nexus_token');
+  return (
+    <div>
+      <SectionHeader variant="h3" component="h2">Files</SectionHeader>
+      <Paper>
+        <StyledTableContainer>
+          <Table stickyHeader>
+            <TableHead>
+              <TableRow>
+                {columns.map((column) => (
+                  <TableCell key={column.id}>
+                    {column.label}
+                  </TableCell>
+                ))}
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {rows.map((row) => (
+                <TableRow>
+                  <TableCell>
+                    <StyledLink
+                      href={`${assetsEndpoint}/${uuid}/${row.rel_path}?token=${cook}`}
+                      variant="body1"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      download
+                    >
+                      {row.rel_path}
+                    </StyledLink>
+                  </TableCell>
+                  <TableCell>{row.size}</TableCell>
+                  <TableCell>{row.type}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </StyledTableContainer>
+      </Paper>
+    </div>
+  );
+}
+
+export default DetailFileTable;
