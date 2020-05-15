c01e455b6a497ec0b25fa3378a39205c2b903ccb
John Conroy <john-conroy@users.noreply.github.com>
2020-05-14 23:05:43 -0400

    Update DetailSummary per new design

--- context/app/static/js/components/DetailSummary.jsx
+++ context/app/static/js/components/DetailSummary.jsx
@@ -4,14 +4,26 @@ import styled from 'styled-components';
 import Typography from '@material-ui/core/Typography';
 import Divider from '@material-ui/core/Divider';
 import Link from '@material-ui/core/Link';
+import Paper from '@material-ui/core/Paper';
+import SectionHeader from './SectionHeader';
+import SectionItem from './SectionItem';
 
 const FlexContainer = styled.div`
   display: flex;
 `;
 
+const ColumnContainer = styled(FlexContainer)`
+  flex-direction: column;
+`;
+
 const FlexItemRight = styled.div`
   display: flex;
   margin-left: auto;
+`;
+
+const FlexColumnRight = styled(FlexItemRight)`
+    flex-direction: column;
+    justify-content: space-evenly;
 `;
 
 const StyledTypography = styled(Typography)`
@@ -21,17 +33,21 @@ const StyledTypography = styled(Typography)`
 
 const StyledLink = styled(Link)`
   color: #3781D1;
+  align-self: flex-end;
 `;
 
 const StyledDivider = styled(Divider)`
   margin-left: 5px;
   margin-right: 5px;
   height: 90%;
-  background-color: rgba(0, 0, 0, 0.87);
+  background-color: #444A65;
+  align-self:center;
 `;
 
-const LightText = styled.span`
-  color: rgba(0, 0, 0, 0.54);
+const StyledPaper = styled(Paper)`
+  display:flex;
+  min-height:155px;
+  padding: 15px 40px 15px 15px;
 `;
 
 function AssaySpecificItem(props) {
@@ -41,15 +57,6 @@ function AssaySpecificItem(props) {
       <StyledDivider orientation="vertical" flexItem />
       <Typography variant="body1">{children}</Typography>
     </>
-  );
-}
-
-function DateItem(props) {
-  const { children, label } = props;
-  return (
-    <StyledTypography variant="body1" ml={1}>
-      <LightText>{label}</LightText> {new Date(children).toDateString()}
-    </StyledTypography>
   );
 }
 
@@ -70,38 +77,43 @@ function DetailSummary(props) {
 
 
   return (
-    <>
-      <Typography variant="h1" component="h1">{display_doi}</Typography>
+    <div>
+      <Typography variant="h4" component="h1" color="primary">{entity_type}</Typography>
       <FlexContainer>
-        <FlexContainer>
-          <Typography variant="body1">{entity_type}</Typography>
-          {organ_type && organ_type.length
-            ? (<AssaySpecificItem>{organ_type}</AssaySpecificItem>) : null}
-          {specimen_type && specimen_type.length
-            ? (<AssaySpecificItem>{specimen_type}</AssaySpecificItem>) : null}
-          {data_types && data_types.length
-            ? (<AssaySpecificItem>{data_types.constructor.name === 'Array' ? data_types.join(' / ') : data_types}</AssaySpecificItem>) : null}
-          {status && status.length
-            ? (<AssaySpecificItem>{status}</AssaySpecificItem>) : null}
-        </FlexContainer>
+        <ColumnContainer>
+          <SectionHeader variant="h1" component="h2">{display_doi}</SectionHeader>
+          <FlexContainer>
+            {organ_type && organ_type.length
+              ? (<AssaySpecificItem>{organ_type}</AssaySpecificItem>) : null}
+            {specimen_type && specimen_type.length
+              ? (<AssaySpecificItem>{specimen_type}</AssaySpecificItem>) : null}
+            {data_types && data_types.length
+              ? (<AssaySpecificItem>{data_types.constructor.name === 'Array' ? data_types.join(' / ') : data_types}</AssaySpecificItem>) : null}
+            {status && status.length
+              ? (<AssaySpecificItem>{status}</AssaySpecificItem>) : null}
+          </FlexContainer>
+        </ColumnContainer>
         <FlexItemRight>
-          <DateItem label="Created: ">{create_timestamp}</DateItem>
-          <DateItem label="Modified: ">{last_modified_timestamp}</DateItem>
-          <StyledDivider orientation="vertical" flexItem />
           <StyledLink
             href={`/browse/${entity_type.toLowerCase()}/${uuid}.json`}
             variant="body1"
             component="button"
-            download
+            target="_blank"
             ml={1}
           >
             JSON
           </StyledLink>
         </FlexItemRight>
       </FlexContainer>
-      <Divider />
-      {description && description.length ? <StyledTypography variant="body1" mt={1}>{description}</StyledTypography> : null}
-    </>
+
+      <StyledPaper>
+        <StyledTypography variant="body1" mt={1}>{description || 'No description defined'}</StyledTypography>
+        <FlexColumnRight>
+          <SectionItem label="Creation Date">{new Date(create_timestamp).toDateString()}</SectionItem>
+          <SectionItem label="Modification Date">{new Date(last_modified_timestamp).toDateString()}</SectionItem>
+        </FlexColumnRight>
+      </StyledPaper>
+    </div>
   );
 }
 
