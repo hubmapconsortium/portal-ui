03c8d2db4bc7fd381b538aed4416506b99c22cba
John Conroy <john-conroy@users.noreply.github.com>
2020-05-15 14:35:36 -0400

    Update DetailMetadata to handle samples

--- context/app/static/js/components/DetailMetadata.jsx
+++ context/app/static/js/components/DetailMetadata.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable react/jsx-props-no-spreading */
+/* eslint-disable camelcase */
 import React from 'react';
 import styled from 'styled-components';
 import Typography from '@material-ui/core/Typography';
@@ -9,28 +11,51 @@ const StyledTypography = styled(Typography)`
   margin: 2px 0px 2px 0px;
 `;
 
-
 const FlexPaper = styled(Paper)`
   display: flex;
   padding: 30px 40px 30px 40px;
 `;
 
-function DonorItems() {
+function MetadataItem(props) {
+  const { label, value, ml } = props;
+  return (
+    <SectionItem label={label} ml={ml}>
+      <StyledTypography variant="body1">
+        {value || `${label} not defined`}
+      </StyledTypography>
+    </SectionItem>
+  );
+}
+
+function DonorItems(props) {
+  const { genderFinding, currentAge, bodyMassIndex } = props;
   return (
     <>
-      <SectionItem label="Gender Finding"><StyledTypography variant="body1">Gender Finding not defined</StyledTypography></SectionItem>
-      <SectionItem label="Current Chronological Age" ml={1}><StyledTypography variant="body1">Age not defined</StyledTypography></SectionItem>
-      <SectionItem label="Body Mass Index" ml={1}><StyledTypography variant="body1">BMI not defined</StyledTypography></SectionItem>
+      <MetadataItem label="Gender Finding" value={genderFinding} />
+      <MetadataItem label="Current Chronological Age" ml={1} value={currentAge} />
+      <MetadataItem label="Body Mass Index" ml={1} value={bodyMassIndex} />
     </>
   );
 }
 
-function DetailMetadata() {
+function SampleItems(props) {
+  const { organType, specimenType, tissueLocation } = props;
+  return (
+    <>
+      <MetadataItem label="Organ Type" value={organType} />
+      <MetadataItem label="Specimen Type" ml={1} value={specimenType} />
+      <MetadataItem label="Tissue Location" ml={1} value={tissueLocation} />
+    </>
+  );
+}
+
+function DetailMetadata(props) {
+  const { entityType } = props;
   return (
     <div>
       <SectionHeader variant="h3" component="h2">Metadata</SectionHeader>
       <FlexPaper>
-        <DonorItems />
+        {entityType === 'donor' ? <DonorItems {...props} /> : <SampleItems {...props} />}
       </FlexPaper>
     </div>
   );
