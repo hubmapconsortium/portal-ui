a3ec9cbb4b6bfba8c02139f6b8f36a7e3cf07390
John Conroy <john-conroy@users.noreply.github.com>
2020-05-17 16:50:48 -0400

    Wrap home page call to action text in brackets

--- context/app/static/js/components/Home.jsx
+++ context/app/static/js/components/Home.jsx
@@ -9,9 +9,13 @@ function Home() {
     <Container>
       <Typography component="h1" variant="h1">Welcome to HuBMAP</Typography>
       <Typography component="h2" variant="h5">
-        Browse our <Link href="/search?entity_type[0]=Donor">donors</Link>
-        , <Link href="/search?entity_type[0]=Sample">samples</Link>
-        , or <Link href="/search?entity_type[0]=Dataset">datasets</Link>.
+        {'Browse our '}
+        <Link href="/search?entity_type[0]=Donor">donors</Link>
+        {', '}
+        <Link href="/search?entity_type[0]=Sample">samples</Link>
+        {', or '}
+        <Link href="/search?entity_type[0]=Dataset">datasets</Link>
+        .
       </Typography>
     </Container>
   );
