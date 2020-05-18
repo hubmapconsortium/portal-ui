eef37a205f5e6cf6e5f0106c882cc0811cc13643
mccalluc <mccalluc@users.noreply.github.com>
2020-05-18 11:07:59 -0400

    Get rid of function-function. Fix #564

--- context/app/static/js/components/Search/SearchWrapper.jsx
+++ context/app/static/js/components/Search/SearchWrapper.jsx
@@ -73,40 +73,39 @@ function makeTableComponent(resultFields, detailsUrlPrefix, idField) {
   };
 }
 
-function makeMaskedSelectedFiltersComponent(hiddenFilterIds) {
-  return () => {
-    const SelectedFilter = (filterProps) => {
-      const style = hiddenFilterIds.indexOf(filterProps.filterId) === -1
-        ? {} : { display: 'None' };
-      // Copy and paste from
-      // http://docs.searchkit.co/v0.8.3/docs/components/navigation/selected-filters.html
-      // plus typo corrections and wrapping div.
-      /* eslint-disable jsx-a11y/click-events-have-key-events */
-      /* eslint-disable jsx-a11y/no-static-element-interactions */
-      return (
+function MaskedSelectedFilters(props) {
+  const { hiddenFilterIds } = props;
+  const SelectedFilter = (filterProps) => {
+    const style = hiddenFilterIds.indexOf(filterProps.filterId) === -1
+      ? {} : { display: 'None' };
+    // Copy and paste from
+    // http://docs.searchkit.co/v0.8.3/docs/components/navigation/selected-filters.html
+    // plus typo corrections and wrapping div.
+    /* eslint-disable jsx-a11y/click-events-have-key-events */
+    /* eslint-disable jsx-a11y/no-static-element-interactions */
+    return (
+      <div
+        style={style}
+        className={filterProps.bemBlocks.option()
+          .mix(filterProps.bemBlocks.container('item'))
+          .mix(`selected-filter--${filterProps.filterId}`)}
+      >
         <div
-          style={style}
-          className={filterProps.bemBlocks.option()
-            .mix(filterProps.bemBlocks.container('item'))
-            .mix(`selected-filter--${filterProps.filterId}`)}
+          className={filterProps.bemBlocks.option('name')}
         >
-          <div
-            className={filterProps.bemBlocks.option('name')}
-          >
-            {filterProps.labelKey}: {filterProps.labelValue}
-          </div>
-          <div
-            className={filterProps.bemBlocks.option('remove-action')}
-            onClick={filterProps.removeFilter}
-          >
-            x
-          </div>
+          {filterProps.labelKey}: {filterProps.labelValue}
         </div>
-      );
-      /* eslint-enable */
-    };
-    return <SelectedFilters itemComponent={SelectedFilter} />;
+        <div
+          className={filterProps.bemBlocks.option('remove-action')}
+          onClick={filterProps.removeFilter}
+        >
+          x
+        </div>
+      </div>
+    );
+    /* eslint-enable */
   };
+  return <SelectedFilters itemComponent={SelectedFilter} />;
 }
 
 function SearchWrapper(props) {
@@ -131,8 +130,6 @@ function SearchWrapper(props) {
     /* eslint-enable */
   });
 
-  const MaskedSelectedFilters = makeMaskedSelectedFiltersComponent(hiddenFilterIds);
-
   return (
     <SearchkitProvider searchkit={searchkit}>
       <LayoutBody>
@@ -150,7 +147,7 @@ function SearchWrapper(props) {
                 'hitstats.results_found': '{hitCount} results found',
               }}
               />
-              <MaskedSelectedFilters />
+              <MaskedSelectedFilters hiddenFilterIds={hiddenFilterIds} />
               <SortingSelector options={sortOptions} />
             </ActionBarRow>
           </ActionBar>
