a2438f83eaf1af9b0a8a8385c01b8a79d4f7391d
John Conroy <john-conroy@users.noreply.github.com>
2020-05-20 12:13:08 -0400

    Create StatusIcon component

---
+++ context/app/static/js/components/Detail/StatusIcon.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import LensIcon from '@material-ui/icons/Lens';
+
+const ColoredStatusIcon = styled(LensIcon)`
+  color: ${(props) => props.theme.palette[props.$iconColor].main};
+  font-size: 16px;
+  margin-right: 3px;
+`;
+
+function getColor(status) {
+  if (['NEW', 'REOPENED', 'QA', 'LOCKED', 'PROCESSING', 'HOLD'].includes(status)) {
+    return 'info';
+  }
+
+  if (['INVALID', 'ERROR'].includes(status)) {
+    return 'error';
+  }
+
+  if (['UNPUBLISHED', 'DEPRECATED'].includes(status)) {
+    return 'warning';
+  }
+
+  if (status === 'PUBLISHED') {
+    return 'success';
+  }
+
+  return null;
+}
+
+function StatusIcon(props) {
+  const { status: irregularCaseStatus } = props;
+  const status = irregularCaseStatus.toUpperCase();
+  const color = getColor(status);
+
+
+  return (<ColoredStatusIcon $iconColor={color} />);
+}
+
+StatusIcon.propTypes = {
+  status: PropTypes.string.isRequired,
+};
+
+export default StatusIcon;
