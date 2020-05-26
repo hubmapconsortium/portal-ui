4f18d948f84a74768f004e629f47fb9e201ed486
John Conroy <john-conroy@users.noreply.github.com>
2020-05-26 15:19:38 -0400

    Create DetailLayout component

---
+++ context/app/static/js/components/Detail/DetailLayout.jsx
@@ -0,0 +1,74 @@
+/* eslint-disable camelcase */
+import React from 'react';
+import styled from 'styled-components';
+import NoticeAlert from '../NoticeAlert';
+import TableOfContents from './TableOfContents';
+
+const FlexColumn = styled.div`
+  flex-grow: 1;
+  display: flex;
+  flex-direction: column;
+  max-width: 1280px;
+`;
+
+const FlexRow = styled.div`
+  flex-grow: 1;
+  display: flex;
+  justify-content: center;
+`;
+
+function DetailLayout(props) {
+  const {
+    displayViz,
+    displayProtocol,
+    displayMetadata,
+    displayMetadataTable,
+    displayFiles,
+    flashed_messages,
+    children,
+  } = props;
+
+  const updateSections = () => {
+    const sections = new Map([
+      ['summary', { text: 'Summary', hash: 'summary' }],
+      ['metadata', { text: 'Metadata', hash: 'metadata' }],
+      ['visualization', { text: 'Visualization', hash: 'visualization' }],
+      ['attribution', { text: 'Attribution', hash: 'attribution' }],
+      ['provenance', { text: 'Provenance', hash: 'provenance' }],
+      ['protocol', { text: 'Protocol', hash: 'protocol' }],
+      ['metadataTable', { text: 'Metadata', hash: 'metadata-table' }],
+      ['files', { text: 'Files', hash: 'files' }],
+    ]);
+    if (!displayMetadata) {
+      sections.delete('metadata');
+    }
+    if (!displayViz) {
+      sections.delete('visualization');
+    }
+    if (!displayProtocol) {
+      sections.delete('protocol');
+    }
+
+    if (!displayMetadataTable) {
+      sections.delete('metadataTable');
+    }
+    if (!displayFiles) {
+      sections.delete('files');
+    }
+    return sections;
+  };
+
+  const sections = updateSections();
+
+  return (
+    <FlexRow>
+      <TableOfContents items={[...sections.values()]} />
+      <FlexColumn maxWidth="lg">
+        {(flashed_messages && flashed_messages.length) && <NoticeAlert errors={flashed_messages} />}
+        {children}
+      </FlexColumn>
+    </FlexRow>
+  );
+}
+
+export default DetailLayout;
