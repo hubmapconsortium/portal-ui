e1b505ed2fd30ee9777791f7488735c4a1eacc02
mccalluc <mccalluc@users.noreply.github.com>
2020-05-28 13:14:06 -0400

    Only update changelog if needed

--- push.sh
+++ push.sh
@@ -8,18 +8,20 @@ git diff --quiet || die 'Uncommitted changes: Stash or commit before push.'
 diff VERSION <(curl -s https://raw.githubusercontent.com/hubmapconsortium/portal-ui/master/context/app/static/VERSION) \
   && die 'Update VERSION and commit.'
 
-(
-  echo '##' `cat VERSION` - `date +"%F"`
-  echo
-  cat CHANGELOG-*.md
-  echo
-  echo
-  cat CHANGELOG.md
-) > CHANGELOG.md.new
-mv CHANGELOG.md.new CHANGELOG.md
-git rm CHANGELOG-*.md || echo 'No files to remove; CHANGELOG.md may be messy.'
-git add .
-git commit -m 'Update CHANGELOG'
+if ls CHANGELOG-*.md; then
+  (
+    echo '##' `cat VERSION` - `date +"%F"`
+    echo
+    cat CHANGELOG-*.md
+    echo
+    echo
+    cat CHANGELOG.md
+  ) > CHANGELOG.md.new
+  mv CHANGELOG.md.new CHANGELOG.md
+  git rm CHANGELOG-*.md
+  git add .
+  git commit -m 'Update CHANGELOG'
+fi
 
 VERSION=`cat VERSION`
 VERSION_IMAGE_NAME=hubmap/portal-ui:$VERSION
