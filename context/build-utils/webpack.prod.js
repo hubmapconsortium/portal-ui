3eea9ac33e2c99dc76cec1db18c478fcc4170774
John Conroy <john-conroy@users.noreply.github.com>
2020-06-01 14:34:43 -0400

    Remove .js extension from require statement

--- context/build-utils/webpack.prod.js
+++ context/build-utils/webpack.prod.js
@@ -1,6 +1,6 @@
 /* eslint-disable import/no-extraneous-dependencies */
 const merge = require('webpack-merge');
-const common = require('./webpack.common.js');
+const common = require('./webpack.common');
 
 const envConfig = {
   mode: 'production',
