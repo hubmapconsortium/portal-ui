70368637ef89887d44b3dfc065b9daa2aa7ff4c3
John Conroy <john-conroy@users.noreply.github.com>
2020-06-03 11:19:30 -0400

    Remove headers and padding from DagProv

--- context/app/static/js/components/Detail/DagProv.jsx
+++ context/app/static/js/components/Detail/DagProv.jsx
@@ -1,29 +1,19 @@
 /* eslint-disable react/no-array-index-key */
 import React from 'react';
 import styled from 'styled-components';
-import Paper from '@material-ui/core/Paper';
 import List from '@material-ui/core/List';
 import Typography from '@material-ui/core/Typography';
 import Divider from '@material-ui/core/Divider';
-import SectionHeader from './SectionHeader';
-import SectionContainer from './SectionContainer';
 
 const ListKey = styled.span`
   font-weight: bold;
   margin-right: 4px;
 `;
 
-const PaddedPaper = styled(Paper)`
-  padding: 30px 40px 30px 40px;
-`;
-
-function DagList(props) {
+function DagProv(props) {
   const { dagListData } = props;
   return (
     <>
-      <Typography variant="h5" component="h3" color="primary">
-        dag_provenance_list
-      </Typography>
       <List>
         {dagListData.map((item, i) => (
           <React.Fragment key={`dag-provenance-list-item${i}`}>
@@ -40,18 +30,6 @@ function DagList(props) {
         ))}
       </List>
     </>
-  );
-}
-
-function DagProv(props) {
-  const { dagListData } = props;
-  return (
-    <SectionContainer id="dag-provenance">
-      <SectionHeader variant="h3" component="h2">
-        DAG Provenance
-      </SectionHeader>
-      <PaddedPaper>{dagListData && <DagList dagListData={dagListData} />}</PaddedPaper>
-    </SectionContainer>
   );
 }
 
