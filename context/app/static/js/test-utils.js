09fb12145a91f23ed6ef9aaa77a5d74156326e87
John Conroy <john-conroy@users.noreply.github.com>
2020-06-05 14:20:25 -0400

    Add test-utils to wrap Providers for tests

---
+++ context/app/static/js/test-utils.js
@@ -0,0 +1,16 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import React from 'react';
+import { render } from '@testing-library/react';
+import Providers from './Providers';
+
+const AllTheProviders = ({ children }) => {
+  return <Providers>{children}</Providers>;
+};
+
+const customRender = (ui, options) => render(ui, { wrapper: AllTheProviders, ...options });
+
+// re-export everything
+export * from '@testing-library/react';
+
+// override render method
+export { customRender as render };
