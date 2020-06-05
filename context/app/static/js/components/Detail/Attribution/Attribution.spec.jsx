88222d1b84e1adf4f7eb73be7637a9672e8e5f7a
John Conroy <john-conroy@users.noreply.github.com>
2020-06-05 14:45:55 -0400

    Add basic Attribution test

---
+++ context/app/static/js/components/Detail/Attribution/Attribution.spec.jsx
@@ -0,0 +1,19 @@
+/* eslint-disable camelcase */
+import React from 'react';
+import { render } from '../../../test-utils';
+import Attribution from './Attribution';
+
+test('text displays properly', () => {
+  const group_name = 'Fake TMC';
+  const created_by_user_displayname = 'Fake Name';
+  const created_by_user_email = 'fake@fake.com';
+  const { getByText } = render(
+    <Attribution
+      group_name={group_name}
+      created_by_user_displayname={created_by_user_displayname}
+      created_by_user_email={created_by_user_email}
+    />,
+  );
+  const textToTest = ['Creator', 'Fake TMC', 'Creator', 'Fake Name', 'fake@fake.com'];
+  textToTest.forEach((text) => expect(getByText(text)).toBeInTheDocument());
+});
