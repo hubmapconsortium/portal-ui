810087e4555080d6a316d3151822534913c44744
John Conroy <john-conroy@users.noreply.github.com>
2020-06-07 11:00:59 -0400

    Add moduleNameMapper to jest config

--- context/jest.config.js
+++ context/jest.config.js
@@ -1,4 +1,9 @@
 module.exports = {
   testPathIgnorePatterns: ['jest.config.js', '/node_modules/', '/cypress/'],
   setupFilesAfterEnv: ['./setuptests.js'],
+  moduleNameMapper: {
+    '^components(.*)$': '<rootDir>/app/static/js/components$1',
+    '^static(.*)$': '<rootDir>/app/static/$1',
+    '^test-utils.js': '<rootDir>/test-utils.js',
+  },
 };
