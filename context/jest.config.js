299ed096cdfcf43ee8ca97b137e3151eef1b8caa
John Conroy <john-conroy@users.noreply.github.com>
2020-06-08 13:30:43 -0400

    Fix errors and add transforms for js and yml files in jest config

--- context/jest.config.js
+++ context/jest.config.js
@@ -1,10 +1,16 @@
 module.exports = {
   testPathIgnorePatterns: ['jest.config.js', '/node_modules/', '/cypress/'],
-  setupFilesAfterEnv: ['./test-utils/setuptests.js'],
+  setupFilesAfterEnv: ['<rootDir>/test-utils/setupTests.js'],
   moduleNameMapper: {
-    '\\.css$': require.resolve('./test-utils/style-mock'),
+    '^.+\\.css$': '<rootDir>/test-utils/style-mock',
     '^components(.*)$': '<rootDir>/app/static/js/components$1',
     '^static(.*)$': '<rootDir>/app/static/$1',
-    '^test-utils': '<rootDir>/test-utils/test-utils.js',
+    '^test-utils$': '<rootDir>/test-utils/test-utils.js',
+    'search-schema-definitions$': '<rootDir>/search-schema/data/definitions.yaml',
+    'metadata-field-descriptions$': '<rootDir>/ingest-validation-tools/docs/field-descriptions.yaml',
+  },
+  transform: {
+    '^.+\\.[t|j]sx?$': 'babel-jest',
+    '^.+\\.ya?ml$': '<rootDir>/test-utils/ymlLoader.js',
   },
 };
