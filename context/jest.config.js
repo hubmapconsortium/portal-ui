c4579e71b4e5ccfe6ba738e163f719abdeb68f22
John Conroy <john-conroy@users.noreply.github.com>
2020-06-09 10:46:47 -0400

    Update test-utils file structure

--- context/jest.config.js
+++ context/jest.config.js
@@ -2,15 +2,15 @@ module.exports = {
   testPathIgnorePatterns: ['jest.config.js', '/node_modules/', '/cypress/'],
   setupFilesAfterEnv: ['<rootDir>/test-utils/setupTests.js'],
   moduleNameMapper: {
-    '^.+\\.css$': '<rootDir>/test-utils/style-mock',
+    '^.+\\.css$': '<rootDir>/test-utils/__mocks__/style.js',
     '^components(.*)$': '<rootDir>/app/static/js/components$1',
     '^static(.*)$': '<rootDir>/app/static/$1',
-    '^test-utils$': '<rootDir>/test-utils/test-utils.js',
+    '^test-utils(.*)$': '<rootDir>/test-utils$1',
     'search-schema-definitions$': '<rootDir>/search-schema/data/definitions.yaml',
     'metadata-field-descriptions$': '<rootDir>/ingest-validation-tools/docs/field-descriptions.yaml',
   },
   transform: {
     '^.+\\.[t|j]sx?$': 'babel-jest',
-    '^.+\\.ya?ml$': '<rootDir>/test-utils/ymlLoader.js',
+    '^.+\\.ya?ml$': '<rootDir>/test-utils/loaders/ymlLoader.js',
   },
 };
