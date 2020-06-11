1abfbaddd7389b4fc096e3ecc4ad1f0d34167f67
John Conroy <john-conroy@users.noreply.github.com>
2020-06-11 15:27:21 -0400

    Add vertical margins to DataSummary

--- context/app/static/js/components/Home/DataSummary/style.js
+++ context/app/static/js/components/Home/DataSummary/style.js
@@ -5,6 +5,8 @@ const FlexRow = styled.div`
   flex-wrap: wrap;
   width: 100%;
   justify-content: space-between;
+  margin-top: ${(props) => props.theme.spacing(2)}px;
+  margin-bottom: ${(props) => props.theme.spacing(2)}px;
 
   @media (max-width: ${(props) => props.theme.breakpoints.values.sm}px) {
     flex-direction: column;
