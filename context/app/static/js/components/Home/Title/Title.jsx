20b1b3cc9593f008b8ca8c544e68b30f5ec7659a
John Conroy <john-conroy@users.noreply.github.com>
2020-06-12 11:20:30 -0400

    Add Title component for home page

---
+++ context/app/static/js/components/Home/Title/Title.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
+
+function Title() {
+  return (
+    <>
+      <Typography variant="h1">Human Biomolecular Atlas Program</Typography>
+      <Typography variant="h4" component="h3" color="secondary">
+        An open, global atlas of the human body at the cellular level
+      </Typography>
+    </>
+  );
+}
+
+export default React.memo(Title);
