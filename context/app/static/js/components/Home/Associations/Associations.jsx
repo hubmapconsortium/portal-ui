96cafc37cecf2abb68e88d5adf462d6de3c66335
John Conroy <john-conroy@users.noreply.github.com>
2020-06-15 16:32:55 -0400

    Add links to AssociationsCards and memoize Associations

--- context/app/static/js/components/Home/Associations/Associations.jsx
+++ context/app/static/js/components/Home/Associations/Associations.jsx
@@ -20,6 +20,7 @@ function Associations() {
         <AssociationsCard
           title="HuBMAP Consortium"
           text="Members of the HuBMAP Consortium are developing the tools to create an open, global atlas of the human body at the..."
+          link="https://hubmapconsortium.org/"
           mb={1}
         >
           <HubmapLogos />
@@ -27,14 +28,15 @@ function Associations() {
         <AssociationsCard
           title="NIH Common Fund"
           text="The NIH Common Fund is a component of the NIH budget which is managed by the Office of Strategic Communication..."
+          link="https://commonfund.nih.gov/hubmap"
           mb={1}
         >
           <StyledImage src={NihLogo} alt="NIH Logo" />
         </AssociationsCard>
         <AssociationsCard
           title="HuBMAP Data Submission"
-          text="User authentication is required to generate HuBMAP IDs, please click the button below and you will be redirected to....
-"
+          text="User authentication is required to generate HuBMAP IDs, please click the button below and you will be redirected to...."
+          link="https://ingest.hubmapconsortium.org/"
         >
           <HubmapLogos />
         </AssociationsCard>
@@ -43,4 +45,4 @@ function Associations() {
   );
 }
 
-export default Associations;
+export default React.memo(Associations);
