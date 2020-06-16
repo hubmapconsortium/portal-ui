434627a796486e5c8f87b9f06ff746fd82de6daf
John Conroy <john-conroy@users.noreply.github.com>
2020-06-16 09:50:16 -0400

    Add height to footer

--- context/app/static/js/components/Footer/style.js
+++ context/app/static/js/components/Footer/style.js
@@ -2,8 +2,10 @@ import styled from 'styled-components';
 import Container from '@material-ui/core/Container';
 
 const Flex = styled(Container)`
+  height: 60px;
   display: flex;
   justify-content: space-between;
+  align-items: center;
 `;
 
 export { Flex };
