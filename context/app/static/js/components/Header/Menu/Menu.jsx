d486df0b9b0fc2ad43a001969b61098bf66dc3e1
John Conroy <john-conroy@users.noreply.github.com>
2020-06-17 13:56:32 -0400

    Update Menu

--- context/app/static/js/components/Header/Menu/Menu.jsx
+++ context/app/static/js/components/Header/Menu/Menu.jsx
@@ -1,41 +1,60 @@
-import React from 'react';
-import MaterialMenu from '@material-ui/core/Menu';
+import React, { useReducer } from 'react';
+import PropTypes from 'prop-types';
 import MenuItem from '@material-ui/core/MenuItem';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
+import CloseIcon from '@material-ui/icons/Close';
+import MenuList from '@material-ui/core/MenuList';
+import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
+import ArrowDropUpIcon from '@material-ui/icons/ArrowDropUp';
+import Link from '@material-ui/core/Link';
+import { AppBar } from '@material-ui/core';
 
-function Menu() {
-  const [anchorEl, setAnchorEl] = React.useState(null);
+import ShowcaseLinks from '../ShowcaseLinks';
+import { WidePopper, WidePaper, ShowcaseMenuItem } from './style';
 
-  const handleClick = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
+function Menu(props) {
+  const [open, toggle] = useReducer((v) => !v, false);
+  const [openShowcase, toggleShowcase] = useReducer((v) => !v, false);
+  const { anchorRef } = props;
 
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
   return (
     <>
-      <IconButton edge="start" color="inherit" aria-label="menu" aria-haspopup="true" onClick={handleClick}>
-        <MenuIcon />
+      <IconButton color="inherit" aria-describedby="main-menu" aria-haspopup="true" onClick={toggle}>
+        {open ? <CloseIcon /> : <MenuIcon />}
       </IconButton>
-      <MaterialMenu id="simple-menu" anchorEl={anchorEl} open={Boolean(anchorEl)} onClose={handleClose}>
-        {['Donor', 'Sample', 'Dataset'].map((type) => (
-          <MenuItem key={type}>
-            <a href={`/search?entity_type[0]=${type}`}>{`${type}s`}</a>
-          </MenuItem>
-        ))}
-        <MenuItem>
-          <a href="/ccf-eui" target="_blank" rel="noopener noreferrer">
-            CCF
-          </a>
-        </MenuItem>
-        <MenuItem>
-          <a href="/help">Help</a>
-        </MenuItem>
-      </MaterialMenu>
+      <WidePopper id="main-menu" open={open} anchorEl={anchorRef.current}>
+        <WidePaper>
+          <MenuList>
+            {['Donor', 'Sample', 'Dataset'].map((type) => (
+              <MenuItem key={type}>
+                <Link href={`/search?entity_type[0]=${type}`} color="primary" underline="none">{`${type}s`}</Link>
+              </MenuItem>
+            ))}
+            <ShowcaseMenuItem onClick={toggleShowcase}>
+              Showcases
+              {openShowcase ? <ArrowDropDownIcon /> : <ArrowDropUpIcon />}
+            </ShowcaseMenuItem>
+            {openShowcase && <ShowcaseLinks />}
+            <MenuItem>
+              <Link href="/ccf-eui" target="_blank" rel="noopener noreferrer" color="primary" underline="none">
+                CCF
+              </Link>
+            </MenuItem>
+            <MenuItem>
+              <Link href="/help" color="primary" underline="none">
+                Help
+              </Link>
+            </MenuItem>
+          </MenuList>
+        </WidePaper>
+      </WidePopper>
     </>
   );
 }
+
+Menu.propTypes = {
+  anchorRef: PropTypes.shape({ current: PropTypes.instanceOf(AppBar) }).isRequired,
+};
 
 export default Menu;
