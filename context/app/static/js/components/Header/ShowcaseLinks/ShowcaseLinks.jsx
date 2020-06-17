f36bbdf42664e2f330fc8b33b5d5bd0a4a26e711
John Conroy <john-conroy@users.noreply.github.com>
2020-06-17 13:40:18 -0400

    Don't underline ShowcaseLinks

--- context/app/static/js/components/Header/ShowcaseLinks/ShowcaseLinks.jsx
+++ context/app/static/js/components/Header/ShowcaseLinks/ShowcaseLinks.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import MenuItem from '@material-ui/core/MenuItem';
+import Link from '@material-ui/core/Link';
 
 function ShowcaseLinks() {
   return (
     <>
       {['Spraggins'].map((showcaseName, i) => (
         <MenuItem key={showcaseName} mt={i === 0}>
-          <a href={`/showcase/${showcaseName.toLowerCase()}`}>{showcaseName}</a>
+          <Link href={`/showcase/${showcaseName.toLowerCase()}`} color="primary" underline="none">
+            {showcaseName}
+          </Link>
         </MenuItem>
       ))}
     </>
