f54d5dfb8ce8c6a4057eb42040512bec57e2ea96
mccalluc <mccalluc@users.noreply.github.com>
2020-06-17 12:43:24 -0400

    Tweak config...

--- context/app/default_config.py
+++ context/app/default_config.py
@@ -3,19 +3,22 @@ from datetime import timedelta
 
 class DefaultConfig(object):
     # This should be updated when app.conf is updated:
-    # Test runs will see this config rather than app.conf.
+    # Test runs will only see this config and not app.conf.
+    #
     # Tests should not make API calls...
     # but they may expect certain keys to be present.
 
     PERMANENT_SESSION_LIFETIME = timedelta(minutes=60)
 
     ENTITY_API_TIMEOUT = 5
+    PORTAL_INDEX_PATH = '/portal/search'
+    CCF_INDEX_PATH = '/entities/search'
+
+    # Everything else should be overridden in app.conf:
+
     ENTITY_API_BASE = 'should-be-overridden'
 
     GROUP_ID = 'should-be-overridden'
-
-    PORTAL_INDEX_PATH = '/entities/search'  # TODO: '/portal/search'
-    CCF_INDEX_PATH = '/entities/search'
 
     ELASTICSEARCH_ENDPOINT = 'should-be-overridden'
     ASSETS_ENDPOINT = 'should-be-overridden'
