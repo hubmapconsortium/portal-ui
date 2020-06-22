078d4b7f5498a37894adf974b8fa620497a4a594
mccalluc <mccalluc@users.noreply.github.com>
2020-06-21 21:43:24 -0400

    add href prop

--- context/app/static/js/components/Home/DataSummaryItem/DataSummaryItem.jsx
+++ context/app/static/js/components/Home/DataSummaryItem/DataSummaryItem.jsx
@@ -3,10 +3,10 @@ import PropTypes from 'prop-types';
 import { StyledTypography, StyledLink } from './style';
 
 function DataSummaryItem(props) {
-  const { value, icon: Icon, label } = props;
+  const { value, icon: Icon, label, href } = props;
 
   return (
-    <StyledLink href="/">
+    <StyledLink href={href}>
       <Icon color="primary" style={{ fontSize: '36px' }} />
       <StyledTypography variant="h4">
         {value} {label}
