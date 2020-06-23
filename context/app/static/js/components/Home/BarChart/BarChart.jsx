ba3084e2443f0c59c6de569470c55bf3f2d7d93d
John Conroy <john-conroy@users.noreply.github.com>
2020-06-23 14:49:50 -0400

    Remove prettier ignore in BarChart

--- context/app/static/js/components/Home/BarChart/BarChart.jsx
+++ context/app/static/js/components/Home/BarChart/BarChart.jsx
@@ -5,8 +5,7 @@ function BarChart(props) {
   const { data, spec: partialSpec } = props;
   const spec = { ...partialSpec, data: { name: 'table' } };
 
-  // eslint-disable-next-line prettier/prettier
-  return <ReactVega spec={spec} data={{table: data}} renderer="canvas" scaleFactor={1} />;
+  return <ReactVega spec={spec} data={{ table: data }} renderer="canvas" scaleFactor={1} />;
 }
 
 export default BarChart;
