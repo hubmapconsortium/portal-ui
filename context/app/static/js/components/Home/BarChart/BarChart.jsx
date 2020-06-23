df5202c0078b63c7d31be446f27d146e2bc4a63f
John Conroy <john-conroy@users.noreply.github.com>
2020-06-23 11:28:24 -0400

    Update BarChart

--- context/app/static/js/components/Home/BarChart/BarChart.jsx
+++ context/app/static/js/components/Home/BarChart/BarChart.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
-import { Placeholder } from './style';
+import { Vega as ReactVega } from 'react-vega';
 
-function BarChart() {
-  return <Placeholder />;
+function BarChart(props) {
+  const { data, spec: partialSpec } = props;
+  const spec = { ...partialSpec, data: { name: 'table' } };
+
+  // eslint-disable-next-line prettier/prettier
+  return <ReactVega spec={spec} data={{table: data}} renderer="canvas" scaleFactor={1} />;
 }
 
 export default BarChart;
--- context/app/static/js/components/Home/BarChart/style.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-const Placeholder = styled.div`
-  grid-area: bar;
-  background-color: ${(props) => props.theme.palette.transparentGray.main};
-  height: 750px;
-`;
-
-export { Placeholder };
