8de7df16e1700ec2fe5c5b397a241e18872225a8
John Conroy <john-conroy@users.noreply.github.com>
2020-06-25 16:09:10 -0400

    Create Collections dir, component and export

---
+++ context/app/static/js/components/Collections/Collections/Collections.jsx
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from 'react';
+import Card from '../Card';
+
+function Collections(props) {
+  const { entityEndpoint } = props;
+  const [collectionsData, setCollectionsData] = useState([]);
+  useEffect(() => {
+    async function getAllCollections() {
+      const response = await fetch(`${entityEndpoint}/collections`, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+      if (!response.ok) {
+        console.error('Search API failed', response);
+        return;
+      }
+      const data = await response.json();
+      setCollectionsData(data);
+    }
+    getAllCollections();
+  }, [entityEndpoint]);
+
+  return (
+    <div>
+      {collectionsData.map((col) => (
+        <Card key={col.uuid} data={col} />
+      ))}
+    </div>
+  );
+}
+
+export default Collections;
