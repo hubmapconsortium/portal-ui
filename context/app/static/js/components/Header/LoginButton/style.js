f0c34ce4f021ebe45b7b800ac58b69dab000a0f9
John Conroy <62477388+john-conroy@users.noreply.github.com>
2020-06-25 11:28:05 -0400

    John conroy/login button hover (#510)
    * Update hover for login button
    
    * Add changelog file
    
    Co-authored-by: John Conroy <john-conroy@users.noreply.github.com>

--- context/app/static/js/components/Header/LoginButton/style.js
+++ context/app/static/js/components/Header/LoginButton/style.js
@@ -5,6 +5,11 @@ const WhiteButton = styled(Button)`
   width: 136px;
   background-color: #ffffff;
   margin-left: 10px;
+
+  &:hover {
+    background-color: rgb(255, 255, 255, 0.92);
+    box-shadow: ${(props) => props.theme.shadows[8]};
+  }
 `;
 
 const Link = styled.a`
