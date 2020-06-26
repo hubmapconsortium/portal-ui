569c2bfb0a06e4f26dbbf3a206086bf935d14ff8
John Conroy <john-conroy@users.noreply.github.com>
2020-06-26 16:24:51 -0400

    Make collections Panel responsive and update border color

--- context/app/static/js/components/Collections/Panel/Panel.jsx
+++ context/app/static/js/components/Collections/Panel/Panel.jsx
@@ -1,16 +1,16 @@
 /* eslint-disable react/no-array-index-key */
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
-import { PanelWrapper } from './style';
+import { PanelWrapper, Name } from './style';
 
 function Panel(props) {
   const { name, doi_id, dataset_uuids } = props;
   return (
     <PanelWrapper>
       <div>
-        <Typography variant="subtitle1" component="h3">
+        <Name variant="subtitle1" component="h3">
           {name}
-        </Typography>
+        </Name>
         <Typography variant="body2" color="secondary">
           {doi_id}
         </Typography>
--- context/app/static/js/components/Collections/Panel/style.js
+++ context/app/static/js/components/Collections/Panel/style.js
@@ -1,10 +1,19 @@
 import styled from 'styled-components';
+import Typography from '@material-ui/core/Typography';
 
 const PanelWrapper = styled.div`
   padding: 15px 20px;
-  border-bottom: 2px solid ${(props) => props.theme.palette.transparentGray.main};
+  border-bottom: 2px solid ${(props) => props.theme.palette.collectionsDivider.main};
   display: flex;
-  justify-content: space-between;
+  flex-direction: column;
+  @media (min-width: ${(props) => props.theme.breakpoints.values.md}px) {
+    flex-direction: row;
+    justify-content: space-between;
+  }
 `;
 
-export { PanelWrapper };
+const Name = styled(Typography)`
+  word-wrap: break-word;
+`;
+
+export { PanelWrapper, Name };
