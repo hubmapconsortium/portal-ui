964e65a64634ce322b794d6b2f6622e3ec3db1a3
John Conroy <john-conroy@users.noreply.github.com>
2020-06-29 10:34:27 -0400

    Add fetch to Collection

--- context/app/static/js/components/Detail/Collection/Collection.jsx
+++ context/app/static/js/components/Detail/Collection/Collection.jsx
@@ -1,7 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+// eslint-disable-next-line import/no-unresolved
+import { readCookie } from 'helpers/functions';
 
-function Collection() {
-  return <div>Collection</div>;
+function Collection(props) {
+  const { entityEndpoint, uuid } = props;
+  const [collectionData, setCollectionData] = useState();
+  useEffect(() => {
+    async function getAllCollections() {
+      const nexus_token = readCookie('nexus_token');
+      const requestInit = nexus_token
+        ? {
+            headers: {
+              Authorization: `Bearer ${nexus_token}`,
+            },
+          }
+        : {};
+      const response = await fetch(`${entityEndpoint}/collections/${uuid}`, requestInit);
+      if (!response.ok) {
+        console.error('Entity API failed', response);
+        return;
+      }
+      const data = await response.json();
+      // eslint-disable-next-line no-console
+      console.log(data);
+      setCollectionData(data);
+    }
+    getAllCollections();
+  }, [entityEndpoint, uuid]);
+
+  return <div>{collectionData && collectionData.uuid}</div>;
 }
 
 export default Collection;
