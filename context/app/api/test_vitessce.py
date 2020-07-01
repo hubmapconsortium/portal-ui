0e93e3e64fa7f2569fa635e021aaf42e6515f3a4
ilan-gold <ilanbassgold@gmail.com>
2020-07-01 11:36:11 -0400

    Update test

--- context/app/api/test_vitessce.py
+++ context/app/api/test_vitessce.py
@@ -46,7 +46,7 @@ def test_build_image_schema():
     assert schema["type"] == "ome-tiff"
     assert (
         schema["metadata"]["omeTiffOffsetsUrl"]
-        == f"https://example.com/uuid/ppneorh7/example.offsets.json?token={TEST_NEXUS_TOKEN}"
+        == f"https://example.com/uuid/output_offsets/example.offsets.json?token={TEST_NEXUS_TOKEN}"
     )
 
 
