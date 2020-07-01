358240d75bdeeae880846ec5f89832e35f083ae5
John Conroy <john-conroy@users.noreply.github.com>
2020-07-01 19:45:18 -0400

    Comment FileBrowser useEffect

--- context/app/static/js/components/Detail/FileBrowser/FileBrowser.jsx
+++ context/app/static/js/components/Detail/FileBrowser/FileBrowser.jsx
@@ -20,21 +20,29 @@ function FileBrowser(props) {
 
       levels.reduce((prev, lvl, i) => {
         const path = `${lvl}/`;
+        // level is sub dir
+
         if (i < levels.length - 1) {
           // eslint-disable-next-line no-param-reassign
-          prev[path] = prev[path] || {};
-          return prev;
-        }
-        if (path in prev && 'files' in prev[path]) {
-          // eslint-disable-next-line no-param-reassign
-          prev[path].files.push(file);
+          prev[path] = prev[path] || {}; // if level exists do not change, else add empty object at level
           return prev;
         }
 
+        // level is file
+
+        // level exists and files object exists within
+        if (path in prev && 'files' in prev[path]) {
+          // eslint-disable-next-line no-param-reassign
+          prev[path].files.push(file); // push file into existing array
+          return prev;
+        }
+
+        // level exists
         if (path in prev) {
           Object.assign(prev[path], { files: [file] });
           return prev;
         }
+
         // eslint-disable-next-line no-param-reassign
         prev[path] = { files: [file] };
         return prev;
