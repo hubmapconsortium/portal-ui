86b803957d3c9461658f837cc91c563e9da7db5b
John Conroy <john-conroy@users.noreply.github.com>
2020-07-02 18:29:19 -0400

    Add comments to FileBrowserNode

--- context/app/static/js/components/Detail/FileBrowserNode/FileBrowserNode.jsx
+++ context/app/static/js/components/Detail/FileBrowserNode/FileBrowserNode.jsx
@@ -7,6 +7,7 @@ import { Column } from './style';
 function FileBrowserNode(props) {
   const { fileSubTree, depth } = props;
   return Object.entries(fileSubTree).map(([k, v]) => {
+    // if the object contains array of files, display all files
     if (k === 'files') {
       return (
         <Column key={`${k}-${depth}`}>
@@ -16,6 +17,7 @@ function FileBrowserNode(props) {
         </Column>
       );
     }
+    // if the object contains additionall directories, display dir and continue down
     return (
       <FileBrowserDirectory key={`${k}-${depth}`} dirName={k.slice(0, -1)} depth={depth}>
         <FileBrowserNode fileSubTree={v} depth={depth + 1} />
