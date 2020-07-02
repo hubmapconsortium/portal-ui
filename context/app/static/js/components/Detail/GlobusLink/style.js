89efcb63f848269b9337c1ce64eceb8d0e154c43
John Conroy <john-conroy@users.noreply.github.com>
2020-07-02 14:15:47 -0400

    Add margin to GlobusLink

--- context/app/static/js/components/Detail/GlobusLink/style.js
+++ context/app/static/js/components/Detail/GlobusLink/style.js
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 import Button from '@material-ui/core/Button';
 
 const StyledButton = styled(Button)`
+  margin-top: ${(props) => props.theme.spacing(2)}px;
   background-color: #ffffff;
   border: 1px solid black;
 `;
