c45081f4778e7a62f15db003da5146832e259f92
mccalluc <mccalluc@users.noreply.github.com>
2020-07-04 15:11:59 -0400

    More basic css for MD

--- context/app/static/js/components/Markdown/style.js
+++ context/app/static/js/components/Markdown/style.js
@@ -1,7 +1,11 @@
 import styled from 'styled-components';
 import Paper from '@material-ui/core/Paper';
 
+// TODO: Copied and pasted the padding.
+// Should find a way to manage it in one place.
 const StyledPaper = styled(Paper)`
+  padding: 30px 40px 30px 40px;
+
   & img {
     max-width: 100%;
   }
@@ -11,13 +15,20 @@ const StyledPaper = styled(Paper)`
     border-collapse: collapse;
   }
   & th,
-  td {
+  & td {
     border: 1px solid grey;
+    padding: 0.25em 0.5em;
   }
 
-  padding: 30px 40px 30px 40px;
+  blockquote {
+    border-left: 4px solid grey;
+    margin-left: 0;
+    padding-left: 1em;
+  }
+
+  ul {
+    list-style: square outside none;
+  }
 `;
-// TODO: Copied and pasted the padding.
-// Should find a way to manage it in one place.
 
 export { StyledPaper };
