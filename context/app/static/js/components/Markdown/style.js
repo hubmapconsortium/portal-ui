c308bf75207db65d18bd48327723cbc17823b6ec
Chuck McCallum <mccalluc@users.noreply.github.com>
2020-07-04 18:15:21 -0400

    Mccalluc/style docs (#612)
    * MD table borders
    
    * More basic css for MD
    
    * Fix list styling

--- context/app/static/js/components/Markdown/style.js
+++ context/app/static/js/components/Markdown/style.js
@@ -1,13 +1,34 @@
 import styled from 'styled-components';
 import Paper from '@material-ui/core/Paper';
 
+// TODO: Copied and pasted the padding.
+// Should find a way to manage it in one place.
 const StyledPaper = styled(Paper)`
+  padding: 30px 40px 30px 40px;
+
   & img {
     max-width: 100%;
   }
-  padding: 30px 40px 30px 40px;
+
+  & table {
+    border-spacing: 0px;
+    border-collapse: collapse;
+  }
+  & th,
+  & td {
+    border: 1px solid grey;
+    padding: 0.25em 0.5em;
+  }
+
+  blockquote {
+    border-left: 4px solid grey;
+    margin-left: 0;
+    padding-left: 1em;
+  }
+
+  li {
+    list-style: square;
+  }
 `;
-// TODO: Copied and pasted the padding.
-// Should find a way to manage it in one place.
 
 export { StyledPaper };
