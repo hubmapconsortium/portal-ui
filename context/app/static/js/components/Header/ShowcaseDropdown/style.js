ffa88986936bc40878943710b34c9d5059beb169
John Conroy <john-conroy@users.noreply.github.com>
2020-07-09 15:40:15 -0400

    Delete ShowcaseDropdown

--- context/app/static/js/components/Header/ShowcaseDropdown/style.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-import Popper from '@material-ui/core/Popper';
-
-const OffsetPopper = styled(Popper)`
-  margin-top: 14px;
-  z-index: 5;
-`;
-
-export { OffsetPopper };
