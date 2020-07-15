b3a8e23fbac40b7d1ba55c1443ddc84a9de241fe
mccalluc <mccalluc@users.noreply.github.com>
2020-07-15 18:06:08 -0400

    Clean up subprocesses, and run cypress on both environments

--- test.sh
+++ test.sh
@@ -5,6 +5,20 @@ start() { echo travis_fold':'start:$1; echo $1; }
 end() { echo travis_fold':'end:$1; }
 die() { set +v; echo "$*" 1>&2 ; sleep 1; exit 1; }
 
+server_up() {
+  TRIES=0
+  MAX_TRIES=150
+  URL=http://localhost:$1
+  until curl --silent --fail $URL; do
+    [ ${TRIES} -gt ${MAX_TRIES} ] && die "Server not running at $URL"
+    printf '.'
+    sleep 1
+    TRIES=$(($TRIES+1))
+  done
+  echo "Server starts up, and $URL returns 200."
+}
+
+
 start changelog
 if [ "$TRAVIS_BRANCH" != 'master' ] && [[ "$TRAVIS_BRANCH" != *'release'* ]]; then
   git remote set-branches --add origin master
@@ -14,6 +28,7 @@ if [ "$TRAVIS_BRANCH" != 'master' ] && [[ "$TRAVIS_BRANCH" != *'release'* ]]; th
     || die 'Add a CHANGELOG-something.md'
 fi
 end changelog
+
 
 start dev-start
 if [ ! -z "$TRAVIS" ]; then
@@ -27,10 +42,13 @@ if [ ! -z "$TRAVIS" ]; then
     cat context/instance/app.conf
   )
 fi
-./dev-start.sh &
+set -m; ./dev-start.sh & set +m  # Without job control, I had trouble killing parent and children.
+PID=$!
+server_up 5001  # Not really needed: Cypress will wait for response.
 cypress-etc/test.sh
-kill $!  # Kill dev server
+kill -TERM -$PID  # Kill dev server processes
 end dev-start
+
 
 start flake8
 # Unit tests require dev dependencies beyond requirements.txt.
@@ -40,11 +58,15 @@ flake8 --exclude=$EXCLUDE \
   || die "Try: autopep8 --in-place --aggressive -r . --exclude $EXCLUDE"
 end flake8
 
+
 start pytest
 pytest
 end pytest
 
+
 start docker
 ./docker.sh 5001  # Needs to match port in cypress.json.
+server_up 5001  # Without this, Cypress gets an undefined content-type and immediately fails.
 cypress-etc/test.sh
+docker kill hubmap-portal-ui
 end docker
