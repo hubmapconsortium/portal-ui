4614f44f13b8be4685f9454efca0dcf2d6f82140
Nils Gehlenborg <nils@hms.harvard.edu>
2020-07-17 18:13:28 -0400

    Fixed capitalization of new DUA notice.

--- context/app/static/js/components/Home/DataUseGuidelines/DataUseGuidelines.jsx
+++ context/app/static/js/components/Home/DataUseGuidelines/DataUseGuidelines.jsx
@@ -9,9 +9,9 @@ function DataUseGuidelines() {
         Data Use Guidelines
       </Typography>
       <MainText mt={1} variant="body1">
-        The majority of Data available on the HuBMAP Portal is Open Access Data to be used for Research of Human
-        Biology. Certain Data types with potential for re-identification are available in restricted access either
-        directly through this Portal by login-dependent permissions or through dbGAP. All Data users are expected to
+        The majority of data available on the HuBMAP Portal is open access data to be used for research of human
+       biology. Certain data types with potential for re-identification are available in restricted access either
+        directly through this portal by login-dependent permissions or through dbGAP. All data users are expected to
         respect the privacy and confidentiality of the donors who provided samples. Data are not to be used to
         re-identify donors or their family members without further approval from HuBMAP.
       </MainText>
