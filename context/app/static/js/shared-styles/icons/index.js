d5d531656e90b6e5658693766c6366f7f443ef55
John Conroy <john-conroy@users.noreply.github.com>
2020-07-17 11:42:21 -0400

    Add ExternalLinkIcon to icons in shared-styles

--- context/app/static/js/shared-styles/icons/index.js
+++ context/app/static/js/shared-styles/icons/index.js
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 import BubbleChartIcon from '@material-ui/icons/BubbleChart';
 import PersonIcon from '@material-ui/icons/Person';
 import TableChartIcon from '@material-ui/icons/TableChart';
+import LaunchRoundedIcon from '@material-ui/icons/LaunchRounded';
 
 const DonorIcon = styled(PersonIcon)`
   font-size: ${(props) => props.fontSize};
@@ -15,4 +16,8 @@ const DatasetIcon = styled(TableChartIcon)`
   font-size: ${(props) => props.fontSize};
 `;
 
-export { DonorIcon, SampleIcon, DatasetIcon };
+const ExternalLinkIcon = styled(LaunchRoundedIcon)`
+  font-size: ${(props) => props.fontSize};
+`;
+
+export { DonorIcon, SampleIcon, DatasetIcon, ExternalLinkIcon };
