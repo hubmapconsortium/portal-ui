5733dd7565f28f5e46860ab591566e983b88dea0
Chuck McCallum <mccalluc@users.noreply.github.com>
2020-07-17 21:46:03 -0400

    Update context/app/static/js/components/Home/DataUseGuidelines/DataUseGuidelines.jsx

--- context/app/static/js/components/Home/DataUseGuidelines/DataUseGuidelines.jsx
+++ context/app/static/js/components/Home/DataUseGuidelines/DataUseGuidelines.jsx
@@ -10,7 +10,7 @@ function DataUseGuidelines() {
       </Typography>
       <MainText mt={1} variant="body1">
         The majority of data available on the HuBMAP Portal is open access data to be used for research of human
-       biology. Certain data types with potential for re-identification are available in restricted access either
+        biology. Certain data types with potential for re-identification are available in restricted access either
         directly through this portal by login-dependent permissions or through dbGAP. All data users are expected to
         respect the privacy and confidentiality of the donors who provided samples. Data are not to be used to
         re-identify donors or their family members without further approval from HuBMAP.
