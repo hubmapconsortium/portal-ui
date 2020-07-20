0420fc9e38882938f72ea128651aa984429d8fc1
John Conroy <john-conroy@users.noreply.github.com>
2020-07-20 09:42:47 -0400

    Update GlobusLinkMessage per code review feedback

--- context/app/static/js/components/Detail/GlobusLinkMessage/GlobusLinkMessage.jsx
+++ context/app/static/js/components/Detail/GlobusLinkMessage/GlobusLinkMessage.jsx
@@ -6,7 +6,6 @@ import { LightBlueLink } from 'shared-styles/Links';
 import { StyledExternalLinkIcon } from './style';
 
 const messages = {
-  200: 'Files are available through the Globus Research Data Management System. Access dataset',
   401: 'Unauthorized access to the Globus Research Management System (bad or expired token). If you believe this to be an error, please contact',
   403: 'Access to files on the Globus Research Management system are restricted. You may not have access to these files because the Consortium is still curating data and/or the data is protected data that requires you to be a member of the Consortium “Protected Data Group”. Such protected data will be available via dbGaP in the future. If you believe this to be an error, please contact',
   404: 'Files are not available through the Globus Research Management system. If you believe this to be an error, please contact',
@@ -15,25 +14,38 @@ const messages = {
 
 function GlobusLinkMessage(props) {
   const { statusCode, url, display_doi } = props;
+
+  if (statusCode === 200) {
+    return (
+      <Typography variant="body2">
+        {`'Files are available through the Globus Research Data Management System. Access dataset' ${display_doi} on `}
+        <LightBlueLink underline="none" variant="body2" href={url}>
+          Globus <StyledExternalLinkIcon />
+        </LightBlueLink>
+      </Typography>
+    );
+  }
+
+  if (statusCode in Object.keys(messages)) {
+    return (
+      <Typography variant="body2">
+        {`${messages[statusCode]} `}
+        <LightBlueLink underline="none" variant="body2" href="mailto:help@hubmapconsortium.org">
+          help@hubmapconsortium.org
+        </LightBlueLink>
+        .
+      </Typography>
+    );
+  }
+
+  // TODO get fallback message for unanticipated response statuses
   return (
     <Typography variant="body2">
-      {statusCode === 200 ? (
-        <>
-          {messages[statusCode]} {display_doi}
-          {' on '}
-          <LightBlueLink underline="none" variant="body2" href={url}>
-            Globus <StyledExternalLinkIcon />
-          </LightBlueLink>
-        </>
-      ) : (
-        <>
-          {messages[statusCode]}{' '}
-          <LightBlueLink underline="none" variant="body2" href="mailto:help@hubmapconsortium.org">
-            help@hubmapconsortium.org
-          </LightBlueLink>
-          .
-        </>
-      )}
+      {`Unexpected ${statusCode}. Report error to `}
+      <LightBlueLink underline="none" variant="body2" href="mailto:help@hubmapconsortium.org">
+        help@hubmapconsortium.org
+      </LightBlueLink>
+      .
     </Typography>
   );
 }
