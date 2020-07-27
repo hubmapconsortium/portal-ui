1a982163408a090f3118f68f802fc254ccb48766
John Conroy <john-conroy@users.noreply.github.com>
2020-07-27 18:25:13 -0400

    Remove method argument from useProtocolData fetch

--- context/app/static/js/hooks/useProtocolData.js
+++ context/app/static/js/hooks/useProtocolData.js
@@ -4,9 +4,7 @@ function useProtocolData(doi, lastVersion = 1) {
   const [protocol, setProtocol] = React.useState({});
   React.useEffect(() => {
     async function getAndSetProtocol() {
-      const response = await fetch(`https://www.protocols.io/api/v3/protocols/${doi}?last_version=${lastVersion}`, {
-        method: 'GET',
-      });
+      const response = await fetch(`https://www.protocols.io/api/v3/protocols/${doi}?last_version=${lastVersion}`);
       if (!response.ok) {
         console.error('Protocol API failed', response);
         return;
