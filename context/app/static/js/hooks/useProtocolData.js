8ded193c1c2c805f11129bfc5c6ed52015fd3036
John Conroy <john-conroy@users.noreply.github.com>
2020-07-27 10:26:51 -0400

    Add useProtocolData hook

---
+++ context/app/static/js/hooks/useProtocolData.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+function useProtocolData(doi, lastVersion = 1) {
+  const [protocol, setProtocol] = React.useState({});
+  React.useEffect(() => {
+    async function getAndSetProtocol() {
+      const response = await fetch(`https://www.protocols.io/api/v3/protocols/${doi}?last_version=${lastVersion}`, {
+        method: 'GET',
+      });
+      if (!response.ok) {
+        console.error('Protocol API failed', response);
+        return;
+      }
+      const data = await response.json();
+      setProtocol(data);
+    }
+    getAndSetProtocol();
+  }, [doi, lastVersion]);
+
+  return protocol;
+}
+
+export default useProtocolData;
