d452b5abb96ab729b7c310bfb921953083a9c98f
ilan-gold <ilanbassgold@gmail.com>
2020-07-27 11:40:16 -0400

    Update test.

--- context/app/test_routes_main.py
+++ context/app/test_routes_main.py
@@ -92,7 +92,7 @@ def mock_search_donor_post(path, **kwargs):
 
 @pytest.mark.parametrize(
     'path',
-    ['/', '/browse/donor/fake-uuid', '/ccf-eui', '/docs', '/VERSION', '/preview/spraggins']
+    ['/', '/browse/donor/fake-uuid', '/ccf-eui', '/docs', '/VERSION', '/preview/multimodal-molecular-imaging-data']
 )
 def test_200_html_page(client, path, mocker):
     mocker.patch('requests.get', side_effect=mock_prov_get)
