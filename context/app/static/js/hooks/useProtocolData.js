0c5dd409740f3079c49b9760a3d61e3b1878760a
John Conroy <john-conroy@users.noreply.github.com>
2020-07-28 13:17:51 -0400

    Fix useProtocolData prettier issues

--- context/app/static/js/hooks/useProtocolData.js
+++ context/app/static/js/hooks/useProtocolData.js
@@ -4,7 +4,9 @@ function useProtocolData(doiSuffix, lastVersion = 1) {
   const [protocol, setProtocol] = React.useState({});
   React.useEffect(() => {
     async function getAndSetProtocol() {
-      const response = await fetch(`https://www.protocols.io/api/v3/protocols/${doiSuffix}?last_version=${lastVersion}`);
+      const response = await fetch(
+        `https://www.protocols.io/api/v3/protocols/${doiSuffix}?last_version=${lastVersion}`,
+      );
       if (!response.ok) {
         console.error('Protocol API failed', response);
         return;
