8aa44f80f0625eb0e02f0a2126d0d0b6ec0bb53a
mccalluc <mccalluc@users.noreply.github.com>
2020-07-29 15:46:44 -0400

    Make all columns sortable

--- context/app/static/js/components/Search/Search.jsx
+++ context/app/static/js/components/Search/Search.jsx
@@ -35,6 +35,7 @@ const httpHeaders = nexus_token
       Authorization: `Bearer ${nexus_token}`,
     }
   : {};
+const resultFields = resultFieldsByType[type];
 const searchProps = {
   // The default behavior is to add a "_search" path.
   // We don't want that.
@@ -46,14 +47,21 @@ const searchProps = {
   // Search results field which will be appended to detailsUrlPrefix:
   idField: 'uuid',
   // Search results fields to display in table:
-  resultFields: resultFieldsByType[type],
+  resultFields,
   // Default hitsPerPage is 10:
   hitsPerPage: 20,
   // Sidebar facet configuration;
   // "type" should be one of the filters described here:
   // http://docs.searchkit.co/stable/components/navigation/
   filters: filtersByType[type],
-  sortOptions: lastModifiedSort,
+  sortOptions: resultFields
+    .map((field) => {
+      return [
+        { defaultOption: false, label: `${field.name} down`, field: `${field.id}.keyword`, order: 'desc' },
+        { defaultOption: false, label: `${field.name} up`, field: `${field.id}.keyword`, order: 'asc' },
+      ];
+    })
+    .flat(),
   hiddenFilterIds: hiddenFilters.map((hiddenFilter) => hiddenFilter.props.id),
   queryFields: ['everything'],
   isLoggedIn: Boolean(nexus_token),
