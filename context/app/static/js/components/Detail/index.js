3abfb4a759a76cee2c217bd781b65dc3c1f610c0
John Conroy <john-conroy@users.noreply.github.com>
2020-07-29 11:06:27 -0400

    Delete unused index files

--- context/app/static/js/components/Detail/index.js
+++
@@ -1,6 +0,0 @@
-import Donor from './Donor';
-import Sample from './Sample';
-import Dataset from './Dataset';
-import Collection from './Collection';
-
-export { Donor, Sample, Dataset, Collection };
