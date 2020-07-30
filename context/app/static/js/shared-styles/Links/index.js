b7b029fca49f6e798642dbff68ceea3989a074b3
John Conroy <john-conroy@users.noreply.github.com>
2020-07-30 16:49:15 -0400

    Make no underline default for LightBlueLink

--- context/app/static/js/shared-styles/Links/index.js
+++ context/app/static/js/shared-styles/Links/index.js
@@ -1,7 +1,11 @@
+import React from 'react';
 import styled from 'styled-components';
 import Link from '@material-ui/core/Link';
 
-const LightBlueLink = styled(Link)`
+// eslint-disable-next-line react/jsx-props-no-spreading
+const LinkWithoutUnderline = (props) => <Link {...props} underline="none" />;
+
+const LightBlueLink = styled(LinkWithoutUnderline)`
   color: ${(props) => props.theme.palette.info.main};
 `;
 
