cb4cb5db70eb3e2627a058f521d0b13c2f7b01c9
John Conroy <john-conroy@users.noreply.github.com>
2020-07-31 10:39:18 -0400

    Update Preview

--- context/app/static/js/components/Preview/style.js
+++ context/app/static/js/components/Preview/style.js
@@ -1,7 +1,6 @@
 import styled from 'styled-components';
 import ReactMarkdown from 'react-markdown';
 import Paper from '@material-ui/core/Paper';
-import InfoIcon from '@material-ui/icons/Info';
 
 const StyledMarkdown = styled(ReactMarkdown)`
   li {
@@ -9,12 +8,10 @@ const StyledMarkdown = styled(ReactMarkdown)`
     list-style-type: disc;
   }
 `;
+
 const StyledPaper = styled(Paper)`
   display: flex;
   padding: 30px 40px 30px 40px;
 `;
-const StyledInfoIcon = styled(InfoIcon)`
-  margin-right: ${(props) => props.theme.spacing(1)}px;
-`;
 
-export { StyledMarkdown, StyledPaper, StyledInfoIcon };
+export { StyledMarkdown, StyledPaper };
