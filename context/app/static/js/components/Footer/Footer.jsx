ef5b1c115df361aaf25afadd014f7be82a3973c4
John Conroy <john-conroy@users.noreply.github.com>
2020-07-31 13:31:22 -0400

    Update Citing HuBMAP link in Footer

--- context/app/static/js/components/Footer/Footer.jsx
+++ context/app/static/js/components/Footer/Footer.jsx
@@ -48,7 +48,9 @@ function Footer() {
               >
                 Data Use Agreement
               </LightBlueLink>
-              <LightBlueLink variant="body2">Citing HuBMAP</LightBlueLink>
+              <LightBlueLink href="/docs/about" variant="body2">
+                Citing HuBMAP
+              </LightBlueLink>
             </FlexColumn>
             <FlexColumn>
               <Typography variant="subtitle2">Funding</Typography>
