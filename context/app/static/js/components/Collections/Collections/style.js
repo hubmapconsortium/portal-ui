2994e513814d3400c0f763b5b888a50120f7d76f
John Conroy <john-conroy@users.noreply.github.com>
2020-07-31 14:24:13 -0400

    Increase spacing before footer in Collections

--- context/app/static/js/components/Collections/Collections/style.js
+++ context/app/static/js/components/Collections/Collections/style.js
@@ -3,6 +3,7 @@ import Paper from '@material-ui/core/Paper';
 
 // 88px = header height + header margin
 const PageWrapper = styled.div`
+  margin-bottom: ${(props) => props.theme.spacing(5)}px;
   @media (min-width: ${(props) => props.theme.breakpoints.values.md}px) {
     height: calc(100vh - 88px);
     display: flex;
@@ -14,7 +15,7 @@ const ScrollBox = styled(Paper)`
   @media (min-width: ${(props) => props.theme.breakpoints.values.md}px) {
     flex-grow: 1;
     overflow-y: scroll;
-    margin-top: ${(props) => props.theme.spacing(1)}px 0px;
+    margin-top: ${(props) => props.theme.spacing(1)}px;
   }
 `;
 
