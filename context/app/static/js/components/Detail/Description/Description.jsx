fa564dffc78356d9ada03f397a3e61c40ee733f4
John Conroy <john-conroy@users.noreply.github.com>
2020-07-31 10:39:11 -0400

    Create Description component

---
+++ context/app/static/js/components/Detail/Description/Description.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
+
+import SectionContainer from '../SectionContainer';
+import { StyledPaper, StyledInfoIcon } from './style';
+
+function Description(props) {
+  const { children } = props;
+  return (
+    <SectionContainer id="description">
+      <StyledPaper>
+        <StyledInfoIcon color="primary" />
+        <Typography variant="body1">{children}</Typography>
+      </StyledPaper>
+    </SectionContainer>
+  );
+}
+
+export default Description;
