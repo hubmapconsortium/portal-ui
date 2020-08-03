f43421adeb577e589aa6924c0311bfe99c536788
John Conroy <john-conroy@users.noreply.github.com>
2020-08-03 10:14:52 -0400

    Display data access level in SummaryData

--- context/app/static/js/components/Detail/SummaryData/SummaryData.jsx
+++ context/app/static/js/components/Detail/SummaryData/SummaryData.jsx
@@ -1,21 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import { capitalizeString } from 'helpers/functions';
 import { Flex, FlexRight, FlexCenterAlign, JsonButton, StyledFileIcon } from './style';
 import SummaryItem from '../SummaryItem';
 import StatusIcon from '../StatusIcon';
 
 function SummaryData(props) {
-  const { entity_type, uuid, status, children } = props;
+  const { entity_type, uuid, status, data_access_level, children } = props;
 
   return (
     <Flex>
       {children && <FlexCenterAlign>{children}</FlexCenterAlign>}
       <FlexRight>
-        {entity_type === 'Dataset' && status && status.length > 0 && (
+        {entity_type === 'Dataset' && (
           <FlexCenterAlign>
             <StatusIcon status={status} />
             <SummaryItem>{status}</SummaryItem>
+            <SummaryItem>{`${capitalizeString(data_access_level)} Access`}</SummaryItem>
           </FlexCenterAlign>
         )}
         <JsonButton href={`/browse/${entity_type.toLowerCase()}/${uuid}.json`} target="_blank" component="a">
@@ -29,12 +31,12 @@ function SummaryData(props) {
 SummaryData.propTypes = {
   entity_type: PropTypes.string.isRequired,
   uuid: PropTypes.string.isRequired,
-  status: PropTypes.string,
+  status: PropTypes.string.isRequired,
+  data_access_level: PropTypes.string.isRequired,
   children: PropTypes.element,
 };
 
 SummaryData.defaultProps = {
-  status: '',
   children: undefined,
 };
 
