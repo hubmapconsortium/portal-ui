a124a7f7319f7154837cc9c3978b878d3ff6fe58
John Conroy <john-conroy@users.noreply.github.com>
2020-08-03 18:40:05 -0400

    Update useSendPageView to not send uuids

--- context/app/static/js/components/Routes/useSendPageView.js
+++ context/app/static/js/components/Routes/useSendPageView.js
@@ -3,6 +3,11 @@ import ReactGA from 'react-ga';
 
 export function useSendPageView(path) {
   useEffect(() => {
+    if (path.startsWith('/browse')) {
+      const pathWithoutUUID = path.match(/\/\w+\/\w+/)[0];
+      ReactGA.pageview(pathWithoutUUID);
+      return;
+    }
     if (path.startsWith('/search') || path.startsWith('/dev-search')) {
       const urlParams = new URLSearchParams(window.location.search);
       const entityTypeKey = 'entity_type[0]';
