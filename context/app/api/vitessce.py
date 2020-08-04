94fbddcac4a83bf77c451b0b97920aa0ba72ee40
ilan-gold <ilanbassgold@gmail.com>
2020-08-04 17:33:05 -0400

    Address comment

--- context/app/api/vitessce.py
+++ context/app/api/vitessce.py
@@ -473,12 +473,10 @@ class Vitessce:
         # IMC also needs to be handled specially.
         # TODO: Clean this up by handling multiple assay types (#982).
         if any(
-            [
-                "IMS_PosMode" in file["rel_path"]
-                or "IMS_NegMode" in file["rel_path"]
-                or "IMC" in self.entity["data_types"]
-                for file in self.entity["files"]
-            ]
+            "IMS_PosMode" in file["rel_path"]
+            or "IMS_NegMode" in file["rel_path"]
+            or "IMC" in self.entity["data_types"]
+            for file in self.entity["files"]
         ):
             conf["staticLayout"][-1]["props"]["view"]["zoom"] = -2
             conf["staticLayout"][-1]["props"]["view"]["target"] = [1000, 1000, 0]
