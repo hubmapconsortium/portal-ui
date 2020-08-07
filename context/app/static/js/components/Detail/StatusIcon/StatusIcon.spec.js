f943cc000eeca7d6c3f918d8507a7eb2e25f6fd4
John Conroy <62477388+john-conroy@users.noreply.github.com>
2020-08-07 09:44:59 -0400

    Update context/app/static/js/components/Detail/StatusIcon/StatusIcon.spec.js
    Co-authored-by: Chuck McCallum <mccalluc@users.noreply.github.com>

--- context/app/static/js/components/Detail/StatusIcon/StatusIcon.spec.js
+++ context/app/static/js/components/Detail/StatusIcon/StatusIcon.spec.js
@@ -3,17 +3,22 @@ import React from 'react';
 import { render, screen } from 'test-utils/functions';
 import StatusIcon from './StatusIcon';
 
+const blue = '#3781D1';
+const red = '#DA348A';
+const orange = '#D25435';
+const green = '#6C8938';
+
 test.each([
-  ['Reopened', '#3781D1'],
-  ['QA', '#3781D1'],
-  ['Locked', '#3781D1'],
-  ['Processing', '#3781D1'],
-  ['Hold', '#3781D1'],
-  ['Invalid', '#DA348A'],
-  ['Error', '#DA348A'],
-  ['Unpublished', '#D25435'],
-  ['Deprecated', '#D25435'],
-  ['Published', '#6C8938'],
+  ['Reopened', blue],
+  ['QA', blue],
+  ['Locked', blue],
+  ['Processing', blue],
+  ['Hold', blue],
+  ['Invalid', red],
+  ['Error', red],
+  ['Unpublished', orange],
+  ['Deprecated', orange],
+  ['Published', green],
 ])('status %s displays correct color %s', (status, color) => {
   render(<StatusIcon status={status} />);
 
