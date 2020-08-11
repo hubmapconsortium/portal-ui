be497498568d56fb300225e125e4ef46ae393fe4
John Conroy <john-conroy@users.noreply.github.com>
2020-08-11 13:12:14 -0400

    Add on click test to FilesConditionalLink

--- context/app/static/js/components/Detail/FilesConditionalLink/FilesConditionalLink.spec.js
+++ context/app/static/js/components/Detail/FilesConditionalLink/FilesConditionalLink.spec.js
@@ -1,9 +1,11 @@
 /* eslint-disable import/no-unresolved */
 import React from 'react';
 import { render, screen } from 'test-utils/functions';
+import userEvent from '@testing-library/user-event';
+
 import FilesConditionalLink from './FilesConditionalLink';
 
-const fakeOpenDUA = () => {};
+const fakeOpenDUA = jest.fn();
 
 test('the element is a link when dua has been agreed to', () => {
   render(
@@ -25,4 +27,16 @@ test('the element is button when dua has not been agreed to', () => {
   expect(screen.getByRole('button')).toBeInTheDocument();
   expect(screen.getByRole('button')).toHaveTextContent('child');
   expect(screen.queryByRole('link')).toBeNull();
+});
+
+test('the button has on click works', () => {
+  const mockOpenDUA = jest.fn();
+  render(
+    <FilesConditionalLink hasAgreedToDUA={null} openDUA={mockOpenDUA} href="fakeref">
+      child
+    </FilesConditionalLink>,
+  );
+  expect(mockOpenDUA).toHaveBeenCalledTimes(0);
+  userEvent.click(screen.getByRole('button'));
+  expect(mockOpenDUA).toHaveBeenCalledTimes(1);
 });
