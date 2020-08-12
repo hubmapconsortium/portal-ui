8f77727bbc2601bf5907c8e48ce9431e63b35475
mccalluc <mccalluc@users.noreply.github.com>
2020-08-12 18:07:25 -0400

    Text and centering

--- context/app/static/js/components/Maintenance/index.jsx
+++ context/app/static/js/components/Maintenance/index.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import Container from '@material-ui/core/Container';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
 import Providers from '../Providers';
 import Footer from '../Footer';
 import { Header } from '../Header';
@@ -7,7 +10,25 @@ function Maintenance() {
   return (
     <Providers>
       <Header isMaintenanceMode />
-      <div className="main-content">Under Construction!</div>
+      <div className="main-content">
+        <Container
+          maxWidth="sm"
+          style={{ position: 'absolute', top: '40%', transform: 'translateY(-40%) translateX(-50%)', left: '50%' }}
+        >
+          <Card>
+            <CardContent>
+              <h1>Portal Maintenance</h1>
+              <p>
+                <strong>Portal unavailable for scheduled maintenance.</strong>
+              </p>
+              <p>
+                While the portal is under maintenance, visit the{' '}
+                <a href="https://hubmapconsortium.org/">HuBMAP Consortium</a> website.
+              </p>
+            </CardContent>
+          </Card>
+        </Container>
+      </div>
       <Footer isMaintenanceMode />
     </Providers>
   );
