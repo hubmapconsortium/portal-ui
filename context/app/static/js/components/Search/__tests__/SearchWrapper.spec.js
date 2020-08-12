d8e38d53030287b59515f59ed502b7736cc65e7d
mccalluc <mccalluc@users.noreply.github.com>
2020-08-12 09:57:45 -0400

    Add more tests of getByPath

--- context/app/static/js/components/Search/__tests__/SearchWrapper.spec.js
+++ context/app/static/js/components/Search/__tests__/SearchWrapper.spec.js
@@ -7,5 +7,17 @@ describe('SearchWrapper helper functions', () => {
       const field = { id: 'a.b.c' };
       expect(getByPath(document, field)).toEqual('xyz');
     });
+
+    it('concatenates array values', () => {
+      const document = { a: { b: { c: ['x', 'y', 'z'] } } };
+      const field = { id: 'a.b.c' };
+      expect(getByPath(document, field)).toEqual('x / y / z');
+    });
+
+    it('concatenates array values', () => {
+      const document = { a: { b: { c: '42' } } };
+      const field = { id: 'a.b.c', translations: { '42': 'The Answer' } };
+      expect(getByPath(document, field)).toEqual('The Answer');
+    });
   });
 });
