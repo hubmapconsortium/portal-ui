e07116410ff6143f3d904d32b9e3f66a5327f38e
John Conroy <john-conroy@users.noreply.github.com>
2020-08-18 14:56:13 -0400

    Add Error tests

---
+++ context/app/static/js/pages/Error/Error.spec.js
@@ -0,0 +1,25 @@
+/* eslint-disable import/no-unresolved */
+import React from 'react';
+import { render, screen } from 'test-utils/functions';
+
+import Error from './Error';
+
+test.each([
+  [400, 'Bad Request'],
+  [401, 'Unauthorized'],
+  [403, 'Access Denied'],
+  [404, 'Page Not Found'],
+  [504, 'Gateway Timeout'],
+  [500, 'Internal Server Error'],
+])('%i error page displays correct titles %s', (errorCode, title) => {
+  render(<Error errorCode={errorCode} />);
+
+  expect(screen.getByText(errorCode.toString(), { exact: false })).toBeInTheDocument();
+  expect(screen.getAllByText(title, { exact: false })).toHaveLength(2);
+});
+
+test('should display maintenance titles', () => {
+  render(<Error isMaintenancePage />);
+  expect(screen.getByText('Portal Maintenance')).toBeInTheDocument();
+  expect(screen.getByText('Portal unavailable for scheduled maintenance.')).toBeInTheDocument();
+});
