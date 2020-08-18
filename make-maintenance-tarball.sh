b3dcaef2f3cda9ce2eec7a5e787ab9b3b1fd64bf
John Conroy <john-conroy@users.noreply.github.com>
2020-08-18 15:18:01 -0400

    Update maintenance build script

--- make-maintenance-tarball.sh
+++ make-maintenance-tarball.sh
@@ -7,17 +7,10 @@ reset=`tput sgr0`
 die() { set +v; echo "$red$*$reset" 1>&2 ; exit 1; }
 
 TAR_GZ='hubmap-portal-maintenance.tar.gz'
-OUTPUT_DIR='tmp/hubmap-portal-maintenance/'
-PUBLIC_DIR='../../context/app/static/js/maintenance/public'
 
 cd context && npm run build:maintain
-cd -
-
-
-mkdir -p "$OUTPUT_DIR"
-cd "$OUTPUT_DIR"
-rm -rf *
-cp -r "$PUBLIC_DIR" .
+cd app/static/js/maintenance/
 
 tar -czvf "$TAR_GZ" public
-echo "${green}Send $TAR_GZ to Bill.$reset" 
+mv "$TAR_GZ" public
+echo "${green}Send $TAR_GZ in context/app/static/js/maintenance/public to Bill.$reset" 
