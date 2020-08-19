520afba9ea3db7e1d3de9b02c2591e7a40ba012a
John Conroy <john-conroy@users.noreply.github.com>
2020-08-19 17:31:25 -0400

    Uncomment react-ga code in maintenance App

--- context/app/static/js/maintenance/App.jsx
+++ context/app/static/js/maintenance/App.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
-// import ReactGA from 'react-ga';
+import ReactGA from 'react-ga';
 import Providers from 'js/components/Providers';
 import Footer from 'js/components/Footer';
 import Error from 'js/pages/Error';
 import MaintenanceHeader from './MaintenanceHeader';
 
 function App() {
-  // ReactGA.initialize('UA-133341631-3');
-  // ReactGA.pageview('/maintenance');
+  ReactGA.initialize('UA-133341631-3');
+  ReactGA.pageview('/maintenance');
   return (
     <Providers>
       <MaintenanceHeader />
