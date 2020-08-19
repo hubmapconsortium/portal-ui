800ff5b5133be5bef40a25091770d5575dd72f9d
John Conroy <john-conroy@users.noreply.github.com>
2020-08-19 14:46:05 -0400

    Add Providers to maintenance App

--- context/app/static/js/maintenance/App.jsx
+++ context/app/static/js/maintenance/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 // import ReactGA from 'react-ga';
+import Providers from 'js/components/Providers';
 import MaintenanceHeader from './MaintenanceHeader';
 import MaintenanceError from './MaintenanceError';
 import MaintenanceFooter from './MaintenanceFooter';
@@ -8,11 +9,11 @@ function App() {
   // ReactGA.initialize('UA-133341631-3');
   // ReactGA.pageview('/maintenance');
   return (
-    <>
+    <Providers>
       <MaintenanceHeader />
       <MaintenanceError />
       <MaintenanceFooter />
-    </>
+    </Providers>
   );
 }
 
