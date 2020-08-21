d66c580ad030d52c946467fce28c40effaffc208
John Conroy <john-conroy@users.noreply.github.com>
2020-08-21 13:54:23 -0400

    Pass nexus token as prop to Search and DevSearch

--- context/app/static/js/components/Search/DevSearch.jsx
+++ context/app/static/js/components/Search/DevSearch.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
 import Typography from '@material-ui/core/Typography';
 import { ExistsQuery, BoolMustNot } from 'searchkit';
 
@@ -6,50 +8,48 @@ import SearchWrapper from './SearchWrapper';
 // eslint-disable-next-line import/named
 import { field, filter, checkboxFilter } from './utils';
 
-// eslint-disable-next-line no-undef
-const nexusToken = nexus_token || '';
-const httpHeaders = nexusToken
-  ? {
-      Authorization: `Bearer ${nexusToken}`,
-    }
-  : {};
-
-const searchProps = {
-  // The default behavior is to add a "_search" path.
-  // We don't want that.
-  searchUrlPath: '',
-  // Pass Globus token:
-  httpHeaders,
-  // Prefix for details links: (Entities which are not datasets will redirect.)
-  detailsUrlPrefix: '/browse/dataset/',
-  // Search results field which will be appended to detailsUrlPrefix:
-  idField: 'uuid',
-  // Search results fields to display in table:
-  resultFields: [
-    field('entity_type', 'type'),
-    field('display_doi', 'ID'),
-    field('mapped_last_modified_timestamp', 'Last Modified'),
-    field('mapper_metadata.size', 'Doc Size'),
-  ],
-  // Default hitsPerPage is 10:
-  hitsPerPage: 20,
-  // Sidebar facet configuration:
-  filters: [
-    filter('entity_type', 'Entity Type'),
-    filter('mapper_metadata.version', 'Mapper Version'),
-    checkboxFilter('has_metadata', 'Has metadata?', ExistsQuery('metadata.metadata')),
-    checkboxFilter('no_metadata', 'No metadata?', BoolMustNot(ExistsQuery('metadata.metadata'))),
-    checkboxFilter('has_files', 'Has files?', ExistsQuery('files')),
-    checkboxFilter('no_files', 'No files?', BoolMustNot(ExistsQuery('files'))),
-    checkboxFilter('has_files', 'Spatially Located (CCF)?', ExistsQuery('rui_location')),
-    checkboxFilter('no_files', 'Not Spatially Located (CCF)?', BoolMustNot(ExistsQuery('rui_location'))),
-  ],
-  queryFields: ['everything'],
-  isLoggedIn: Boolean(nexusToken),
-};
-
 function DevSearch(props) {
-  const { elasticsearchEndpoint } = props;
+  const { elasticsearchEndpoint, nexusToken } = props;
+  const httpHeaders = nexusToken
+    ? {
+        Authorization: `Bearer ${nexusToken}`,
+      }
+    : {};
+
+  const searchProps = {
+    // The default behavior is to add a "_search" path.
+    // We don't want that.
+    searchUrlPath: '',
+    // Pass Globus token:
+    httpHeaders,
+    // Prefix for details links: (Entities which are not datasets will redirect.)
+    detailsUrlPrefix: '/browse/dataset/',
+    // Search results field which will be appended to detailsUrlPrefix:
+    idField: 'uuid',
+    // Search results fields to display in table:
+    resultFields: [
+      field('entity_type', 'type'),
+      field('display_doi', 'ID'),
+      field('mapped_last_modified_timestamp', 'Last Modified'),
+      field('mapper_metadata.size', 'Doc Size'),
+    ],
+    // Default hitsPerPage is 10:
+    hitsPerPage: 20,
+    // Sidebar facet configuration:
+    filters: [
+      filter('entity_type', 'Entity Type'),
+      filter('mapper_metadata.version', 'Mapper Version'),
+      checkboxFilter('has_metadata', 'Has metadata?', ExistsQuery('metadata.metadata')),
+      checkboxFilter('no_metadata', 'No metadata?', BoolMustNot(ExistsQuery('metadata.metadata'))),
+      checkboxFilter('has_files', 'Has files?', ExistsQuery('files')),
+      checkboxFilter('no_files', 'No files?', BoolMustNot(ExistsQuery('files'))),
+      checkboxFilter('has_files', 'Spatially Located (CCF)?', ExistsQuery('rui_location')),
+      checkboxFilter('no_files', 'Not Spatially Located (CCF)?', BoolMustNot(ExistsQuery('rui_location'))),
+    ],
+    queryFields: ['everything'],
+    isLoggedIn: Boolean(nexusToken),
+  };
+
   const allProps = { ...searchProps, apiUrl: elasticsearchEndpoint };
 
   // eslint-disable-next-line react/jsx-props-no-spreading
@@ -63,5 +63,14 @@ function DevSearch(props) {
     </>
   );
 }
+
+DevSearch.propTypes = {
+  elasticsearchEndpoint: PropTypes.string.isRequired,
+  nexusToken: PropTypes.string,
+};
+
+DevSearch.defaultProps = {
+  nexusToken: '',
+};
 
 export default DevSearch;
--- context/app/static/js/components/Search/Search.jsx
+++ context/app/static/js/components/Search/Search.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Typography from '@material-ui/core/Typography';
+
 import LookupEntity from 'js/helpers/LookupEntity';
 import SearchWrapper from './SearchWrapper';
 import { donorConfig, sampleConfig, datasetConfig, fallbackConfig } from './config';
@@ -7,59 +9,57 @@ import { donorConfig, sampleConfig, datasetConfig, fallbackConfig } from './conf
 import { filter } from './utils';
 import AncestorNote from './AncestorNote';
 
-const hiddenFilters = [filter('ancestor_ids', 'Ancestor ID'), filter('entity_type', 'Entity Type')];
-
-const filtersByType = {
-  '': fallbackConfig.filters,
-  donor: donorConfig.filters.concat(hiddenFilters),
-  sample: sampleConfig.filters.concat(hiddenFilters),
-  dataset: datasetConfig.filters.concat(hiddenFilters),
-};
-
-const resultFieldsByType = {
-  '': fallbackConfig.fields,
-  donor: donorConfig.fields,
-  sample: sampleConfig.fields,
-  dataset: datasetConfig.fields,
-};
-
-const { searchParams } = new URL(document.location);
-const type = (searchParams.get('entity_type[0]') || '').toLowerCase();
-const hasAncestorParam = searchParams.has('ancestor_ids[0]');
-
-// eslint-disable-next-line no-undef
-const nexusToken = nexus_token || '';
-const httpHeaders = nexusToken
-  ? {
-      Authorization: `Bearer ${nexusToken}`,
-    }
-  : {};
-const resultFields = resultFieldsByType[type];
-const searchProps = {
-  // The default behavior is to add a "_search" path.
-  // We don't want that.
-  searchUrlPath: '',
-  // Pass Globus token:
-  httpHeaders,
-  // Prefix for details links:
-  detailsUrlPrefix: `/browse/${type || 'dataset'}/`,
-  // Search results field which will be appended to detailsUrlPrefix:
-  idField: 'uuid',
-  // Search results fields to display in table:
-  resultFields,
-  // Default hitsPerPage is 10:
-  hitsPerPage: 20,
-  // Sidebar facet configuration;
-  // "type" should be one of the filters described here:
-  // http://docs.searchkit.co/stable/components/navigation/
-  filters: filtersByType[type],
-  hiddenFilterIds: hiddenFilters.map((hiddenFilter) => hiddenFilter.props.id),
-  queryFields: ['everything'],
-  isLoggedIn: Boolean(nexusToken),
-};
-
 function Search(props) {
-  const { title, elasticsearchEndpoint } = props;
+  const { title, elasticsearchEndpoint, nexusToken } = props;
+
+  const hiddenFilters = [filter('ancestor_ids', 'Ancestor ID'), filter('entity_type', 'Entity Type')];
+
+  const filtersByType = {
+    '': fallbackConfig.filters,
+    donor: donorConfig.filters.concat(hiddenFilters),
+    sample: sampleConfig.filters.concat(hiddenFilters),
+    dataset: datasetConfig.filters.concat(hiddenFilters),
+  };
+
+  const resultFieldsByType = {
+    '': fallbackConfig.fields,
+    donor: donorConfig.fields,
+    sample: sampleConfig.fields,
+    dataset: datasetConfig.fields,
+  };
+
+  const { searchParams } = new URL(document.location);
+  const type = (searchParams.get('entity_type[0]') || '').toLowerCase();
+  const hasAncestorParam = searchParams.has('ancestor_ids[0]');
+
+  const httpHeaders = nexusToken
+    ? {
+        Authorization: `Bearer ${nexusToken}`,
+      }
+    : {};
+  const resultFields = resultFieldsByType[type];
+  const searchProps = {
+    // The default behavior is to add a "_search" path.
+    // We don't want that.
+    searchUrlPath: '',
+    // Pass Globus token:
+    httpHeaders,
+    // Prefix for details links:
+    detailsUrlPrefix: `/browse/${type || 'dataset'}/`,
+    // Search results field which will be appended to detailsUrlPrefix:
+    idField: 'uuid',
+    // Search results fields to display in table:
+    resultFields,
+    // Default hitsPerPage is 10:
+    hitsPerPage: 20,
+    // Sidebar facet configuration;
+    // "type" should be one of the filters described here:
+    // http://docs.searchkit.co/stable/components/navigation/
+    filters: filtersByType[type],
+    hiddenFilterIds: hiddenFilters.map((hiddenFilter) => hiddenFilter.props.id),
+    queryFields: ['everything'],
+    isLoggedIn: Boolean(nexusToken),
+  };
   const allProps = Object.assign(searchProps, { apiUrl: elasticsearchEndpoint });
 
   // eslint-disable-next-line react/jsx-props-no-spreading
@@ -78,5 +78,15 @@ function Search(props) {
     </>
   );
 }
+
+Search.propTypes = {
+  elasticsearchEndpoint: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  nexusToken: PropTypes.string,
+};
+
+Search.defaultProps = {
+  nexusToken: '',
+};
 
 export default Search;
