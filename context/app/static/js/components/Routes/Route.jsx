13c05097ceed7beb1cdfa700a18a4a11e2468c1a
John Conroy <john-conroy@users.noreply.github.com>
2020-08-25 10:37:51 -0400

    Create Route component

---
+++ context/app/static/js/components/Routes/Route.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import Container from '@material-ui/core/Container';
+
+const StyledContainer = styled(Container)`
+  margin-top: ${(props) => (props.$mt ? props.theme.spacing(2) : 0)}px;
+`;
+
+function Route({ children, mt, constrainWidth }) {
+  const constrainWidthProps = constrainWidth ? { maxWidth: 'lg' } : { maxWidth: false, disableGutters: true };
+
+  return (
+    // eslint-disable-next-line react/jsx-props-no-spreading
+    <StyledContainer $mt={mt} {...constrainWidthProps}>
+      {children}
+    </StyledContainer>
+  );
+}
+
+Route.propTypes = {
+  mt: PropTypes.bool,
+};
+
+Route.defaultProps = {
+  mt: false,
+};
+
+export default Route;
