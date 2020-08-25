4ba196a6b054b1ea772b824f9562b81d82a85deb
mccalluc <mccalluc@users.noreply.github.com>
2020-08-25 14:02:56 -0400

    Rearrange

--- cypress-etc/artillery/artillery.sh
+++ cypress-etc/artillery/artillery.sh
@@ -10,11 +10,17 @@ export DATA_TYPES_QUERY=`cat queries/data-types.json`
 export ENTITY_TYPES_QUERY=`cat queries/entity-types.json`
 export DATASETS_QUERY=`cat queries/datasets.json`
 
-mkdir outputs || echo 'outputs dir already exists...'
-for TARGET in search; do
-  for RATE in 1 5 10 15 20 25; do
+mkdir outputs || echo 'outputs/ already exists...'
+rm outputs/* || echo 'outputs/ already empty...'
+
+for TARGET in scenarios/*.yml; do
+  BASE=`basename $TARGET`
+  for RATE in 1 5 10 15; do
+    ZERO_PADDED=`printf "%02d" $RATE`
     export RATE
-    ../node_modules/.bin/artillery run $TARGET.yml --output outputs/$TARGET-`printf "%02d" $RATE`-per-second.json
+    ../node_modules/.bin/artillery \
+      run $TARGET \
+      --output outputs/$BASE-$ZERO_PADDED-per-second.json
   done
 done
 
