111baa43330798ae6cc72d6fdc63023088fba383
mccalluc <mccalluc@users.noreply.github.com>
2020-08-25 17:25:11 -0400

    flake8

--- cypress-etc/artillery/summarize.py
+++ cypress-etc/artillery/summarize.py
@@ -4,13 +4,13 @@ from json import load
 from pathlib import Path
 
 sorted_paths = sorted(
-  Path(__file__).parent.glob('outputs/*.json'),
-  key=lambda path: path.stem)
+    Path(__file__).parent.glob('outputs/*.json'),
+    key=lambda path: path.stem)
 for path in sorted_paths:
-  with path.open() as f:
-    report = load(f)
-    print('\t'.join([
-      path.stem,
-      str(report['aggregate']['latency']['median']),
-      ';'.join([f'{k}: {v}' for k,v in report['aggregate']['codes'].items()])
-    ]))
+    with path.open() as f:
+        report = load(f)
+        print('\t'.join([
+            path.stem,
+            str(report['aggregate']['latency']['median']),
+            ';'.join([f'{k}: {v}' for k, v in report['aggregate']['codes'].items()])
+        ]))
