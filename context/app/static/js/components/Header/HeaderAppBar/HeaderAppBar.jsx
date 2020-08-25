312df2aba63ce16bd5fe6e435409455dbfc88ef8
John Conroy <john-conroy@users.noreply.github.com>
2020-08-25 10:37:40 -0400

    Remove bottom margin from header

--- context/app/static/js/components/Header/HeaderAppBar/HeaderAppBar.jsx
+++ context/app/static/js/components/Header/HeaderAppBar/HeaderAppBar.jsx
@@ -1,18 +1,17 @@
 import React from 'react';
 import Container from '@material-ui/core/Container';
 import Toolbar from '@material-ui/core/Toolbar';
-
-import { StyledAppBar } from './style';
+import AppBar from '@material-ui/core/AppBar';
 
 function HeaderAppBar(props) {
   const { children, anchorRef } = props;
 
   return (
-    <StyledAppBar position="sticky" ref={anchorRef} elevation={0}>
+    <AppBar position="sticky" ref={anchorRef} elevation={0}>
       <Container maxWidth="lg">
         <Toolbar disableGutters>{children} </Toolbar>
       </Container>
-    </StyledAppBar>
+    </AppBar>
   );
 }
 
