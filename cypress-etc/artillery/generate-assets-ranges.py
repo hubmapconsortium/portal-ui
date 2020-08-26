e85fc64099dad1ba4014e5fc142a3f5b870c006f
mccalluc <mccalluc@users.noreply.github.com>
2020-08-26 19:21:29 -0400

    Add main sections to scripts

--- cypress-etc/artillery/generate-assets-ranges.py
+++ cypress-etc/artillery/generate-assets-ranges.py
@@ -4,41 +4,45 @@ import random
 
 import requests
 
-es_docs = 20
-es_url = 'https://search-api.stage.hubmapconsortium.org/portal/search'
-assets_url = 'https://assets.stage.hubmapconsortium.org'
-rows_per_file = 20
-chunk_size = 2**16
+def generate():
+    es_docs = 20
+    es_url = 'https://search-api.stage.hubmapconsortium.org/portal/search'
+    assets_url = 'https://assets.stage.hubmapconsortium.org'
+    rows_per_file = 20
+    chunk_size = 2**16
 
-response = requests.post(
-    es_url,
-    json={
-        "post_filter": {
-            "exists": {"field": "files"}
+    response = requests.post(
+        es_url,
+        json={
+            "post_filter": {
+                "exists": {"field": "files"}
+            },
+            "size": es_docs,
+            "_source": ["uuid", "files"],
+            "sort": {"uuid.keyword": "asc"}
         },
-        "size": es_docs,
-        "_source": ["uuid", "files"],
-        "sort": {"uuid.keyword": "asc"}
-    },
-    headers={
-        'Content-Type': 'application/json'
-    }).json()
+        headers={
+            'Content-Type': 'application/json'
+        }).json()
 
-file_sizes = {}
+    file_sizes = {}
 
-for hit in response['hits']['hits']:
-    uuid = hit['_source']['uuid']
-    for file_info in hit['_source']['files']:
-        if file_info['rel_path'].endswith('.ome.tiff'):
-            full_url = f'{assets_url}/{uuid}/{file_info["rel_path"]}'
-            file_sizes[full_url] = file_info['size']
+    for hit in response['hits']['hits']:
+        uuid = hit['_source']['uuid']
+        for file_info in hit['_source']['files']:
+            if file_info['rel_path'].endswith('.ome.tiff'):
+                full_url = f'{assets_url}/{uuid}/{file_info["rel_path"]}'
+                file_sizes[full_url] = file_info['size']
 
-for (url, size) in file_sizes.items():
-    chunk_count = size // chunk_size
-    if chunk_count < rows_per_file:
-        continue
-    chunks = random.sample(range(chunk_count), rows_per_file)
-    for i in chunks:
-        bottom = i * chunk_size
-        top = (i + 1) * chunk_size - 1
-        print(f'{size},{bottom}-{top},{url}')
+    for (url, size) in file_sizes.items():
+        chunk_count = size // chunk_size
+        if chunk_count < rows_per_file:
+            continue
+        chunks = random.sample(range(chunk_count), rows_per_file)
+        for i in chunks:
+            bottom = i * chunk_size
+            top = (i + 1) * chunk_size - 1
+            print(f'{size},{bottom}-{top},{url}')
+
+if __name__ == "__main__":
+    generate()
--- cypress-etc/artillery/summarize.py
+++ cypress-etc/artillery/summarize.py
@@ -3,14 +3,18 @@
 from json import load
 from pathlib import Path
 
-sorted_paths = sorted(
-    Path(__file__).parent.glob('outputs/*.json'),
-    key=lambda path: path.stem)
-for path in sorted_paths:
-    with path.open() as f:
-        report = load(f)
-        print('\t'.join([
-            path.stem,
-            str(report['aggregate']['latency']['median']),
-            ', '.join([f'{k}: {v}' for k, v in report['aggregate']['codes'].items()])
-        ]))
+def summarize():
+    sorted_paths = sorted(
+        Path(__file__).parent.glob('outputs/*.json'),
+        key=lambda path: path.stem)
+    for path in sorted_paths:
+        with path.open() as f:
+            report = load(f)
+            print('\t'.join([
+                path.stem,
+                str(report['aggregate']['latency']['median']),
+                ', '.join([f'{k}: {v}' for k, v in report['aggregate']['codes'].items()])
+            ]))
+
+if __name__ == "__main__":
+    summarize()
