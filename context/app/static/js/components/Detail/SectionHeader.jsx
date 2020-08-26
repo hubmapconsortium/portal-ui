013600a90d2b8746a252c9df670b07003810178e
John Conroy <john-conroy@users.noreply.github.com>
2020-08-26 14:59:33 -0400

    Fix anchor scroll in SectionHeader

--- context/app/static/js/components/Detail/SectionHeader.jsx
+++ context/app/static/js/components/Detail/SectionHeader.jsx
@@ -2,8 +2,12 @@ import React from 'react';
 import styled from 'styled-components';
 import Typography from '@material-ui/core/Typography';
 
+/* Anchor offset for fixed header
+70px = fixed header height + 10px */
 const StyledTypography = styled(Typography)`
   margin-bottom: ${(props) => props.theme.spacing(1)}px;
+  padding-top: 70px;
+  margin-top: -70px;
 `;
 
 function SectionHeader(props) {
