12850ece1aa792d621527f3e58418071ab7cf72f
John Conroy <john-conroy@users.noreply.github.com>
2020-08-26 15:59:20 -0400

    Remove disable margin top prop from Route

--- context/app/static/js/components/Routes/Route.jsx
+++ context/app/static/js/components/Routes/Route.jsx
@@ -4,27 +4,23 @@ import styled from 'styled-components';
 import Container from '@material-ui/core/Container';
 
 const StyledContainer = styled(Container)`
-  margin-top: ${(props) => (props.$disableMarginTop ? 0 : props.theme.spacing(2))}px;
+  margin-top: ${(props) => props.theme.spacing(2)}px;
 `;
 
-function Route({ children, disableMarginTop, disableWidthConstraint }) {
+function Route({ children, disableWidthConstraint }) {
   const constrainWidthProps = disableWidthConstraint ? { maxWidth: false, disableGutters: true } : { maxWidth: 'lg' };
 
   return (
     // eslint-disable-next-line react/jsx-props-no-spreading
-    <StyledContainer $disableMarginTop={disableMarginTop} {...constrainWidthProps}>
-      {children}
-    </StyledContainer>
+    <StyledContainer {...constrainWidthProps}>{children}</StyledContainer>
   );
 }
 
 Route.propTypes = {
-  disableMarginTop: PropTypes.bool,
   disableWidthConstraint: PropTypes.bool,
 };
 
 Route.defaultProps = {
-  disableMarginTop: false,
   disableWidthConstraint: false,
 };
 
