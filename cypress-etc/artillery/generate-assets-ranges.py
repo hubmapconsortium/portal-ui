54fcaa017201104bc51bdc871ed5a7a6f457bae3
mccalluc <mccalluc@users.noreply.github.com>
2020-08-26 18:26:26 -0400

    Working range test

--- cypress-etc/artillery/generate-assets-ranges.py
+++ cypress-etc/artillery/generate-assets-ranges.py
@@ -8,7 +8,7 @@ import requests
 es_docs = 20
 es_url = 'https://search-api.stage.hubmapconsortium.org/portal/search'
 assets_url = 'https://assets.stage.hubmapconsortium.org'
-rows_per_file = 10
+rows_per_file = 20
 chunk_size = 2**16
 
 response = requests.post(
@@ -34,18 +34,12 @@ for hit in response['hits']['hits']:
             full_url = f'{assets_url}/{uuid}/{file_info["rel_path"]}'
             file_sizes[full_url] = file_info['size']
 
-if not len(file_sizes.keys()):
-    print(response)
-    print('This response was unexpected.')
-    exit(1)
-
 for (url, size) in file_sizes.items():
-    chunk_count = size % chunk_size
+    chunk_count = size // chunk_size
     if chunk_count < rows_per_file:
         continue
-    # chunks = random.sample(range(chunk_count), rows_per_file)
-    chunks = range(10)
+    chunks = random.sample(range(chunk_count), rows_per_file)
     for i in chunks:
         bottom = i * chunk_size
         top = (i + 1) * chunk_size - 1
-        print(f'{bottom}-{top},{url}')
+        print(f'{size},{bottom}-{top},{url}')
