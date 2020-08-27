3e257fd4a1d40fec957d794431d46bea74c44fa2
John Conroy <john-conroy@users.noreply.github.com>
2020-08-27 16:30:32 -0400

    Move offset from SectionHeader to SectionContainer

--- context/app/static/js/components/Detail/SectionContainer.jsx
+++ context/app/static/js/components/Detail/SectionContainer.jsx
@@ -1,7 +1,11 @@
 import styled from 'styled-components';
 
+/* Anchor offset for fixed header
+74px = fixed header height + 10px */
 const SectionContainer = styled.div`
   margin-bottom: ${(props) => props.theme.spacing(5)}px;
+  padding-top: 74px;
+  margin-top: -74px;
 `;
 
 export default SectionContainer;
--- context/app/static/js/components/Detail/SectionHeader.jsx
+++ context/app/static/js/components/Detail/SectionHeader.jsx
@@ -2,21 +2,15 @@ import React from 'react';
 import styled from 'styled-components';
 import Typography from '@material-ui/core/Typography';
 
-/* Anchor offset for fixed header
-70px = fixed header height + 10px */
 const StyledTypography = styled(Typography)`
   margin-bottom: ${(props) => props.theme.spacing(1)}px;
-  padding-top: 70px;
-  margin-top: -70px;
 `;
 
 function SectionHeader(props) {
-  const { children, isSummary, ...rest } = props;
-
-  const variant = isSummary ? 'h2' : 'h4';
+  const { children, ...rest } = props;
   return (
     // eslint-disable-next-line react/jsx-props-no-spreading
-    <StyledTypography variant={variant} component="h2" {...rest}>
+    <StyledTypography variant="h4" component="h2" {...rest}>
       {children}
     </StyledTypography>
   );
