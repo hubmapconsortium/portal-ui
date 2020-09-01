95f84213d7c9e9988a6fc58558b13b4da88248b7
John Conroy <john-conroy@users.noreply.github.com>
2020-09-01 14:07:54 -0400

    Add background icon button to shared styles

---
+++ context/app/static/js/shared-styles/buttons/index.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+import IconButton from '@material-ui/core/IconButton';
+
+const BackgroundIconButton = styled(IconButton)`
+  background-color: #fff;
+  padding: 10px;
+  border-radius: 4px;
+`;
+
+export { BackgroundIconButton };
