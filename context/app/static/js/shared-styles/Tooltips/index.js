d9bbd084e372b4f403a41d41f46758d4c9a83e54
John Conroy <john-conroy@users.noreply.github.com>
2020-09-01 16:06:54 -0400

    Export secondary tooltip as higher order component

--- context/app/static/js/shared-styles/Tooltips/index.js
+++ context/app/static/js/shared-styles/Tooltips/index.js
@@ -1,4 +1,16 @@
+/* eslint-disable react/jsx-props-no-spreading */
+import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
+import Tooltip from '@material-ui/core/Tooltip';
+
+function TooltipBase(props) {
+  const { classes, children, ...rest } = props;
+  return (
+    <Tooltip {...rest} classes={classes} arrow>
+      {children}
+    </Tooltip>
+  );
+}
 
 const useRoundedSecondaryTooltipStyles = makeStyles((theme) => ({
   tooltip: {
@@ -6,5 +18,10 @@ const useRoundedSecondaryTooltipStyles = makeStyles((theme) => ({
     borderRadius: '0.4rem',
   },
 }));
+const SecondaryTooltip = (props) => {
+  const secondaryClasses = useRoundedSecondaryTooltipStyles();
 
-export { useRoundedSecondaryTooltipStyles };
+  return <TooltipBase {...props} classes={secondaryClasses} />;
+};
+
+export { SecondaryTooltip };
