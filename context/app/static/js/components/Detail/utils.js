96a54f4f339eb42a9278562d5209429b0ab10ec8
John Conroy <john-conroy@users.noreply.github.com>
2020-09-02 19:59:00 -0400

    Add getSectionOrder util function for detail components

---
+++ context/app/static/js/components/Detail/utils.js
@@ -0,0 +1,5 @@
+export function getSectionOrder(possibleSections, optionalSectionsToInclude) {
+  return possibleSections.filter(
+    (section) => !(section in optionalSectionsToInclude) || optionalSectionsToInclude[section] === true,
+  );
+}
