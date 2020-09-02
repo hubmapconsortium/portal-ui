5a460478a407c085db596119f5031bd3234d375e
John Conroy <john-conroy@users.noreply.github.com>
2020-09-02 13:20:13 -0400

    Rename BackgroundIconButton to WhiteBackgroundIconButton and add TooltipToggleButton to pass props through tooltip

--- context/app/static/js/shared-styles/buttons/index.js
+++ context/app/static/js/shared-styles/buttons/index.js
@@ -1,10 +1,34 @@
-import styled from 'styled-components';
+/* eslint-disable react/jsx-props-no-spreading */
+import React from 'react';
+import styled, { css } from 'styled-components';
 import IconButton from '@material-ui/core/IconButton';
+import ToggleButton from '@material-ui/lab/ToggleButton';
 
-const BackgroundIconButton = styled(IconButton)`
+const WhiteBackgroundButtonCSS = css`
   background-color: #fff;
   padding: 10px;
   border-radius: 4px;
 `;
 
-export { BackgroundIconButton };
+const WhiteBackgroundIconButton = styled(IconButton)`
+  ${WhiteBackgroundButtonCSS}
+`;
+
+const WhiteBackgroundToggleButton = styled(ToggleButton)`
+  ${WhiteBackgroundButtonCSS}
+  border: 0px;
+`;
+
+function TooltipToggleButton(props) {
+  const { children, tooltipComponent, tooltipTitle, ...rest } = props;
+  const Tooltip = tooltipComponent;
+  return (
+    <Tooltip title={tooltipTitle}>
+      <WhiteBackgroundToggleButton {...rest}>{children}</WhiteBackgroundToggleButton>
+    </Tooltip>
+  );
+}
+
+export default TooltipToggleButton;
+
+export { WhiteBackgroundButtonCSS, WhiteBackgroundIconButton, TooltipToggleButton };
