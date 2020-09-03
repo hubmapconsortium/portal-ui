99f46c0288beef90e58cce89fa0045a49dafc5b0
John Conroy <john-conroy@users.noreply.github.com>
2020-09-03 12:04:38 -0400

    Update providers in test utils

--- context/test-utils/functions.js
+++ context/test-utils/functions.js
@@ -4,8 +4,20 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import Providers from 'js/components/Providers';
 
+const appProviderEndpoints = {
+  elasticsearchEndpoint: 'fakeElasticsearchEndpoint',
+  entityEndpoint: 'fakeEntityEndpoint',
+  assetsEndpoint: 'fakeAssetsEndpoint',
+};
+
+const appProviderToken = 'fakeNexusToken';
+
 const AllTheProviders = ({ children }) => {
-  return <Providers>{children}</Providers>;
+  return (
+    <Providers endpoints={appProviderEndpoints} nexusToken={appProviderToken}>
+      {children}
+    </Providers>
+  );
 };
 
 const customRender = (ui, options) => render(ui, { wrapper: AllTheProviders, ...options });
@@ -14,4 +26,4 @@ const customRender = (ui, options) => render(ui, { wrapper: AllTheProviders, ...
 export * from '@testing-library/react';
 
 // override render method
-export { customRender as render };
+export { customRender as render, appProviderEndpoints, appProviderToken };
