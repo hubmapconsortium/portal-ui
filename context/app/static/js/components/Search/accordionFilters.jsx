13717b6a7092aad37dba2d22d61dab0652a96828
mccalluc <mccalluc@users.noreply.github.com>
2020-09-08 21:49:33 -0400

    Hide empty facets

--- context/app/static/js/components/Search/accordionFilters.jsx
+++ context/app/static/js/components/Search/accordionFilters.jsx
@@ -8,6 +8,17 @@ import { InnerAccordion, InnerAccordionSummary, InnerAccordionDetails } from './
 
 function Details(props) {
   const { title, children } = props;
+  if (Array.isArray(children) && children[0].props.docCount === 0) {
+    // Presumably a list filter...
+    return null;
+  }
+  if (
+    !Array.isArray(children) &&
+    children.props.items.map((bucket) => bucket.doc_count).every((count) => count === 0)
+  ) {
+    // Presumably a range filter...
+    return null;
+  }
   return (
     <InnerAccordion key={title} defaultExpanded>
       <InnerAccordionSummary expandIcon={<ExpandMoreIcon />}>
