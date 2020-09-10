83ecb6d1e7c53fc050f8725600adace31d1deb95
John Conroy <john-conroy@users.noreply.github.com>
2020-09-10 12:08:34 -0400

    Rename hook

--- context/app/static/js/components/Routes/useSetUrlPathAndSearchBeforeLogin.js
+++ context/app/static/js/components/Routes/useSetUrlPathAndSearchBeforeLogin.js
@@ -1,13 +1,13 @@
 import { useEffect } from 'react';
 import Cookies from 'universal-cookie';
 
-function useSetUrlBeforeLogin(path) {
+function useSetPathAndSearchBeforeLogin(path) {
   useEffect(() => {
     const cookies = new Cookies();
     if (path !== '/login' && path !== '/logout') {
-      cookies.set('urlBeforeLogin', path, { path: '/' });
+      cookies.set('urlPathAndSearchBeforeLogin', path, { path: '/' });
     }
   }, [path]);
 }
 
-export default useSetUrlBeforeLogin;
+export default useSetPathAndSearchBeforeLogin;
