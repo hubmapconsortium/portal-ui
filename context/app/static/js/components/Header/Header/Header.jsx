0388eeaf110c248b9455ea0a6a71bdc2bf85f256
John Conroy <john-conroy@users.noreply.github.com>
2020-09-24 12:13:37 -0400

    Display detail header if summary is not in view

--- context/app/static/js/components/Header/Header/Header.jsx
+++ context/app/static/js/components/Header/Header/Header.jsx
@@ -1,14 +1,21 @@
 import React, { useRef } from 'react';
+
+import useStore from 'js/components/store';
+import DetailHeader from 'js/components/Detail/Header';
 import HeaderAppBar from '../HeaderAppBar';
 import HeaderContent from '../HeaderContent';
 
 function Header() {
   const anchorRef = useRef(null);
+  const summaryInView = useStore((state) => state.summaryInView);
 
   return (
-    <HeaderAppBar anchorRef={anchorRef}>
-      <HeaderContent anchorRef={anchorRef} />
-    </HeaderAppBar>
+    <>
+      <HeaderAppBar anchorRef={anchorRef}>
+        <HeaderContent anchorRef={anchorRef} />
+      </HeaderAppBar>
+      {!summaryInView && <DetailHeader />}
+    </>
   );
 }
 
