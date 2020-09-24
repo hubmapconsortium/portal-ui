abadb6b9d91edf18583bb8b8cd2fd0e367809442
John Conroy <john-conroy@users.noreply.github.com>
2020-09-24 13:02:32 -0400

    Add transition to detail header

--- context/app/static/js/components/Detail/Header/Header.jsx
+++ context/app/static/js/components/Detail/Header/Header.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
+import { useTransition, animated } from 'react-spring';
 
 import { HeaderPaper } from './style';
 
+const AnimatedPaper = animated(HeaderPaper);
+
 function Header() {
-  return <HeaderPaper>dasdasd</HeaderPaper>;
+  const transitions = useTransition([true], null, {
+    from: { overflow: 'hidden', height: 0 },
+    enter: { height: 40 },
+    leave: { height: 0 },
+  });
+
+  return transitions.map(
+    ({ item, key, props }) =>
+      item && (
+        <AnimatedPaper key={key} style={props}>
+          asdasd
+        </AnimatedPaper>
+      ),
+  );
 }
 
 export default Header;
--- context/app/static/js/components/Detail/Header/style.js
+++ context/app/static/js/components/Detail/Header/style.js
@@ -2,7 +2,6 @@ import styled from 'styled-components';
 import Paper from '@material-ui/core/Paper';
 
 const HeaderPaper = styled(Paper)`
-  height: 40px;
   position: sticky;
   top: 64px;
   z-index: 1000;
