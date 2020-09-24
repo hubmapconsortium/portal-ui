20214dc23c8efbc25444c822a83c031713b9fad0
John Conroy <john-conroy@users.noreply.github.com>
2020-09-24 12:11:16 -0400

    Create basic header component

---
+++ context/app/static/js/components/Detail/Header/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import { HeaderPaper } from './style';
+
+function Header() {
+  return <HeaderPaper>dasdasd</HeaderPaper>;
+}
+
+export default Header;
---
+++ context/app/static/js/components/Detail/Header/style.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+import Paper from '@material-ui/core/Paper';
+
+const HeaderPaper = styled(Paper)`
+  height: 40px;
+  position: sticky;
+  top: 64px;
+  z-index: 1000;
+`;
+
+export { HeaderPaper };
