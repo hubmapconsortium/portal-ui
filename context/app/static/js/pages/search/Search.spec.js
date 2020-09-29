f5382ebb314464a1e457876d07f5dbd6f0f250f3
mccalluc <mccalluc@users.noreply.github.com>
2020-09-29 11:20:20 -0400

    Try and fail to supress warning messages

--- context/app/static/js/pages/search/Search.spec.js
+++ context/app/static/js/pages/search/Search.spec.js
@@ -4,8 +4,17 @@ import { render } from 'test-utils/functions';
 
 import Search from './Search';
 
-test('Search error if no URL param', () => {
+test('Search error if missing entity_type param', () => {
+  // TODO: I want to supressed the log noise during this test... but this doesn't work. :(
+  global.console = { error: jest.fn().mockImplementation(() => {}) };
+
   expect(() => render(<Search elasticsearchEndpoint="not-used" title="not-used" />)).toThrow(
     'Unexpected URL param "entity_type[0]="; Should be one of {donor, sample, dataset}',
   );
 });
+
+// test('Search works with entity_type param', () => {
+//   expect(() => render(<Search elasticsearchEndpoint="not-used" title="not-used" />)).toThrow(
+//     'Unexpected URL param "entity_type[0]="; Should be one of {donor, sample, dataset}',
+//   );
+// });
