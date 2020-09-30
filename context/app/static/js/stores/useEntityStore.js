c3bd2bebb5734ab38f38b00c7ce97c4ee7a8b1d1
John Conroy <john-conroy@users.noreply.github.com>
2020-09-30 16:34:37 -0400

    Separate useEntityStore and useVisualizationStore

--- context/app/static/js/stores/useEntityStore.js
+++ context/app/static/js/stores/useEntityStore.js
@@ -3,10 +3,6 @@ import create from 'zustand';
 const useEntityStore = create((set) => ({
   summaryInView: false,
   setSummaryInView: (val) => set({ summaryInView: val }),
-  vizIsFullscreen: false,
-  setVizIsFullscreen: (val) => set({ vizIsFullscreen: val }),
-  vizTheme: 'light',
-  setVizTheme: (val) => set({ vizTheme: val }),
   assayMetadata: {},
   setAssayMetadata: (val) => set({ assayMetadata: val }),
 }));
