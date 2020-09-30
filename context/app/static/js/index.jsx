ef68f3558d5b57cfdb8eed1cd0963149e4333be4
mccalluc <mccalluc@users.noreply.github.com>
2020-09-30 16:34:08 -0400

    Log if there are schema validation errors

--- context/app/static/js/index.jsx
+++ context/app/static/js/index.jsx
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom';
 
 import App from './components/App';
 
+// eslint-disable-next-line no-undef
+const { validation_errors } = flaskData.entity.mapper_metadata;
+if (validation_errors && validation_errors.length) {
+  console.warn('Schema validation errors', validation_errors);
+}
+
 ReactDOM.render(
   // eslint-disable-next-line no-undef
   <App flaskData={flaskData} />,
