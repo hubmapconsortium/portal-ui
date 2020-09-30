63c54fb20fde9a32eb27e9c4ff4b35e2dc77f600
Chuck McCallum <mccalluc@users.noreply.github.com>
2020-09-30 13:38:20 -0400

    fix unexpected URL param [skip ci]
    Co-authored-by: John Conroy <62477388+john-conroy@users.noreply.github.com>

--- context/app/static/js/pages/search/Search.spec.js
+++ context/app/static/js/pages/search/Search.spec.js
@@ -5,12 +5,14 @@ import { render } from 'test-utils/functions';
 import Search from './Search';
 
 test('Search error if missing entity_type param', () => {
-  // TODO: I want to supress the log noise during this test... but this doesn't work.
-  global.console = { error: jest.fn().mockImplementation(() => {}) };
+  const originalConsoleError = console.error;
+  console.error = jest.fn();
 
   expect(() => render(<Search elasticsearchEndpoint="not-used" title="not-used" />)).toThrow(
     'Unexpected URL param "entity_type[0]="; Should be one of {donor, sample, dataset}',
   );
+
+  console.error = originalConsoleError;
 });
 
 test('Search works with entity_type param', () => {
