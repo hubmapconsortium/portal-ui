a087f16def7c84c94b9ae475a7e42b64bf8f65a6
John Conroy <john-conroy@users.noreply.github.com>
2020-09-30 17:30:00 -0400

    Fix Header elevation logic

--- context/app/static/js/components/Header/Header/Header.jsx
+++ context/app/static/js/components/Header/Header/Header.jsx
@@ -1,13 +1,19 @@
 import React, { useRef } from 'react';
 
 import EntityHeader from 'js/components/Detail/entityHeader/EntityHeader';
+import useEntityStore from 'js/stores/useEntityStore';
 import HeaderAppBar from '../HeaderAppBar';
 import HeaderContent from '../HeaderContent';
 
+const entityStoreSelector = (state) => state.summaryInView;
+
 function Header() {
   const anchorRef = useRef(null);
+  const summaryInView = useEntityStore(entityStoreSelector);
   const displayEntityHeader =
-    window.location.pathname.startsWith('/browse') && !window.location.pathname.startsWith('/browse/collection');
+    !summaryInView &&
+    window.location.pathname.startsWith('/browse') &&
+    !window.location.pathname.startsWith('/browse/collection');
 
   return (
     <>
