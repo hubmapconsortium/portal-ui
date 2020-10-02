18fbfda4e5063c48b3d9ddbb8bc175ca91aafbbb
mccalluc <mccalluc@users.noreply.github.com>
2020-10-02 16:31:48 -0400

    Fix cypress

--- context/app/static/js/index.jsx
+++ context/app/static/js/index.jsx
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 
 // eslint-disable-next-line no-undef
-const { validation_errors } = flaskData.entity.mapper_metadata;
+const validation_errors = flaskData.entity?.mapper_metadata?.validation_errors;
 if (validation_errors && validation_errors.length) {
   console.warn('Schema validation errors', validation_errors);
 }
