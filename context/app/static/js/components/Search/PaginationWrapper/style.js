62bb51d2646a5294950718698f146d87cfb7260a
John Conroy <john-conroy@users.noreply.github.com>
2020-10-06 10:14:14 -0400

    Fix layout of pagination and results found

--- context/app/static/js/components/Search/PaginationWrapper/style.js
+++ context/app/static/js/components/Search/PaginationWrapper/style.js
@@ -2,6 +2,9 @@ import styled from 'styled-components';
 
 const Flex = styled.div`
   display: flex;
+  align-items: flex-end;
+  justify-content: space-between;
+  margin-top: 24px;
 `;
 
 export { Flex };
