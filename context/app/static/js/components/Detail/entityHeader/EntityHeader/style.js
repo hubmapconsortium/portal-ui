a2fee86f9817e40c95793d2a14c1f99d8beed94d
John Conroy <john-conroy@users.noreply.github.com>
2020-10-09 13:53:42 -0400

    Sync height variables

--- context/app/static/js/components/Detail/entityHeader/EntityHeader/style.js
+++ context/app/static/js/components/Detail/entityHeader/EntityHeader/style.js
@@ -1,10 +1,12 @@
 import styled from 'styled-components';
 import Paper from '@material-ui/core/Paper';
+
 import { headerHeight } from 'js/components/Header/HeaderAppBar/style';
+import { iconButtonHeight } from 'js/shared-styles/buttons';
 
 const StyledPaper = styled(Paper)`
   position: fixed;
-  height: 40px;
+  height: ${iconButtonHeight}px;
   width: 100%;
   top: ${headerHeight}px;
   z-index: 1000;
--- context/app/static/js/shared-styles/buttons/index.js
+++ context/app/static/js/shared-styles/buttons/index.js
@@ -3,10 +3,12 @@ import styled, { css } from 'styled-components';
 import IconButton from '@material-ui/core/IconButton';
 import ToggleButton from '@material-ui/lab/ToggleButton';
 
+const iconButtonHeight = 40;
+
 const WhiteBackgroundButtonCSS = css`
   background-color: #fff;
-  height: 40px;
-  width: 40px;
+  height: ${iconButtonHeight}px;
+  width: ${iconButtonHeight}px;
   border-radius: 4px;
   padding: 0px;
 `;
@@ -39,4 +41,4 @@ function TooltipToggleButton(props) {
 
 export default TooltipToggleButton;
 
-export { WhiteBackgroundButtonCSS, WhiteBackgroundIconButton, TooltipToggleButton };
+export { WhiteBackgroundButtonCSS, WhiteBackgroundIconButton, TooltipToggleButton, iconButtonHeight };
