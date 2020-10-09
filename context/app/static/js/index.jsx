bad821beec39a68fc2560fdd91badd2c5758915f
mccalluc <mccalluc@users.noreply.github.com>
2020-10-09 17:34:03 -0400

    Add creators

--- context/app/static/js/index.jsx
+++ context/app/static/js/index.jsx
@@ -11,12 +11,30 @@ ReactDOM.render(
 
 function getDatasetLD(entity) {
   const donor = entity.donor.mapped_metadata;
-  const donorString = `${donor.sex}, ${donor.age_value} ${donor.age_unit} old`;
+  const shortDonorString = `${donor.sex}, ${donor.age_value} ${donor.age_unit} old`;
+  const longDonorString = `${donor.height_value}${donor.height_unit}, ${donor.weight_value}${donor.weight_unit}, ${donor.race} ${shortDonorString}`;
+  const assayOrganString = `${entity.mapped_data_types} of ${entity.origin_sample.mapped_organ}`;
+  const medicalHistory = donor.medicalHistory ? `${donor.medicalHistory.join(', ')}` : 'no medical history';
+  const name = `${assayOrganString} from ${shortDonorString}`;
+  const fallbackDescription = `${assayOrganString} from ${longDonorString} with ${medicalHistory}`;
+
   return {
     '@context': 'https://schema.org/',
     '@type': 'Dataset',
-    name: `${entity.mapped_data_types} of ${entity.origin_sample.mapped_organ} from ${donorString}`,
-    description: entity.description,
+    name,
+    description: entity.description || fallbackDescription,
+    creator: [
+      {
+        '@type': 'Person',
+        // sameAs: 'http://orcid.org/0000-0000-0000-0001',
+        name: entity.created_by_user_displayname,
+      },
+      {
+        '@type': 'Organization',
+        // TODO: sameAs: 'http://ror.org/xxxxxxxxx',
+        name: entity.group_name,
+      },
+    ],
   };
 }
 
