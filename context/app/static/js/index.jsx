c8068a3e7141cd394529e0d20a4eca2617c5e987
Chuck McCallum <mccalluc@users.noreply.github.com>
2020-10-14 15:39:15 -0400

    Mccalluc/machine readable metadata (#1292)
    * JSON-LD
    
    * Changelog
    
    * Add creators
    
    * Move to separate file
    
    * Add defaults, and a test

--- context/app/static/js/index.jsx
+++ context/app/static/js/index.jsx
@@ -2,9 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './components/App';
+import { setJsonLD } from './schema.org';
 
 ReactDOM.render(
   // eslint-disable-next-line no-undef
   <App flaskData={flaskData} />,
   document.getElementById('react-content'),
 );
+
+/* eslint-disable no-undef */
+if (flaskData?.entity?.entity_type === 'Dataset') {
+  setJsonLD(flaskData.entity);
+}
+/* eslint-enable */
---
+++ context/app/static/js/schema.org.js
@@ -0,0 +1,43 @@
+function getDatasetLD(entity) {
+  // Based on https://developers.google.com/search/docs/data-types/dataset#guidelines
+  const donor = entity.donor.mapped_metadata;
+  const shortDonorString = `${donor.sex || '(Unknown sex)'}, ${donor.age_value || '(Unknown age)'} ${
+    donor.age_unit || '(Unknown age unit)'
+  } old`;
+  const heightString = `${donor.height_value || ''}${donor.height_unit || ''}`;
+  const weightString = `${donor.weight_value || ''}${donor.weight_unit || ''}`;
+  const longDonorString = `${heightString}, ${weightString}, ${donor.race || ''} ${shortDonorString}`;
+  const assayOrganString = `${entity.mapped_data_types} of ${entity.origin_sample.mapped_organ}`;
+  const medicalHistory = donor.medicalHistory ? `${donor.medicalHistory.join(', ')}` : 'no medical history';
+  const name = `${assayOrganString} from ${shortDonorString}`;
+  const fallbackDescription = `${assayOrganString} from ${longDonorString} with ${medicalHistory}`;
+
+  return {
+    '@context': 'https://schema.org/',
+    '@type': 'Dataset',
+    name,
+    description: entity.description || fallbackDescription,
+    creator: [
+      {
+        '@type': 'Person',
+        // sameAs: 'http://orcid.org/0000-0000-0000-0001',
+        name: entity.created_by_user_displayname,
+      },
+      {
+        '@type': 'Organization',
+        // TODO: sameAs: 'http://ror.org/xxxxxxxxx',
+        name: entity.group_name,
+      },
+    ],
+  };
+}
+
+function setJsonLD(entity) {
+  const ld = getDatasetLD(entity);
+  const script = document.createElement('script');
+  script.setAttribute('type', 'application/ld+json');
+  script.textContent = JSON.stringify(ld, null, 2);
+  document.head.appendChild(script);
+}
+
+export { getDatasetLD, setJsonLD };
