18d873ec1bb97caca028cf6cc7f06f391fc61798
John Conroy <john-conroy@users.noreply.github.com>
2020-10-16 12:35:31 -0400

    Style layout of ResultsTiles

--- context/app/static/js/components/Search/ResultsTiles/ResultsTiles.jsx
+++ context/app/static/js/components/Search/ResultsTiles/ResultsTiles.jsx
@@ -1,23 +1,27 @@
 import React from 'react';
 import EntityTile from 'js/components/entity-tile/EntityTile';
 
+import { TilesLayout } from './style';
+
 function ResultsTiles(props) {
   const { hits } = props;
   /* eslint-disable no-underscore-dangle */
   return (
-    <>
+    <TilesLayout>
       {hits.map((hit) => (
-        <EntityTile
-          entity_type="Dataset"
-          uuid={hit._source.uuid}
-          id={hit._source.display_doi}
-          entityData={hit._source}
-          descendantCounts={
-            'descendant_counts' in hit._source ? hit._source.descendant_counts.entity_type : { Dataset: 0 }
-          }
-        />
+        <div>
+          <EntityTile
+            entity_type="Dataset"
+            uuid={hit._source.uuid}
+            id={hit._source.display_doi}
+            entityData={hit._source}
+            descendantCounts={
+              'descendant_counts' in hit._source ? hit._source.descendant_counts.entity_type : { Dataset: 0 }
+            }
+          />
+        </div>
       ))}
-    </>
+    </TilesLayout>
   );
   /* eslint-enable no-underscore-dangle */
 }
