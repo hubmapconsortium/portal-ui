5fb048718ba3b4d70b3c876ab92e60b3f9416854
John Conroy <john-conroy@users.noreply.github.com>
2020-10-20 15:01:57 -0400

    Don't pass isLoggedIn prop to SearchWrapper

--- context/app/static/js/pages/search/Search.jsx
+++ context/app/static/js/pages/search/Search.jsx
@@ -59,7 +59,6 @@ function Search(props) {
     // Sidebar facet configuration:
     filters: filtersByType[type],
     queryFields: ['everything'],
-    isLoggedIn: Boolean(nexusToken),
   };
   const allProps = { apiUrl: elasticsearchEndpoint, ...searchProps }; // TODO: Not needed?
 
