5ded9539c56e832c92c4eb0906bfc02d78dd2286
John Conroy <john-conroy@users.noreply.github.com>
2020-10-21 14:56:47 -0400

    Fix startsWith path in useSearchViewStore

--- context/app/static/js/stores/useSearchViewStore.js
+++ context/app/static/js/stores/useSearchViewStore.js
@@ -2,7 +2,7 @@ import create from 'zustand';
 
 const useSearchViewStore = create((set) => ({
   searchView:
-    (window.location.pathname.startsWith('search') && new URLSearchParams(window.location.search).get('view')) ||
+    (window.location.pathname.startsWith('/search') && new URLSearchParams(window.location.search).get('view')) ||
     'table',
   setSearchView: (val) => set({ searchView: val }),
 }));
