da4a0bb94d9e4d1c8a11c1725a9efb830555da1d
John Conroy <62477388+john-conroy@users.noreply.github.com>
2020-10-23 15:12:36 -0400

    Apply suggestions from code review
    Co-authored-by: Chuck McCallum <mccalluc@users.noreply.github.com>

--- context/app/static/js/components/Search/noHitsComponents/noHitsComponents.jsx
+++ context/app/static/js/components/Search/noHitsComponents/noHitsComponents.jsx
@@ -3,11 +3,11 @@ import React from 'react';
 import { Alert } from 'js/shared-styles/alerts';
 
 function NoResults() {
-  return <Alert severity="warning">No Results Found. Check your spelling or unselect filters.</Alert>;
+  return <Alert severity="warning">No results found. Check your spelling or unselect filters.</Alert>;
 }
 
 function SearchError() {
-  return <Alert severity="error">An issue occurred when fetching your results. Please try again. </Alert>;
+  return <Alert severity="error">An error occurred when fetching your results. Please try again.</Alert>;
 }
 
 export { NoResults, SearchError };
