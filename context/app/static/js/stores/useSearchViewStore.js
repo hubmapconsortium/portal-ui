1f1db8a983d3036577df2160809c9ddaf191e51f
John Conroy <john-conroy@users.noreply.github.com>
2020-10-26 13:36:51 -0400

    Revert change to initial state in useSearchViewStore

--- context/app/static/js/stores/useSearchViewStore.js
+++ context/app/static/js/stores/useSearchViewStore.js
@@ -1,9 +1,7 @@
 import create from 'zustand';
 
 const useSearchViewStore = create((set) => ({
-  searchView:
-    (window.location.pathname.startsWith('/search') && new URLSearchParams(window.location.search).get('view')) ||
-    'table',
+  searchView: new URLSearchParams(window.location.search).get('view') || 'table',
   setSearchView: (val) => set({ searchView: val }),
 }));
 
