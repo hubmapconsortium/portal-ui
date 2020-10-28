183559e02172d58f687bbc3a676177dad7f76144
John Conroy <john-conroy@users.noreply.github.com>
2020-10-28 14:05:15 -0400

    Reorganize DropdownSelectItem into DropdownListboxOption

--- context/app/static/js/shared-styles/dropdowns/DropdownListboxOption/style.js
+++ context/app/static/js/shared-styles/dropdowns/DropdownListboxOption/style.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import styled from 'styled-components';
 import MenuItem from '@material-ui/core/MenuItem';
 import CheckRoundedIcon from '@material-ui/icons/CheckRounded';
@@ -18,14 +17,4 @@ const StyledSpan = styled.span`
   ${(props) => !props.isSelected && `margin-left: calc(${iconSize} + ${props.theme.spacing(1)}px)`};
 `;
 
-function DropdownSelectItem(props) {
-  const { children, selected, className, ...rest } = props;
-  return (
-    <FlexMenuItem className={className} selected={selected} {...rest} role="option">
-      {selected && <CheckIcon color="primary" />}
-      <StyledSpan isSelected={selected}>{children}</StyledSpan>
-    </FlexMenuItem>
-  );
-}
-
-export { DropdownSelectItem };
+export { FlexMenuItem, CheckIcon, StyledSpan };
