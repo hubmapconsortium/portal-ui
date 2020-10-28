b52282f54c39d740f56f1fb328e3200df3242047
John Conroy <john-conroy@users.noreply.github.com>
2020-10-28 14:06:03 -0400

    Create DropdownListbox

---
+++ context/app/static/js/shared-styles/dropdowns/DropdownListbox/DropdownListbox.jsx
@@ -0,0 +1,68 @@
+import React, { useState, useRef } from 'react';
+import Paper from '@material-ui/core/Paper';
+import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDownRounded';
+import ArrowDropUpIcon from '@material-ui/icons/ArrowDropUpRounded';
+import ClickAwayListener from '@material-ui/core/ClickAwayListener';
+import MenuList from '@material-ui/core/MenuList';
+
+import { StyledPopper } from './style';
+
+function DropdownListbox(props) {
+  const {
+    selectedItemLabel,
+    buttonComponent: SelectionButton,
+    optionComponent: Option,
+    buttonProps,
+    options,
+    selectOnClick,
+    getOptionLabel,
+  } = props;
+  const anchorRef = useRef(null);
+  const [isOpen, setIsOpen] = useState(false);
+
+  function selectOption(option) {
+    selectOnClick(option);
+    setIsOpen(false);
+  }
+
+  function isSelectedOption(option) {
+    return getOptionLabel(option) === selectedItemLabel;
+  }
+
+  return (
+    <>
+      <SelectionButton
+        ref={anchorRef}
+        aria-haspopup="listbox"
+        disableElevation
+        variant="contained"
+        color="primary"
+        onClick={() => setIsOpen(true)}
+        {...buttonProps}
+      >
+        {selectedItemLabel} {isOpen ? <ArrowDropUpIcon /> : <ArrowDropDownIcon />}
+      </SelectionButton>
+      <StyledPopper open={isOpen} anchorEl={anchorRef.current} placement="bottom-start">
+        <Paper>
+          <ClickAwayListener onClickAway={() => setIsOpen(false)}>
+            <MenuList role="listbox">
+              {options.map((option) => (
+                <Option
+                  onClick={() => selectOption(option)}
+                  key={getOptionLabel(option)}
+                  autoFocus={isSelectedOption(option)}
+                  selected={isSelectedOption(option)}
+                  aria-selected={isSelectedOption(option)}
+                >
+                  {getOptionLabel(option)}
+                </Option>
+              ))}
+            </MenuList>
+          </ClickAwayListener>
+        </Paper>
+      </StyledPopper>
+    </>
+  );
+}
+
+export default DropdownListbox;
---
+++ context/app/static/js/shared-styles/dropdowns/DropdownListbox/style.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+import Popper from '@material-ui/core/Popper';
+
+const StyledPopper = styled(Popper)`
+  z-index: 50;
+`;
+
+export { StyledPopper };
