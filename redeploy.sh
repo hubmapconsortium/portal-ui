d8f9a222ce1598d16be188f2eef1ca4e17332b72
yuanzhou <yuanzhou19@gmail.com>
2020-11-03 22:15:17 -0500

    Updates for portal-prod.stage

--- redeploy.sh
+++ redeploy.sh
@@ -3,7 +3,7 @@ set -o errexit
 
 die() { set +v; echo "$*" 1>&2 ; exit 1; }
 
-[ "$#" -eq 2 ] || die "usage: $0 USERNAME [ dev | test ]"
+[ "$#" -eq 2 ] || die "usage: $0 USERNAME [ dev | test | stage]"
 USER="$1"
 TARGET="$2"
 
@@ -18,7 +18,14 @@ cd /home/centos/hubmap/portal-ui/compose
 echo 'portal running?' `docker ps | grep portal-ui`
 
 echo 'stopping...'
-docker-compose -f hubmap.yml down
+
+# Also stop and remove the portal-ui-pord container on STAGE only
+if [ "$2" = "stage" ]; then
+    docker-compose -f hubmap.yml -f portal-prod.stage.yml down
+else
+    docker-compose -f hubmap.yml down
+fi
+
 echo 'portal running?' `docker ps | grep portal-ui`
 
 echo 'removing old "latest"...'
@@ -26,7 +33,14 @@ echo 'removing old "latest"...'
 docker rmi hubmap/portal-ui:latest
 
 echo 'starting...'
-docker-compose -f hubmap.yml up -d
+
+# Also start the portal-ui-pord container on STAGE only
+if [ "$2" = "stage" ]; then
+    docker-compose -f hubmap.yml -f portal-prod.stage.yml up -d
+else
+    docker-compose -f hubmap.yml up -d
+fi
+
 echo 'portal running?' `docker ps | grep portal-ui`
 
 # TODO: Move VERSION into context, and cat it.
@@ -35,3 +49,8 @@ docker exec -it portal-ui cat package.json
 EOF
 
 echo "Visit --> http://portal.$TARGET.hubmapconsortium.org/"
+
+if [ "$2" = "stage" ]; then
+    echo "Visit --> http://portal-prod.$TARGET.hubmapconsortium.org/"
+fi
+
