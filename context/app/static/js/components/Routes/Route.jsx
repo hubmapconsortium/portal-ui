24176ac7a05f3547db2f1b296c02bce9efbb7093
John Conroy <john-conroy@users.noreply.github.com>
2020-11-05 14:14:41 -0500

    Add suspense loader to Route

--- context/app/static/js/components/Routes/Route.jsx
+++ context/app/static/js/components/Routes/Route.jsx
@@ -1,16 +1,36 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import Container from '@material-ui/core/Container';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
 const StyledContainer = styled(Container)`
   margin-top: ${(props) => props.theme.spacing(2)}px;
+  flex-grow: 1;
+`;
+
+const LoadingWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  flex-grow: 1;
 `;
 
 function Route({ children, disableWidthConstraint }) {
   const constrainWidthProps = disableWidthConstraint ? { maxWidth: false, disableGutters: true } : { maxWidth: 'lg' };
 
-  return <StyledContainer {...constrainWidthProps}>{children}</StyledContainer>;
+  return (
+    <Suspense
+      fallback={
+        <LoadingWrapper>
+          <CircularProgress />
+        </LoadingWrapper>
+      }
+    >
+      <StyledContainer {...constrainWidthProps}>{children}</StyledContainer>{' '}
+    </Suspense>
+  );
 }
 
 Route.propTypes = {
