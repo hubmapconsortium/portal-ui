04fa260e1994401f85fc6c1b10f86faef2ebf55f
John Conroy <john-conroy@users.noreply.github.com>
2020-11-09 09:44:46 -0500

    Add button to display only qa qc files

--- context/app/static/js/components/files/FileBrowser/FileBrowser.jsx
+++ context/app/static/js/components/files/FileBrowser/FileBrowser.jsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
+import Chip from '@material-ui/core/Chip';
+import DoneIcon from '@material-ui/icons/Done';
 
 import { relativeFilePathsToTree } from './utils';
 import FileBrowserNode from '../FileBrowserNode';
@@ -8,16 +10,28 @@ import { ScrollPaper } from './style';
 function FileBrowser(props) {
   const { files } = props;
   const [fileTree, setFileTree] = useState({});
+  const [qaFileTree, setQaFileTree] = useState({});
+  const [displayOnlyQaFiles, setDisplayOnlyQaFiles] = useState(false);
 
   useEffect(() => {
-    const treePath = relativeFilePathsToTree(files);
-    setFileTree(treePath);
+    setFileTree(relativeFilePathsToTree(files));
+    setQaFileTree(relativeFilePathsToTree(files.filter((file) => file?.is_qa_qc)));
   }, [files]);
 
   return (
-    <ScrollPaper data-testid="file-browser">
-      <FileBrowserNode fileSubTree={fileTree} depth={0} />
-    </ScrollPaper>
+    <>
+      <Chip
+        label="Show QA Files Only"
+        clickable
+        color="primary"
+        onClick={() => setDisplayOnlyQaFiles((prevState) => !prevState)}
+        icon={<DoneIcon />}
+        component="button"
+      />
+      <ScrollPaper data-testid="file-browser">
+        <FileBrowserNode fileSubTree={displayOnlyQaFiles ? qaFileTree : fileTree} depth={0} />
+      </ScrollPaper>
+    </>
   );
 }
 
