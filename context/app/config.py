141d1a34c2d46cad55fac9d2194743bbb4cbd26d
John Conroy <john-conroy@users.noreply.github.com>
2020-11-17 16:25:42 -0500

    Update config to include collection

--- context/app/config.py
+++ context/app/config.py
@@ -1,5 +1,6 @@
 types = {
     'donor': {'display_name': 'Donors', 'in_header': True},
     'sample': {'display_name': 'Samples', 'in_header': True},
-    'dataset': {'display_name': 'Datasets', 'in_header': True}
+    'dataset': {'display_name': 'Datasets', 'in_header': True},
+    'collection': {'display_name': 'Collections', 'in_header': True}
 }
