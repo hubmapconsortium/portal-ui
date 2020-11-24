72b28b712e4776dd50f9949dc1906714f35470d4
John Conroy <62477388+john-conroy@users.noreply.github.com>
2020-11-24 09:16:23 -0500

    John conroy/qa files tests (#1376)
    * Update FileBrowserFile tests
    
    * Update FileBrowser tests
    
    * Refactor FileBrowser test
    
    * Apply suggestions from code review
    
    Co-authored-by: Chuck McCallum <mccalluc@users.noreply.github.com>
    
    * Add FileBrowserFile test per code review suggestion
    
    Co-authored-by: John Conroy <john-conroy@users.noreply.github.com>
    Co-authored-by: Chuck McCallum <mccalluc@users.noreply.github.com>

--- context/app/static/js/components/files/FileBrowser/FileBrowser.spec.js
+++ context/app/static/js/components/files/FileBrowser/FileBrowser.spec.js
@@ -21,6 +21,9 @@ const FilesProviders = ({ children }) => {
   );
 };
 
+const expectArrayOfStringsToExist = (arr) => arr.forEach((text) => expect(screen.getByText(text)).toBeInTheDocument());
+const expectArrayOfStringsToNotExist = (arr) => arr.forEach((text) => expect(screen.queryByText(text)).toBeNull());
+
 test('displays files and directories', () => {
   const sharedEntries = {
     edam_term: 'faketerm',
@@ -59,13 +62,75 @@ test('displays files and directories', () => {
   );
 
   const textInDocumentBeforeOpenDirectory = ['path1', 'path3', 'fake5.txt'];
-  textInDocumentBeforeOpenDirectory.forEach((text) => expect(screen.getByText(text)).toBeInTheDocument());
+  expectArrayOfStringsToExist(textInDocumentBeforeOpenDirectory);
 
   const textNotInDocumentBeforeOpenDirectory = ['path2', 'fake1.txt', 'fake2.txt', 'fake3.txt', 'fake4.txt'];
-  textNotInDocumentBeforeOpenDirectory.forEach((text) => expect(screen.queryByText(text)).toBeNull());
+  expectArrayOfStringsToNotExist(textNotInDocumentBeforeOpenDirectory);
 
   userEvent.click(screen.getByRole('button', { name: 'path1' }));
 
   const textInDocumentAfterOpenDirectory = [...textInDocumentBeforeOpenDirectory, 'fake3.txt', 'path2'];
-  textInDocumentAfterOpenDirectory.forEach((text) => expect(screen.getByText(text)).toBeInTheDocument());
+  expectArrayOfStringsToExist(textInDocumentAfterOpenDirectory);
+});
+
+test('Displays correct files before and after display only QA files chip is clicked', () => {
+  const sharedEntries = {
+    edam_term: 'faketerm',
+    description: 'fakedescription',
+    size: 1000,
+    type: 'faketype',
+  };
+
+  const testFiles = [
+    {
+      rel_path: 'path1/path2/fake1.txt',
+      ...sharedEntries,
+      is_qa_qc: true,
+    },
+    {
+      rel_path: 'path1/path2/fake2.txt',
+      ...sharedEntries,
+    },
+    {
+      rel_path: 'path1/fake3.txt',
+      ...sharedEntries,
+      is_qa_qc: true,
+    },
+    {
+      rel_path: 'path3/fake4.txt',
+      ...sharedEntries,
+    },
+    {
+      rel_path: 'fake5.txt',
+      ...sharedEntries,
+    },
+  ];
+
+  render(
+    <FilesProviders>
+      <FileBrowser files={testFiles} />
+    </FilesProviders>,
+  );
+
+  // intial
+  const textInDocumentBeforeQAFilesOnly = ['path1', 'path3', 'fake5.txt'];
+  expectArrayOfStringsToExist(textInDocumentBeforeQAFilesOnly);
+
+  const textNotInDocumentBeforeQAFilesOnly = ['path2', 'fake1.txt', 'fake2.txt', 'fake3.txt', 'fake4.txt'];
+  expectArrayOfStringsToNotExist(textNotInDocumentBeforeQAFilesOnly);
+
+  userEvent.click(screen.getByRole('button', { name: 'Show QA Files Only' }));
+
+  // after QA files chip clicked
+  const textInDocumentAfterQAFilesOnly = ['path1', 'path2', 'fake1.txt', 'fake3.txt'];
+  expectArrayOfStringsToExist(textInDocumentAfterQAFilesOnly);
+
+  const textNotInDocumentAfterQAFilesOnly = ['path3', 'fake4.txt', 'fake2.txt', 'fake5.txt'];
+  expectArrayOfStringsToNotExist(textNotInDocumentAfterQAFilesOnly);
+
+  userEvent.click(screen.getByRole('button', { name: 'Show QA Files Only' }));
+
+  // returns to all dirs closed
+  expectArrayOfStringsToExist(textInDocumentBeforeQAFilesOnly);
+  expectArrayOfStringsToNotExist(textNotInDocumentBeforeQAFilesOnly);
 });
