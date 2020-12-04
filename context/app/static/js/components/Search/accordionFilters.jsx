b940650c1339e8282e3551498257bcbad4174f8c
John Conroy <john-conroy@users.noreply.github.com>
2020-12-04 14:50:34 -0500

    Add ids to inner accordion filters in search sidebar

--- context/app/static/js/components/Search/accordionFilters.jsx
+++ context/app/static/js/components/Search/accordionFilters.jsx
@@ -25,7 +25,7 @@ function Details(props) {
           {title}
         </Typography>
       </InnerAccordionSummary>
-      <InnerAccordionDetails>{children}</InnerAccordionDetails>
+      <InnerAccordionDetails id={title.replace(/ /g, '-')}>{children}</InnerAccordionDetails>
     </InnerAccordion>
   );
 }
