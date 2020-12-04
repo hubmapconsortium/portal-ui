7c0d995aeed7314678b4a2502a6526c60c48426b
John Conroy <john-conroy@users.noreply.github.com>
2020-12-04 14:52:06 -0500

    Handle close prompt and run tutorial in DatasetSearchPrompt

--- context/app/static/js/components/tutorials/DatasetSearchPrompt/DatasetSearchPrompt.jsx
+++ context/app/static/js/components/tutorials/DatasetSearchPrompt/DatasetSearchPrompt.jsx
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
 import CloseRoundedIcon from '@material-ui/icons/CloseRounded';
 
 import { CenteredDiv, Flex, StyledTypography, StyledPaper, StyledInfoIcon, StyledButton } from './style';
 
-function DatasetSearchPrompt() {
-  return (
+function DatasetSearchPrompt({ setRunTutorial }) {
+  const [isDisplayed, setIsDisplayed] = useState(true);
+
+  return isDisplayed ? (
     <StyledPaper>
       <CenteredDiv>
         <Flex>
@@ -18,17 +20,17 @@ function DatasetSearchPrompt() {
         <StyledTypography>
           Welcome to the HuBMAP Data Portal. Get a quick tour of different sections of the dataset search page.
         </StyledTypography>
-        <StyledButton color="primary" variant="contained">
+        <StyledButton color="primary" variant="contained" onClick={() => setRunTutorial(true)}>
           Take The Dataset Search Tutorial
         </StyledButton>
       </CenteredDiv>
       <div>
-        <IconButton aria-label="close">
+        <IconButton aria-label="close" onClick={() => setIsDisplayed(false)}>
           <CloseRoundedIcon />
         </IconButton>
       </div>
     </StyledPaper>
-  );
+  ) : null;
 }
 
 export default DatasetSearchPrompt;
