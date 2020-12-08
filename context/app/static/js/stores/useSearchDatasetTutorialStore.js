31fe1766a7b5e60efc02242859869ef7cc55c071
John Conroy <john-conroy@users.noreply.github.com>
2020-12-08 10:40:49 -0500

    Fix setter in useSearchDatasetTutorial store

--- context/app/static/js/stores/useSearchDatasetTutorialStore.js
+++ context/app/static/js/stores/useSearchDatasetTutorialStore.js
@@ -2,7 +2,7 @@ import create from 'zustand';
 
 const useSearchDatasetTutorialStore = create((set) => ({
   runSearchDatasetTutorial: false,
-  setRunSearchDatasetTutorial: (val) => set({ searchView: val }),
+  setRunSearchDatasetTutorial: (val) => set({ runSearchDatasetTutorial: val }),
 }));
 
 export default useSearchDatasetTutorialStore;
