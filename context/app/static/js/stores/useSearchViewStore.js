83602c37e40c028cb5cd3768f02aa673a5ed87f4
John Conroy <john-conroy@users.noreply.github.com>
2020-12-08 16:38:05 -0500

    Add comment to useSearchViewStore

--- context/app/static/js/stores/useSearchViewStore.js
+++ context/app/static/js/stores/useSearchViewStore.js
@@ -3,6 +3,7 @@ import create from 'zustand';
 const useSearchViewStore = create((set) => ({
   searchView: new URLSearchParams(window.location.search).get('view') || 'table',
   setSearchView: (val) => set({ searchView: val }),
+  // toggleItem is provided by searchkit to toggle search view
   toggleItem: null,
   setToggleItem: (val) => set({ toggleItem: val }),
 }));
