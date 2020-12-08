1a4c7e6aad83a703a1bbedbd0e1aba1dccc25645
John Conroy <john-conroy@users.noreply.github.com>
2020-12-08 11:55:13 -0500

    Only display view more step in tutorial if element exists

--- context/app/static/js/components/Search/SearchDatasetTutorial/SearchDatasetTutorial.jsx
+++ context/app/static/js/components/Search/SearchDatasetTutorial/SearchDatasetTutorial.jsx
@@ -1,10 +1,44 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import Joyride, { STATUS } from 'react-joyride';
 import TutorialTooltip from 'js/components/tutorials/TutorialTooltip';
 import { ThemeContext } from 'styled-components';
 
+const viewMoreSelector = '#Data-Type > div.sk-refinement-list__view-more-action';
+
+const defaultSteps = [
+  {
+    target: '#Data-Type > div.sk-item-list > div:nth-child(1)',
+    disableBeacon: true,
+    content:
+      'The Dataset Metadata menu on the left side allows filtering datasets by any combination of metadata categories: Data Type, Organ and Specimen Type. Search results update automatically as you edit the selection of filters. ',
+    title: 'Filter Your Browsing',
+  },
+  {
+    target: 'div.sk-search-box',
+    content:
+      'To further narrow the relevant datasets, type search terms or phrases in “quotes" into the Search bar. Multiple “search terms” should be separated by OR.',
+    disableBeacon: true,
+    title: 'Search Datasets by Free Text',
+  },
+  {
+    target: 'div.sk-layout__results.sk-results-list > table > thead > tr > th:nth-child(3)',
+    content:
+      'Press the arrow button by the relevant column to sort search results. A bolded arrow indicates the current sorting selection. Selecting a bolded arrow will reverse the sorting order.   ',
+    disableBeacon: true,
+    title: 'Sort Search Results',
+  },
+];
+
+const stepToAddIfViewMoreExists = {
+  target: '#Data-Type > div.sk-refinement-list__view-more-action',
+  content: 'Click the View All button to display the entire list of filters in the selected category.',
+  disableBeacon: true,
+  title: 'View More Filters',
+};
+
 function SearchDatasetTutorial({ runTutorial, setRunTutorial, stepIndex }) {
   const themeContext = useContext(ThemeContext);
+  const [steps, setSteps] = useState(defaultSteps);
 
   const handleJoyrideCallback = (data) => {
     const { status, action } = data;
@@ -15,35 +49,16 @@ function SearchDatasetTutorial({ runTutorial, setRunTutorial, stepIndex }) {
     }
   };
 
-  const steps = [
-    {
-      target: '#Data-Type > div.sk-item-list > div:nth-child(1)',
-      disableBeacon: true,
-      content:
-        'The Dataset Metadata menu on the left side allows filtering datasets by any combination of metadata categories: Data Type, Organ and Specimen Type. Search results update automatically as you edit the selection of filters. ',
-      title: 'Filter Your Browsing',
-    },
-    {
-      target: '#Data-Type > div.sk-refinement-list__view-more-action',
-      content: 'Click the View All button to display the entire list of filters in the selected category.',
-      disableBeacon: true,
-      title: 'View More Filters',
-    },
-    {
-      target: 'div.sk-search-box',
-      content:
-        'To further narrow the relevant datasets, type search terms or phrases in “quotes" into the Search bar. Multiple “search terms” should be separated by OR.',
-      disableBeacon: true,
-      title: 'Search Datasets by Free Text',
-    },
-    {
-      target: 'div.sk-layout__results.sk-results-list > table > thead > tr > th:nth-child(3)',
-      content:
-        'Press the arrow button by the relevant column to sort search results. A bolded arrow indicates the current sorting selection. Selecting a bolded arrow will reverse the sorting order.   ',
-      disableBeacon: true,
-      title: 'Sort Search Results',
-    },
-  ];
+  React.useEffect(() => {
+    if (runTutorial) {
+      const element = document.querySelector(viewMoreSelector);
+      if (element) {
+        const defaultStepsCopy = [...defaultSteps];
+        defaultStepsCopy.splice(1, 0, stepToAddIfViewMoreExists);
+        setSteps(defaultStepsCopy);
+      }
+    }
+  }, [runTutorial]);
 
   return (
     <Joyride
