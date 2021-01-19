e3e564102a0e24b1d59985055ddb0d72897452cb
John Conroy <john-conroy@users.noreply.github.com>
2021-01-19 14:28:01 -0500

    Create basic saved lists page

---
+++ context/app/static/js/pages/SavedLists/SavedLists.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
+
+function SavedLists() {
+  return (
+    <>
+      <Typography variant="h1">My Lists</Typography>
+      <Typography variant="h2">My Saves Lists</Typography>
+      <Typography variant="h2">All Created Lists</Typography>
+    </>
+  );
+}
+
+export default SavedLists;
