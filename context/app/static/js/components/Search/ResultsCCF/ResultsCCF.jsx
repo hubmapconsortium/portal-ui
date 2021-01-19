f390767107891fbe84726df3fc42e1f3e1b245ba
mccalluc <mccalluc@users.noreply.github.com>
2021-01-19 17:44:00 -0500

    Add proptypes

--- context/app/static/js/components/Search/ResultsCCF/ResultsCCF.jsx
+++ context/app/static/js/components/Search/ResultsCCF/ResultsCCF.jsx
@@ -1,9 +1,31 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 function ResultsCCF(props) {
   // eslint-disable-next-line no-unused-vars
   const { hits, resultFields, detailsUrlPrefix, idField, sortOptions } = props;
   return <div>TODO</div>;
 }
+
+ResultsCCF.propTypes = {
+  sortOptions: PropTypes.arrayOf(PropTypes.object).isRequired,
+  resultFields: PropTypes.arrayOf(PropTypes.object).isRequired,
+  detailsUrlPrefix: PropTypes.string.isRequired,
+  idField: PropTypes.string.isRequired,
+  hits: PropTypes.arrayOf(
+    PropTypes.shape({
+      sort: PropTypes.array,
+      _id: PropTypes.string,
+      _index: PropTypes.string,
+      _score: PropTypes.number,
+      _source: PropTypes.object,
+      _type: PropTypes.string,
+    }),
+  ),
+};
+
+ResultsCCF.defaultProps = {
+  hits: undefined,
+};
 
 export default ResultsCCF;
