aa16fa40315860c9805fd3616c3b0373aa1d52b6
John Conroy <john-conroy@users.noreply.github.com>
2021-01-19 14:25:36 -0500

    Handle opening alert in save button

--- context/app/static/js/components/Detail/SaveButton/SaveButton.jsx
+++ context/app/static/js/components/Detail/SaveButton/SaveButton.jsx
@@ -1,16 +1,23 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
 import useSavedEntitiesStore from 'js/stores/useSavedEntitiesStore';
+import useEntityStore from 'js/stores/useEntityStore';
 
 const useSavedEntitiesStoreSelector = (state) => ({ savedEntities: state.savedEntities, saveEntity: state.saveEntity });
+const entityStoreSelector = (state) => state.toggleShouldDisplaySavedListsAlert;
 
 function SaveButton({ uuid }) {
   const { savedEntities, saveEntity } = useSavedEntitiesStore(useSavedEntitiesStoreSelector);
+  const toggleShouldDisplaySavedListsAlert = useEntityStore(entityStoreSelector);
+
   return (
     <Button
       color="primary"
       variant="contained"
-      onClick={() => saveEntity(uuid)}
+      onClick={() => {
+        saveEntity(uuid);
+        toggleShouldDisplaySavedListsAlert();
+      }}
       disabled={savedEntities.includes(uuid)}
     >
       Save
