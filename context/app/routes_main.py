5a6070a05ae2632e23eb1cf7a3cd9b24f042cdba
John Conroy <john-conroy@users.noreply.github.com>
2021-01-19 16:45:44 -0500

    Add my lists to python routes

--- context/app/routes_main.py
+++ context/app/routes_main.py
@@ -182,6 +182,16 @@ def collections():
     )
 
 
+@blueprint.route('/my-lists')
+def my_lists():
+    flask_data = {'endpoints': _get_endpoints()}
+    return render_template(
+        'pages/base_react.html',
+        title='My Lists',
+        flask_data=flask_data
+    )
+
+
 @blueprint.route('/robots.txt')
 def robots_txt():
     allowed_hostname = 'portal.hubmapconsortium.org'
