15d988c163c0255fb39a0970357a60e9b244df90
mccalluc <mccalluc@users.noreply.github.com>
2021-01-19 09:48:22 -0500

    css tweak from John

--- context/app/static/js/pages/Collection/style.js
+++ context/app/static/js/pages/Collection/style.js
@@ -2,7 +2,8 @@ import styled from 'styled-components';
 import OpenInNewRoundedIcon from '@material-ui/icons/OpenInNewRounded';
 
 const StyledOpenInNewRoundedIcon = styled(OpenInNewRoundedIcon)`
-  font-size: 1em;
+  font-size: 1.1rem;
+  vertical-align: text-bottom;
 `;
 
 export { StyledOpenInNewRoundedIcon };
