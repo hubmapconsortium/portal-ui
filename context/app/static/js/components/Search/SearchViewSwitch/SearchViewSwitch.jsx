21d82dad5de095f37cb42876b89e86de008b8b7a
mccalluc <mccalluc@users.noreply.github.com>
2021-01-19 14:20:37 -0500

    Move into factory function

--- context/app/static/js/components/Search/SearchViewSwitch/SearchViewSwitch.jsx
+++ context/app/static/js/components/Search/SearchViewSwitch/SearchViewSwitch.jsx
@@ -15,43 +15,48 @@ const searchViewStoreSelector = (state) => ({
   setToggleItem: state.setToggleItem,
 });
 
-function SearchViewSwitch(props) {
-  const { toggleItem } = props;
-  const { searchView, setSearchView, setToggleItem } = useSearchViewStore(searchViewStoreSelector);
+function createSearchViewSwitch(labelIconPairs) {
+  return function SearchViewSwitch(props) {
+    const { toggleItem } = props;
+    const { searchView, setSearchView, setToggleItem } = useSearchViewStore(searchViewStoreSelector);
 
-  useEffect(() => {
-    setToggleItem(toggleItem);
-  }, [setToggleItem, toggleItem]);
+    useEffect(() => {
+      setToggleItem(toggleItem);
+    }, [setToggleItem, toggleItem]);
 
-  function handleChangeView(view) {
-    if (!['table', 'tile', 'ccf'].includes(view)) {
-      return;
+    function handleChangeView(view) {
+      const validViews = labelIconPairs.map(({ label }) => label.toLowerCase());
+      if (!validViews.includes(view)) {
+        return;
+      }
+      setSearchView(view);
+      toggleItem(view);
     }
-    setSearchView(view);
-    toggleItem(view);
-  }
 
-  return (
-    <ToggleButtonGroup value={searchView} exclusive onChange={(e, view) => handleChangeView(view)}>
-      {[
-        { label: 'Table', Icon: ListRoundedIcon },
-        { label: 'Tile', Icon: GridOnRoundedIcon },
-        { label: 'CCF', Icon: BodyRoundedIcon },
-      ].map(({ label, Icon }) => (
-        <TooltipToggleButton
-          tooltipComponent={SecondaryBackgroundTooltip}
-          tooltipTitle={`Switch to ${label} View`}
-          disableRipple
-          value={label.toLowerCase()}
-          id={`${label.toLowerCase()}-view-toggle-button`}
-          key={label}
-        >
-          <Icon color={searchView === label.toLowerCase() ? 'primary' : 'secondary'} />
-        </TooltipToggleButton>
-      ))}
-    </ToggleButtonGroup>
-  );
+    return (
+      <ToggleButtonGroup value={searchView} exclusive onChange={(e, view) => handleChangeView(view)}>
+        {labelIconPairs.map(({ label, Icon }) => (
+          <TooltipToggleButton
+            tooltipComponent={SecondaryBackgroundTooltip}
+            tooltipTitle={`Switch to ${label} View`}
+            disableRipple
+            value={label.toLowerCase()}
+            id={`${label.toLowerCase()}-view-toggle-button`}
+            key={label}
+          >
+            <Icon color={searchView === label.toLowerCase() ? 'primary' : 'secondary'} />
+          </TooltipToggleButton>
+        ))}
+      </ToggleButtonGroup>
+    );
+  };
 }
+
+const SearchViewSwitch = createSearchViewSwitch([
+  { label: 'Table', Icon: ListRoundedIcon },
+  { label: 'Tile', Icon: GridOnRoundedIcon },
+  { label: 'CCF', Icon: BodyRoundedIcon },
+]);
 
 SearchViewSwitch.propTypes = {
   toggleItem: PropTypes.func.isRequired,
