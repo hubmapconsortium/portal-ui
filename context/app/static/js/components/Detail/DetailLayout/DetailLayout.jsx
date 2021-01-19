481b57e06234d551860055501298ad93f46db3cc
John Conroy <john-conroy@users.noreply.github.com>
2021-01-19 14:25:15 -0500

    Add alert to detail layout

--- context/app/static/js/components/Detail/DetailLayout/DetailLayout.jsx
+++ context/app/static/js/components/Detail/DetailLayout/DetailLayout.jsx
@@ -1,21 +1,37 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import useEntityStore from 'js/stores/useEntityStore';
 import TableOfContents from '../TableOfContents';
-import { Content, FlexRow } from './style';
+import { Content, FlexRow, StyledAlert } from './style';
 import { getSections } from './utils';
+
+const entityStoreSelector = (state) => ({
+  shouldDisplaySavedListsAlert: state.shouldDisplaySavedListsAlert,
+  toggleShouldDisplaySavedListsAlert: state.toggleShouldDisplaySavedListsAlert,
+});
 
 function DetailLayout(props) {
   const { sectionOrder, children } = props;
+
+  const { shouldDisplaySavedListsAlert, toggleShouldDisplaySavedListsAlert } = useEntityStore(entityStoreSelector);
 
   // section hash must match section id in each component
   const sections = new Map(getSections(sectionOrder));
 
   return (
-    <FlexRow>
-      <TableOfContents items={[...sections.values()]} />
-      <Content>{children}</Content>
-    </FlexRow>
+    <>
+      {shouldDisplaySavedListsAlert && (
+        <StyledAlert severity="success" onClose={() => toggleShouldDisplaySavedListsAlert()}>
+          Successfully added to My Saves List. All data are currently stored on local storage and are not transferable
+          between devices.
+        </StyledAlert>
+      )}
+      <FlexRow>
+        <TableOfContents items={[...sections.values()]} />
+        <Content>{children}</Content>
+      </FlexRow>
+    </>
   );
 }
 
