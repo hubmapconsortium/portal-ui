1d07b12ca67bcd5b57dbd39d8e110eea8c2795a6
John Conroy <john-conroy@users.noreply.github.com>
2021-01-21 10:42:21 -0500

    Add createList fn to lists store

--- context/app/static/js/stores/useSavedListsStore.js
+++ context/app/static/js/stores/useSavedListsStore.js
@@ -1,11 +1,19 @@
 import create from 'zustand';
 import { persist } from 'zustand/middleware';
+import produce from 'immer';
 
-const useSavedListsStore = create(
+export const immer = (config) => (set, get) => config((fn) => set(produce(fn)), get);
+
+const [useSavedListsStore] = create(
   persist(
-    () => ({
+    immer((set) => ({
       savedLists: {},
-    }),
+      createList: ({ title, description }) =>
+        set((state) => {
+          // eslint-disable-next-line no-param-reassign
+          state.savedLists[title] = { donors: [], samples: [], datasets: [], description };
+        }),
+    })),
     {
       name: 'saved_lists',
     },
