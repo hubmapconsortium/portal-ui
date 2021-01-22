e832f0dcc7ed517343cbb7616628e8c7a507e28a
John Conroy <john-conroy@users.noreply.github.com>
2021-01-22 13:04:14 -0500

    Use DialogModal in CreateListDialog

--- context/app/static/js/components/savedLists/CreateListDialog/CreateListDialog.jsx
+++ context/app/static/js/components/savedLists/CreateListDialog/CreateListDialog.jsx
@@ -1,12 +1,9 @@
 import React, { useState } from 'react';
 import Button from '@material-ui/core/Button';
-import Dialog from '@material-ui/core/Dialog';
-import DialogActions from '@material-ui/core/DialogActions';
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogTitle from '@material-ui/core/DialogTitle';
 
 import useSavedListsStore from 'js/stores/useSavedListsStore';
-import { StyledPrimaryOutlineTextField, StyledDivider } from './style';
+import DialogModal from 'js/shared-styles/DialogModal';
+import { StyledPrimaryOutlineTextField } from './style';
 
 const maxTitleLength = 50;
 const useSavedListsStoreSelector = (state) => state.createList;
@@ -30,46 +27,51 @@ function CreateListDialog({ dialogIsOpen, setDialogIsOpen }) {
   };
 
   return (
-    <Dialog open={dialogIsOpen} onClose={handleClose} aria-labelledby="form-dialog-title" fullWidth>
-      <DialogTitle id="create-list-form-dialog-title">Create New List</DialogTitle>
-      <DialogContent>
-        <StyledPrimaryOutlineTextField
-          autoFocus
-          margin="dense"
-          id="title"
-          label="Title"
-          fullWidth
-          variant="outlined"
-          placeholder='Like “Spleen-Related Data” or “ATAC-seq Visualizations"'
-          inputProps={{ maxLength: maxTitleLength }}
-          onChange={handleTitleChange}
-          required
-          helperText={`${title.length}/${maxTitleLength} Characters`}
-          value={title}
-        />
-        <StyledPrimaryOutlineTextField
-          id="description"
-          label="Description (optional)"
-          fullWidth
-          variant="outlined"
-          placeholder="Input description of list"
-          multiline
-          rows={5}
-          inputProps={{ maxLength: 1000 }}
-          onChange={handleDescriptionChange}
-          value={description}
-        />
-      </DialogContent>
-      <StyledDivider />
-      <DialogActions>
-        <Button onClick={handleClose} color="primary">
-          Cancel
-        </Button>
-        <Button onClick={() => createList({ title, description })} color="primary" disabled={title.length === 0}>
-          Save
-        </Button>
-      </DialogActions>
-    </Dialog>
+    <DialogModal
+      title="Create New List"
+      content={
+        <>
+          <StyledPrimaryOutlineTextField
+            autoFocus
+            margin="dense"
+            id="title"
+            label="Title"
+            fullWidth
+            variant="outlined"
+            placeholder='Like “Spleen-Related Data” or “ATAC-seq Visualizations"'
+            inputProps={{ maxLength: maxTitleLength }}
+            onChange={handleTitleChange}
+            required
+            helperText={`${title.length}/${maxTitleLength} Characters`}
+            value={title}
+          />
+          <StyledPrimaryOutlineTextField
+            id="description"
+            label="Description (optional)"
+            fullWidth
+            variant="outlined"
+            placeholder="Input description of list"
+            multiline
+            rows={5}
+            inputProps={{ maxLength: 1000 }}
+            onChange={handleDescriptionChange}
+            value={description}
+          />
+        </>
+      }
+      actions={
+        <>
+          <Button onClick={handleClose} color="primary">
+            Cancel
+          </Button>
+          <Button onClick={() => createList({ title, description })} color="primary" disabled={title.length === 0}>
+            Save
+          </Button>
+        </>
+      }
+      isOpen={dialogIsOpen}
+      handleClose={handleClose}
+    />
   );
 }
 
