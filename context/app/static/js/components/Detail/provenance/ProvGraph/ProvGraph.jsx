0d2635d6bbab554ea8f1415efdd45b8519976a3c
mccalluc <mccalluc@users.noreply.github.com>
2021-01-26 09:14:07 -0500

    Type is not a 1-1 mapping

--- context/app/static/js/components/Detail/provenance/ProvGraph/ProvGraph.jsx
+++ context/app/static/js/components/Detail/provenance/ProvGraph/ProvGraph.jsx
@@ -3,50 +3,51 @@ import PropTypes from 'prop-types';
 
 import SectionItem from 'js/components/Detail/SectionItem';
 import { renderProvVis } from '../ProvVis';
-import { StyledTypography, StyledLink, FlexPaper } from './style';
+import { StyledTypography, FlexPaper } from './style';
 import '@hms-dbmi-bgm/react-workflow-viz/dist/react-workflow-viz.min.css';
 
 function ProvGraph(props) {
   const { provData } = props;
   const isOld = 'ex' in provData.prefix;
   const idKey = isOld ? 'hubmap:displayDOI' : 'hubmap:hubmap_id';
+  const timeKey = isOld ? 'prov:generatedAtTime' : 'hubmap:created_timestamp';
+  const typeKey = isOld ? 'prov:type' : 'hubmap:entity_type';
   const runRenderProvVis = () =>
     renderProvVis('prov-vis-react', provData, {
       getNameForActivity: (id, prov) => {
         const activity = prov.activity[id];
-        return `${activity['prov:type']} - ${activity[idKey]}`;
+        return `${activity['hubmap:creation_action']} - ${activity[idKey]}`;
       },
       getNameForEntity: (id, prov) => {
         const entity = prov.entity[id];
         // NOTE: The initial entity node was not included in the sample data;
         // Fallback to ID, if needed. https://github.com/hubmapconsortium/prov-vis/issues/15
-        return entity ? `${entity['prov:type']} - ${entity[idKey]}` : id;
+        return entity ? `${entity[typeKey]} - ${entity[idKey]}` : id;
       },
       renderDetailPane: (prov) => {
         const typeEl =
-          'prov:type' in prov ? (
+          typeKey in prov ? (
             <SectionItem label="Type">
-              <StyledTypography variant="body1">{prov['prov:type']}</StyledTypography>
+              <StyledTypography variant="body1">{prov[typeKey]}</StyledTypography>
             </SectionItem>
           ) : (
             <SectionItem label="Type">
-              <StyledTypography variant="body1">Root</StyledTypography>
+              <StyledTypography variant="body1">{prov['prov:type']}</StyledTypography>
             </SectionItem>
           );
         const idEl =
-          'prov:type' in prov && ['Donor', 'Sample', 'Dataset'].includes(prov['prov:type']) ? (
+          typeKey in prov && ['Donor', 'Sample', 'Dataset'].includes(prov[typeKey]) ? (
             <SectionItem label="ID" ml>
               <StyledTypography variant="body1">
-                <StyledLink href={`/browse/${prov['prov:type'].toLowerCase()}/${prov['hubmap:uuid']}`}>
-                  {prov[idKey]}
-                </StyledLink>
+                {/* TODO: Using StyledLink causes "TypeError: props.theme.palette is undefined" */}
+                <a href={`/browse/${prov[typeKey].toLowerCase()}/${prov['hubmap:uuid']}`}>{prov[idKey]}</a>
               </StyledTypography>
             </SectionItem>
           ) : null;
         const createdEl =
-          'prov:generatedAtTime' in prov ? (
+          timeKey in prov ? (
             <SectionItem label="Created" ml>
-              <StyledTypography variant="body1">{prov['prov:generatedAtTime']}</StyledTypography>
+              <StyledTypography variant="body1">{prov[timeKey]}</StyledTypography>
             </SectionItem>
           ) : null;
         return (
