9c8e1b1169a6d054cb052ff9aae562e024d27aaf
mccalluc <mccalluc@users.noreply.github.com>
2021-01-26 08:45:15 -0500

    Works with old format, too

--- context/app/static/js/components/Detail/provenance/ProvGraph/ProvGraph.jsx
+++ context/app/static/js/components/Detail/provenance/ProvGraph/ProvGraph.jsx
@@ -8,17 +8,19 @@ import '@hms-dbmi-bgm/react-workflow-viz/dist/react-workflow-viz.min.css';
 
 function ProvGraph(props) {
   const { provData } = props;
+  const isOld = 'ex' in provData.prefix;
+  const idKey = isOld ? 'hubmap:displayDOI' : 'hubmap:hubmap_id';
   const runRenderProvVis = () =>
     renderProvVis('prov-vis-react', provData, {
       getNameForActivity: (id, prov) => {
         const activity = prov.activity[id];
-        return `${activity['prov:type']} - ${activity['hubmap:hubmap_id']}`;
+        return `${activity['prov:type']} - ${activity[idKey]}`;
       },
       getNameForEntity: (id, prov) => {
         const entity = prov.entity[id];
         // NOTE: The initial entity node was not included in the sample data;
         // Fallback to ID, if needed. https://github.com/hubmapconsortium/prov-vis/issues/15
-        return entity ? `${entity['prov:type']} - ${entity['hubmap:hubmap_id']}` : id;
+        return entity ? `${entity['prov:type']} - ${entity[idKey]}` : id;
       },
       renderDetailPane: (prov) => {
         const typeEl =
@@ -36,7 +38,7 @@ function ProvGraph(props) {
             <SectionItem label="ID" ml>
               <StyledTypography variant="body1">
                 <StyledLink href={`/browse/${prov['prov:type'].toLowerCase()}/${prov['hubmap:uuid']}`}>
-                  {prov['hubmap:hubmap_id']}
+                  {prov[idKey]}
                 </StyledLink>
               </StyledTypography>
             </SectionItem>
