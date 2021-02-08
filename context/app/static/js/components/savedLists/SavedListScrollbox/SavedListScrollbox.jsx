a202ec2f5628c194942f79d3ab0bca6790f80394
John Conroy <62477388+john-conroy@users.noreply.github.com>
2021-02-08 15:21:35 -0500

    John conroy/my lists scrollbox max height (#1520)
    * Pull SavedListScrollbox into own component and set max height
    
    * Use new component in SavedLists
    
    * Update context/app/static/js/components/savedLists/SavedListScrollbox/SavedListScrollbox.jsx
    
    Co-authored-by: Chuck McCallum <mccalluc@users.noreply.github.com>
    
    * Increase max height  of scrollbox to 415px
    
    Co-authored-by: John Conroy <john-conroy@users.noreply.github.com>
    Co-authored-by: Chuck McCallum <mccalluc@users.noreply.github.com>

---
+++ context/app/static/js/components/savedLists/SavedListScrollbox/SavedListScrollbox.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+import Description from 'js/shared-styles/sections/Description';
+import CreateListDialog from 'js/components/savedLists/CreateListDialog';
+import SavedListPanel from 'js/components/savedLists/SavedListPanel';
+import { SeparatedFlexRow, FlexBottom, MaxHeightScrollbox } from './style';
+
+function SavedListScrollbox({ savedLists }) {
+  const [dialogIsOpen, setDialogIsOpen] = useState(false);
+
+  return (
+    <>
+      <SeparatedFlexRow>
+        <div>
+          <Typography variant="h3" component="h2">
+            All Created Lists
+          </Typography>
+          <Typography variant="subtitle1">{Object.keys(savedLists).length} Lists</Typography>
+        </div>
+        <FlexBottom>
+          <Button variant="contained" color="primary" onClick={() => setDialogIsOpen(true)}>
+            Create New List
+          </Button>
+          <CreateListDialog dialogIsOpen={dialogIsOpen} setDialogIsOpen={setDialogIsOpen} />
+        </FlexBottom>
+      </SeparatedFlexRow>
+      {Object.keys(savedLists).length === 0 ? (
+        <Description padding="20px">No lists created yet.</Description>
+      ) : (
+        <MaxHeightScrollbox>
+          {Object.entries(savedLists).map(([key, value]) => {
+            return <SavedListPanel key={key} title={key} entityObject={value} />;
+          })}
+        </MaxHeightScrollbox>
+      )}
+    </>
+  );
+}
+
+export default SavedListScrollbox;
