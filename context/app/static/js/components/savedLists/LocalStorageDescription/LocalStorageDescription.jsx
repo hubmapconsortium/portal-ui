9cc0f2e2848b7cf93cfebb75ba0c4901e7211d27
John Conroy <john-conroy@users.noreply.github.com>
2021-02-09 15:26:53 -0500

    Add spacing below local storage description

--- context/app/static/js/components/savedLists/LocalStorageDescription/LocalStorageDescription.jsx
+++ context/app/static/js/components/savedLists/LocalStorageDescription/LocalStorageDescription.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
-import Description from 'js/shared-styles/sections/Description';
+
+import { Description } from 'js/shared-styles/sections/Description';
+import { StyledDiv } from './style';
 
 function LocalStorageDescription() {
   return (
-    <Description padding="20px 20px">
-      Your lists are currently stored on local storage and are not transferable between devices.
-    </Description>
+    <StyledDiv>
+      <Description padding="20px 20px">
+        Your lists are currently stored on local storage and are not transferable between devices.
+      </Description>
+    </StyledDiv>
   );
 }
 
---
+++ context/app/static/js/components/savedLists/LocalStorageDescription/style.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+const StyledDiv = styled.div`
+  margin-bottom: ${(props) => props.theme.spacing(2)}px;
+`;
+
+export { StyledDiv };
