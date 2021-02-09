224ceadbc86eef93f6a85f92134f6a73537c4d44
John Conroy <john-conroy@users.noreply.github.com>
2021-02-09 16:16:48 -0500

    Revert spacing in local storage description

--- context/app/static/js/components/savedLists/LocalStorageDescription/LocalStorageDescription.jsx
+++ context/app/static/js/components/savedLists/LocalStorageDescription/LocalStorageDescription.jsx
@@ -1,15 +1,12 @@
 import React from 'react';
 
 import { Description } from 'js/shared-styles/sections/Description';
-import { StyledDiv } from './style';
 
 function LocalStorageDescription() {
   return (
-    <StyledDiv>
-      <Description padding="20px 20px">
-        Your lists are currently stored on local storage and are not transferable between devices.
-      </Description>
-    </StyledDiv>
+    <Description padding="20px 20px">
+      Your lists are currently stored on local storage and are not transferable between devices.
+    </Description>
   );
 }
 
--- context/app/static/js/components/savedLists/LocalStorageDescription/style.js
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components';
-
-const StyledDiv = styled.div`
-  margin-bottom: ${(props) => props.theme.spacing(2)}px;
-`;
-
-export { StyledDiv };
