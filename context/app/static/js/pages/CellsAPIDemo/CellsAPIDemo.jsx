716ae27228807548350c6818beeedcbdb7826aca
mccalluc <mccalluc@users.noreply.github.com>
2021-02-09 17:02:12 -0500

    Material UI

--- context/app/static/js/pages/CellsAPIDemo/CellsAPIDemo.jsx
+++ context/app/static/js/pages/CellsAPIDemo/CellsAPIDemo.jsx
@@ -1,8 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import Paper from '@material-ui/core/Paper';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 
 // eslint-disable-next-line no-unused-vars
 function CellsAPIDemo(props) {
-  return <p>Hello World!</p>;
+  const [inputType, setInputType] = useState('cell');
+  const [outputType, setOutputType] = useState('gene');
+
+  function handleSubmit() {
+    // eslint-disable-next-line no-alert
+    alert(`submit ${inputType} -> ${outputType}`);
+  }
+
+  function handleChange(event) {
+    const { target } = event;
+    const { name } = target;
+    const setFields = {
+      input: setInputType,
+      output: setOutputType,
+    };
+    setFields[name](event.target.value);
+  }
+
+  return (
+    <Paper>
+      <TextField label="input type" value={inputType} name="input" variant="outlined" onChange={handleChange} />
+      <TextField label="output type" value={outputType} name="output" variant="outlined" onChange={handleChange} />
+      <Button onClick={handleSubmit}>Submit</Button>
+    </Paper>
+  );
 }
 
 export default CellsAPIDemo;
