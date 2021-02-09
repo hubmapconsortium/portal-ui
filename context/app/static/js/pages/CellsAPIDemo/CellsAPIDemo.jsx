5d73acd04aaa1e1ced76e5300d26eadd6fc448d0
mccalluc <mccalluc@users.noreply.github.com>
2021-02-09 17:35:10 -0500

    More fields

--- context/app/static/js/pages/CellsAPIDemo/CellsAPIDemo.jsx
+++ context/app/static/js/pages/CellsAPIDemo/CellsAPIDemo.jsx
@@ -6,28 +6,53 @@ import Button from '@material-ui/core/Button';
 
 // eslint-disable-next-line no-unused-vars
 function CellsAPIDemo(props) {
-  const [inputType, setInputType] = useState('cell');
-  const [outputType, setOutputType] = useState('gene');
+  const [input_type, setInputType] = useState('cell');
+  const [output_type, setOutputType] = useState('gene');
+  const [genomic_modality, setGenomicModality] = useState('rna');
+  const [has, setHas] = useState('VIM > 0.5');
 
   function handleSubmit() {
+    const query = {
+      input_type,
+      output_type,
+      genomic_modality,
+      input_set: [has],
+    };
+
     // eslint-disable-next-line no-alert
-    alert(`submit ${inputType} -> ${outputType}`);
+    alert(JSON.stringify(query));
   }
 
   function handleChange(event) {
     const { target } = event;
     const { name } = target;
     const setFields = {
-      input: setInputType,
-      output: setOutputType,
+      input_type: setInputType,
+      output_type: setOutputType,
+      genomic_modality: setGenomicModality,
+      has: setHas,
     };
     setFields[name](event.target.value);
   }
 
   return (
     <Paper>
-      <TextField label="input type" value={inputType} name="input" variant="outlined" onChange={handleChange} />
-      <TextField label="output type" value={outputType} name="output" variant="outlined" onChange={handleChange} />
+      <TextField label="input type" value={input_type} name="input_type" variant="outlined" onChange={handleChange} />
+      <TextField
+        label="output type"
+        value={output_type}
+        name="output_type"
+        variant="outlined"
+        onChange={handleChange}
+      />
+      <TextField
+        label="genomic modality"
+        value={genomic_modality}
+        name="genomic_modality"
+        variant="outlined"
+        onChange={handleChange}
+      />
+      <TextField label="has" value={has} name="has" variant="outlined" onChange={handleChange} />
       <Button onClick={handleSubmit}>Submit</Button>
     </Paper>
   );
