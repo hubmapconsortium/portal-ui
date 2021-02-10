d215e7fb89cb80dbf702550ca1ae97248176184f
John Conroy <john-conroy@users.noreply.github.com>
2021-02-10 11:14:38 -0500

    Add spacing to SavedEntitiesTable

--- context/app/static/js/components/savedLists/SavedEntitiesTable/style.js
+++ context/app/static/js/components/savedLists/SavedEntitiesTable/style.js
@@ -1,4 +1,13 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+import { WhiteBackgroundIconButton } from 'js/shared-styles/buttons';
+import RightAlignedButtonRow from 'js/shared-styles/sections/RightAlignedButtonRow';
+
+const buttonSpacing = css`
+  margin-left: ${(props) => props.theme.spacing(1)}px;
+`;
 
 const Flex = styled.div`
   display: flex;
@@ -6,4 +15,20 @@ const Flex = styled.div`
   justify-content: space-between;
 `;
 
-export { Flex };
+const LeftMarginButton = styled(Button)`
+  ${buttonSpacing}
+`;
+
+const LeftMarginIconButton = styled(WhiteBackgroundIconButton)`
+  ${buttonSpacing}
+`;
+
+const StyledButtonRow = styled(RightAlignedButtonRow)`
+  margin-bottom: ${(props) => props.theme.spacing(1)}px;
+`;
+
+const BottomAlignedTypography = styled(Typography)`
+  align-self: flex-end;
+`;
+
+export { Flex, LeftMarginButton, LeftMarginIconButton, StyledButtonRow, BottomAlignedTypography };
