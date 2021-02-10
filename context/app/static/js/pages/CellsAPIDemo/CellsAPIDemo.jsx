ff2ed3c6e101bc6e4996dc8ecc5506d9f89e123b
mccalluc <mccalluc@users.noreply.github.com>
2021-02-10 09:55:04 -0500

    Handle error responses!

--- context/app/static/js/pages/CellsAPIDemo/CellsAPIDemo.jsx
+++ context/app/static/js/pages/CellsAPIDemo/CellsAPIDemo.jsx
@@ -3,24 +3,37 @@ import React, { useState } from 'react';
 import Paper from '@material-ui/core/Paper';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
 
 // eslint-disable-next-line no-unused-vars
 function CellsAPIDemo(props) {
-  const [input_type, setInputType] = useState('cell');
-  const [output_type, setOutputType] = useState('gene');
+  const [input_type, setInputType] = useState('gene');
+  const [output_type, setOutputType] = useState('cell');
   const [genomic_modality, setGenomicModality] = useState('rna');
   const [has, setHas] = useState('VIM > 0.5');
 
-  function handleSubmit() {
-    const query = {
-      input_type,
-      output_type,
-      genomic_modality,
-      input_set: [has],
-    };
+  // const [results, setResults] = useState([]);
+  const [error, setError] = useState(null);
 
-    // eslint-disable-next-line no-alert
-    alert(JSON.stringify(query));
+  async function handleSubmit() {
+    const formData = new FormData();
+    formData.append('input_type', input_type);
+    formData.append('genomic_modality', genomic_modality);
+    formData.append('input_set', has);
+    // TODO: Pull endpoint from context!
+    const response = await fetch(`https://cells.dev.hubmapconsortium.org/api/${output_type}/`, {
+      method: 'POST',
+      body: formData,
+    });
+    const responseJson = await response.json();
+    if ('message' in responseJson) {
+      setError(responseJson.message);
+    } else {
+      // TODO
+    }
   }
 
   function handleChange(event) {
@@ -53,7 +66,22 @@ function CellsAPIDemo(props) {
         onChange={handleChange}
       />
       <TextField label="has" value={has} name="has" variant="outlined" onChange={handleChange} />
+      <br />
       <Button onClick={handleSubmit}>Submit</Button>
+      <br />
+      {error}
+      <Table>
+        <TableBody>
+          <TableRow>
+            <TableCell>test</TableCell>
+            <TableCell>test</TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell>a</TableCell>
+            <TableCell>b</TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
     </Paper>
   );
 }
