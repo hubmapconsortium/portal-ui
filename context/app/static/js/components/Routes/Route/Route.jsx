0acc811f739c8c00f3382b10243b173235578df5
John Conroy <john-conroy@users.noreply.github.com>
2021-02-12 13:53:50 -0500

    Add web vitals hook to route

--- context/app/static/js/components/Routes/Route/Route.jsx
+++ context/app/static/js/components/Routes/Route/Route.jsx
@@ -2,10 +2,13 @@ import React, { Suspense } from 'react';
 import PropTypes from 'prop-types';
 
 import RouteLoader from '../RouteLoader';
+import useSendWebVitals from '../useSendWebVitals';
 import { StyledContainer, MainWrapper } from './style';
 
 function Route({ children, disableWidthConstraint }) {
   const constrainWidthProps = disableWidthConstraint ? { maxWidth: false, disableGutters: true } : { maxWidth: 'lg' };
+
+  useSendWebVitals();
 
   return (
     <Suspense fallback={<RouteLoader />}>
