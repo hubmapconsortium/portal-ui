22a61f9db528ef675c8d30e21054da4b91e4b628
ilan-gold <ilanbassgold@gmail.com>
2021-03-03 12:28:50 -0500

    Fix indent.

--- context/app/api/vitessce_confs/constants.py
+++ context/app/api/vitessce_confs/constants.py
@@ -36,25 +36,25 @@ class AssetPaths(Enum):
 
 
 class CommonsTypeClient(object, metaclass=SingletonMetaClass):
-    """
+  """
   This class builds some constant data structures based on calls to the type client.  It is a
   singleton, which means only a single instance is ever created, so the tables are built only
   once and reused as needed.
   """
 
-    def __init__(self):
-        """
+  def __init__(self):
+    """
     Initialize the type client and build the required tables, which will be accessed as
     attributes of the singleton instance.
     """
-        tc = TypeClient(current_app.config["TYPE_SERVICE_ENDPOINT"])
-        self._assays = []
-        for assay in tc.iterAssays():
-            self._assays.append(assay)
+    tc = TypeClient(current_app.config["TYPE_SERVICE_ENDPOINT"])
+    self._assays = []
+    for assay in tc.iterAssays():
+        self._assays.append(assay)
 
-    def get_assay(self, data_type):
-        """
+  def get_assay(self, data_type):
+    """
     Return the assay class for the given data type
     """
-        assay = list(filter(lambda x: x.name == data_type, self._assays))[0]
-        return assay
+    assay = list(filter(lambda x: x.name == data_type, self._assays))[0]
+    return assay
