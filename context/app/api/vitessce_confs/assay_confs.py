98f65da343c32c799972bc9f5570014a1bdedf55
ilan-gold <ilanbassgold@gmail.com>
2021-03-09 17:56:59 -0500

    Fix no conf found error.

--- context/app/api/vitessce_confs/assay_confs.py
+++ context/app/api/vitessce_confs/assay_confs.py
@@ -174,3 +174,4 @@ def get_view_config_class_for_data_types(entity, nexus_token):
         return RNASeqConf(entity=entity, nexus_token=nexus_token, is_mock=False)
     if "atac" in hints:
         return ATACSeqConf(entity=entity, nexus_token=nexus_token, is_mock=False)
+    return None
