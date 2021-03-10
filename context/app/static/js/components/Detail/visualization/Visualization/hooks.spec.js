64b0019d34fe1f8b29db20b81b9cbc3dbe049db8
Chuck McCallum <mccalluc@users.noreply.github.com>
2021-03-10 09:48:05 -0500

    Mccalluc/vitessce anchor link error (#1612)
    * No more error, but should factor out copy and paste
    
    * Factor out copy and paste
    
    * Test passes... but not actually demonstrating the behavior that matters
    
    * Mock testting.
    
    Co-authored-by: ilan-gold <ilanbassgold@gmail.com>

--- context/app/static/js/components/Detail/visualization/Visualization/hooks.spec.js
+++ context/app/static/js/components/Detail/visualization/Visualization/hooks.spec.js
@@ -4,11 +4,30 @@ import { renderHook, act } from '@testing-library/react-hooks';
 import { useVitessceConfig } from './hooks';
 
 describe('Run Vitessce Hooks Test', () => {
-  test('Vitessce Config Hook Test', () => {
-    // Mocked vitessce config returns ({ name: 'conf1', attr: 'bar' }) in __mocks__/vitessce
+  test('Vitessce Config Hook Test Without Vitessce Fragment Returns vitData', () => {
     delete window.location;
     window.location = {
       hash: `#mock_url_to_be_ignored`,
+    };
+    const vitData = [{ name: 'conf1' }, { name: 'conf2' }];
+    const props = { vitData, setVitessceState: () => {}, setVitessceErrors: () => {} };
+    const { result } = renderHook(() => useVitessceConfig(props));
+
+    expect(result.current.vitessceSelection).toEqual(0);
+    expect(result.current.vitessceConfig[0].name).toEqual('conf1');
+    expect(result.current.vitessceConfig[0].attr).toEqual(undefined);
+    act(() => {
+      result.current.setVitessceSelection(1);
+    });
+
+    expect(result.current.vitessceSelection).toEqual(1);
+  });
+
+  test('Vitessce Config Hook Test With Vitessce Fragment Return Decoded Mock', () => {
+    // Mocked vitessce config returns ({ name: 'conf1', attr: 'bar' }) in __mocks__/vitessce
+    delete window.location;
+    window.location = {
+      hash: `#vitessce_conf_=mock_is_called`,
     };
     const vitData = [{ name: 'conf1' }, { name: 'conf2' }];
     const props = { vitData, setVitessceState: () => {}, setVitessceErrors: () => {} };
