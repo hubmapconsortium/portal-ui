4b004891bd32d1e4dfa3f9575651728eca8d51ac
John Conroy <john-conroy@users.noreply.github.com>
2021-03-11 21:00:45 -0500

    Create PanelList component

---
+++ context/app/static/js/components/Collections/PanelList/PanelList.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import { Panel, PanelScrollBox } from 'js/shared-styles/panels';
+
+function PanelList({ collectionsData }) {
+  return (
+    <PanelScrollBox>
+      {collectionsData.map(({ _source }) => (
+        <Panel
+          href={`/browse/collection/${_source.uuid}`}
+          title={_source.title}
+          entityCounts={{ datasets: _source.datasets.length }}
+          secondaryText={_source.display_doi}
+        />
+      ))}
+    </PanelScrollBox>
+  );
+}
+
+export default PanelList;
