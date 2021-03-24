00aabb58a3856a2838e2536ccc809ac408281cc1
ilan-gold <ilanbassgold@gmail.com>
2021-03-24 17:23:57 -0400

    New assays

--- context/app/api/vitessce_confs/assay_confs.py
+++ context/app/api/vitessce_confs/assay_confs.py
@@ -2,10 +2,12 @@ import re
 
 from vitessce import (
     VitessceConfig,
+    AnnDataWrapper,
     MultiImageWrapper,
     OmeTiffWrapper,
     DataType as dt,
     FileType as ft,
+    Component as cm,
 )
 
 from .utils import (
@@ -137,6 +139,86 @@ class ATACSeqConf(ScatterplotViewConf):
             },
         ]
 
+class RNASeqAnnDataConf(ViewConf):
+
+    def build_vitessce_conf(self):
+        vc = VitessceConfig(name=self._uuid)
+        adata_url = self._build_assets_url('hubmap_ui/anndata-zarr/secondary_analysis.zarr', use_token=False)
+        dataset = vc.add_dataset(name=self._uuid).add_object(AnnDataWrapper(
+                adata_url=adata_url,
+                mappings_obsm=["X_umap"],
+                mappings_obsm_names=["UMAP"],
+                cell_set_obs=["leiden"],
+                cell_set_obs_names=["Leiden"],
+                expression_matrix="X",
+                matrix_gene_var_filter="marker_genes_for_heatmap",
+                factors_obs=[
+                    "marker_gene_0",
+                    "marker_gene_1",
+                    "marker_gene_2",
+                    "marker_gene_3",
+                    "marker_gene_4"
+                ],
+                request_init={
+                    "headers": {
+                        "Authorization": f"Bearer {self._nexus_token}"
+                    }
+                }
+            )
+        )
+        vc = self._setup_anndata_view_config(vc, dataset)
+        return vc.to_dict()
+
+    def _setup_anndata_view_config(self, vc, dataset):
+        vc.add_view(dataset, cm.SCATTERPLOT, mapping="UMAP", x=0, y=0, w=5, h=6)
+        vc.add_view(dataset, cm.CELL_SET_EXPRESSION, x=5, y=0, w=4, h=6)
+        vc.add_view(dataset, cm.CELL_SETS, x=9, y=0, w=3, h=3)
+        vc.add_view(dataset, cm.GENES, x=9, y=4, w=3, h=3)
+        vc.add_view(dataset, cm.HEATMAP, x=0, y=6, w=12, h=4)
+        return vc
+
+class SlideSeqAnnDataConf(ViewConf):
+
+    def build_vitessce_conf(self):
+        vc = VitessceConfig(name=self._uuid)
+        adata_url = self._build_assets_url('hubmap_ui/anndata-zarr/secondary_analysis.zarr', use_token=False)
+        dataset = vc.add_dataset(name=self._uuid).add_object(AnnDataWrapper(
+                adata_url=adata_url,
+                spatial_centroid_obsm=["X_spatial"],
+                mappings_obsm=["X_umap"],
+                mappings_obsm_names=["UMAP"],
+                cell_set_obs=["leiden"],
+                cell_set_obs_names=["Leiden"],
+                expression_matrix="X",
+                matrix_gene_var_filter="marker_genes_for_heatmap",
+                factors_obs=[
+                    "marker_gene_0",
+                    "marker_gene_1",
+                    "marker_gene_2",
+                    "marker_gene_3",
+                    "marker_gene_4"
+                ],
+                request_init={
+                    "headers": {
+                        "Authorization": f"Bearer {self._nexus_token}"
+                    }
+                }
+            )
+        )
+        vc = self._setup_anndata_view_config(vc, dataset)
+        return vc.to_dict()
+
+    def _setup_anndata_view_config(self, vc, dataset):
+        vc.add_view(dataset, cm.SCATTERPLOT, mapping="UMAP", x=0, y=0, w=5, h=6)
+        vc.add_view(dataset, cm.SPATIAL, x=0, y=6, w=5, h=6)
+        vc.add_view(dataset, cm.CELL_SET_EXPRESSION, x=5, y=0, w=4, h=6)
+        vc.add_view(dataset, cm.CELL_SETS, x=9, y=0, w=3, h=3)
+        vc.add_view(dataset, cm.GENES, x=9, y=4, w=3, h=3)
+        vc.add_view(dataset, cm.HEATMAP, x=5, y=6, w=12, h=4)
+        return vc
+
+
+
 
 class IMSConf(ImagePyramidViewConf):
     def __init__(self, entity, nexus_token, is_mock=False):
@@ -173,6 +255,8 @@ def get_view_config_class_for_data_types(entity, nexus_token):
         return ImagePyramidViewConf(
             entity=entity, nexus_token=nexus_token)
     if "rna" in hints:
+        if any(['.zgroup' in file['rel_path'] for file in entity['files']]):
+            return RNASeqAnnDataConf(entity=entity, nexus_token=nexus_token)
         return RNASeqConf(entity=entity, nexus_token=nexus_token)
     if "atac" in hints:
         return ATACSeqConf(entity=entity, nexus_token=nexus_token)
