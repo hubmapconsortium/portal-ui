241a80f53cdbfd0bbffd0dfbefed857fbb1e8c4b
John Conroy <62477388+john-conroy@users.noreply.github.com>
2021-03-25 14:44:07 -0400

    John conroy/prov node tooltip (#1672)
    * Add tooltip to NodeElement
    
    * Add changelog
    
    * Changelog text
    
    Co-authored-by: John Conroy <john-conroy@users.noreply.github.com>

--- context/app/static/js/components/Detail/provenance/NodeElement/NodeElement.jsx
+++ context/app/static/js/components/Detail/provenance/NodeElement/NodeElement.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import useProvenanceStore from 'js/stores/useProvenanceStore';
+import { SecondaryBackgroundTooltip } from 'js/shared-styles/tooltips';
 import { AsteriskWrapper } from './style';
 
 const provenanceStoreUUIDSelector = (state) => state.uuid;
@@ -13,41 +14,13 @@ function NodeElement(props) {
 
   const uuid = useProvenanceStore(provenanceStoreUUIDSelector);
 
-  function tooltip() {
-    let output = '';
-
-    // Node Type
-    if (node.nodeType === 'step') {
-      output += `<small>Step ${(node.column - 1) / 2 + 1}</small>`;
-    } else {
-      let { nodeType } = node;
-      nodeType = nodeType.charAt(0).toUpperCase() + nodeType.slice(1);
-      output += `<small>${nodeType}</small>`;
-    }
-
-    // Title
-    output += `<h5 class="text-600 tooltip-title">${node.title || node.name}</h5>`;
-
-    // Description
-    if (typeof node.description === 'string' || (node.meta && typeof node.meta.description === 'string')) {
-      output += `<div>${node.description || node.meta.description}</div>`;
-    }
-
-    return output;
-  }
-
   return (
-    <div
-      data-testid={displayTitle}
-      className="node-visible-element"
-      data-tip={tooltip()}
-      data-place="top"
-      data-html
-      style={style}
-    >
-      <div className="node-name">{displayTitle}</div>
-      {uuid === node.meta.prov['hubmap:uuid'] && <AsteriskWrapper aria-label="">*</AsteriskWrapper>}
-    </div>
+    <SecondaryBackgroundTooltip title={displayTitle}>
+      <div data-testid={displayTitle} className="node-visible-element" style={style}>
+        <div className="node-name">{displayTitle}</div>
+        {uuid === node.meta.prov['hubmap:uuid'] && <AsteriskWrapper aria-label="">*</AsteriskWrapper>}
+      </div>
+    </SecondaryBackgroundTooltip>
   );
 }
 
