e54008f717115c0adcf3b6f2bf200fdec7d7d237
John Conroy <john-conroy@users.noreply.github.com>
2021-04-01 12:09:23 -0400

    Add tooltip to bar chart

--- context/app/static/js/components/home-revision/AssayTypeBarChart/AssayTypeBarChart.jsx
+++ context/app/static/js/components/home-revision/AssayTypeBarChart/AssayTypeBarChart.jsx
@@ -4,6 +4,9 @@ import { BarStackHorizontal } from '@visx/shape';
 import { Group } from '@visx/group';
 import { AxisTop, AxisLeft } from '@visx/axis';
 import { withParentSize } from '@visx/responsive';
+import { useTooltip, useTooltipInPortal } from '@visx/tooltip';
+import { localPoint } from '@visx/event';
+import Typography from '@material-ui/core/Typography';
 
 import { AppContext } from 'js/components/Providers';
 import useSearchData from 'js/hooks/useSearchData';
@@ -90,62 +93,92 @@ function AssayTypeBarChart({ parentWidth, parentHeight }) {
   docCountScale.rangeRound([0, xMax]);
   dataTypeScale.rangeRound([yMax, 0]);
 
+  const { tooltipData, tooltipLeft, tooltipTop, tooltipOpen, showTooltip, hideTooltip } = useTooltip();
+
+  const { containerRef, TooltipInPortal } = useTooltipInPortal({
+    detectBounds: true,
+    scroll: true,
+    debounce: 100,
+  });
+
+  const handleMouse = (event, bar) => {
+    const coords = localPoint(event.target.ownerSVGElement, event);
+    showTooltip({
+      tooltipLeft: coords.x,
+      tooltipTop: coords.y,
+      tooltipData: bar,
+    });
+  };
+
   return (
-    <svg width={parentWidth} height={parentHeight}>
-      <Group top={margin.top} left={margin.left}>
-        <BarStackHorizontal
-          data={formattedData}
-          keys={organTypes}
-          height={yMax}
-          y={getDataType}
-          xScale={docCountScale}
-          yScale={dataTypeScale}
-          color={colorScale}
-        >
-          {(barStacks) =>
-            barStacks.map((barStack) =>
-              barStack.bars.map(
-                (bar) =>
-                  bar.width > 0 && (
-                    <rect
-                      key={`barstack-horizontal-${barStack.index}-${bar.index}`}
-                      x={bar.x}
-                      y={bar.y}
-                      width={bar.width}
-                      height={bar.height}
-                      fill={bar.color}
-                    />
-                  ),
-              ),
-            )
-          }
-        </BarStackHorizontal>
-        <AxisLeft
-          hideTicks
-          scale={dataTypeScale}
-          stroke="black"
-          numTicks={33}
-          tickLabelProps={() => ({
-            fill: 'black',
-            fontSize: 8,
-            textAnchor: 'end',
-            dy: '0.33em',
-          })}
-        />
-        <AxisTop
-          hideTicks
-          top={1}
-          scale={docCountScale}
-          stroke="black"
-          tickStroke="black"
-          tickLabelProps={() => ({
-            fill: 'black',
-            fontSize: 11,
-            textAnchor: 'middle',
-          })}
-        />
-      </Group>
-    </svg>
+    <div>
+      <svg width={parentWidth} height={parentHeight} ref={containerRef}>
+        <Group top={margin.top} left={margin.left}>
+          <BarStackHorizontal
+            data={formattedData}
+            keys={organTypes}
+            height={yMax}
+            y={getDataType}
+            xScale={docCountScale}
+            yScale={dataTypeScale}
+            color={colorScale}
+          >
+            {(barStacks) =>
+              barStacks.map((barStack) =>
+                barStack.bars.map(
+                  (bar) =>
+                    bar.width > 0 && (
+                      <rect
+                        key={`barstack-horizontal-${barStack.index}-${bar.index}`}
+                        x={bar.x}
+                        y={bar.y}
+                        width={bar.width}
+                        height={bar.height}
+                        fill={bar.color}
+                        onMouseEnter={(event) => handleMouse(event, bar, barStack.index)}
+                        onMouseLeave={hideTooltip}
+                      />
+                    ),
+                ),
+              )
+            }
+          </BarStackHorizontal>
+          <AxisLeft
+            hideTicks
+            scale={dataTypeScale}
+            stroke="black"
+            numTicks={33}
+            tickLabelProps={() => ({
+              fill: 'black',
+              fontSize: 11,
+              textAnchor: 'end',
+              dy: '0.33em',
+            })}
+          />
+          <AxisTop
+            hideTicks
+            top={1}
+            scale={docCountScale}
+            stroke="black"
+            tickStroke="black"
+            tickLabelProps={() => ({
+              fill: 'black',
+              fontSize: 11,
+              textAnchor: 'middle',
+            })}
+          />
+        </Group>
+      </svg>
+      {tooltipOpen && (
+        <TooltipInPortal top={tooltipTop} left={tooltipLeft}>
+          <Typography variant="subtitle2">{tooltipData.bar.data.mapped_data_type}</Typography>
+          <Typography>{tooltipData.key}</Typography>
+          <Typography variant="h3" component="p">
+            {tooltipData.bar.data[tooltipData.key]}
+          </Typography>
+        </TooltipInPortal>
+      )}
+    </div>
   );
 }
 
