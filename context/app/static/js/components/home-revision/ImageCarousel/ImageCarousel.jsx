f478d3f4acc3b17873527fae45b2f01a7e879ea9
John Conroy <john-conroy@users.noreply.github.com>
2021-04-11 16:56:25 -0400

    Use nuka carousel in image carousel

--- context/app/static/js/components/home-revision/ImageCarousel/ImageCarousel.jsx
+++ context/app/static/js/components/home-revision/ImageCarousel/ImageCarousel.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import 'react-responsive-carousel/lib/styles/carousel.min.css'; // requires a loader
-import { Carousel } from 'react-responsive-carousel';
+import Carousel from 'nuka-carousel';
 import VitessceSlide640w from 'images/vitessce-slide-640w.png';
 import VitessceSlide800w from 'images/vitessce-slide-800w.png';
 import VitessceSlide1024w from 'images/vitessce-slide-1024w.png';
@@ -19,34 +18,37 @@ import CCFSlide1392w from 'images/ccf-slide-1392w.png';
 
 function ImageCarousel({ selectedImageIndex }) {
   return (
-    <Carousel showThumbs={false} showStatus={false} selectedItem={selectedImageIndex}>
-      <div>
-        <img
-          srcSet={`${VitessceSlide640w} 640w,
+    <Carousel
+      disableAnimation
+      slideIndex={selectedImageIndex}
+      withoutControls
+      swiping={false}
+      dragging={false}
+      enableKeyboardControls
+      wrapAround
+    >
+      <img
+        srcSet={`${VitessceSlide640w} 640w,
           ${VitessceSlide800w} 800w,${VitessceSlide1024w} 1024w,
         ${VitessceSlide1280w} 1280w,
         ${VitessceSlide1392w} 1392w`}
-          alt="CCF Portal"
-        />
-      </div>
-      <div>
-        <img
-          srcSet={`${AzimuthSlide640w} 640w,
+        alt="CCF Portal"
+      />
+      <img
+        srcSet={`${AzimuthSlide640w} 640w,
           ${AzimuthSlide800w} 800w,${AzimuthSlide1024w} 1024w,
         ${AzimuthSlide1280w} 1280w,
         ${AzimuthSlide1392w} 1392w`}
-          alt="Azimuth Tool"
-        />
-      </div>
-      <div>
-        <img
-          srcSet={`${CCFSlide640w} 640w,
+        alt="Azimuth Tool"
+      />
+
+      <img
+        srcSet={`${CCFSlide640w} 640w,
           ${CCFSlide800w} 800w,${CCFSlide1024w} 1024w,
         ${CCFSlide1280w} 1280w,
         ${CCFSlide1392w} 1392w`}
-          alt="CCF Portal"
-        />
-      </div>
+        alt="CCF Portal"
+      />
     </Carousel>
   );
 }
