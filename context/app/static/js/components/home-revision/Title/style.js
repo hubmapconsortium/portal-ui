06f719fe87a4d82b6dda22c3c75526a8435bad7e
John Conroy <john-conroy@users.noreply.github.com>
2021-04-11 22:22:27 -0400

    Remove additional top margin on mobile screen sizes

--- context/app/static/js/components/home-revision/Title/style.js
+++ context/app/static/js/components/home-revision/Title/style.js
@@ -1,7 +1,10 @@
 import styled from 'styled-components';
 
 const StyledDiv = styled.div`
-  margin-top: ${(props) => props.theme.spacing(4)}px;
+  // additional margin beyond the default 16px page margin
+  @media (min-width: ${(props) => props.theme.breakpoints.values.sm}px) {
+    margin-top: ${(props) => props.theme.spacing(4)}px;
+  }
 `;
 
 export { StyledDiv };
