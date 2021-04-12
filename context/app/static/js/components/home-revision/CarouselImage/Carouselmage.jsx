8ee7a6378d3cf4adb5021a90de48a2e742054899
John Conroy <john-conroy@users.noreply.github.com>
2021-04-12 13:40:16 -0400

    Refactor carousel images into new component

---
+++ context/app/static/js/components/home-revision/CarouselImage/Carouselmage.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { useTheme } from '@material-ui/core/styles';
+
+import { routeContainerMaxWidth, routeContainerPadding } from 'js/components/Routes/Route/style';
+import { callToActionMdOrLargerWidth } from 'js/components/home-revision/ImageCarouselContainer/style';
+import { StyledImage } from './style';
+
+// Responsive images: https://developer.mozilla.org/en-US/docs/Learn/HTML/Multimedia_and_embedding/Responsive_images
+// Setting height and width: https://www.smashingmagazine.com/2020/03/setting-height-width-images-important-again/
+// 1392 was the max resoultion provided by the designer.
+
+function Carouselmage({ src640w, src800w, src1024w, src1280w, src1392w, alt }) {
+  const theme = useTheme();
+  const mdBreakpoint = theme.breakpoints.values.md;
+
+  const maxImageWidth = routeContainerMaxWidth - callToActionMdOrLargerWidth;
+  const maxImageHeight = (maxImageWidth * 9) / 16; // 16:9 aspect ratio
+
+  return (
+    <StyledImage
+      srcSet={`${src640w} 640w,
+          ${src800w} 800w,${src1024w} 1024w,
+        ${src1280w} 1280w,
+        ${src1392w} 1392w`}
+      sizes={`(max-width: ${mdBreakpoint}px) calc(100vw - ${routeContainerPadding}px), max(${maxImageWidth}px, calc(100vw - ${callToActionMdOrLargerWidth}px - ${routeContainerPadding}px))`}
+      width={maxImageWidth}
+      height={maxImageHeight}
+      alt={alt}
+    />
+  );
+}
+
+export default Carouselmage;
--- context/app/static/js/components/home-revision/ImageCarousel/ImageCarousel.jsx
+++ context/app/static/js/components/home-revision/ImageCarousel/ImageCarousel.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Carousel from 'nuka-carousel';
-import { useTheme } from '@material-ui/core/styles';
 
+import CarouselImage from 'js/components/home-revision/CarouselImage';
 import VitessceSlide640w from 'images/vitessce-slide-640w.png';
 import VitessceSlide800w from 'images/vitessce-slide-800w.png';
 import VitessceSlide1024w from 'images/vitessce-slide-1024w.png';
@@ -17,21 +17,8 @@ import CCFSlide800w from 'images/ccf-slide-800w.png';
 import CCFSlide1024w from 'images/ccf-slide-1024w.png';
 import CCFSlide1280w from 'images/ccf-slide-1280w.png';
 import CCFSlide1392w from 'images/ccf-slide-1392w.png';
-import { routeContainerMaxWidth, routeContainerPadding } from 'js/components/Routes/Route/style';
-import { callToActionMdOrLargerWidth } from 'js/components/home-revision/ImageCarouselContainer/style';
-import { StyledImage } from './style';
-
-// Responsive images: https://developer.mozilla.org/en-US/docs/Learn/HTML/Multimedia_and_embedding/Responsive_images
-// Setting height and width: https://www.smashingmagazine.com/2020/03/setting-height-width-images-important-again/
 
 function ImageCarousel({ selectedImageIndex }) {
-  const theme = useTheme();
-  const mdBreakpoint = theme.breakpoints.values.md;
-
-  const maxImageWidth = routeContainerMaxWidth - callToActionMdOrLargerWidth;
-  // 16:9 aspect ratio
-  const maxImageHeight = (maxImageWidth * 9) / 16;
-
   return (
     <Carousel
       disableAnimation
@@ -42,36 +29,29 @@ function ImageCarousel({ selectedImageIndex }) {
       enableKeyboardControls
       wrapAround
     >
-      <StyledImage
-        srcSet={`${VitessceSlide640w} 640w,
-          ${VitessceSlide800w} 800w,${VitessceSlide1024w} 1024w,
-        ${VitessceSlide1280w} 1280w,
-        ${VitessceSlide1392w} 1392w`}
-        sizes={`(max-width: ${mdBreakpoint}px) calc(100vw - ${routeContainerPadding}px), max(${maxImageWidth}px, calc(100vw - ${callToActionMdOrLargerWidth}px - ${routeContainerPadding}px))`}
-        width={maxImageWidth}
-        height={maxImageHeight}
+      <CarouselImage
+        src640w={VitessceSlide640w}
+        src800w={VitessceSlide800w}
+        src1024w={VitessceSlide1024w}
+        src1280w={VitessceSlide1280w}
+        src1392w={VitessceSlide1392w}
+        alt="Vitessce"
+      />
+      <CarouselImage
+        src640w={CCFSlide640w}
+        src800w={CCFSlide800w}
+        src1024w={CCFSlide1024w}
+        src1280w={CCFSlide1280w}
+        src1392w={CCFSlide1392w}
         alt="CCF Portal"
       />
-      <StyledImage
-        srcSet={`${AzimuthSlide640w} 640w,
-          ${AzimuthSlide800w} 800w,${AzimuthSlide1024w} 1024w,
-        ${AzimuthSlide1280w} 1280w,
-        ${AzimuthSlide1392w} 1392w`}
-        sizes={`(max-width: ${mdBreakpoint}px) calc(100vw - ${routeContainerPadding}px), max(${maxImageWidth}px, calc(100vw - ${callToActionMdOrLargerWidth}px - ${routeContainerPadding}px))`}
-        width={maxImageWidth}
-        height={maxImageHeight}
-        alt="Azimuth Tool"
-      />
-
-      <StyledImage
-        srcSet={`${CCFSlide640w} 640w,
-          ${CCFSlide800w} 800w,${CCFSlide1024w} 1024w,
-        ${CCFSlide1280w} 1280w,
-        ${CCFSlide1392w} 1392w`}
-        sizes={`(max-width: ${mdBreakpoint}px) calc(100vw - ${routeContainerPadding}px), max(${maxImageWidth}px, calc(100vw - ${callToActionMdOrLargerWidth}px - ${routeContainerPadding}px))`}
-        width={maxImageWidth}
-        height={maxImageHeight}
-        alt="CCF Portal"
+      <CarouselImage
+        src640w={AzimuthSlide640w}
+        src800w={AzimuthSlide800w}
+        src1024w={AzimuthSlide1024w}
+        src1280w={AzimuthSlide1280w}
+        src1392w={AzimuthSlide1392w}
+        alt="Azimuth"
       />
     </Carousel>
   );
