68938169a100b16154115d0141df49e0d2299fd6
John Conroy <john-conroy@users.noreply.github.com>
2021-04-12 11:16:25 -0400

    Add sizes and height/width attrs to images

--- context/app/static/js/components/home-revision/ImageCarousel/ImageCarousel.jsx
+++ context/app/static/js/components/home-revision/ImageCarousel/ImageCarousel.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Carousel from 'nuka-carousel';
+
 import VitessceSlide640w from 'images/vitessce-slide-640w.png';
 import VitessceSlide800w from 'images/vitessce-slide-800w.png';
 import VitessceSlide1024w from 'images/vitessce-slide-1024w.png';
@@ -15,6 +16,10 @@ import CCFSlide800w from 'images/ccf-slide-800w.png';
 import CCFSlide1024w from 'images/ccf-slide-1024w.png';
 import CCFSlide1280w from 'images/ccf-slide-1280w.png';
 import CCFSlide1392w from 'images/ccf-slide-1392w.png';
+import { StyledImage } from './style';
+
+// Responsive images: https://developer.mozilla.org/en-US/docs/Learn/HTML/Multimedia_and_embedding/Responsive_images
+// Setting height and width: https://www.smashingmagazine.com/2020/03/setting-height-width-images-important-again/
 
 function ImageCarousel({ selectedImageIndex }) {
   return (
@@ -27,26 +32,35 @@ function ImageCarousel({ selectedImageIndex }) {
       enableKeyboardControls
       wrapAround
     >
-      <img
+      <StyledImage
         srcSet={`${VitessceSlide640w} 640w,
           ${VitessceSlide800w} 800w,${VitessceSlide1024w} 1024w,
         ${VitessceSlide1280w} 1280w,
         ${VitessceSlide1392w} 1392w`}
+        sizes="(max-width: 960px) calc(100vw - 32px), max(1232px - 400px, calc(100vw - 400px - 32px))"
+        width={832}
+        height={468}
         alt="CCF Portal"
       />
-      <img
+      <StyledImage
         srcSet={`${AzimuthSlide640w} 640w,
           ${AzimuthSlide800w} 800w,${AzimuthSlide1024w} 1024w,
         ${AzimuthSlide1280w} 1280w,
         ${AzimuthSlide1392w} 1392w`}
+        sizes="(max-width: 960px) calc(100vw - 32px), max(832px, calc(100vw - 400px - 32px))"
+        width={832}
+        height={468}
         alt="Azimuth Tool"
       />
 
-      <img
+      <StyledImage
         srcSet={`${CCFSlide640w} 640w,
           ${CCFSlide800w} 800w,${CCFSlide1024w} 1024w,
         ${CCFSlide1280w} 1280w,
         ${CCFSlide1392w} 1392w`}
+        sizes="(max-width: 960px) calc(100vw - 32px), max(832px, calc(100vw - 400px - 32px))"
+        width={832}
+        height={468}
         alt="CCF Portal"
       />
     </Carousel>
