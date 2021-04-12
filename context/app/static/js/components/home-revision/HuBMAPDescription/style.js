f74e2c0750fc4d2a4a50958b69a6979bb7c54201
John Conroy <john-conroy@users.noreply.github.com>
2021-04-11 21:55:51 -0400

    Adjust description padding for screen sizes md or smaller

--- context/app/static/js/components/home-revision/HuBMAPDescription/style.js
+++ context/app/static/js/components/home-revision/HuBMAPDescription/style.js
@@ -2,7 +2,10 @@ import styled from 'styled-components';
 import Paper from '@material-ui/core/Paper';
 
 const StyledPaper = styled(Paper)`
-  padding: ${(props) => props.theme.spacing(2)}px;
+  padding: ${(props) => props.theme.spacing(2)}px ${(props) => props.theme.spacing(0.5)}px;
+  @media (min-width: ${(props) => props.theme.breakpoints.values.md}px) {
+    padding: ${(props) => props.theme.spacing(2)}px;
+  }
 `;
 
 export { StyledPaper };
