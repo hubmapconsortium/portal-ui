477a445a671d41d504aa7d496159f0dfb86d369c
Chuck McCallum <mccalluc@users.noreply.github.com>
2021-04-19 10:26:34 -0400

    Update context/app/api/client.py

--- context/app/api/client.py
+++ context/app/api/client.py
@@ -108,7 +108,9 @@ class ApiClient():
             if len(entity['descendants']) > 1:
                 current_app.logger.error(f'Expected only one descendant on {entity["uuid"]}')
             derived_entity = deepcopy(entity['descendants'][0])
-            # TODO: Why are files in metadata?
+            # TODO: Entity structure will change in the future to be consistent 
+            # about "files". Bill confirms that when the new structure comes in
+            # there will be a period of backward compatibility to allow us to migrate.
             derived_entity['files'] = derived_entity['metadata']['files']
             return self.get_vitessce_conf(derived_entity)
         if 'files' not in entity or 'data_types' not in entity:
