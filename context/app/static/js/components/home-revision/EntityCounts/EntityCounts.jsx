0d5e8ca7f5d213916a545326e1bb44b151c57d7a
John Conroy <john-conroy@users.noreply.github.com>
2021-04-20 14:24:39 -0400

    Refactor entity counts

--- context/app/static/js/components/home-revision/EntityCounts/EntityCounts.jsx
+++ context/app/static/js/components/home-revision/EntityCounts/EntityCounts.jsx
@@ -11,6 +11,21 @@ const entityCountsQuery = {
   aggs: { entity_type: { terms: { field: 'entity_type.keyword' } } },
 };
 
+const entities = [
+  {
+    icon: DonorIcon,
+    entity_type: 'Donor',
+  },
+  {
+    icon: SampleIcon,
+    entity_type: 'Sample',
+  },
+  {
+    icon: DatasetIcon,
+    entity_type: 'Dataset',
+  },
+];
+
 function EntityCounts() {
   const { elasticsearchEndpoint, nexusToken } = useContext(AppContext);
   const [entityCounts, setEntityCountsData] = useState(undefined);
@@ -29,19 +44,14 @@ function EntityCounts() {
   return (
     <Background>
       <FlexContainer>
-        <EntityCount icon={DonorIcon} count={entityCounts?.Donor} label="Donors" href="/search?entity_type[0]=Donor" />
-        <EntityCount
-          icon={SampleIcon}
-          count={entityCounts?.Sample}
-          label="Samples"
-          href="/search?entity_type[0]=Sample"
-        />
-        <EntityCount
-          icon={DatasetIcon}
-          count={entityCounts?.Dataset}
-          label="Datasets"
-          href="/search?entity_type[0]=Dataset"
-        />
+        {entities.map(({ icon, entity_type }) => (
+          <EntityCount
+            icon={icon}
+            count={entityCounts?.[entity_type]}
+            label={`${entity_type}s`}
+            href={`/search?entity_type[0]=${entity_type}`}
+          />
+        ))}
         <EntityCount icon={CollectionIcon} count={entityCounts?.Collection} label="Collections" href="/collections" />
       </FlexContainer>
     </Background>
