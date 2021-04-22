329fa9e2f911088fe5a456b63e981cbc228a82bc
John Conroy <john-conroy@users.noreply.github.com>
2021-04-22 14:28:37 -0400

    Fix twitter timeline sizing bug

--- context/app/static/js/components/home-revision/TwitterTimeline/style.js
+++ context/app/static/js/components/home-revision/TwitterTimeline/style.js
@@ -2,10 +2,17 @@ import styled from 'styled-components';
 import Paper from '@material-ui/core/Paper';
 
 const Wrapper = styled(Paper)`
-  max-height: 374px;
+  height: 374px;
+  // The autoHeight from react-twitter-embed is buggy, setting max-height for the iframe fixes that
+  iframe {
+    max-height: 374px;
+  }
 
   @media (min-width: ${(props) => props.theme.breakpoints.values.md}px) {
     height: 100%;
+    iframe {
+      max-height: 100%;
+    }
   }
 `;
 
