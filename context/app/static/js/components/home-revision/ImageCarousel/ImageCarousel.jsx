0facbc21aefd0981891a910f89ebf42e5b895ab3
John Conroy <62477388+john-conroy@users.noreply.github.com>
2021-04-26 12:26:43 -0400

    Set random initial carousel image index (#1767)
    * Install react-responsive-carousel
    
    * Add carousel images
    
    * Add image carousel
    
    * Add image carousel control buttons
    
    * Add image carousel call to action
    
    * Add image carousel container
    
    * Add image carousel to home revision
    
    * Install nuka-carousel
    
    * Use nuka carousel in image carousel
    
    * Alter control button sizes
    
    * Add home revision description component
    
    * Add home revision title component
    
    * Replace fragments with divs in call to action
    
    * Add grid to home revision
    
    * Position carousel components
    
    * Only display bar chart on screens larger than medium
    
    * Change carousel component order screens md or smaller
    
    * Fix placement of control buttons on screens md or smaller
    
    * Add spacing and refactor call to action
    
    * Adjust description padding for screen sizes md or smaller
    
    * Change above the fold to screens lg
    
    * Remove additional top margin on mobile screen sizes
    
    * Change header element in title
    
    * Uninstall react-responsive-carousel
    
    * Add sizes and height/width attrs to images
    
    * Remove unused css for containers
    
    * Use vars for img size calc
    
    * Refactor carousel images into new component
    
    * Add 320w images for carousel
    
    * Add changelog
    
    * Wrap bar chart in container
    
    * Add margin between carousel text and images
    
    * Fix carousel spacing
    
    * Set afterslide to handle keyboard slides
    
    * Remove images
    
    * Add submodule
    
    * Update paths
    
    * Create entity count component
    
    * Add entity counts component
    
    * Add collection icon to icons
    
    * Add entity counts to home revision
    
    * Add real data for entity counts
    
    * Make entity counts more responsive
    
    * Fix prop/attribute warning
    
    * Add links to entity count
    
    * Add skeleton while waiting for entity count data
    
    * Fix entity count styles
    
    * Add home revision data use guidelines
    
    * Add home revision twitter timeline
    
    * Add lower grid to home revision
    
    * Add ExternalLink
    
    * Add ExternalLinks
    
    * Add external links to home revision
    
    * Remove header from twitter timeline
    
    * Style external link icon
    
    * Add hover for external link
    
    * Use icon from shared-styles
    
    * Add email icon to data use guidelines
    
    * Increase padding in lower grid components
    
    * Add margin to bottom of home revision
    
    * Merge master into john-conroy/home-revision-lower-grid
    
    * Bump portal-images
    
    * Add changelog
    
    * Add headers to home revision sections
    
    * Upgrade portal-images
    
    * Upgrade portal-images
    
    * Add images to external links
    
    * Fix twitter timeline sizing bug
    
    * Remove above the fold styling and fix spacing
    
    * Add spacing between title and description
    
    * Set random initial carousel image index
    
    * Add changelog
    
    * Move image list up a level so the count does not need to be stored separately (#1768)
    
    * Move image list up a level so the number does not need to be stored separately
    
    * Pull title and body into array
    
    Co-authored-by: John Conroy <john-conroy@users.noreply.github.com>
    Co-authored-by: Chuck McCallum <mccalluc@users.noreply.github.com>

--- context/app/static/js/components/home-revision/ImageCarousel/ImageCarousel.jsx
+++ context/app/static/js/components/home-revision/ImageCarousel/ImageCarousel.jsx
@@ -1,21 +1,7 @@
 import React from 'react';
 import Carousel from 'nuka-carousel';
 
-import CarouselImage from 'js/components/home-revision/CarouselImage';
-import VitessceSlide320w from 'portal-images/vitessce-slide-320w.png';
-import VitessceSlide640w from 'portal-images/vitessce-slide-640w.png';
-import VitessceSlide1280w from 'portal-images/vitessce-slide-1280w.png';
-import VitessceSlide1392w from 'portal-images/vitessce-slide-1392w.png';
-import AzimuthSlide320w from 'portal-images/azimuth-slide-320w.png';
-import AzimuthSlide640w from 'portal-images/azimuth-slide-640w.png';
-import AzimuthSlide1280w from 'portal-images/azimuth-slide-1280w.png';
-import AzimuthSlide1392w from 'portal-images/azimuth-slide-1392w.png';
-import CCFSlide320w from 'portal-images/ccf-slide-320w.png';
-import CCFSlide640w from 'portal-images/ccf-slide-640w.png';
-import CCFSlide1280w from 'portal-images/ccf-slide-1280w.png';
-import CCFSlide1392w from 'portal-images/ccf-slide-1392w.png';
-
-function ImageCarousel({ selectedImageIndex, setSelectedImageIndex }) {
+function ImageCarousel({ selectedImageIndex, setSelectedImageIndex, images }) {
   return (
     <Carousel
       disableAnimation
@@ -32,27 +18,7 @@ function ImageCarousel({ selectedImageIndex, setSelectedImageIndex }) {
         }
       }}
     >
-      <CarouselImage
-        src320w={VitessceSlide320w}
-        src640w={VitessceSlide640w}
-        src1280w={VitessceSlide1280w}
-        src1392w={VitessceSlide1392w}
-        alt="Vitessce"
-      />
-      <CarouselImage
-        src320w={CCFSlide320w}
-        src640w={CCFSlide640w}
-        src1280w={CCFSlide1280w}
-        src1392w={CCFSlide1392w}
-        alt="CCF Portal"
-      />
-      <CarouselImage
-        src320w={AzimuthSlide320w}
-        src640w={AzimuthSlide640w}
-        src1280w={AzimuthSlide1280w}
-        src1392w={AzimuthSlide1392w}
-        alt="Azimuth"
-      />
+      {images}
     </Carousel>
   );
 }
