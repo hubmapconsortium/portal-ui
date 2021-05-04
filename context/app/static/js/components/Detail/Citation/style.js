249f4887dbe8e738f88e85ca5ea0944545e27f6e
Chuck McCallum <mccalluc@users.noreply.github.com>
2021-05-04 13:36:45 -0400

    Update context/app/static/js/components/Detail/Citation/style.js
    Co-authored-by: John Conroy <62477388+john-conroy@users.noreply.github.com>

--- context/app/static/js/components/Detail/Citation/style.js
+++ context/app/static/js/components/Detail/Citation/style.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 import Paper from '@material-ui/core/Paper';
 
 const FlexPaper = styled(Paper)`
-  padding: 30px 40px 30px 40px;
+  padding: 30px 40px;
 `;
 
 export { FlexPaper };
