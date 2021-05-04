98eb8fea1ebabecc1a57cd5ae5027f8a2ed5dee9
Chuck McCallum <mccalluc@users.noreply.github.com>
2021-05-04 13:18:16 -0400

    Apply suggestions from code review
    Co-authored-by: John Conroy <62477388+john-conroy@users.noreply.github.com>

--- context/app/static/js/components/savedLists/SavedListPanel/SavedListPanel.jsx
+++ context/app/static/js/components/savedLists/SavedListPanel/SavedListPanel.jsx
@@ -2,15 +2,10 @@ import React from 'react';
 import { Panel } from 'js/shared-styles/panels';
 
 function getEntityCounts(listSavedEntities) {
-  const counts = new Proxy(
-    // If a "Support" entity appears, we want to count it, but it shouldn't be included otherwise.
-    { Donor: 0, Sample: 0, Dataset: 0 },
-    {
-      get: (target, name) => (name in target ? target[name] : 0),
-    },
-  );
+  const counts = { Donor: 0, Sample: 0, Dataset: 0 };
   Object.values(listSavedEntities).forEach((entity) => {
-    counts[entity.entity_type] += 1;
+    const { entity_type } = entity;
+    counts[entity_type] ? counts[entity_type] += 1 : counts[entity_type] = 1;
   });
   return counts;
 }
