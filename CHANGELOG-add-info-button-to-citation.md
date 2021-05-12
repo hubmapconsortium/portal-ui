82a33c5cc556850d1eb7f13f82eb3f7bd4bb7348
Chuck McCallum <mccalluc@users.noreply.github.com>
2021-05-12 16:23:11 -0400

    citation button (#1838)
    * citation button
    
    * Style header; pad on left

---
+++ CHANGELOG-add-info-button-to-citation.md
@@ -0,0 +1,1 @@
+- Add missing info button to citation.
--- context/app/static/js/components/Detail/Citation/style.js
+++ context/app/static/js/components/Detail/Citation/style.js
@@ -1,8 +1,20 @@
 import styled from 'styled-components';
 import Paper from '@material-ui/core/Paper';
+import SectionHeader from 'js/shared-styles/sections/SectionHeader';
+
+import { InfoIcon } from 'js/shared-styles/icons';
 
 const FlexPaper = styled(Paper)`
   padding: 30px 40px;
 `;
 
-export { FlexPaper };
+const StyledInfoIcon = styled(InfoIcon)`
+  margin-left: ${(props) => props.theme.spacing(0.5)}px;
+`;
+
+const StyledSectionHeader = styled(SectionHeader)`
+  display: flex;
+  align-items: center;
+`;
+
+export { FlexPaper, StyledInfoIcon, StyledSectionHeader };
