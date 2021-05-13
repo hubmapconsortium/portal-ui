02922a07de7c30e80816c2000240855d4d2d8608
mccalluc <mccalluc@users.noreply.github.com>
2021-05-13 13:19:12 -0400

    Explain queries in UI

--- context/app/static/js/pages/CellsAPIDemo/CellsAPIDemo.jsx
+++ context/app/static/js/pages/CellsAPIDemo/CellsAPIDemo.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import SectionHeader from 'js/shared-styles/sections/SectionHeader';
+import { Alert } from 'js/shared-styles/alerts';
 
 import CellCountsForDatasets from './CellCountsForDatasets';
 import CellExpressionInDataset from './CellExpressionInDataset';
@@ -10,14 +11,36 @@ import DatasetsSelectedByGene from './DatasetsSelectedByGene';
 function CellsAPIDemo(props) {
   return (
     <>
+      <Alert severity="warning">
+        We&apos;ve gotten ahead of what the python client and the API server support, but this demonstrates what
+        we&apos;re aiming for.
+      </Alert>
+
       <SectionHeader>Datasets selected by gene</SectionHeader>
-      <em>Does not work with current client version</em>
+      <p>
+        <b>Given</b> a gene, a minimum expression level, and a minimum percentage of cells at that expression level
+      </p>
+      <p>
+        <b>Return</b> a list of UUIDs for datasets which meet those minimums
+      </p>
       <DatasetsSelectedByGene />
 
       <SectionHeader>Cell counts for datasets</SectionHeader>
+      <p>
+        <b>Given</b> a list of dataset UUIDs, a gene, and a minimum expression level
+      </p>
+      <p>
+        <b>Return</b> for each UUID the number of cells above that minimum, and the total number of cells
+      </p>
       <CellCountsForDatasets />
 
       <SectionHeader>Cell expression in dataset</SectionHeader>
+      <p>
+        <b>Given</b> a dataset UUID and a gene
+      </p>
+      <p>
+        <b>Return</b> the expression level of that gene for every cell in the dataset
+      </p>
       <CellExpressionInDataset />
     </>
   );
