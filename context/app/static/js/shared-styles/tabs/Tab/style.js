5a58d6c00c0ad2f2ae271e7d26b353c01d9a1460
John Conroy <john-conroy@users.noreply.github.com>
2021-05-14 12:59:20 -0400

    Decrease tab height

--- context/app/static/js/shared-styles/tabs/Tab/style.js
+++ context/app/static/js/shared-styles/tabs/Tab/style.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 import Tab from '@material-ui/core/Tab';
 
 const StyledTab = styled(Tab)`
-  min-height: 60px;
+  min-height: 45px;
 `;
 
 export { StyledTab };
