70bc611dcd2ce3a72764e50b078393155fbb9041
John Conroy <john-conroy@users.noreply.github.com>
2021-05-19 15:44:45 -0400

    Change tabs aria label

--- context/app/static/js/components/Detail/DerivedEntitiesTabs/DerivedEntitiesTabs.jsx
+++ context/app/static/js/components/Detail/DerivedEntitiesTabs/DerivedEntitiesTabs.jsx
@@ -10,7 +10,7 @@ function DerivedEntitiesTabs({ entities, openIndex, setOpenIndex }) {
   };
   return (
     <>
-      <Tabs value={openIndex} onChange={handleChange} aria-label="Provenance Tabs">
+      <Tabs value={openIndex} onChange={handleChange} aria-label="Derived Datasets and Samples Tabs">
         {entities.map((entity, i) => (
           <Tab label={entity.tabLabel} index={i} />
         ))}
