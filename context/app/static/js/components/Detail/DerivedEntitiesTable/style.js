33b579102d7a68191a390788dceeaec70ea54ec5
John Conroy <john-conroy@users.noreply.github.com>
2021-05-20 16:52:04 -0400

    Fix alert alignment

--- context/app/static/js/components/Detail/DerivedEntitiesTable/style.js
+++ context/app/static/js/components/Detail/DerivedEntitiesTable/style.js
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 
 const StyledDiv = styled.div`
   height: 100%;
+  width: 100%;
   overflow-y: auto;
   min-height: 0px; // flex overflow fix
 `;
--- context/app/static/js/components/Detail/DerivedEntitiesTabs/style.js
+++ context/app/static/js/components/Detail/DerivedEntitiesTabs/style.js
@@ -3,8 +3,10 @@ import { TabPanel, Tabs } from 'js/shared-styles/tabs';
 import { Alert } from 'js/shared-styles/alerts';
 
 const StyledTabPanel = styled(TabPanel)`
-  flex: 1;
+  ${(props) => props.value === props.index && `flex-grow: 1`};
+  display: flex;
   min-height: 0px; // flex overflow fix
+  align-items: center;
 `;
 
 const StyledTabs = styled(Tabs)`
@@ -12,9 +14,8 @@ const StyledTabs = styled(Tabs)`
 `;
 
 const StyledAlert = styled(Alert)`
-  align-self: center;
-  flex-grow: 1;
   margin: 10px;
+  flex-grow: 1;
 `;
 
 export { StyledTabPanel, StyledTabs, StyledAlert };
