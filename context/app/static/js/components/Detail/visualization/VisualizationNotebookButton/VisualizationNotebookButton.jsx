995a27092ff8d2bf7a63b1b549095d14a51e406e
mccalluc <mccalluc@users.noreply.github.com>
2021-05-27 18:09:20 -0400

    Margin

--- context/app/static/js/components/Detail/visualization/VisualizationNotebookButton/VisualizationNotebookButton.jsx
+++ context/app/static/js/components/Detail/visualization/VisualizationNotebookButton/VisualizationNotebookButton.jsx
@@ -1,17 +1,25 @@
 import React from 'react';
 import GetAppRoundedIcon from '@material-ui/icons/GetAppRounded';
+import styled from 'styled-components';
 
 import { SecondaryBackgroundTooltip } from 'js/shared-styles/tooltips';
 import { WhiteBackgroundIconButton } from 'js/shared-styles/buttons';
 
+// TODO: Would it be ok to create visualization/style.js and put this there,
+// so all the buttons would use it?
+// Individual buttons padding on the left and right seems fragile, if they get rearranged.
+const StyledSecondaryBackgroundTooltip = styled(SecondaryBackgroundTooltip)`
+  margin: 0 ${(props) => props.theme.spacing(1)}px;
+`;
+
 function VisualizationNotebookButton(props) {
   const { uuid } = props;
   return (
-    <SecondaryBackgroundTooltip title="Download Jupyter Notebook">
+    <StyledSecondaryBackgroundTooltip title="Download Jupyter Notebook">
       <WhiteBackgroundIconButton component="a" href={`${uuid}.ipynb`}>
         <GetAppRoundedIcon color="primary" />
       </WhiteBackgroundIconButton>
-    </SecondaryBackgroundTooltip>
+    </StyledSecondaryBackgroundTooltip>
   );
 }
 
