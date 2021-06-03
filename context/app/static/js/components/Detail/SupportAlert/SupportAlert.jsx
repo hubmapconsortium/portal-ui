82419a536ca3c42bf73e4a3e85aca4de091e7c75
John Conroy <john-conroy@users.noreply.github.com>
2021-06-03 16:33:39 -0400

    Move support alert link logic out of jsx

--- context/app/static/js/components/Detail/SupportAlert/SupportAlert.jsx
+++ context/app/static/js/components/Detail/SupportAlert/SupportAlert.jsx
@@ -9,20 +9,17 @@ function SupportAlert({ immediate_ancestors, uuid }) {
   const immediateDatasetAncestors = getImmediateDatasetAncestors(immediate_ancestors);
 
   // We only expect a single immediate parent dataset for each support, but best to provide a fallback.
-  const singleDatasetAncestor = immediateDatasetAncestors.length === 1;
+  const hasSingleDatasetAncestor = immediateDatasetAncestors.length === 1;
+  const parentDatasetHref = hasSingleDatasetAncestor
+    ? `/browse/dataset/${immediateDatasetAncestors[0]}`
+    : `/search?descendant_ids[0]=${uuid}&entity_type[0]=Dataset`;
 
   return (
     <StyledAlert severity="warning">
       <Typography variant="body2">
         “Support” entities provide derived, low-level data for visualizations. Navigate to{' '}
-        {singleDatasetAncestor ? (
-          <LightBlueLink href={`/browse/dataset/${immediateDatasetAncestors[0]}`}>the parent dataset</LightBlueLink>
-        ) : (
-          <LightBlueLink href={`/search?descendant_ids[0]=${uuid}&entity_type[0]=Dataset`}>
-            the parent datasets
-          </LightBlueLink>
-        )}{' '}
-        for a view of this information in context.
+        <LightBlueLink href={parentDatasetHref}>the parent dataset{!hasSingleDatasetAncestor && 's'}</LightBlueLink> for
+        a view of this information in context.
       </Typography>
     </StyledAlert>
   );
