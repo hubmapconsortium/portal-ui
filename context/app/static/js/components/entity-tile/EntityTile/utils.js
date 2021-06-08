b7ce97ddd60747b4d156423cca831a61e1106bc5
John Conroy <john-conroy@users.noreply.github.com>
2021-06-08 14:26:22 -0400

    Move share descendant count utils and add derived samples for sample tiles

--- context/app/static/js/components/entity-tile/EntityTile/utils.js
+++ context/app/static/js/components/entity-tile/EntityTile/utils.js
@@ -1,15 +1,15 @@
 function filterDescendantCountsByType(descendant_counts, type) {
-  if (['Sample', 'Dataset'].includes(type)) {
+  if (type === 'Dataset') {
     return descendant_counts?.Dataset ? { Dataset: descendant_counts.Dataset } : {};
   }
   return descendant_counts;
 }
 
-function getDescendantCounts(source, type) {
-  const defaultDescendantCounts = type === 'Donor' ? { Sample: 0, Dataset: 0 } : { Dataset: 0 };
+function getTileDescendantCounts(source, type) {
+  const defaultDescendantCounts = ['Donor', 'Sample'].includes(type) ? { Sample: 0, Dataset: 0 } : { Dataset: 0 };
   return 'descendant_counts' in source
     ? { ...defaultDescendantCounts, ...filterDescendantCountsByType(source.descendant_counts.entity_type, type) }
     : defaultDescendantCounts;
 }
 
-export { getDescendantCounts };
+export { getTileDescendantCounts };
