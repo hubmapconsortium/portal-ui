aaa05e83275971d23d739bf154b2764182e7b6fe
John Conroy <john-conroy@users.noreply.github.com>
2021-06-15 11:50:10 -0400

    Make initial storybook config with providers

---
+++ context/.storybook/main.js
@@ -0,0 +1,11 @@
+const path = require("path")
+
+module.exports = {
+  "stories": [
+    '../app/static/js/shared-styles/**/*.stories.js'
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials"
+  ],
+}
