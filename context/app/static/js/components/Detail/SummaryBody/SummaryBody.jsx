31ef6ec4ac673a05c5111567175f10cf6d81c045
John Conroy <john-conroy@users.noreply.github.com>
2021-06-21 13:28:40 -0400

    Add dates and citation to SummaryBody

--- context/app/static/js/components/Detail/SummaryBody/SummaryBody.jsx
+++ context/app/static/js/components/Detail/SummaryBody/SummaryBody.jsx
@@ -2,14 +2,35 @@ import React from 'react';
 
 import SummaryPaper from 'js/shared-styles/sections/SectionPaper';
 import LabelledSectionText from 'js/shared-styles/sections/LabelledSectionText';
+import LabelledSectionDate from 'js/shared-styles/sections/LabelledSectionDate';
+import Citation from 'js/components/Detail/Citation';
+import { Flex } from './style';
 
-function SummaryBody({ description, citation, createdTimestamp, modifiedTimestamp }) {
+function SummaryBody({
+  description,
+  create_timestamp,
+  last_modified_timestamp,
+  contributors,
+  citationTitle,
+  doi_url,
+  doi,
+}) {
   return (
     <SummaryPaper>
       <LabelledSectionText label="Description">{description}</LabelledSectionText>
-      <LabelledSectionText label="Citation">{citation}</LabelledSectionText>
-      <LabelledSectionText label="Creation Date">{createdTimestamp}</LabelledSectionText>
-      <LabelledSectionText label="Modification Date">{modifiedTimestamp}</LabelledSectionText>
+      {doi && (
+        <Citation
+          contributors={contributors}
+          citationTitle={citationTitle}
+          createTimestamp={create_timestamp}
+          doi_url={doi_url}
+          doi={doi}
+        />
+      )}
+      <Flex>
+        <LabelledSectionDate label="Creation Date" timestamp={create_timestamp} />
+        <LabelledSectionDate label="Modification Date" timestamp={last_modified_timestamp} />
+      </Flex>
     </SummaryPaper>
   );
 }
