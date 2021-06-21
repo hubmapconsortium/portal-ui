5612cbfdc009d8bc8f0df5975fb76910dc81119f
John Conroy <john-conroy@users.noreply.github.com>
2021-06-21 13:24:58 -0400

    Update Citation component

--- context/app/static/js/components/Detail/Citation/Citation.jsx
+++ context/app/static/js/components/Detail/Citation/Citation.jsx
@@ -2,51 +2,41 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Typography from '@material-ui/core/Typography';
 
-import { SecondaryBackgroundTooltip } from 'js/shared-styles/tooltips';
 import { LightBlueLink } from 'js/shared-styles/Links';
-import SectionContainer from 'js/shared-styles/sections/SectionContainer';
-import { FlexPaper, Flex, FlexRight, StyledInfoIcon, StyledSectionHeader } from './style';
-import OutboundLinkButton from '../../../shared-styles/Links/OutboundLinkButton';
+import LabelledSectionText from 'js/shared-styles/sections/LabelledSectionText';
+import OutboundLink from 'js/shared-styles/Links/OutboundLink';
 
-function buildNLMCitation({ contributors, title, timestamp }) {
+function buildNLMCitation({ contributors, citationTitle, createTimestamp }) {
   const joinedContributors = contributors
     .map(({ last_name, first_name }) => `${last_name} ${first_name[0]}`)
     .join(', ');
-  const year = new Date(timestamp).getFullYear();
-  return `${joinedContributors}. ${title} [Internet]. HuBMAP Consortium; ${year}.`;
+  const year = new Date(createTimestamp).getFullYear();
+  return `${joinedContributors}. ${citationTitle} [Internet]. HuBMAP Consortium; ${year}.`;
 }
 
-function Citation({ contributors, title, timestamp, url, doi }) {
-  const citation = buildNLMCitation({ contributors, title, timestamp });
+function Citation({ contributors, citationTitle, createTimestamp, doi_url, doi, className }) {
+  const citation = buildNLMCitation({ contributors, citationTitle, createTimestamp });
 
   return (
-    <SectionContainer id="citation">
-      <Flex>
-        <StyledSectionHeader>
-          Citation{' '}
-          <SecondaryBackgroundTooltip title="Citation is provided in NLM format. If DataCite page is available, click button to view alternate ways to cite.">
-            <StyledInfoIcon color="primary" />
-          </SecondaryBackgroundTooltip>
-        </StyledSectionHeader>
-        <FlexRight>
-          <OutboundLinkButton href={`https://search.datacite.org/works/${doi}`}>View DataCite Page</OutboundLinkButton>
-        </FlexRight>
-      </Flex>
-      <FlexPaper>
-        <Typography variant="body1">
-          {citation} Available from: <LightBlueLink href={url}>{url}</LightBlueLink>
-        </Typography>
-      </FlexPaper>
-    </SectionContainer>
+    <LabelledSectionText
+      label="Citation"
+      iconTooltipText="Citation is provided in NLM format. If DataCite page is available, click button to view alternate ways to cite."
+      className={className}
+    >
+      <Typography variant="body1">
+        {citation} Available from: <LightBlueLink href={doi_url}>{doi_url}</LightBlueLink>
+      </Typography>
+      <OutboundLink href={`https://search.datacite.org/works/${doi}`}>View DataCite Page</OutboundLink>
+    </LabelledSectionText>
   );
 }
 
 Citation.propTypes = {
   // eslint-disable-next-line react/forbid-prop-types
   contributors: PropTypes.array.isRequired,
-  title: PropTypes.string.isRequired,
-  timestamp: PropTypes.number.isRequired,
-  url: PropTypes.string.isRequired,
+  citationTitle: PropTypes.string.isRequired,
+  createTimestamp: PropTypes.number.isRequired,
+  doi_url: PropTypes.string.isRequired,
   doi: PropTypes.string.isRequired,
 };
 
