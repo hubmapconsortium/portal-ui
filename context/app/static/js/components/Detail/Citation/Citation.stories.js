d9ba270a09ae440b3a26b5cd67a80cb927115889
John Conroy <john-conroy@users.noreply.github.com>
2021-06-21 13:25:08 -0400

    Add citation story

---
+++ context/app/static/js/components/Detail/Citation/Citation.stories.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import Citation from './Citation';
+
+export default {
+  title: 'EntityDetail/Citation',
+  component: Citation,
+};
+
+const Template = (args) => <Citation {...args} />;
+export const Default = Template.bind({});
+Default.args = {
+  contributors: [
+    { last_name: 'Aanders', first_name: 'Aanne' },
+    { last_name: 'Banders', first_name: 'Banne' },
+    { last_name: 'Canders', first_name: 'Canne' },
+  ],
+  citationTitle: 'Something Science-y',
+  createTimestamp: 1520153805000,
+  doi: 'fakeDoi',
+  doi_url: 'fakeDoiUrl',
+};
