9cf65d28345bcabcba5bf80bf7f36547feac9800
John Conroy <john-conroy@users.noreply.github.com>
2021-06-21 16:22:29 -0400

    Just pass wrapped component props using rest

--- context/app/static/js/shared-styles/sections/LabelledSectionDate/LabelledSectionDate.jsx
+++ context/app/static/js/shared-styles/sections/LabelledSectionDate/LabelledSectionDate.jsx
@@ -4,24 +4,17 @@ import format from 'date-fns/format';
 
 import LabelledSectionText from '../LabelledSectionText';
 
-function LabelledSectionDate({ label, iconTooltipText, timestamp, dateFormat }) {
+function LabelledSectionDate({ timestamp, dateFormat, ...rest }) {
   const dateString = format(timestamp, dateFormat || 'yyyy-mm-dd');
-  return (
-    <LabelledSectionText iconTooltipText={iconTooltipText} label={label}>
-      {dateString}
-    </LabelledSectionText>
-  );
+  return <LabelledSectionText {...rest}>{dateString}</LabelledSectionText>;
 }
 
 LabelledSectionDate.propTypes = {
-  label: PropTypes.string.isRequired,
-  iconTooltipText: PropTypes.string,
   timestamp: PropTypes.number.isRequired,
   dateFormat: PropTypes.string,
 };
 
 LabelledSectionDate.defaultProps = {
-  iconTooltipText: undefined,
   dateFormat: undefined,
 };
 
