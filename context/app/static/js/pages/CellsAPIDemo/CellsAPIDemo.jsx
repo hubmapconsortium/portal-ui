46331d7ccaa963bdbd5687b48874cc77e5cfe5e1
Chuck McCallum <mccalluc@users.noreply.github.com>
2021-06-24 14:07:59 -0400

    factor out repeated react code (#1941)
    * factor out repeated react code
    
    * whitespace

--- context/app/static/js/pages/CellsAPIDemo/CellsAPIDemo.jsx
+++ context/app/static/js/pages/CellsAPIDemo/CellsAPIDemo.jsx
@@ -8,56 +8,60 @@ import CellPercentagesForDatasets from './CellPercentagesForDatasets';
 import CellExpressionInDataset from './CellExpressionInDataset';
 import DatasetsSelectedByGene from './DatasetsSelectedByGene';
 
+function About(props) {
+  const { title, given, returns } = props;
+  return (
+    <>
+      <SectionHeader>{title}</SectionHeader>
+      <p>
+        <b>Given</b> {given}
+      </p>
+      <p>
+        <b>Returns</b> {returns}
+      </p>
+    </>
+  );
+}
+
 // eslint-disable-next-line no-unused-vars
 function CellsAPIDemo(props) {
   return (
     <>
       <Alert severity="warning">UI is still being designed, but we want to demonstrate that the API works.</Alert>
 
-      <SectionHeader>Search genes by substring</SectionHeader>
-      <p>
-        <b>Given</b> a substring from a gene name.
-      </p>
-      <p>
-        <b>Return</b> a list of the first few matches.
-      </p>
+      <About
+        title="Search genes by substring"
+        given="a substring from a gene name"
+        returns="a list of the first few matches"
+      />
       <SearchBySubstring targetEntity="genes" />
 
-      <SectionHeader>Search proteins by substring</SectionHeader>
-      <p>
-        <b>Given</b> a substring from a protein name.
-      </p>
-      <p>
-        <b>Return</b> a list of the first few matches.
-      </p>
+      <About
+        title="Search proteins by substring"
+        given="a substring from a protein name"
+        returns="a list of the first few matches"
+      />
       <SearchBySubstring targetEntity="proteins" />
 
-      <SectionHeader>Datasets selected by gene</SectionHeader>
-      <p>
-        <b>Given</b> a gene, a minimum expression level, and a minimum percentage of cells at that expression level
-      </p>
-      <p>
-        <b>Return</b> a list of UUIDs for datasets which meet those minimums
-      </p>
+      <About
+        title="Datasets selected by gene"
+        given="a gene, a minimum expression level, and a minimum percentage of cells at that expression level"
+        returns="a list of UUIDs for datasets which meet those minimums"
+      />
       <DatasetsSelectedByGene />
 
-      <SectionHeader>Cell percentages for datasets</SectionHeader>
-      <p>
-        <b>Given</b> a list of dataset UUIDs, a gene, and a minimum expression level
-      </p>
-      <p>
-        <b>Return</b> for each UUID the percentage of cells above that minimum
-      </p>
-      <p>(Getting counts of cells for each dataset is feasible, but would require multiple API calls.)</p>
+      <About
+        title="Cell percentages for datasets"
+        given="a list of dataset UUIDs, a gene, and a minimum expression level"
+        returns="for each UUID the percentage of cells above that minimum"
+      />
       <CellPercentagesForDatasets />
 
-      <SectionHeader>Cell expression in dataset</SectionHeader>
-      <p>
-        <b>Given</b> a dataset UUID and a list of genes
-      </p>
-      <p>
-        <b>Return</b> the expression level of those genes for every cell in the dataset
-      </p>
+      <About
+        title="Cell expression in dataset"
+        given="a dataset UUID and a list of genes"
+        returns="the expression level of those genes for every cell in the dataset"
+      />
       <CellExpressionInDataset />
     </>
   );
