66fd628ca6fafaeb5fba05174587025ffdcdd9be
Chuck McCallum <mccalluc@users.noreply.github.com>
2021-06-24 18:05:15 -0400

    Mccalluc/get all genes autosuggest (#1938)
    * Searching return pre- and post- match
    
    * Search proteins, too
    
    * Update context/app/routes_cells.py
    
    * Add whitespace
    
    * fix typos
    
    Co-authored-by: ilan-gold <ilanbassgold@gmail.com>
    
    * stub autocomplete
    
    * set options
    
    * Get rid of results table
    
    * skip search for empty string
    
    * Add renderOption
    
    * changelog
    
    * Add a doctest
    
    Co-authored-by: ilan-gold <ilanbassgold@gmail.com>

--- context/app/static/js/pages/CellsAPIDemo/SearchBySubstring.jsx
+++ context/app/static/js/pages/CellsAPIDemo/SearchBySubstring.jsx
@@ -1,53 +1,62 @@
 import React, { useState } from 'react';
 
+import Autocomplete from '@material-ui/lab/Autocomplete';
 import Paper from '@material-ui/core/Paper';
 import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button';
-
-import ResultsTable from './ResultsTable';
 
 // eslint-disable-next-line no-unused-vars
 function SearchBySubstring(props) {
   const { targetEntity } = props;
 
   const [substring, setSubstring] = useState('');
+  const [options, setOptions] = useState([]);
 
-  const [results, setResults] = useState([]);
-  const [message, setMessage] = useState(null);
+  async function handleChange(event) {
+    const { target } = event;
+    setSubstring(target.value);
 
-  async function handleSubmit() {
+    if (target.value === '') {
+      setOptions([]);
+      return;
+    }
+
     const urlParams = new URLSearchParams();
-    urlParams.append('substring', substring);
+    urlParams.append('substring', target.value);
 
     const firstResponse = await fetch(`/cells/${targetEntity}-by-substring.json?${urlParams}`, {
       method: 'POST',
     });
     const responseJson = await firstResponse.json();
     if ('message' in responseJson) {
-      setMessage(responseJson.message);
+      console.warn(responseJson.message);
     }
     if ('results' in responseJson) {
-      setResults(responseJson.results);
+      setOptions(responseJson.results);
     }
-  }
-
-  function handleChange(event) {
-    const { target } = event;
-    const { name } = target;
-    const setFields = {
-      substring: setSubstring,
-    };
-    setFields[name](event.target.value);
   }
 
   return (
     <Paper>
-      <TextField label="substring" value={substring} name="substring" variant="outlined" onChange={handleChange} />
-      <br />
-      <Button onClick={handleSubmit}>Submit</Button>
-      <br />
-      {message}
-      <ResultsTable results={results} />
+      <Autocomplete
+        options={options}
+        renderOption={(option) => (
+          <>
+            {option.pre}
+            <b>{option.match}</b>
+            {option.post}
+          </>
+        )}
+        renderInput={(params) => (
+          <TextField
+            label="substring"
+            value={substring}
+            name="substring"
+            variant="outlined"
+            onChange={handleChange}
+            {...params}
+          />
+        )}
+      />
     </Paper>
   );
 }
