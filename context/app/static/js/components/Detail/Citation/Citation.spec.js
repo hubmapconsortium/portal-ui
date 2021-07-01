b23dae3783d6546ba218bd17254a5c9a6534b6cb
John Conroy <john-conroy@users.noreply.github.com>
2021-07-01 12:28:45 -0400

    Fix citation test

--- context/app/static/js/components/Detail/Citation/Citation.spec.js
+++ context/app/static/js/components/Detail/Citation/Citation.spec.js
@@ -11,8 +11,8 @@ const { Default } = composeStories(stories);
 const defaultCitation = 'Aanders A, Banders B, Canders C. Something Science-y [Internet]. HuBMAP Consortium; 2018.';
 
 test('builds NLM citation', () => {
-  const { contributors, citationTitle, createTimestamp } = Default.args;
-  expect(buildNLMCitation({ contributors, citationTitle, createTimestamp })).toEqual(defaultCitation);
+  const { contributors, citationTitle, create_timestamp } = Default.args;
+  expect(buildNLMCitation({ contributors, citationTitle, create_timestamp })).toEqual(defaultCitation);
 });
 
 test('Displays correct text', () => {
