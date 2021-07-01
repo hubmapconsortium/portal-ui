e4961da91460c640714e1576246f0de9468c3313
Roselkis Morla Adames <roselkismorlaadames@Roselkiss-MacBook-Air.local>
2021-07-01 15:29:21 -0400

    I use useSearchDate to create this table

--- context/app/static/js/pages/Vis/Vis.jsx
+++ context/app/static/js/pages/Vis/Vis.jsx
@@ -1,7 +1,92 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import Paper from '@material-ui/core/Paper';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+
+import { StyledTableContainer, HeaderCell } from 'js/shared-styles/Table';
+import { AppContext } from 'js/components/Providers';
+import useSearchData from 'js/hooks/useSearchData';
+
+/* JSON query
+ {
+     "size": 0,
+     "query": {
+         "bool": {
+             "filter": {
+                 "term": {
+                     "entity_type": "donor"
+                 }
+             }
+         }
+     },
+     "aggs": {
+         "mapped_metadata.sex": {
+             "terms": {
+                 "field": "mapped_metadata.sex.keyword"
+             }
+         }
+     }
+ }
+
+ */
+
+const donorSexQuery = {
+  size: 0,
+  query: {
+    bool: {
+      filter: {
+        term: {
+          entity_type: 'donor',
+        },
+      },
+    },
+  },
+  aggs: {
+    'mapped_metadata.race': {
+      terms: {
+        field: 'mapped_metadata.race.keyword',
+      },
+    },
+  },
+};
+
+// change to match the data
+const columns = [
+  { id: 'whie', label: 'White' },
+  { id: 'black', label: 'Black or African American' },
+];
 
 function Vis() {
-  return <div>Hello World</div>;
+  const { elasticsearchEndpoint, nexusToken } = useContext(AppContext);
+  // eslint-disable-next-line no-unused-vars
+  const { searchData } = useSearchData(donorSexQuery, elasticsearchEndpoint, nexusToken);
+
+  return (
+    Object.keys(searchData).length && (
+      <Paper>
+        <StyledTableContainer>
+          <Table stickyHeader>
+            <TableHead>
+              <TableRow>
+                {columns.map((column) => (
+                  <HeaderCell key={column.id}>{column.label}</HeaderCell>
+                ))}
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              <TableRow>
+                <TableCell> {searchData.aggregations['mapped_metadata.race'].buckets[0].doc_count}</TableCell>
+                <TableCell> {searchData.aggregations['mapped_metadata.race'].buckets[1].doc_count}</TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </StyledTableContainer>
+      </Paper>
+    )
+  );
 }
 
 export default Vis;
