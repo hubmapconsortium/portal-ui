2ea7f997a0d760feee347ae7033c30c7468fdf5c
John Conroy <john-conroy@users.noreply.github.com>
2021-07-14 11:48:17 -0400

    Add react ga events to search dataset tutorial store

--- context/app/static/js/stores/useSearchDatasetTutorialStore.js
+++ context/app/static/js/stores/useSearchDatasetTutorialStore.js
@@ -1,22 +1,46 @@
 import create from 'zustand';
+import ReactGA from 'react-ga';
 
 const localStorageKey = 'has_exited_dataset_search_tutorial';
 
-const useSearchDatasetTutorialStore = create((set) => ({
+const useSearchDatasetTutorialStore = create((set, get) => ({
   runSearchDatasetTutorial: false,
-  setRunSearchDatasetTutorial: (val) => set({ runSearchDatasetTutorial: val }),
+  setRunSearchDatasetTutorial: (val) => {
+    ReactGA.event({
+      category: 'Dataset Search Tutorial',
+      action: 'Clicked start button',
+    });
+    set({ runSearchDatasetTutorial: val });
+  },
   searchDatasetTutorialStep: 0,
-  incrementSearchDatasetTutorialStep: () =>
+  incrementSearchDatasetTutorialStep: () => {
+    ReactGA.event({
+      category: 'Dataset Search Tutorial',
+      action: 'Clicked next step',
+      value: get().searchDatasetTutorialStep,
+    });
     set((state) => ({
       searchDatasetTutorialStep: state.searchDatasetTutorialStep + 1,
-    })),
-  decrementSearchDatasetTutorialStep: () =>
+    }));
+  },
+  decrementSearchDatasetTutorialStep: () => {
+    ReactGA.event({
+      category: 'Dataset Search Tutorial',
+      action: 'Clicked previous step',
+      value: get().searchDatasetTutorialStep,
+    });
     set((state) => ({
       searchDatasetTutorialStep: state.searchDatasetTutorialStep - 1,
-    })),
+    }));
+  },
   tutorialHasExited: localStorage.getItem(localStorageKey),
   setTutorialHasExited: (val) => localStorage.setItem(localStorageKey, val),
   closeSearchDatasetTutorial: () => {
+    ReactGA.event({
+      category: 'Dataset Search Tutorial',
+      action: 'Clicked to close',
+      value: get().searchDatasetTutorialStep,
+    });
     localStorage.setItem(localStorageKey, true);
     set({ runSearchDatasetTutorial: false, tutorialHasExited: true });
   },
