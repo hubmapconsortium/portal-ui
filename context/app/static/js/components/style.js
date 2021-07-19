6729a081d5b51beb8c064944971a43ee0c0da389
mccalluc <mccalluc@users.noreply.github.com>
2021-07-19 10:51:55 -0400

    Add explicit z-index

--- context/app/static/js/components/style.js
+++ context/app/static/js/components/style.js
@@ -6,6 +6,7 @@ import { Alert } from 'js/shared-styles/alerts';
 const StyledAlert = styled(Alert)`
   width: ${(props) => props.theme.breakpoints.values.lg}px;
   margin-top: ${(props) => props.theme.spacing(3)}px;
+  z-index: 0; // Does not disaply on preview pages without this; Not sure sure why not.
 `;
 
 const FlexContainer = styled(Container)`
