4b908724063c24c9640e05201fd5e7b7a11ffe34
John Conroy <john-conroy@users.noreply.github.com>
2021-07-19 16:43:25 -0400

    Refactor store method

--- context/app/static/js/stores/useSearchDatasetTutorialStore.js
+++ context/app/static/js/stores/useSearchDatasetTutorialStore.js
@@ -3,32 +3,29 @@ import ReactGA from 'react-ga';
 
 const localStorageKey = 'has_exited_dataset_search_tutorial';
 
+function recordEvent(action, value) {
+  ReactGA.event({
+    category: 'Dataset Search Tutorial',
+    action,
+    value,
+  });
+}
+
 const useSearchDatasetTutorialStore = create((set, get) => ({
   runSearchDatasetTutorial: false,
   setRunSearchDatasetTutorial: (val) => {
-    ReactGA.event({
-      category: 'Dataset Search Tutorial',
-      action: 'Clicked start button',
-    });
+    recordEvent('Clicked start button');
     set({ runSearchDatasetTutorial: val });
   },
   searchDatasetTutorialStep: 0,
   incrementSearchDatasetTutorialStep: () => {
-    ReactGA.event({
-      category: 'Dataset Search Tutorial',
-      action: 'Clicked next step',
-      value: get().searchDatasetTutorialStep,
-    });
+    recordEvent('Clicked next step', get().searchDatasetTutorialStep);
     set((state) => ({
       searchDatasetTutorialStep: state.searchDatasetTutorialStep + 1,
     }));
   },
   decrementSearchDatasetTutorialStep: () => {
-    ReactGA.event({
-      category: 'Dataset Search Tutorial',
-      action: 'Clicked previous step',
-      value: get().searchDatasetTutorialStep,
-    });
+    recordEvent('Clicked previous step', get().searchDatasetTutorialStep);
     set((state) => ({
       searchDatasetTutorialStep: state.searchDatasetTutorialStep - 1,
     }));
@@ -36,11 +33,7 @@ const useSearchDatasetTutorialStore = create((set, get) => ({
   tutorialHasExited: localStorage.getItem(localStorageKey),
   setTutorialHasExited: (val) => localStorage.setItem(localStorageKey, val),
   closeSearchDatasetTutorial: () => {
-    ReactGA.event({
-      category: 'Dataset Search Tutorial',
-      action: 'Clicked to close',
-      value: get().searchDatasetTutorialStep,
-    });
+    recordEvent('Clicked to close', get().searchDatasetTutorialStep);
     localStorage.setItem(localStorageKey, true);
     set({ runSearchDatasetTutorial: false, tutorialHasExited: true });
   },
