9bbcd103ffc0256ee7c27ebab7e8c4e12b698695
mccalluc <mccalluc@users.noreply.github.com>
2021-07-19 10:00:31 -0400

    Maxwidth is set, but it is left aligned

--- context/app/static/js/components/style.js
+++ context/app/static/js/components/style.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 import { Alert } from 'js/shared-styles/alerts';
 
 const StyledAlert = styled(Alert)`
+  max-width: ${(props) => props.theme.breakpoints.values.lg}px;
   margin: ${(props) => props.theme.spacing(3)}px;
   margin-bottom: 0;
 `;
