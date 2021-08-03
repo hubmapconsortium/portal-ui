0b6007af3070486ca69c04282336b5f8e755f542
mccalluc <mccalluc@users.noreply.github.com>
2021-08-03 09:34:48 -0400

    Show the hover value as an exponent, too

--- context/app/static/js/components/cells/LogSliderWrapper/LogSliderWrapper.jsx
+++ context/app/static/js/components/cells/LogSliderWrapper/LogSliderWrapper.jsx
@@ -2,6 +2,14 @@ import React from 'react';
 
 import Slider from '@material-ui/core/Slider';
 
+function powerOf10(value) {
+  return (
+    <>
+      10<sup>{value}</sup>
+    </>
+  );
+}
+
 function LogSliderWrapper(props) {
   const { value, minLog, maxLog, setter, labelledby } = props;
   const marks = [...Array(1 + maxLog - minLog).keys()].map((k) => k + minLog);
@@ -12,13 +20,10 @@ function LogSliderWrapper(props) {
       max={maxLog}
       valueLabelDisplay="auto"
       step={null} /* Constrains choices to the mark values. */
+      valueLabelFormat={powerOf10}
       marks={marks.map((m) => ({
         value: m,
-        label: (
-          <>
-            10<sup>{m}</sup>
-          </>
-        ),
+        label: powerOf10(m),
       }))}
       onChange={(e, val) => {
         setter(val);
