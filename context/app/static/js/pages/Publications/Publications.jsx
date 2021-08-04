97571db457a920753e7b833e7fb877a45e98478b
mccalluc <mccalluc@users.noreply.github.com>
2021-08-04 11:11:34 -0400

    Fix styling

--- context/app/static/js/pages/Publications/Publications.jsx
+++ context/app/static/js/pages/Publications/Publications.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
 
+import { LightBlueLink } from 'js/shared-styles/Links';
 import SectionHeader from 'js/shared-styles/sections/SectionHeader';
 import SectionContainer from 'js/shared-styles/sections/SectionContainer';
+import SectionPaper from '../../shared-styles/sections/SectionPaper/SectionPaper';
 
 function Publication(props) {
   const { titles } = props;
@@ -14,13 +16,15 @@ function Publication(props) {
         <SectionHeader variant="h1" component="h1">
           Publications!
         </SectionHeader>
-        <ul>
-          {Object.entries(titles).map(([path, title]) => (
-            <li key={path}>
-              <a href={`publication/${path}`}>{title}</a>
-            </li>
-          ))}
-        </ul>
+        <SectionPaper>
+          <ul>
+            {Object.entries(titles).map(([path, title]) => (
+              <li key={path}>
+                <LightBlueLink href={`publication/${path}`}>{title}</LightBlueLink>
+              </li>
+            ))}
+          </ul>
+        </SectionPaper>
       </SectionContainer>
     </>
   );
