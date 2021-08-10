9f94e69a9731c04730761633338f012c44c2f37d
John Conroy <john-conroy@users.noreply.github.com>
2021-08-10 12:05:25 -0400

    Update jest config to handle react-pdf

--- context/jest.config.js
+++ context/jest.config.js
@@ -8,6 +8,7 @@ module.exports = {
     '^js/(.*)$': '<rootDir>/app/static/js/$1',
     '^test-utils/(.*)$': '<rootDir>/test-utils/$1',
     'metadata-field-descriptions$': '<rootDir>/ingest-validation-tools/docs/field-descriptions.yaml',
+    'react-pdf/dist/esm/entry.webpack': 'react-pdf/dist/umd/entry.jest',
   },
   transform: {
     '^.+\\.jsx?$': 'babel-jest',
