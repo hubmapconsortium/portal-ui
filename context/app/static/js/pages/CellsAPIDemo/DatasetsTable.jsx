fa524c1262b50844165cc738e4f8f81910a17b5b
John Conroy <john-conroy@users.noreply.github.com>
2021-08-16 15:07:17 -0400

    Add more metadata to cells api datasets table

--- context/app/static/js/pages/CellsAPIDemo/DatasetsTable.jsx
+++ context/app/static/js/pages/CellsAPIDemo/DatasetsTable.jsx
@@ -5,7 +5,21 @@ import TableBody from '@material-ui/core/TableBody';
 import TableRow from '@material-ui/core/TableRow';
 import { TableCell } from '@material-ui/core';
 
-const columns = [{ id: 'hubmap_id', label: 'HuBMAP ID' }];
+const columns = [
+  { id: 'hubmap_id', label: 'HuBMAP ID' },
+  { id: 'origin_sample.mapped_organ', label: 'Organ' },
+  { id: 'mapped_data_types', label: 'Mapped Data Types' },
+  { id: 'donor.mapped_metadata.age_value', label: 'Donor Age' },
+  { id: 'donor.mapped_metadata.body_mass_index_value', label: 'Donor BMI' },
+  { id: 'donor.mapped_metadata.sex', label: 'Donor Sex' },
+  { id: 'donor.mapped_metadata.race', label: 'Donor Race' },
+
+  { id: 'last_modified_timestamp', label: 'Last Modified' },
+];
+
+function DonorMetadataCell({ unit, value }) {
+  return <TableCell>{`${value} ${unit}`}</TableCell>;
+}
 
 function DatasetsTable({ datasets }) {
   return (
@@ -21,6 +35,17 @@ function DatasetsTable({ datasets }) {
         {datasets.map(({ _source }) => (
           <TableRow key={_source.hubmap_id}>
             <TableCell>{_source.hubmap_id}</TableCell>
+            <TableCell>{_source.origin_sample.mapped_organ}</TableCell>
+            <TableCell>{_source.mapped_data_types.join(', ')}</TableCell>
+            {['age', 'body_mass_index'].map((base) => (
+              <DonorMetadataCell
+                value={_source.donor.mapped_metadata[`${base}_value`]}
+                unit={_source.donor.mapped_metadata[`${base}_unit`]}
+              />
+            ))}
+            <TableCell>{_source.donor.mapped_metadata.sex}</TableCell>
+            <TableCell>{_source.donor.mapped_metadata.race.join(', ')}</TableCell>
+            <TableCell>{_source.last_modified_timestamp}</TableCell>
           </TableRow>
         ))}
       </TableBody>
