28eb83a7f75d2008abf20f11d0790059ee7095ba
John Conroy <john-conroy@users.noreply.github.com>
2021-08-19 12:29:19 -0400

    Remove comment

--- context/app/static/js/shared-styles/Table/ExpandableRow/store.js
+++ context/app/static/js/shared-styles/Table/ExpandableRow/store.js
@@ -1,8 +1,6 @@
 import create from 'zustand';
 import createContext from 'zustand/context';
 
-// Best practice: You can move the below createContext() and createStore to a separate file(store.js) and import the Provider, useStore here/wherever you need.
-
 const { Provider, useStore } = createContext();
 
 const createStore = () =>
