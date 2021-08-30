5439c06701288504f83e7ee929c169774c7578c0
mccalluc <mccalluc@users.noreply.github.com>
2021-08-30 15:45:10 -0400

    stub subsections

--- context/app/static/js/pages/Organ/Organ.jsx
+++ context/app/static/js/pages/Organ/Organ.jsx
@@ -1,9 +1,35 @@
 import React from 'react';
 
+import Paper from '@material-ui/core/Paper';
+
+import SectionHeader from 'js/shared-styles/sections/SectionHeader';
+import SectionContainer from 'js/shared-styles/sections/SectionContainer';
+
 function Organ(props) {
   const { organ } = props;
 
-  return <pre>{JSON.stringify(organ, null, 2)}</pre>;
+  const shouldDisplaySection = {
+    description: Boolean(organ?.description),
+    asctb: Boolean(organ?.asctb),
+    azimuth: Boolean(organ?.azimuth),
+    assays: true,
+    samples: true,
+  };
+
+  return (
+    <SectionContainer>
+      <SectionHeader variant="h1" component="h1">
+        {organ.title}
+      </SectionHeader>
+      <Paper>
+        {shouldDisplaySection.description && <>TODO: description</>}
+        {shouldDisplaySection.asctb && <>TODO: asctb</>}
+        {shouldDisplaySection.azimuth && <>TODO: azimuth</>}
+        {shouldDisplaySection.assays && <>TODO: assays</>}
+        {shouldDisplaySection.samples && <>TODO: samples</>}
+      </Paper>
+    </SectionContainer>
+  );
 }
 
 export default Organ;
