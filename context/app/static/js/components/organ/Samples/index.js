4a155008541d3c878dc74ceddbe3c30e3894e439
Chuck McCallum <mccalluc@users.noreply.github.com>
2021-08-31 10:23:06 -0400

    Mccalluc/stub organ detail (#2110)
    * stub subsections
    
    * changelog
    
    * Skeleton for organ detail pages

---
+++ context/app/static/js/components/organ/Samples/index.js
@@ -0,0 +1,3 @@
+import Samples from './Samples';
+
+export default Samples;
--- context/app/static/js/pages/Organ/Organ.jsx
+++ context/app/static/js/pages/Organ/Organ.jsx
@@ -1,9 +1,35 @@
 import React from 'react';
 
+import SectionHeader from 'js/shared-styles/sections/SectionHeader';
+import Asctb from 'js/components/organ/Asctb';
+import Azimuth from 'js/components/organ/Azimuth';
+import Assays from 'js/components/organ/Assays';
+import Description from 'js/components/organ/Description';
+import Samples from 'js/components/organ/Samples';
+
 function Organ(props) {
   const { organ } = props;
 
-  return <pre>{JSON.stringify(organ, null, 2)}</pre>;
+  const shouldDisplaySection = {
+    description: Boolean(organ?.description),
+    asctb: Boolean(organ?.asctb),
+    azimuth: Boolean(organ?.azimuth),
+    assays: true,
+    samples: true,
+  };
+
+  return (
+    <>
+      <SectionHeader variant="h1" component="h1">
+        {organ.title}
+      </SectionHeader>
+      {shouldDisplaySection.description && <Description description={organ.description} />}
+      {shouldDisplaySection.asctb && <Asctb asctb={organ.asctb} />}
+      {shouldDisplaySection.azimuth && <Azimuth description={organ.azimuth} />}
+      {shouldDisplaySection.assays && <Assays title={organ.title} />}
+      {shouldDisplaySection.samples && <Samples title={organ.title} />}
+    </>
+  );
 }
 
 export default Organ;
