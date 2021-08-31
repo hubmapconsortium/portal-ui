66e3c276bf1a18c844623a7636aa69d1ba506ce8
mccalluc <mccalluc@users.noreply.github.com>
2021-08-31 13:12:02 -0400

    Add CSS from CDN

--- context/app/static/js/pages/LineUpPage/LineUpPage.jsx
+++ context/app/static/js/pages/LineUpPage/LineUpPage.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import LineUp from 'lineupjsx';
+import Paper from '@material-ui/core/Paper';
 
 import SectionHeader from 'js/shared-styles/sections/SectionHeader';
 import SectionContainer from 'js/shared-styles/sections/SectionContainer';
@@ -9,14 +10,14 @@ function LineUpPage(props) {
 
   return (
     <SectionContainer>
+      <link href="https://unpkg.com/lineupjsx/build/LineUpJSx.css" rel="stylesheet" />
       {/* <Typography variant="subtitle1">Subtitle</Typography> */}
       <SectionHeader variant="h1" component="h1">
         LineUp
       </SectionHeader>
-      {/* <StyledDescription>
-        Describe?
-      </StyledDescription> */}
-      <LineUp data={entities} />
+      <Paper>
+        <LineUp data={entities} />
+      </Paper>
     </SectionContainer>
   );
 }
