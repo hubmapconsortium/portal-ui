c6db68ad2a0c7c781cb2c20699554b85c2ea59f2
mccalluc <mccalluc@users.noreply.github.com>
2021-09-02 15:29:35 -0400

    comment out publications until they are ready

--- context/app/static/js/components/Header/ResourceLinks/ResourceLinks.jsx
+++ context/app/static/js/components/Header/ResourceLinks/ResourceLinks.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import DropdownLink from '../DropdownLink';
-import { StyledDivider } from '../HeaderContent/style';
+// import { StyledDivider } from '../HeaderContent/style';
 
 function ResourceLinks(props) {
   const { isIndented } = props;
   return (
     <>
-      <DropdownLink href="/publication" isIndented={isIndented}>
+      {/* <DropdownLink href="/publication" isIndented={isIndented}>
         Publications
       </DropdownLink>
-      <StyledDivider />
+      <StyledDivider /> */}
       {['Multimodal Molecular Imaging Data', 'Cell Type Annotations'].map((previewName) => (
         <DropdownLink
           key={previewName}
