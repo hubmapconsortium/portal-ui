550cbc5a1f88ec3b43e5e986023c088ee17755cb
mccalluc <mccalluc@users.noreply.github.com>
2021-09-03 18:02:51 -0400

    /iframe/assay-barchart

--- context/app/static/js/pages/Iframe/Iframe.jsx
+++ context/app/static/js/pages/Iframe/Iframe.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import Providers from 'js/components/Providers';
 import EntityCounts from 'js/components/home/EntityCounts';
+import AssayTypeBarChartContainer from 'js/components/home/AssayTypeBarChartContainer';
 
 function Switch() {
   const { pathname } = window.location;
@@ -10,7 +11,7 @@ function Switch() {
     case '/iframe/entity-counts':
       return <EntityCounts />;
     case '/iframe/assay-barchart':
-      return 'TODO: assay-barchart';
+      return <AssayTypeBarChartContainer />;
     default:
       console.error('No such iframe');
       return 'ERROR: No such iframe';
@@ -21,6 +22,7 @@ function Iframe(props) {
   const { flaskData } = props;
   return (
     <Providers endpoints={flaskData.endpoints}>
+      <base target="_parent" /> {/* Set base to make sure links load in the parent of the iframe. */}
       <Switch />
     </Providers>
   );
