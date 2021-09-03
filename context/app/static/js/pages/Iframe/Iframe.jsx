77ab22c05ba3bba1ac59797045ec4962183f28fe
mccalluc <mccalluc@users.noreply.github.com>
2021-09-03 17:24:53 -0400

    pass flask data

--- context/app/static/js/pages/Iframe/Iframe.jsx
+++ context/app/static/js/pages/Iframe/Iframe.jsx
@@ -1,17 +1,29 @@
-// import React from 'react';
+import React from 'react';
 
-function Iframe() {
+import Providers from 'js/components/Providers';
+import EntityCounts from 'js/components/home/EntityCounts';
+
+function Switch() {
   const { pathname } = window.location;
 
   switch (pathname) {
     case '/iframe/entity-counts':
-      return 'TODO: entity-counts';
+      return <EntityCounts />;
     case '/iframe/assay-barchart':
       return 'TODO: assay-barchart';
     default:
       console.error('No such iframe');
       return 'ERROR: No such iframe';
   }
+}
+
+function Iframe(props) {
+  const { flaskData } = props;
+  return (
+    <Providers endpoints={flaskData.endpoints}>
+      <Switch />
+    </Providers>
+  );
 }
 
 export default Iframe;
