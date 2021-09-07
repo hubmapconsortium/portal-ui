6a81ec24d4ae0762a07e2065c5225d66967e0d09
mccalluc <mccalluc@users.noreply.github.com>
2021-09-07 11:28:46 -0400

    Remove extra sectioncontainer

--- context/app/static/js/pages/LineUpPage/LineUpPage.jsx
+++ context/app/static/js/pages/LineUpPage/LineUpPage.jsx
@@ -4,20 +4,19 @@ import 'lineupjsx/build/LineUpJSx.css';
 import Paper from '@material-ui/core/Paper';
 
 import SectionHeader from 'js/shared-styles/sections/SectionHeader';
-import SectionContainer from 'js/shared-styles/sections/SectionContainer';
 
 function LineUpPage(props) {
   const { entities } = props;
 
   return (
-    <SectionContainer>
+    <>
       <SectionHeader variant="h1" component="h1">
         LineUp
       </SectionHeader>
       <Paper>
         <LineUp data={entities} />
       </Paper>
-    </SectionContainer>
+    </>
   );
 }
 
