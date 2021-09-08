c7c90f30186ec0a5f09265b1e17dc7ef109d61e3
mccalluc <mccalluc@users.noreply.github.com>
2021-09-08 14:52:29 -0400

    section containers: space between info box and list

--- context/app/static/js/pages/Publications/Publications.jsx
+++ context/app/static/js/pages/Publications/Publications.jsx
@@ -9,6 +9,7 @@ import TableRow from '@material-ui/core/TableRow';
 
 import { LightBlueLink } from 'js/shared-styles/Links';
 import SectionHeader from 'js/shared-styles/sections/SectionHeader';
+import SectionContainer from 'js/shared-styles/sections/SectionContainer';
 import Description from 'js/shared-styles/sections/Description';
 
 function Publications(props) {
@@ -19,33 +20,37 @@ function Publications(props) {
       <SectionHeader variant="h1" component="h1">
         Publications
       </SectionHeader>
-      <Description>
-        The following publications are a partial list of published HuBMAP research that uses data available through the
-        HuBMAP Data Portal. The full list of HuBMAP-funded publications is available on{' '}
-        <LightBlueLink href="https://scholar.google.com/citations?user=CtGSN80AAAAJ&hl=en">
-          Google Scholar
-        </LightBlueLink>
-        .
-      </Description>
-      <Paper>
-        <Table>
-          <TableBody>
-            {Object.entries(publications || [])
-              // eslint-disable-next-line no-unused-vars
-              .filter(([path, publication]) => publication?.is_public)
-              .map(([path, publication]) => (
-                <TableRow key={path}>
-                  <TableCell>
-                    <Typography variant="subtitle2" component="h3" color="primary">
-                      <LightBlueLink href={`/publication/${path}`}>{publication.title}</LightBlueLink>
-                    </Typography>
-                    {publication.authors}
-                  </TableCell>
-                </TableRow>
-              ))}
-          </TableBody>
-        </Table>
-      </Paper>
+      <SectionContainer>
+        <Description>
+          The following publications are a partial list of published HuBMAP research that uses data available through
+          the HuBMAP Data Portal. The full list of HuBMAP-funded publications is available on{' '}
+          <LightBlueLink href="https://scholar.google.com/citations?user=CtGSN80AAAAJ&hl=en">
+            Google Scholar
+          </LightBlueLink>
+          .
+        </Description>
+      </SectionContainer>
+      <SectionContainer>
+        <Paper>
+          <Table>
+            <TableBody>
+              {Object.entries(publications || [])
+                // eslint-disable-next-line no-unused-vars
+                .filter(([path, publication]) => publication?.is_public)
+                .map(([path, publication]) => (
+                  <TableRow key={path}>
+                    <TableCell>
+                      <Typography variant="subtitle2" component="h3" color="primary">
+                        <LightBlueLink href={`/publication/${path}`}>{publication.title}</LightBlueLink>
+                      </Typography>
+                      {publication.authors}
+                    </TableCell>
+                  </TableRow>
+                ))}
+            </TableBody>
+          </Table>
+        </Paper>
+      </SectionContainer>
     </>
   );
 }
