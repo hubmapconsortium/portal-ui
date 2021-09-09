41cde5f59f8fb66f1722fc16fe1d334a25798009
John Conroy <john-conroy@users.noreply.github.com>
2021-09-09 11:10:20 -0400

    Remove demo cells components

--- context/app/static/js/pages/CellsAPIDemo/CellsAPIDemo.jsx
+++ context/app/static/js/pages/CellsAPIDemo/CellsAPIDemo.jsx
@@ -3,11 +3,7 @@ import React from 'react';
 import SectionHeader from 'js/shared-styles/sections/SectionHeader';
 import { Alert } from 'js/shared-styles/alerts';
 
-import SearchBySubstring from './SearchBySubstring';
-import CellPercentagesForDatasets from './CellPercentagesForDatasets';
-import CellExpressionInDataset from './CellExpressionInDataset';
 import DatasetsSelectedByExpression from './DatasetsSelectedByExpression';
-import AllIndexedUUIDs from './AllIndexedUUIDs';
 
 function About(props) {
   const { title, given, returns } = props;
@@ -24,42 +20,16 @@ function About(props) {
   );
 }
 
-// eslint-disable-next-line no-unused-vars
-function CellsAPIDemo(props) {
+function CellsAPIDemo() {
   return (
     <>
       <Alert severity="warning">UI is still being designed, but we want to demonstrate that the API works.</Alert>
-
-      <About
-        title="Search proteins by substring"
-        given="a substring from a protein name"
-        returns="a list of the first few matches"
-      />
-      <SearchBySubstring targetEntity="proteins" />
-
       <About
         title="Datasets selected by gene expression"
         given="a list of genes, a minimum expression level, and a minimum percentage of cells at that expression level"
         returns="a list of UUIDs for datasets which meet those minimums"
       />
       <DatasetsSelectedByExpression />
-
-      <About
-        title="Cell percentages for datasets"
-        given="a list of dataset UUIDs, a gene, and a minimum expression level"
-        returns="for each UUID the percentage of cells above that minimum"
-      />
-      <CellPercentagesForDatasets />
-
-      <About
-        title="Cell expression in dataset"
-        given="a dataset UUID and a list of genes"
-        returns="the expression level of those genes for every cell in the dataset"
-      />
-      <CellExpressionInDataset />
-
-      <About title="All indexed UUIDs" given="nothing" returns="all UUIDs that have been indexed by the Cells API" />
-      <AllIndexedUUIDs />
     </>
   );
 }
