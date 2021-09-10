fab0f2d5472a3c3bea511485375e546a5b1431ba
mccalluc <mccalluc@users.noreply.github.com>
2021-09-10 13:25:52 -0400

    tooltip

--- context/app/static/js/components/Search/DownloadButton/DownloadButton.jsx
+++ context/app/static/js/components/Search/DownloadButton/DownloadButton.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 
+import { SecondaryBackgroundTooltip } from 'js/shared-styles/tooltips';
+
 import { StyledButton } from './style';
 
 function DownloadButton(props) {
   const { type } = props;
   const lcPluralType = `${type.toLowerCase()}s`;
   return (
-    <StyledButton href={`/api/v0/${lcPluralType}.tsv`} target="_blank" component="a">
-      Download Metadata
-    </StyledButton>
+    <SecondaryBackgroundTooltip title="Download a TSV of the table metadata. This will not take into account any filtering done on the search page.">
+      <StyledButton href={`/api/v0/${lcPluralType}.tsv`} target="_blank" component="a">
+        Download Metadata
+      </StyledButton>
+    </SecondaryBackgroundTooltip>
   );
 }
 
