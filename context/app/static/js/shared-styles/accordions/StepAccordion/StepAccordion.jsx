12f388c5572c98875ceda9ca44df9894a6b35ef0
John Conroy <john-conroy@users.noreply.github.com>
2021-09-13 13:27:46 -0400

    Clone element to pass setter function for accordion text by default

--- context/app/static/js/shared-styles/accordions/StepAccordion/StepAccordion.jsx
+++ context/app/static/js/shared-styles/accordions/StepAccordion/StepAccordion.jsx
@@ -1,13 +1,15 @@
 import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 import Accordion from '@material-ui/core/ExpansionPanel';
 import AccordionDetails from '@material-ui/core/ExpansionPanelDetails';
 import Typography from '@material-ui/core/Typography';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
-import { AccordionSummaryHeading, AccordionSummaryText, StyledAccordionSummary } from './style';
+import { AccordionSummaryHeading, AccordionText, Flex, StyledAccordionSummary, SuccessIcon } from './style';
 
-function StepAccordion({ summaryHeading, summaryText, content }) {
+function StepAccordion({ summaryHeading, content }) {
   const [isExpanded, setIsExpanded] = useState(false);
+  const [stepCompletedText, setStepCompletedText] = useState(null);
 
   function handleExpand(event, expanded) {
     setIsExpanded(expanded);
@@ -18,15 +20,30 @@ function StepAccordion({ summaryHeading, summaryText, content }) {
         <AccordionSummaryHeading variant="subtitle2" $isExpanded={isExpanded}>
           {summaryHeading}
         </AccordionSummaryHeading>
-        <AccordionSummaryText variant="body2" $isExpanded={isExpanded}>
-          {summaryText}
-        </AccordionSummaryText>
+        <Flex>
+          {stepCompletedText && (
+            <>
+              <AccordionText variant="body2" $isExpanded={isExpanded}>
+                {stepCompletedText}
+              </AccordionText>
+              <SuccessIcon />
+            </>
+          )}
+        </Flex>
       </StyledAccordionSummary>
       <AccordionDetails>
-        <Typography>{content}</Typography>
+        <Typography>
+          {React.cloneElement(content, {
+            setStepCompletedText,
+          })}
+        </Typography>
       </AccordionDetails>
     </Accordion>
   );
 }
 
+StepAccordion.propTypes = {
+  summaryHeading: PropTypes.string.isRequired,
+  content: PropTypes.element.isRequired,
+};
 export default StepAccordion;
--- context/app/static/js/shared-styles/accordions/StepAccordion/style.js
+++ context/app/static/js/shared-styles/accordions/StepAccordion/style.js
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 import Typography from '@material-ui/core/Typography';
 import AccordionSummary from '@material-ui/core/ExpansionPanelSummary';
+import CheckCircleRoundedIcon from '@material-ui/icons/CheckCircleRounded';
 
 const AccordionText = styled(Typography)`
   color: ${(props) => (props.$isExpanded ? '#fff' : props.theme.palette.text.primary)};
@@ -11,12 +12,18 @@ const AccordionSummaryHeading = styled(AccordionText)`
   flex-shrink: 0;
 `;
 
-const AccordionSummaryText = styled(AccordionText)`
-  flex-grow: 3;
-`;
-
 const StyledAccordionSummary = styled(AccordionSummary)`
   background-color: ${(props) => (props.$isExpanded ? props.theme.palette.secondary.main : '#E0E0E0')};
 `;
 
-export { AccordionSummaryHeading, AccordionSummaryText, StyledAccordionSummary };
+const SuccessIcon = styled(CheckCircleRoundedIcon)`
+  color: ${(props) => props.theme.palette.success.main};
+`;
+
+const Flex = styled.div`
+  flex-grow: 4;
+  display: flex;
+  justify-content: space-between;
+`;
+
+export { AccordionSummaryHeading, AccordionText, StyledAccordionSummary, SuccessIcon, Flex };
