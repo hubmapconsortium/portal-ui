9677516e4c9c72ef2f54aea25944ab518305d9b9
John Conroy <john-conroy@users.noreply.github.com>
2021-09-13 14:28:35 -0400

    Allow step accordion to be disabled

--- context/app/static/js/shared-styles/accordions/StepAccordion/StepAccordion.jsx
+++ context/app/static/js/shared-styles/accordions/StepAccordion/StepAccordion.jsx
@@ -2,12 +2,11 @@ import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import Accordion from '@material-ui/core/ExpansionPanel';
 import AccordionDetails from '@material-ui/core/ExpansionPanelDetails';
-import Typography from '@material-ui/core/Typography';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
 import { AccordionSummaryHeading, AccordionText, Flex, StyledAccordionSummary, SuccessIcon } from './style';
 
-function StepAccordion({ summaryHeading, content }) {
+function StepAccordion({ summaryHeading, content, disabled }) {
   const [isExpanded, setIsExpanded] = useState(false);
   const [stepCompletedText, setStepCompletedText] = useState(null);
 
@@ -15,7 +14,7 @@ function StepAccordion({ summaryHeading, content }) {
     setIsExpanded(expanded);
   }
   return (
-    <Accordion onChange={handleExpand}>
+    <Accordion onChange={handleExpand} disabled={disabled}>
       <StyledAccordionSummary expandIcon={<ExpandMoreIcon />} $isExpanded={isExpanded}>
         <AccordionSummaryHeading variant="subtitle2" $isExpanded={isExpanded}>
           {summaryHeading}
@@ -32,11 +31,10 @@ function StepAccordion({ summaryHeading, content }) {
         </Flex>
       </StyledAccordionSummary>
       <AccordionDetails>
-        <Typography>
-          {React.cloneElement(content, {
+        {content &&
+          React.cloneElement(content, {
             setStepCompletedText,
           })}
-        </Typography>
       </AccordionDetails>
     </Accordion>
   );
@@ -45,5 +43,11 @@ function StepAccordion({ summaryHeading, content }) {
 StepAccordion.propTypes = {
   summaryHeading: PropTypes.string.isRequired,
   content: PropTypes.element.isRequired,
+  disabled: PropTypes.element,
 };
+
+StepAccordion.defaultProps = {
+  disabled: false,
+};
+
 export default StepAccordion;
