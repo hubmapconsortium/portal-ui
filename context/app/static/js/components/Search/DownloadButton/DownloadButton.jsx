25eddbe77dcab62a7c7771b66d0676a1f62e12e3
mccalluc <mccalluc@users.noreply.github.com>
2021-09-13 09:35:29 -0400

    log pageview with ReactGA

--- context/app/static/js/components/Search/DownloadButton/DownloadButton.jsx
+++ context/app/static/js/components/Search/DownloadButton/DownloadButton.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
+import ReactGA from 'react-ga';
 
 import { SecondaryBackgroundTooltip } from 'js/shared-styles/tooltips';
 
 import { StyledButton } from './style';
+
+function sendTsvEvent(event) {
+  ReactGA.pageview(event.currentTarget.href);
+}
 
 function DownloadButton(props) {
   const { type } = props;
   const lcPluralType = `${type.toLowerCase()}s`;
   return (
     <SecondaryBackgroundTooltip title="Download a TSV of the table metadata. This will not take into account any filtering done on the search page.">
-      <StyledButton href={`/api/v0/${lcPluralType}.tsv`} target="_blank" component="a">
+      <StyledButton href={`/api/v0/${lcPluralType}.tsv`} target="_blank" component="a" onClick={sendTsvEvent}>
         Download Metadata
       </StyledButton>
     </SecondaryBackgroundTooltip>
