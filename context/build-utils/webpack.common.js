2d616bc462ffc39f7dbd428203aa10dd9833e049
John Conroy <62477388+john-conroy@users.noreply.github.com>
2021-09-14 10:00:58 -0400

    Update context/build-utils/webpack.common.js
    Co-authored-by: Chuck McCallum <mccalluc@users.noreply.github.com>

--- context/build-utils/webpack.common.js
+++ context/build-utils/webpack.common.js
@@ -31,7 +31,7 @@ const config = {
         test: /\.js$/,
         use: ['source-map-loader'],
         enforce: 'pre',
-        exclude: [/node_modules\/lineupjs[x]?/],
+        exclude: [/node_modules\/lineupjsx?/],
       },
       {
         test: /\.(js|jsx)$/,
