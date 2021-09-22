0057540c325390acf3f8d123a248859b238068e8
mccalluc <mccalluc@users.noreply.github.com>
2021-09-22 15:35:52 -0400

    styled paper

--- context/app/static/js/pages/Publication/Publication.jsx
+++ context/app/static/js/pages/Publication/Publication.jsx
@@ -4,19 +4,18 @@ import Typography from '@material-ui/core/Typography';
 import Markdown from 'js/components/Markdown';
 import VisualizationWrapper from 'js/components/Detail/visualization/VisualizationWrapper';
 import SectionHeader from 'js/shared-styles/sections/SectionHeader';
-import SectionContainer from 'js/shared-styles/sections/SectionContainer';
+import { StyledPaper } from './style';
 
 function Publication(props) {
   const { vitData, title, markdown } = props;
 
   return (
     <>
-      <SectionContainer>
-        <Typography variant="subtitle1">Publication</Typography>
-        <SectionHeader variant="h1" component="h1">
-          {title}
-        </SectionHeader>
-      </SectionContainer>
+      <Typography variant="subtitle1">Publication</Typography>
+      <SectionHeader variant="h1" component="h1">
+        {title}
+      </SectionHeader>
+      <StyledPaper>TODO</StyledPaper>
       {Boolean(vitData) && <VisualizationWrapper vitData={vitData} />}
       <Markdown markdown={markdown} />
     </>
