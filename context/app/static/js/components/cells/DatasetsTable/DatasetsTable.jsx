730428739af43ea0fcebc70a6b897ec67ec4419c
John Conroy <john-conroy@users.noreply.github.com>
2021-09-23 12:32:09 -0400

    Add height transition

--- context/app/static/js/components/cells/DatasetsTable/DatasetsTable.jsx
+++ context/app/static/js/components/cells/DatasetsTable/DatasetsTable.jsx
@@ -1,10 +1,11 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 import Table from '@material-ui/core/Table';
 import TableHead from '@material-ui/core/TableHead';
 import TableBody from '@material-ui/core/TableBody';
 import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
 import { useTransition, animated, config } from 'react-spring';
+import useResizeObserver from 'use-resize-observer/polyfilled';
 
 import DatasetTableRow from 'js/components/cells/DatasetTableRow';
 
@@ -20,43 +21,47 @@ const columns = [
   { id: 'expand', label: '' },
 ];
 
-const AnimatedTable = animated(Table);
-
 function DatasetsTable({ datasets, minExpression, cellVariableName, queryType, completeStep }) {
   useEffect(() => {
     completeStep(`${datasets.length} Datasets Matching Query Parameters`);
   }, [completeStep, datasets]);
+  const heightRef = useRef(null);
+
+  const { height = 0 } = useResizeObserver({ ref: heightRef });
 
   const transitions = useTransition(true, null, {
-    from: { opacity: 0 },
-    enter: { opacity: 1 },
-    config: config.molasses,
+    from: { opacity: 0, height: 300 },
+    enter: { opacity: 1, height },
+    config: config.slow,
+    update: { height },
   });
 
   return transitions.map(
     ({ item, key, props }) =>
       item && (
-        <AnimatedTable stickyHeader key={key} style={props}>
-          <TableHead>
-            <TableRow>
-              {columns.map((column) => (
-                <TableCell key={column.id}>{column.label}</TableCell>
+        <animated.div key={key} style={props}>
+          <Table stickyHeader ref={heightRef}>
+            <TableHead>
+              <TableRow>
+                {columns.map((column) => (
+                  <TableCell key={column.id}>{column.label}</TableCell>
+                ))}
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {datasets.map(({ _source }) => (
+                <DatasetTableRow
+                  datasetMetadata={_source}
+                  numCells={columns.length}
+                  key={_source.hubmap_id}
+                  minExpression={minExpression}
+                  cellVariableName={cellVariableName}
+                  queryType={queryType}
+                />
               ))}
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {datasets.map(({ _source }) => (
-              <DatasetTableRow
-                datasetMetadata={_source}
-                numCells={columns.length}
-                key={_source.hubmap_id}
-                minExpression={minExpression}
-                cellVariableName={cellVariableName}
-                queryType={queryType}
-              />
-            ))}
-          </TableBody>
-        </AnimatedTable>
+            </TableBody>
+          </Table>
+        </animated.div>
       ),
   );
 }
--- context/app/static/js/pages/Cells/style.js
+++ context/app/static/js/pages/Cells/style.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 const CenteredFlex = styled.div`
   width: 100%;
-  height: 300px;
+  min-height: 300px;
   display: flex;
   justify-content: center;
   align-items: center;
