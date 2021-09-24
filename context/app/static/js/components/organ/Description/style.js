008a449a9c60793fa58af3fcadd1bf1cd3a76118
Chuck McCallum <mccalluc@users.noreply.github.com>
2021-09-24 10:04:28 -0400

    Update context/app/static/js/components/organ/Description/style.js
    Co-authored-by: John Conroy <62477388+john-conroy@users.noreply.github.com>

--- context/app/static/js/components/organ/Description/style.js
+++ context/app/static/js/components/organ/Description/style.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 import Paper from '@material-ui/core/Paper';
 
 const StyledPaper = styled(Paper)`
-  padding: 20px 40px 20px 40px;
+  padding: 20px 40px;
 `;
 
 export { StyledPaper };
