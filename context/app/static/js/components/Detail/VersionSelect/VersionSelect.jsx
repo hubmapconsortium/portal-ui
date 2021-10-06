2bbfa85bc8bbfbaf01495768fea3a2a0d90fdf27
John Conroy <john-conroy@users.noreply.github.com>
2021-10-06 14:37:58 -0400

    Add basic version select component

---
+++ context/app/static/js/components/Detail/VersionSelect/VersionSelect.jsx
@@ -0,0 +1,56 @@
+import React, { useEffect, useState, useContext } from 'react';
+
+import Button from '@material-ui/core/Button';
+import DropdownListbox from 'js/shared-styles/dropdowns/DropdownListbox';
+import DropdownListboxOption from 'js/shared-styles/dropdowns/DropdownListboxOption';
+import { AppContext } from 'js/components/Providers';
+import { getAuthHeader } from 'js/helpers/functions';
+
+function VersionSelect({ uuid }) {
+  const { entityEndpoint, nexusToken } = useContext(AppContext);
+  const authHeader = getAuthHeader(nexusToken);
+  const [versions, setVersions] = useState([]);
+  const [selectedVersionIndex, setSelectedVersionIndex] = useState(0);
+
+  const lowerCaseEntityType = 'dataset';
+  const uuidKey = `${lowerCaseEntityType}_uuid`;
+
+  useEffect(() => {
+    async function fetchVersions() {
+      const response = await fetch(`${entityEndpoint}/${lowerCaseEntityType}s/${uuid}/revisions`, {
+        headers: {
+          'Content-Type': 'application/json',
+          ...authHeader,
+        },
+      });
+      if (!response.ok) {
+        console.error('Entity API failed', response);
+        return;
+      }
+      const results = await response.json();
+      setVersions(results.sort((a, b) => a.revision_number - b.revision_number));
+      setSelectedVersionIndex(results.findIndex((version) => version[uuidKey] === uuid));
+    }
+    fetchVersions();
+  }, [authHeader, entityEndpoint, lowerCaseEntityType, uuid, uuidKey]);
+
+  function visitNewVersion({ i }) {
+    window.location.href = `/browse/dataset/${versions[i][uuidKey]}`;
+  }
+
+  return (
+    versions.length > 0 && (
+      <DropdownListbox
+        id="version-select"
+        optionComponent={DropdownListboxOption}
+        buttonComponent={Button}
+        selectedOptionIndex={selectedVersionIndex}
+        options={versions}
+        selectOnClick={visitNewVersion}
+        getOptionLabel={(v) => `v${v.revision_number}`}
+      />
+    )
+  );
+}
+
+export default VersionSelect;
---
+++ context/app/static/js/components/Detail/VersionSelect/index.js
@@ -0,0 +1,3 @@
+import VersionSelect from './VersionSelect';
+
+export default VersionSelect;
