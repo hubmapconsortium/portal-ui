15c35b9ea95f89cb0ead3e5bc5d8f5ccf4166ac4
mccalluc <mccalluc@users.noreply.github.com>
2021-10-06 14:36:03 -0400

    nonfunctional toc

--- context/app/static/js/pages/Organ/Organ.jsx
+++ context/app/static/js/pages/Organ/Organ.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 
 import SectionHeader from 'js/shared-styles/sections/SectionHeader';
+import TableOfContents from 'js/shared-styles/sections/TableOfContents';
+import { getSections } from 'js/shared-styles/sections/TableOfContents/utils';
+
 import Azimuth from 'js/components/organ/Azimuth';
 import Assays from 'js/components/organ/Assays';
 import Description from 'js/components/organ/Description';
 import OrganInfo from 'js/components/organ/OrganInfo';
 import Samples from 'js/components/organ/Samples';
+
+import { FlexRow, Content } from './style';
 
 function Organ(props) {
   const { organ } = props;
@@ -17,25 +22,31 @@ function Organ(props) {
     search: organ.search.length > 0,
   };
 
+  const sectionOrder = ['Description', 'Organ Info', 'Azimuth', 'Search'];
+  const sections = new Map(getSections(sectionOrder));
+
   return (
-    <>
-      <SectionHeader variant="h1" component="h1">
-        {organ.name}
-      </SectionHeader>
-      {shouldDisplaySection.description && (
-        <Description uberonIri={organ.uberon} uberonShort={organ.uberon_short}>
-          {organ.description}
-        </Description>
-      )}
-      {shouldDisplaySection.organInfo && <OrganInfo uberonIri={organ.uberon} />}
-      {shouldDisplaySection.azimuth && <Azimuth config={organ.azimuth} />}
-      {shouldDisplaySection.search && (
-        <>
-          <Assays searchTerms={organ.search} />
-          <Samples searchTerms={organ.search} />
-        </>
-      )}
-    </>
+    <FlexRow>
+      <TableOfContents items={[...sections.values()]} />
+      <Content>
+        <SectionHeader variant="h1" component="h1">
+          {organ.name}
+        </SectionHeader>
+        {shouldDisplaySection.description && (
+          <Description uberonIri={organ.uberon} uberonShort={organ.uberon_short}>
+            {organ.description}
+          </Description>
+        )}
+        {shouldDisplaySection.organInfo && <OrganInfo uberonIri={organ.uberon} />}
+        {shouldDisplaySection.azimuth && <Azimuth config={organ.azimuth} />}
+        {shouldDisplaySection.search && (
+          <>
+            <Assays searchTerms={organ.search} />
+            <Samples searchTerms={organ.search} />
+          </>
+        )}
+      </Content>
+    </FlexRow>
   );
 }
 
---
+++ context/app/static/js/pages/Organ/style.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+// TODO: Move to shared-styles?
+const FlexRow = styled.div`
+  flex-grow: 1;
+  display: flex;
+  justify-content: center;
+  margin-bottom: ${(props) => props.theme.spacing(5)}px;
+`;
+
+const Content = styled.div`
+  width: calc(100% - 150px);
+`;
+
+export { FlexRow, Content };
