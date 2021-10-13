ecad1c260b4c543dd25976e81a329e3cad2765ce
John Conroy <john-conroy@users.noreply.github.com>
2021-10-13 14:44:51 -0400

    Fix diagnostic info spacing

--- context/app/static/js/components/cells/CellsCharts/style.js
+++ context/app/static/js/components/cells/CellsCharts/style.js
@@ -1,16 +1,25 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+import Typography from '@material-ui/core/Typography';
+
+const chartMargin = css`
+  margin: 0px 10px;
+`;
 
 const Flex = styled.div`
   display: flex;
   height: 400px;
 `;
 
+const StyledTypography = styled(Typography)`
+  ${chartMargin}
+`;
+
 const ChartWrapper = styled.div`
   flex-basis: ${(props) => props.$flexBasis}%;
   height: 100%;
   box-sizing: border-box;
-  margin: 0px 10px;
+  ${chartMargin}
   width: 0px;
 `;
 
-export { Flex, ChartWrapper };
+export { Flex, ChartWrapper, StyledTypography };
