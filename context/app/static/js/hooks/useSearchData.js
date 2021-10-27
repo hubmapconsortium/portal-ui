1294295962ebecb89fc773a7685b498def3b685f
John Conroy <john-conroy@users.noreply.github.com>
2021-10-27 15:51:54 -0400

    Add param to allow for queries without default

--- context/app/static/js/hooks/useSearchData.js
+++ context/app/static/js/hooks/useSearchData.js
@@ -2,11 +2,11 @@ import { useState, useEffect, useContext } from 'react';
 import { getAuthHeader, getDefaultElasticSearchQuery } from 'js/helpers/functions';
 import { AppContext } from 'js/components/Providers';
 
-async function fetchSearchData(query, elasticsearchEndpoint, nexusToken) {
+async function fetchSearchData(query, elasticsearchEndpoint, nexusToken, useDefaultQuery = true) {
   const authHeader = getAuthHeader(nexusToken);
   const response = await fetch(elasticsearchEndpoint, {
     method: 'POST',
-    body: JSON.stringify(getDefaultElasticSearchQuery(query)),
+    body: JSON.stringify(useDefaultQuery ? getDefaultElasticSearchQuery(query) : query),
     headers: {
       'Content-Type': 'application/json',
       ...authHeader,
@@ -21,25 +21,25 @@ async function fetchSearchData(query, elasticsearchEndpoint, nexusToken) {
   return results;
 }
 
-function useSearchData(query) {
+function useSearchData(query, useDefaultQuery) {
   const [searchData, setSearchData] = useState({});
   const [isLoading, setIsLoading] = useState(true);
   const { elasticsearchEndpoint, nexusToken } = useContext(AppContext);
 
   useEffect(() => {
     async function getAndSetSearchHits() {
-      const results = await fetchSearchData(query, elasticsearchEndpoint, nexusToken);
+      const results = await fetchSearchData(query, elasticsearchEndpoint, nexusToken, useDefaultQuery);
       setSearchData(results);
       setIsLoading(false);
     }
     getAndSetSearchHits();
-  }, [elasticsearchEndpoint, nexusToken, query]);
+  }, [elasticsearchEndpoint, nexusToken, query, useDefaultQuery]);
 
   return { searchData, isLoading };
 }
 
-function useSearchHits(query) {
-  const { searchData, isLoading } = useSearchData(query);
+function useSearchHits(query, useDefaultQuery) {
+  const { searchData, isLoading } = useSearchData(query, useDefaultQuery);
   const searchHits = searchData?.hits?.hits || [];
   return { searchHits, isLoading };
 }
