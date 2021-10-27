3288a4022ab4674580a2247d4210bfdaad59a88c
John Conroy <john-conroy@users.noreply.github.com>
2021-10-27 13:51:47 -0400

    Add test

---
+++ context/app/static/js/helpers/__tests__/getDefaultElasticSearchQuery.spec.js
@@ -0,0 +1,67 @@
+const { getDefaultElasticSearchQuery } = require('../functions');
+
+test('it should return the correct query', () => {
+  const baseQuery = {
+    query: {
+      ids: {
+        values: ['2dc0514607ae08af7004d9cb8c9640c9', '72605b56bc5c452a3b6948e4b6b983ed'],
+      },
+    },
+    _source: 'uuid',
+  };
+
+  const expectedQuery = {
+    query: {
+      bool: {
+        must: [
+          {
+            ids: {
+              values: ['2dc0514607ae08af7004d9cb8c9640c9', '72605b56bc5c452a3b6948e4b6b983ed'],
+            },
+          },
+          {
+            bool: {
+              must_not: {
+                exists: {
+                  field: 'next_revision_uuid',
+                },
+              },
+            },
+          },
+        ],
+      },
+    },
+    _source: 'uuid',
+  };
+
+  expect(getDefaultElasticSearchQuery(baseQuery)).toStrictEqual(expectedQuery);
+});
+
+test('it should return the correct query when no inner query is provided', () => {
+  const baseQuery = {
+    size: 0,
+    aggs: { entity_type: { terms: { field: 'entity_type.keyword' } } },
+  };
+
+  const expectedQuery = {
+    query: {
+      bool: {
+        must: [
+          {
+            bool: {
+              must_not: {
+                exists: {
+                  field: 'next_revision_uuid',
+                },
+              },
+            },
+          },
+        ],
+      },
+    },
+    size: 0,
+    aggs: { entity_type: { terms: { field: 'entity_type.keyword' } } },
+  };
+
+  expect(getDefaultElasticSearchQuery(baseQuery)).toStrictEqual(expectedQuery);
+});
