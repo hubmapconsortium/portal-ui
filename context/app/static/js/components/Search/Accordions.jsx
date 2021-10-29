775f6319ee3d64945056dd645c55354d0c905380
John Conroy <john-conroy@users.noreply.github.com>
2021-10-29 17:24:33 -0400

    Refactor accordionFilters into single AccordionFilter component

--- context/app/static/js/components/Search/Accordions.jsx
+++ context/app/static/js/components/Search/Accordions.jsx
@@ -3,18 +3,14 @@ import PropTypes from 'prop-types';
 import Typography from '@material-ui/core/Typography';
 
 import { OuterAccordion, OuterAccordionSummary, OuterAccordionDetails, StyledExpandMoreIcon } from './style';
-import accordionFilters from './accordionFilters';
+import AccordionFilter from './filters/AccordionFilter';
 
 function Accordions(props) {
   const { filters } = props;
   return Object.entries(filters).map(([title, filterGroup], i) => {
     const isFirst = i === 0;
     const innerAccordion = filterGroup.map((def) => {
-      if (!(def.type in accordionFilters)) {
-        throw new Error(`"${def.type}" not in {${Object.keys(accordionFilters).join(', ')}}`);
-      }
-      const Filter = accordionFilters[def.type];
-      return <Filter {...def.props} key={`title-${def.props.title}`} />;
+      return <AccordionFilter type={def.type} {...def.props} key={`title-${def.props.title}`} />;
     });
     if (!title) {
       // We leave the title blank for the group of facets
