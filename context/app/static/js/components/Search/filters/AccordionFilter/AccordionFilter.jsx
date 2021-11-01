b8c8b9f07fd91041312990ab85197769d224fece
John Conroy <john-conroy@users.noreply.github.com>
2021-11-01 16:46:12 -0400

    Add filter items to accordion filter

--- context/app/static/js/components/Search/filters/AccordionFilter/AccordionFilter.jsx
+++ context/app/static/js/components/Search/filters/AccordionFilter/AccordionFilter.jsx
@@ -2,25 +2,28 @@ import React from 'react';
 import { RefinementListFilter, RangeFilter, CheckboxFilter, HierarchicalMenuFilter } from 'searchkit';
 
 import FilterInnerAccordion from 'js/components/Search/filters/FilterInnerAccordion';
+import HierarchicalFilterItem from 'js/components/Search/filters/HierarchicalFilterItem';
+import CheckboxFilterItem from 'js/components/Search/filters/CheckboxFilterItem';
 
 export function getFilter(type) {
   switch (type) {
     case 'AccordionListFilter':
-      return RefinementListFilter;
+      return { Filter: RefinementListFilter, itemComponent: CheckboxFilterItem };
     case 'AccordionRangeFilter':
-      return RangeFilter;
+      return { Filter: RangeFilter };
     case 'AccordionCheckboxFilter':
-      return CheckboxFilter;
+      return { Filter: CheckboxFilter, itemComponent: CheckboxFilterItem };
     case 'AccordionHierarchicalMenuFilter':
-      return HierarchicalMenuFilter;
+      return { Filter: HierarchicalMenuFilter, itemComponent: HierarchicalFilterItem };
     default:
       throw new Error(`"${type}" does not exist`);
   }
 }
 
 function AccordionFilter({ type, ...rest }) {
-  const Filter = getFilter(type);
-  return <Filter containerComponent={FilterInnerAccordion} {...rest} />;
+  const { Filter, itemComponent } = getFilter(type);
+  const item = itemComponent ? { itemComponent } : {};
+  return <Filter containerComponent={FilterInnerAccordion} {...rest} {...item} />;
 }
 
 export default AccordionFilter;
