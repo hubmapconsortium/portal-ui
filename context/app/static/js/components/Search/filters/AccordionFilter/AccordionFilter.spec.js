26919d92e4ed61c91a1448540b980116bf47faeb
John Conroy <john-conroy@users.noreply.github.com>
2021-11-02 12:12:25 -0400

    Update tests

--- context/app/static/js/components/Search/filters/AccordionFilter/AccordionFilter.spec.js
+++ context/app/static/js/components/Search/filters/AccordionFilter/AccordionFilter.spec.js
@@ -1,15 +1,20 @@
 /* eslint-disable import/no-unresolved */
+import React from 'react';
 import { RefinementListFilter, RangeFilter, CheckboxFilter, HierarchicalMenuFilter } from 'searchkit';
+import { render } from 'test-utils/functions';
 
-import { getFilter } from './AccordionFilter';
+import HierarchicalFilterItem from 'js/components/Search/filters/HierarchicalFilterItem';
+import CheckboxFilterItem from 'js/components/Search/filters/CheckboxFilterItem';
+
+import AccordionFilter, { getFilter } from './AccordionFilter';
 
 test.each([
-  ['AccordionListFilter', RefinementListFilter],
-  ['AccordionRangeFilter', RangeFilter],
-  ['AccordionCheckboxFilter', CheckboxFilter],
-  ['AccordionHierarchicalMenuFilter', HierarchicalMenuFilter],
+  ['AccordionListFilter', { Filter: RefinementListFilter, itemComponent: CheckboxFilterItem }],
+  ['AccordionRangeFilter', { Filter: RangeFilter }],
+  ['AccordionCheckboxFilter', { Filter: CheckboxFilter, itemComponent: CheckboxFilterItem }],
+  ['AccordionHierarchicalMenuFilter', { Filter: HierarchicalMenuFilter, itemComponent: HierarchicalFilterItem }],
 ])('%s returns correct filter', (filterName, expectedFilter) => {
-  expect(getFilter(filterName)).toBe(expectedFilter);
+  expect(getFilter(filterName)).toStrictEqual(expectedFilter);
 });
 
 test('should throw with unexpected filter name', () => {
@@ -17,4 +22,13 @@ test('should throw with unexpected filter name', () => {
   expect(() => {
     getFilter(unknownFilter);
   }).toThrow(`"${unknownFilter}" does not exist`);
+});
+
+test.each([
+  ['AccordionListFilter'],
+  ['AccordionRangeFilter'],
+  ['AccordionCheckboxFilter'],
+  ['AccordionHierarchicalMenuFilter'],
+])('%s should render', (filterName) => {
+  render(<AccordionFilter type={filterName} />);
 });
