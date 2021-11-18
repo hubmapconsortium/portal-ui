29e9aa074d0c6cc83fcb512001ad231379b5fbb1
John Conroy <john-conroy@users.noreply.github.com>
2021-11-18 14:59:30 -0500

    Fetch and download tsv

--- context/app/static/js/components/Search/DownloadButton/DownloadButton.jsx
+++ context/app/static/js/components/Search/DownloadButton/DownloadButton.jsx
@@ -1,27 +1,48 @@
 import React from 'react';
-import ReactGA from 'react-ga';
+// import ReactGA from 'react-ga';
 
 import { SecondaryBackgroundTooltip } from 'js/shared-styles/tooltips';
-
+import useSearchViewStore from 'js/stores/useSearchViewStore';
+import { createDownloadUrl } from 'js/helpers/functions';
 import { StyledButton } from './style';
 
+/*
 function sendTsvEvent(event) {
   ReactGA.pageview(event.currentTarget.href);
 }
+*/
 
 function DownloadButton(props) {
   const { type } = props;
   const lcPluralType = `${type.toLowerCase()}s`;
+  const allResultsUUIDs = useSearchViewStore((state) => state.allResultsUUIDs);
+
+  // eslint-disable-next-line consistent-return
+  async function fetchAndDownloadTSV() {
+    const response = await fetch(`/metadata/v0/${lcPluralType}.tsv`, {
+      method: 'POST',
+      body: JSON.stringify({ uuids: allResultsUUIDs }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    if (!response.ok) {
+      console.error('Search API failed', response);
+      return {};
+    }
+    const results = await response.blob();
+
+    const downloadUrl = createDownloadUrl(results, 'text/tab-separated-values');
+    // need to create link to set file name
+    const tempLink = document.createElement('a');
+    tempLink.href = downloadUrl;
+    tempLink.download = `${lcPluralType}.tsv`;
+    tempLink.click();
+  }
+
   return (
     <SecondaryBackgroundTooltip title="Download a TSV of the table metadata. This will not take into account any filtering done on the search page.">
-      <StyledButton
-        href={`/metadata/v0/${lcPluralType}.tsv`}
-        target="_blank"
-        component="a"
-        onClick={sendTsvEvent}
-        variant="outlined"
-        color="primary"
-      >
+      <StyledButton onClick={fetchAndDownloadTSV} variant="outlined" color="primary">
         Download Metadata
       </StyledButton>
     </SecondaryBackgroundTooltip>
