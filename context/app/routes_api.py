b76ff88651282477f9083583d3d82221a9b40ee0
John Conroy <john-conroy@users.noreply.github.com>
2021-11-22 13:41:02 -0500

    Rename error func

--- context/app/routes_api.py
+++ context/app/routes_api.py
@@ -13,7 +13,7 @@ def _get_dict_subset(d, keys_to_remove):
     return {k: d[k] for k in d.keys() - dict.fromkeys(keys_to_remove)}
 
 
-def _raise_api_error(status, message):
+def _get_api_json_error(status, message):
     return jsonify({
         'status': status,
         'message': message,
@@ -30,9 +30,9 @@ def entities_tsv(entity_type):
     else:
         body = request.get_json()
         if request.args:
-            return _raise_api_error(400, 'POST only accepts a JSON body.')
+            return _get_api_json_error(400, 'POST only accepts a JSON body.')
         if _get_dict_subset(body, ['uuids']):
-            return _raise_api_error(400, 'POST only accepts uuids in JSON body.')
+            return _get_api_json_error(400, 'POST only accepts uuids in JSON body.')
         entities = _get_entities(entity_type, {}, body.get('uuids'))
     return _make_tsv_response(_dicts_to_tsv(entities, _first_fields), f'{entity_type}.tsv')
 
