61dff1d1356bfcdc7117868b62234a3a20f2c544
John Conroy <john-conroy@users.noreply.github.com>
2021-11-29 16:03:41 -0500

    Fix resizing issues and offset side

--- context/app/static/js/shared-styles/charts/ChartWrapper/ChartWrapper.jsx
+++ context/app/static/js/shared-styles/charts/ChartWrapper/ChartWrapper.jsx
@@ -3,18 +3,20 @@ import { LegendOrdinal } from '@visx/legend';
 import Typography from '@material-ui/core/Typography';
 
 import { TitleWrapper } from 'js/shared-styles/charts/style';
-import { Flex, FlexGrowChild, DropdownWrapper } from './style';
+import { Flex, FlexGrowChild, DropdownWrapper, DropdownAndLegendWrapper } from './style';
 
 function ChartWrapper({ children, chartTitle, margin, colorScale, dropdown }) {
   return (
     <Flex>
       <FlexGrowChild>
-        <TitleWrapper $leftOffset={margin.left - margin.right}>
-          {chartTitle && <Typography>{chartTitle}</Typography>}
-        </TitleWrapper>
+        {chartTitle && (
+          <TitleWrapper $leftOffset={margin.left - margin.right}>
+            {chartTitle && <Typography>{chartTitle}</Typography>}
+          </TitleWrapper>
+        )}
         {children}
       </FlexGrowChild>
-      <div>
+      <DropdownAndLegendWrapper $topOffset={margin.top}>
         {dropdown && <DropdownWrapper>{dropdown}</DropdownWrapper>}
         <LegendOrdinal
           scale={colorScale}
@@ -23,7 +25,7 @@ function ChartWrapper({ children, chartTitle, margin, colorScale, dropdown }) {
             borderRadius: '3px',
           })}
         />
-      </div>
+      </DropdownAndLegendWrapper>
     </Flex>
   );
 }
