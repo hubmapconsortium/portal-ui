e928657c41d53afd62ac95f12f81b07683657e62
mccalluc <mccalluc@users.noreply.github.com>
2021-12-02 11:08:16 -0500

    Default to categorical, with exceptions for IDs

--- context/app/static/js/pages/LineUpPage/LineUpPage.jsx
+++ context/app/static/js/pages/LineUpPage/LineUpPage.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
-import LineUp, { LineUpStringColumnDesc, LineUpNumberColumnDesc } from 'lineupjsx';
+import LineUp, {
+  LineUpStringColumnDesc,
+  LineUpNumberColumnDesc,
+  LineUpCategoricalColumnDesc,
+  LineUpDateColumnDesc,
+} from 'lineupjsx';
 import 'lineupjsx/build/LineUpJSx.css';
 import Paper from '@material-ui/core/Paper';
 
@@ -9,13 +14,29 @@ import metadataFieldTypes from 'metadata-field-types';
 function LineUpPage(props) {
   const { entities } = props;
   const firstRow = entities[0];
+  const notEnumFields = new Set([
+    // Donors:
+    'hubmap_id',
+    'medical_history',
+    'uuid',
+    // Samples:
+    'donor.hubmap_id',
+    'sample_id',
+    // Datasets:
+    'description',
+    'library_adapter_sequence',
+  ]);
   const columns = Object.keys(firstRow).map((key) => {
-    switch (metadataFieldTypes[key]) {
-      case 'number':
-        return <LineUpNumberColumnDesc column={key} />;
-      default:
-        return <LineUpStringColumnDesc column={key} />;
+    if (metadataFieldTypes[key] === 'number' || metadataFieldTypes[key] === 'integer') {
+      return <LineUpNumberColumnDesc column={key} key={key} />;
     }
+    if (metadataFieldTypes[key] === 'datetime') {
+      return <LineUpDateColumnDesc column={key} key={key} />;
+    }
+    if (notEnumFields.has(key)) {
+      return <LineUpStringColumnDesc column={key} key={key} />;
+    }
+    return <LineUpCategoricalColumnDesc column={key} key={key} />;
   });
 
   return (
