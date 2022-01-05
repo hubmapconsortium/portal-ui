6593d7db10daca51d75289e678ba09c3e6eb4e8f
John Conroy <john-conroy@users.noreply.github.com>
2022-01-05 16:11:29 -0500

    Organize imports

--- context/app/static/js/components/savedLists/SavedEntitiesTableRow/SavedEntitiesTableRow.jsx
+++ context/app/static/js/components/savedLists/SavedEntitiesTableRow/SavedEntitiesTableRow.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import format from 'date-fns/format';
 import TableRow from '@material-ui/core/TableRow';
-
 import TableCell from '@material-ui/core/TableCell';
+
 import { LightBlueLink } from 'js/shared-styles/Links';
-import SelectableRowCell from '../../../shared-styles/tables/SelectableRowCell';
+import SelectableRowCell from 'js/shared-styles/tables/SelectableRowCell';
 
 function SavedEntitiesTableRow({ uuid, rowData, index }) {
   const { hubmap_id, group_name, entity_type, dateSaved, dateAddedToList } = rowData;
