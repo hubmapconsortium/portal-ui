f373d4045c76d69766350cab6521a7addd68eb00
John Conroy <john-conroy@users.noreply.github.com>
2022-01-05 15:22:12 -0500

    Add hoc for selectable table provider

--- context/app/static/js/shared-styles/tables/SelectableTableProvider/SelectableTableProvider.jsx
+++ context/app/static/js/shared-styles/tables/SelectableTableProvider/SelectableTableProvider.jsx
@@ -6,4 +6,10 @@ function SelectableTableProvider({ children, tableLabel }) {
   return <Provider createStore={() => createStore(tableLabel)}>{children}</Provider>;
 }
 
+export const withSelectableTableProvider = (Component, tableLabel) => ({ ...props }) => (
+  <SelectableTableProvider tableLabel={tableLabel}>
+    <Component {...props} />
+  </SelectableTableProvider>
+);
+
 export default SelectableTableProvider;
