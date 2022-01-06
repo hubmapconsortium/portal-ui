8343ee736de73d386a846e2ca80d30e877cc45e9
mccalluc <mccalluc@users.noreply.github.com>
2022-01-06 17:16:12 -0500

    whitespace

--- context/app/test_routes_api.py
+++ context/app/test_routes_api.py
@@ -29,10 +29,12 @@ mock_tsv = '''uuid\thubmap_id\tage_unit\tage_value\r
 ABC123\tHMB123.XYZ\teons\t42\r
 '''
 
+
 def mock_es_post(path, **kwargs):
     class MockResponse():
         def json(self):
             return mock_es
+
         def raise_for_status(self):
             pass
     return MockResponse()
@@ -62,7 +64,7 @@ def test_tsv_post(client, mocker):
 def test_unexpected_json_tsv_post(client, mocker):
     mocker.patch('requests.post', side_effect=mock_es_post)
     response = client.post('/metadata/v0/donors.tsv', json={'unexpected': []})
-    assert response.status == '200 OK' # TODO: Should not be 200!
+    assert response.status == '200 OK'  # TODO: Should not be 200!
     assert response.get_data(as_text=True).strip() \
         == '{"message":"POST only accepts uuids in JSON body.","status":400}'
 
