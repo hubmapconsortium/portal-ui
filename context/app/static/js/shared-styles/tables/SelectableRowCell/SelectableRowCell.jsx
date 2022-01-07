16a6218ef982251fd2c7af21d718e03e89421bd4
John Conroy <john-conroy@users.noreply.github.com>
2022-01-07 13:19:53 -0500

    Fix selectable row toggle

--- context/app/static/js/shared-styles/tables/SelectableRowCell/SelectableRowCell.jsx
+++ context/app/static/js/shared-styles/tables/SelectableRowCell/SelectableRowCell.jsx
@@ -6,10 +6,10 @@ import TableCell from '@material-ui/core/TableCell';
 import { useStore } from 'js/shared-styles/tables/SelectableTableProvider/store';
 
 function SelectableRowCell({ rowKey, index }) {
-  const { selectRow, selectedRows, tableLabel } = useStore();
+  const { toggleRow, selectedRows, tableLabel } = useStore();
 
   return (
-    <TableCell padding="checkbox" onClick={() => selectRow(rowKey)}>
+    <TableCell padding="checkbox" onClick={() => toggleRow(rowKey)}>
       <Checkbox
         checked={selectedRows.has(rowKey)}
         inputProps={{ 'aria-labelledby': `${tableLabel}-row-${index}-checkbox` }}
--- context/app/static/js/shared-styles/tables/SelectableTableProvider/store.js
+++ context/app/static/js/shared-styles/tables/SelectableTableProvider/store.js
@@ -3,16 +3,25 @@ import createContext from 'zustand/context';
 
 const { Provider, useStore } = createContext();
 
+function deselectRowAndReturnRows(state, key) {
+  const setCopy = new Set(state.selectedRows);
+  setCopy.delete(key);
+  return { selectedRows: setCopy };
+}
+
+function selectRowAndReturnRows(state, key) {
+  return { selectedRows: state.selectedRows.add(key) };
+}
+
 const createStore = (tableLabel) =>
   create((set) => ({
     selectedRows: new Set([]),
-    selectRow: (key) => set((state) => ({ selectedRows: state.selectedRows.add(key) })),
-    deselectRow: (key) =>
-      set((state) => {
-        const setCopy = new Set(state.selectedRows);
-        setCopy.delete(key);
-        return { selectedRows: setCopy };
-      }),
+    selectRow: (key) => set((state) => selectRowAndReturnRows(state, key)),
+    deselectRow: (key) => set((state) => deselectRowAndReturnRows(state, key)),
+    toggleRow: (key) =>
+      set((state) =>
+        state.selectedRows.has(key) ? deselectRowAndReturnRows(state, key) : selectRowAndReturnRows(state, key),
+      ),
     setSelectedRows: (keys) => set({ selectedRows: new Set(keys) }),
     deselectAllRows: () => set({ selectedRows: new Set([]) }),
     headerRowIsSelected: false,
