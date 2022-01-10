cb85a605f23ed9866ffa6b69205ce1450698640b
John Conroy <john-conroy@users.noreply.github.com>
2022-01-10 14:38:36 -0500

    Add LoadingTableRows stories

---
+++ context/app/static/js/shared-styles/tables/LoadingTableRows/LoadingTableRows.stories.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import Table from '@material-ui/core/Table';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+
+import LoadingTableRowsComponent from './LoadingTableRows';
+
+export default {
+  title: 'Tables/LoadingTableRows',
+  component: LoadingTableRowsComponent,
+};
+
+export const LoadingTableRows = (args) => (
+  <Table>
+    <TableHead>
+      <TableRow>
+        <TableCell>A</TableCell>
+        <TableCell>B</TableCell>
+        <TableCell>C</TableCell>
+      </TableRow>
+    </TableHead>
+    <TableBody>
+      <LoadingTableRowsComponent {...args} />
+    </TableBody>
+  </Table>
+);
+LoadingTableRows.args = { numberOfRows: 3, numberOfCols: 3 };
+
+LoadingTableRows.storyName = 'LoadingTableRows'; // needed for single story hoisting for multi word component names
