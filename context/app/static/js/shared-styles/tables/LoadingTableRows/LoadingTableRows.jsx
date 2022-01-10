9a6382c309b0503cd2d8b95553ed25cc9d17a886
John Conroy <john-conroy@users.noreply.github.com>
2022-01-10 14:32:47 -0500

    Add props, keys and comment for LoadingTableRows

--- context/app/static/js/shared-styles/tables/LoadingTableRows/LoadingTableRows.jsx
+++ context/app/static/js/shared-styles/tables/LoadingTableRows/LoadingTableRows.jsx
@@ -1,18 +1,27 @@
+/* eslint-disable react/no-array-index-key */
 import React from 'react';
+import PropTypes from 'prop-types';
 import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
 import Skeleton from '@material-ui/lab/Skeleton';
 
+// Using indexes as keys is an anti-pattern and can affect performance, but index can be used for items with no stable ID.
+// A uuid generator cannot be used as keys need to be consistent through renders.
 function LoadingTableRows({ numberOfRows, numberOfCols }) {
-  return [...Array(numberOfRows).keys()].map(() => (
-    <TableRow>
-      {[...Array(numberOfCols).keys()].map(() => (
-        <TableCell>
+  return [...Array(numberOfRows).keys()].map((r, rowIndex) => (
+    <TableRow key={`loading-table-row-${rowIndex}`}>
+      {[...Array(numberOfCols).keys()].map((c, colIndex) => (
+        <TableCell key={`loading-table-row-${colIndex}`}>
           <Skeleton />
         </TableCell>
       ))}
     </TableRow>
   ));
 }
+
+LoadingTableRows.propTypes = {
+  numberOfRows: PropTypes.number.isRequired,
+  numberOfCols: PropTypes.number.isRequired,
+};
 
 export default LoadingTableRows;
