539a204e4f631d6c8dd7cae77f4049738ac2b896
John Conroy <john-conroy@users.noreply.github.com>
2022-01-10 14:38:54 -0500

    Add LoadingTableRows tests

---
+++ context/app/static/js/shared-styles/tables/LoadingTableRows/LoadingTableRows.spec.js
@@ -0,0 +1,14 @@
+/* eslint-disable import/no-unresolved */
+import React from 'react';
+import { render, screen } from 'test-utils/functions';
+
+import LoadingTableRows from './LoadingTableRows';
+
+const numberOfRows = 3;
+const numberOfCols = 5;
+test('should display correct number of rows and cells', () => {
+  render(<LoadingTableRows numberOfRows={numberOfRows} numberOfCols={numberOfCols} />);
+
+  expect(screen.getAllByRole('row').length).toBe(numberOfRows);
+  expect(screen.getAllByRole('cell').length).toBe(numberOfRows * numberOfCols);
+});
