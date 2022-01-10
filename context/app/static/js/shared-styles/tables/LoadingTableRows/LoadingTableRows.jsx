639b1d94c8d3d8d97ce714e10c92f6b59625029c
John Conroy <john-conroy@users.noreply.github.com>
2022-01-10 14:47:31 -0500

    Add getArrayRange helper

--- context/app/static/js/shared-styles/tables/LoadingTableRows/LoadingTableRows.jsx
+++ context/app/static/js/shared-styles/tables/LoadingTableRows/LoadingTableRows.jsx
@@ -5,12 +5,14 @@ import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
 import Skeleton from '@material-ui/lab/Skeleton';
 
+import { getArrayRange } from 'js/helpers/functions';
+
 // Using indexes as keys is an anti-pattern and can affect performance, but index can be used for items with no stable ID.
 // A uuid generator cannot be used as keys need to be consistent through renders.
 function LoadingTableRows({ numberOfRows, numberOfCols }) {
-  return [...Array(numberOfRows).keys()].map((r, rowIndex) => (
+  return getArrayRange(numberOfRows).map((r, rowIndex) => (
     <TableRow key={`loading-table-row-${rowIndex}`}>
-      {[...Array(numberOfCols).keys()].map((c, colIndex) => (
+      {getArrayRange(numberOfCols).map((c, colIndex) => (
         <TableCell key={`loading-table-row-${colIndex}`}>
           <Skeleton />
         </TableCell>
