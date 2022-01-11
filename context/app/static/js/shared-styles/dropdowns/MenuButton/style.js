f182b00a9e86a752eb25e8fd07e958788d124071
John Conroy <john-conroy@users.noreply.github.com>
2022-01-11 14:17:19 -0500

    Add dropdown icons to icons

--- context/app/static/js/shared-styles/dropdowns/MenuButton/style.js
+++ context/app/static/js/shared-styles/dropdowns/MenuButton/style.js
@@ -2,7 +2,6 @@ import styled, { css } from 'styled-components';
 import { UpIcon, DownIcon } from 'js/shared-styles/icons';
 
 const iconStyles = css`
-  font-size: 1.25rem;
   margin-left: ${(props) => props.theme.spacing(0.5)}px;
 `;
 
