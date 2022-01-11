7ef75e8d9f75a53eb7c9b759a13a7dd0ceb2cc38
John Conroy <john-conroy@users.noreply.github.com>
2022-01-11 11:07:49 -0500

    Enable selectable cells to be disabled and move event handler to checkbox

--- context/app/static/js/shared-styles/tables/SelectableRowCell/SelectableRowCell.jsx
+++ context/app/static/js/shared-styles/tables/SelectableRowCell/SelectableRowCell.jsx
@@ -5,14 +5,16 @@ import TableCell from '@material-ui/core/TableCell';
 
 import { useStore } from 'js/shared-styles/tables/SelectableTableProvider/store';
 
-function SelectableRowCell({ rowKey, index }) {
+function SelectableRowCell({ rowKey, index, disabled }) {
   const { toggleRow, selectedRows, tableLabel } = useStore();
 
   return (
-    <TableCell padding="checkbox" onClick={() => toggleRow(rowKey)}>
+    <TableCell padding="checkbox">
       <Checkbox
         checked={selectedRows.has(rowKey)}
         inputProps={{ 'aria-labelledby': `${tableLabel}-row-${index}-checkbox` }}
+        onChange={() => toggleRow(rowKey)}
+        disabled={disabled}
       />
     </TableCell>
   );
