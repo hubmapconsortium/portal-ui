a9f1820ae6b03cd3fd572e461289f3a377fef251
mccalluc <mccalluc@users.noreply.github.com>
2022-01-18 13:48:37 -0500

    nudge a couple pixels

--- context/app/static/js/components/detailPage/StatusIcon/style.js
+++ context/app/static/js/components/detailPage/StatusIcon/style.js
@@ -5,7 +5,7 @@ const ColoredStatusIcon = styled(LensIcon)`
   color: ${(props) => props.theme.palette[props.$iconColor].main};
   font-size: 16px;
   margin-right: 3px;
-  margin-bottom: -2px;
+  margin-bottom: -2.5px;
 `;
 
 export { ColoredStatusIcon };
