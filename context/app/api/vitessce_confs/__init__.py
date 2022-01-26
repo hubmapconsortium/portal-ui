2dbac65eec5eaaabf1a4b5835e8a8f7bd430a087
mccalluc <mccalluc@users.noreply.github.com>
2022-01-26 15:55:13 -0500

    new submodule: point to it with symlink

--- context/app/api/vitessce_confs/__init__.py
+++
@@ -1,2 +0,0 @@
-# flake8: noqa: F401
-from .builder_factory import get_view_config_builder
--- context/app/api/vitessce_confs/builders/doctest_utils.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask
-
-
-def app_context():
-    app = Flask('doctests')
-    app.config['ASSETS_ENDPOINT'] = 'https://example.com'
-    return app.app_context()
