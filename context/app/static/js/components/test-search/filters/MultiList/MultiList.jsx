91aaa2a95db6a3bc0c0fd36c48102be2b0c4b80a
John Conroy <john-conroy@users.noreply.github.com>
2022-02-16 16:52:51 -0500

    Update filters to react to one another

--- context/app/static/js/components/test-search/filters/MultiList/MultiList.jsx
+++ context/app/static/js/components/test-search/filters/MultiList/MultiList.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import { MultiList as ReactiveSearchMultiList } from '@appbaseio/reactivesearch';
+import { useStore } from 'js/pages/TestSearch/searchConfig/store';
 
 function MultiList({ componentId, dataField, title, ...rest }) {
+  const { filters } = useStore();
+
   return (
     <ReactiveSearchMultiList
       componentId={componentId}
       dataField={dataField}
       title={title}
-      size={5}
-      showLoadMore
       showSearch={false}
       URLParams
+      sortBy="count"
+      react={{ and: filters.filter((filter) => filter !== componentId) }}
       {...rest}
     />
   );
