12725fe37322fee420ccf5d10254de2e290a6b86
John Conroy <john-conroy@users.noreply.github.com>
2022-02-28 16:23:19 -0500

    Add IconLink stories

---
+++ context/app/static/js/shared-styles/Links/iconLinks/IconLink/IconLink.stories.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import { ExternalLinkIcon } from 'js/shared-styles/icons';
+import IconLink from './IconLink';
+
+export default {
+  title: 'Links/IconLink',
+  component: IconLink,
+};
+
+const icon = <ExternalLinkIcon fontSize="1rem" />;
+
+const Template = (args) => <IconLink {...args} />;
+export const Start = Template.bind({});
+Start.args = { icon, iconPosition: 'start', children: 'Start' };
+
+export const End = Template.bind({});
+End.args = {
+  icon,
+  iconPosition: 'end',
+  children: 'End',
+};
