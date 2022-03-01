4e79fd68b19c174759d2318e1f60d6563d1d4736
John Conroy <john-conroy@users.noreply.github.com>
2022-03-01 11:36:45 -0500

    Remove duplicate icon

--- context/app/static/js/components/home/DataUseGuidelines/DataUseGuidelines.jsx
+++ context/app/static/js/components/home/DataUseGuidelines/DataUseGuidelines.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import EmailIconLink from 'js/shared-styles/Links/iconLinks/EmailIconLink';
-import { StyledPaper, MainText, StyledEmailIcon } from './style';
+import { StyledPaper, MainText } from './style';
 
 function DataUseGuidelines() {
   return (
@@ -16,7 +16,7 @@ function DataUseGuidelines() {
       <MainText mt={2} variant="body1">
         Please direct any questions to{' '}
         <EmailIconLink href="help@hubmapconsortium.org" iconFontSize="1.1rem">
-          help@hubmapconsortium.org <StyledEmailIcon />
+          help@hubmapconsortium.org
         </EmailIconLink>
       </MainText>
     </StyledPaper>
--- context/app/static/js/components/home/DataUseGuidelines/style.js
+++ context/app/static/js/components/home/DataUseGuidelines/style.js
@@ -2,8 +2,6 @@ import styled from 'styled-components';
 import Typography from '@material-ui/core/Typography';
 import Paper from '@material-ui/core/Paper';
 
-import { EmailIcon } from 'js/shared-styles/icons';
-
 const StyledPaper = styled(Paper)`
   padding: ${(props) => props.theme.spacing(2)}px;
 `;
@@ -12,9 +10,4 @@ const MainText = styled(Typography)`
   margin-top: ${(props) => props.theme.spacing(props.mt)}px;
 `;
 
-const StyledEmailIcon = styled(EmailIcon)`
-  font-size: 1rem;
-  vertical-align: middle;
-`;
-
-export { StyledPaper, MainText, StyledEmailIcon };
+export { StyledPaper, MainText };
