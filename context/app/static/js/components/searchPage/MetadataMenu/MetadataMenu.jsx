f5b074c24d1309c3f9b62dd7c1358b3f177a4e81
mccalluc <mccalluc@users.noreply.github.com>
2022-03-04 09:47:47 -0500

    factor out downloader

--- context/app/static/js/components/searchPage/MetadataMenu/MetadataMenu.jsx
+++ context/app/static/js/components/searchPage/MetadataMenu/MetadataMenu.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactGA from 'react-ga';
-import { format } from 'date-fns';
 import MenuItem from '@material-ui/core/MenuItem';
 
 import useSearchViewStore from 'js/stores/useSearchViewStore';
@@ -11,77 +10,47 @@ import { useStore } from 'js/shared-styles/dropdowns/DropdownMenuProvider/store'
 import DropdownMenu from 'js/shared-styles/dropdowns/DropdownMenu';
 import { StyledDropdownMenuButton, StyledLink, StyledInfoIcon } from './style';
 
+function makeDownloader({ urlPath, allResultsUUIDs, closeMenu, analyticsCategory }) {
+  async function fetchAndDownload() {
+    const response = await fetch(urlPath, {
+      method: 'POST',
+      body: JSON.stringify({ uuids: allResultsUUIDs }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    if (!response.ok) {
+      console.error('Download failed', response);
+      closeMenu();
+      return;
+    }
+    const results = await response.blob();
+    const headers = Object.fromEntries(Array(...response.headers));
+    const name = headers['content-disposition'].split('=')[1];
+    const mime = headers['content-type'];
+
+    const downloadUrl = createDownloadUrl(results, mime);
+    const tempLink = document.createElement('a');
+    tempLink.href = downloadUrl;
+    tempLink.download = name;
+    tempLink.click();
+
+    ReactGA.event({
+      category: analyticsCategory,
+      action: `Download ${mime}`,
+      label: urlPath.split('/').pop(),
+    });
+
+    closeMenu();
+  }
+  return fetchAndDownload;
+}
+
 function MetadataMenu({ type, analyticsCategory }) {
   const lcPluralType = `${type.toLowerCase()}s`;
   const allResultsUUIDs = useSearchViewStore((state) => state.allResultsUUIDs);
 
   const { closeMenu } = useStore();
-  // eslint-disable-next-line consistent-return
-  async function fetchAndDownloadTSV() {
-    const response = await fetch(`/metadata/v0/${lcPluralType}.tsv`, {
-      method: 'POST',
-      body: JSON.stringify({ uuids: allResultsUUIDs }),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
-    if (!response.ok) {
-      console.error('Metadata TSV Failed', response);
-      closeMenu();
-      return;
-    }
-    const results = await response.blob();
-
-    const downloadUrl = createDownloadUrl(results, 'text/tab-separated-values');
-    // need to create link to set file name
-    const tempLink = document.createElement('a');
-    tempLink.href = downloadUrl;
-    const timestamp = format(new Date(), 'yyyy-MM-dd_HH-mm-ss');
-    tempLink.download = `hubmap-${lcPluralType}-metadata-${timestamp}.tsv`;
-    tempLink.click();
-
-    ReactGA.event({
-      category: analyticsCategory,
-      action: `Download Metadata`,
-      label: type,
-    });
-
-    closeMenu();
-  }
-
-  async function fetchAndDownloadNotebook() {
-    // TODO: Clean up the copy and paste if this is accepted.
-    const response = await fetch(`/notebooks/${lcPluralType}.ipynb`, {
-      method: 'POST',
-      body: JSON.stringify({ uuids: allResultsUUIDs }),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
-    if (!response.ok) {
-      console.error('Notebook Failed', response);
-      closeMenu();
-      return;
-    }
-    const results = await response.blob();
-
-    // TODO: Can we get the MIME type from the response?
-    const downloadUrl = createDownloadUrl(results, 'application/x-ipynb+json');
-    // need to create link to set file name
-    const tempLink = document.createElement('a');
-    tempLink.href = downloadUrl;
-    const timestamp = format(new Date(), 'yyyy-MM-dd_HH-mm-ss');
-    tempLink.download = `hubmap-${lcPluralType}-notebook-${timestamp}.ipynb`;
-    tempLink.click();
-
-    ReactGA.event({
-      category: analyticsCategory,
-      action: `Download Notebook`,
-      label: type,
-    });
-
-    closeMenu();
-  }
 
   const menuID = 'metadata-menu';
 
@@ -95,13 +64,27 @@ function MetadataMenu({ type, analyticsCategory }) {
             <StyledInfoIcon color="primary" />
           </SecondaryBackgroundTooltip>
         </MenuItem>
-        <MenuItem onClick={fetchAndDownloadTSV}>
+        <MenuItem
+          onClick={makeDownloader({
+            urlPath: `/metadata/v0/${lcPluralType}.tsv`,
+            allResultsUUIDs,
+            closeMenu,
+            analyticsCategory,
+          })}
+        >
           Download
           <SecondaryBackgroundTooltip title="Download a TSV of the table metadata." placement="bottom-start">
             <StyledInfoIcon color="primary" />
           </SecondaryBackgroundTooltip>
         </MenuItem>
-        <MenuItem onClick={fetchAndDownloadNotebook}>
+        <MenuItem
+          onClick={makeDownloader({
+            urlPath: `/notebooks/${lcPluralType}.ipynb`,
+            allResultsUUIDs,
+            closeMenu,
+            analyticsCategory,
+          })}
+        >
           Notebook
           <SecondaryBackgroundTooltip
             title="Download a Notebook which demonstrates how to programmatically access metadata."
