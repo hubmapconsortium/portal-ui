b38d5ab423f28a94b0117fae8be9847499809c69
mccalluc <mccalluc@users.noreply.github.com>
2022-03-09 13:50:30 -0500

    check syntax of notebook cells

---
+++ CHANGELOG-check-syntax.md
@@ -0,0 +1,1 @@
+- Check syntax of notebook cells.
--- context/app/test_routes_notebooks.py
+++ context/app/test_routes_notebooks.py
@@ -1,3 +1,7 @@
+from pathlib import Path
+import json
+from ast import parse
+
 import pytest
 
 from .main import create_app
@@ -17,3 +21,14 @@ def client():
 def test_truncate_and_redirect(client, entity_type):
     response = client.post(f'/notebooks/{entity_type}.ipynb', json={'uuids': ['fake-uuid']})
     assert response.status == '200 OK'
+
+
+@pytest.mark.parameterize(
+    'notebook_path',
+    (Path(__file__).parent / 'notebook').glob('*.ipynb')
+)
+def test_notebooks_cells_valid(notebook_path):
+    nb = json.loads(notebook_path.read_text())
+    for cell in nb['cells']:
+        source = ''.join(cell['source'])
+        parse(source)
