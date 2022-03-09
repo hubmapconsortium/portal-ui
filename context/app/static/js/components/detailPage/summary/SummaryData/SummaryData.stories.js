f81b1e550466bf5393e870f333480f360b7be0f1
John Conroy <john-conroy@users.noreply.github.com>
2022-03-09 14:48:00 -0500

    Fix summary data revisions mock

--- context/app/static/js/components/detailPage/summary/SummaryData/SummaryData.stories.js
+++ context/app/static/js/components/detailPage/summary/SummaryData/SummaryData.stories.js
@@ -6,6 +6,15 @@ import SummaryData from './SummaryData';
 export default {
   title: 'EntityDetail/Summary/SummaryData',
   component: SummaryData,
+  parameters: {
+    msw: {
+      handlers: [
+        rest.get('http://localhost:6006/undefined/datasets/fakeuuid/revisions', (req, res, ctx) => {
+          return res(ctx.json([{ revision_number: 1, uuid: 'fakeuuid' }]));
+        }),
+      ],
+    },
+  },
 };
 
 const sharedArgs = {
@@ -29,22 +38,26 @@ Dataset.args = {
   entity_type: 'Dataset',
 };
 
-Dataset.parameters = {
-  msw: {
-    handlers: [
-      rest.get('http://localhost:6006/undefined/datasets/fakeuuid/revisions', (req, res, ctx) => {
-        return res(ctx.json([{ revision_number: 1, uuid: 'fakeuuid' }]));
-      }),
-    ],
-  },
-};
-
 export const WithChildren = (args) => (
   <SummaryData {...args}>
     <Typography>Child</Typography>
   </SummaryData>
 );
 WithChildren.args = {
+  ...sharedArgs,
+  entity_type: 'Dataset',
+};
+
+export const WithChildrenOverflow = (args) => (
+  <SummaryData {...args}>
+    <Typography>Child 1</Typography>
+    <Typography>Child 2</Typography>
+    <Typography>Child 3</Typography>
+    <Typography>Child 4</Typography>
+    <Typography>Child 5</Typography>
+  </SummaryData>
+);
+WithChildrenOverflow.args = {
   ...sharedArgs,
   entity_type: 'Dataset',
 };
