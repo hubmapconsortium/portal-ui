e3f871025d61b0384e4bc6c7dcbad1939086bb6f
Chuck McCallum <mccalluc@users.noreply.github.com>
2022-03-10 10:33:13 -0500

    Mccalluc/store notebooks as notebooks (#2489)
    * Stub MD page
    
    * add route for multi-dataset notebooks
    
    * hook up to ui
    
    * get definition
    
    * remove md
    
    * line wrap
    
    * more comments in the generated code
    
    * factor out util
    
    * factor out downloader
    
    * Helper function for cells
    
    * use template
    
    * Move other notebook route
    
    * lint
    
    * separate cells from routes
    
    * dataset notebook works
    
    * failing doc test... does CI catch it?
    
    * Confirm that there are no syntax errors in cells
    
    * unused import
    
    * Break doctest again to test CI
    
    * linewrap
    
    * files section only for datasets
    
    * Confirmed that doctest failed on CI. yay!
    
    * Flask test notebook generation
    
    * Get headers directly
    
    * failover message
    
    * get rid of makeDownloader
    
    * simpler notebook with pandas
    
    * resolve conflicts. oops!
    
    * install pandas
    
    * add head to metadata dump
    
    * Notebook templates as  notebook files
    
    * changelog
    
    * less redundant
    
    * missing a template value
    
    * was /search/search
    
    * use DataFrame for files output

--- context/app/routes_notebooks.py
+++ context/app/routes_notebooks.py
@@ -1,19 +1,36 @@
 from flask import (abort, request, Response, current_app)
+import json
+from pathlib import Path
+from string import Template
+
 import nbformat
 from nbformat.v4 import (new_notebook, new_markdown_cell, new_code_cell)
 
 from .utils import make_blueprint, get_url_base_from_request, entity_types, get_client
-from .notebooks import get_shared_cells, get_file_cells
 
 
 blueprint = make_blueprint(__name__)
 
 
-def _nb_response(name_stem, cells):
+def _nb_response_from_objs(name_stem, cells):
     nb = new_notebook()
     nb['cells'] = cells
+    return _nb_response(name_stem, nbformat.writes(nb))
+
+
+def _nb_response_from_dicts(name_stem, cells):
+    nb = {
+        'cells': cells,
+        'metadata': {},
+        'nbformat': 4,
+        'nbformat_minor': 5
+    }
+    return _nb_response(name_stem, json.dumps(nb))
+
+
+def _nb_response(name_stem, nb_str):
     return Response(
-        response=nbformat.writes(nb),
+        response=nb_str,
         headers={'Content-Disposition': f"attachment; filename={name_stem}.ipynb"},
         mimetype='application/x-ipynb+json'
     )
@@ -39,7 +56,7 @@ def details_notebook(type, uuid):
         *vitessce_conf.cells
     ]
 
-    return _nb_response(hubmap_id, cells)
+    return _nb_response_from_objs(hubmap_id, cells)
 
 
 @blueprint.route('/notebooks/<entity_type>.ipynb', methods=['POST'])
@@ -47,15 +64,23 @@ def notebook(entity_type):
     body = request.get_json()
     uuids = body.get('uuids')
     url_base = get_url_base_from_request()
-    search_url = (
-        current_app.config['ELASTICSEARCH_ENDPOINT']
-        + current_app.config['PORTAL_INDEX_PATH'])
-    cells = [
-        new_markdown_cell(
-            f'This notebook demonstrates how to work with HuBMAP APIs for {entity_type}:'),
-        new_code_cell('!pip install requests pandas'),
-        *get_shared_cells(uuids=uuids, url_base=url_base, entity_type=entity_type),
-    ]
+    cells = _get_metadata_cells(uuids=uuids, url_base=url_base, entity_type=entity_type)
     if entity_type == 'datasets':
-        cells += get_file_cells(search_url=search_url)
-    return _nb_response(entity_type, cells)
+        search_url = (
+            current_app.config['ELASTICSEARCH_ENDPOINT']
+            + current_app.config['PORTAL_INDEX_PATH'])
+        cells += _get_files_cells(search_url=search_url)
+    return _nb_response_from_dicts(entity_type, cells)
+
+
+def _get_metadata_cells(uuids=None, url_base=None, entity_type=None):
+    uuids_str = str(uuids)
+    template = Template((Path(__file__).parent / 'notebook/metadata.ipynb').read_text())
+    filled = template.substitute(uuids=uuids_str, url_base=url_base, entity_type=entity_type)
+    return json.loads(filled)['cells']
+
+
+def _get_files_cells(search_url):
+    template = Template((Path(__file__).parent / 'notebook/files.ipynb').read_text())
+    filled = template.substitute(search_url=search_url)
+    return json.loads(filled)['cells']
