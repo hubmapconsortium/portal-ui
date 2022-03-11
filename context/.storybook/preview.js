20dcc142e4bd44a0f984b239f01b7b5faaffb8e4
John Conroy <john-conroy@users.noreply.github.com>
2022-03-11 15:08:38 -0500

    Configure msw unhandled requests

--- context/.storybook/preview.js
+++ context/.storybook/preview.js
@@ -3,7 +3,19 @@ import React from 'react';
 import Providers from '../app/static/js/components/Providers';
 import { initialize, mswDecorator } from 'msw-storybook-addon';
 
-initialize();
+const allowConditions = [(url) => String(url).endsWith('thumbnail.jpg')];
+initialize({
+  onUnhandledRequest: ({ method, url }) => {
+    if (!allowConditions.some(conditionFn => conditionFn(url))) {
+      console.error(`Unhandled ${method} request to ${url}.
+
+        This exception has been only logged in the console, however, it's strongly recommended to resolve this error as you don't want unmocked data in Storybook stories.
+
+        If you wish to mock an error response, please refer to this guide: https://mswjs.io/docs/recipes/mocking-error-responses
+      `)
+    }
+  },
+});
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
@@ -15,7 +27,7 @@ export const parameters = {
   }
 }
 
-const mockEndpoints = {};
+const mockEndpoints = {assetsEndpoint: "https://assets.hubmapconsortium.org"};
 const mockGroupsToken=""
 
 export const decorators = [
