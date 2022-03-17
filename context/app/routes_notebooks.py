9c43c3330b162f258d680b9ecaa4d0931df95b54
mccalluc <mccalluc@users.noreply.github.com>
2022-03-17 16:39:03 -0400

    single function with kwargs to load templates

--- context/app/routes_notebooks.py
+++ context/app/routes_notebooks.py
@@ -67,23 +67,16 @@ def notebook(entity_type):
     body = request.get_json()
     uuids = body.get('uuids')
     url_base = get_url_base_from_request()
-    cells = _get_metadata_cells(uuids=uuids, url_base=url_base, entity_type=entity_type)
+    cells = _get_cells('metadata.ipynb', uuids=uuids, url_base=url_base, entity_type=entity_type)
     if entity_type == 'datasets':
         search_url = (
             current_app.config['ELASTICSEARCH_ENDPOINT']
             + current_app.config['PORTAL_INDEX_PATH'])
-        cells += _get_files_cells(search_url=search_url)
+        cells += _get_cells('files.ipynb', search_url=search_url)
     return _nb_response_from_dicts(entity_type, cells)
 
 
-def _get_metadata_cells(uuids=None, url_base=None, entity_type=None):
-    uuids_str = str(uuids)
-    template = Template((Path(__file__).parent / 'notebook/metadata.ipynb').read_text())
-    filled = template.substitute(uuids=uuids_str, url_base=url_base, entity_type=entity_type)
-    return json.loads(filled)['cells']
-
-
-def _get_files_cells(search_url):
-    template = Template((Path(__file__).parent / 'notebook/files.ipynb').read_text())
-    filled = template.substitute(search_url=search_url)
+def _get_cells(filename, **kwargs):
+    template = Template((Path(__file__).parent / 'notebook' / filename).read_text())
+    filled = template.substitute(kwargs)
     return json.loads(filled)['cells']
