6db7f2bbe9d5dedc23080c0379a565a64d61c9d1
mccalluc <mccalluc@users.noreply.github.com>
2022-03-23 15:18:43 -0400

    Smaller scope -> shorter variable name

--- context/app/routes_browse.py
+++ context/app/routes_browse.py
@@ -56,11 +56,11 @@ def details(type, uuid):
     }
 
     if type == 'dataset':
-        conf_cells_and_lifted_uuid = client.get_vitessce_conf_cells_and_lifted_uuid(entity)
+        conf_cells_uuid = client.get_vitessce_conf_cells_and_lifted_uuid(entity)
         flask_data.update({
-            'vitessce_conf': conf_cells_and_lifted_uuid.vitessce_conf.conf,
-            'has_notebook': conf_cells_and_lifted_uuid.vitessce_conf.cells is not None,
-            'vis_lifted_uuid': conf_cells_and_lifted_uuid.vis_lifted_uuid
+            'vitessce_conf': conf_cells_uuid.vitessce_conf.conf,
+            'has_notebook': conf_cells_uuid.vitessce_conf.cells is not None,
+            'vis_lifted_uuid': conf_cells_uuid.vis_lifted_uuid
         })
 
     template = 'pages/base_react.html'
