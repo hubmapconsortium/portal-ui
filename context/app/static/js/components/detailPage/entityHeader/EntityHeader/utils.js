0f2202f27544c11c242772e216a7433c6d0e065c
John Conroy <john-conroy@users.noreply.github.com>
2022-03-25 15:07:53 -0400

    Refactor header metadata util

--- context/app/static/js/components/detailPage/entityHeader/EntityHeader/utils.js
+++ context/app/static/js/components/detailPage/entityHeader/EntityHeader/utils.js
@@ -7,41 +7,24 @@ function extractAndLabelMetadata(obj, keys) {
   return acc;
 }
 
-function extractHeaderMetadata(assayMetadata) {
-  const { hubmap_id, entity_type } = assayMetadata;
-
+function extractHeaderMetadata({
+  entity_type,
+  mapped_data_types,
+  mapped_specimen_type,
+  mapped_metadata: { sex, race, age_value, age_unit } = {},
+  origin_sample: { mapped_organ } = {},
+}) {
   if (entity_type === 'Dataset') {
-    const {
-      origin_sample: { mapped_organ },
-      mapped_data_types,
-    } = assayMetadata;
-
-    return extractAndLabelMetadata({ hubmap_id, entity_type, mapped_organ, mapped_data_types }, [
-      'mapped_organ',
-      'mapped_data_types',
-    ]);
+    return extractAndLabelMetadata({ mapped_organ, mapped_data_types }, ['mapped_organ', 'mapped_data_types']);
   }
+
   if (entity_type === 'Sample') {
-    const {
-      origin_sample: { mapped_organ },
-      mapped_specimen_type,
-    } = assayMetadata;
-    return extractAndLabelMetadata({ hubmap_id, entity_type, mapped_organ, mapped_specimen_type }, [
-      'mapped_organ',
-      'mapped_specimen_type',
-    ]);
+    return extractAndLabelMetadata({ mapped_organ, mapped_specimen_type }, ['mapped_organ', 'mapped_specimen_type']);
   }
+
   if (entity_type === 'Donor') {
-    const { mapped_metadata = {} } = assayMetadata;
-    const { sex, race, age_value, age_unit } = mapped_metadata;
-
-    const sampleMetadata = { hubmap_id, entity_type, sex, race, age_value, age_unit };
-    const donorMetadata = extractAndLabelMetadata(sampleMetadata, ['sex', 'race']);
-
-    if ('age_value' in sampleMetadata && 'age_unit' in sampleMetadata) {
-      donorMetadata.age = { value: `${sampleMetadata.age_value} ${sampleMetadata.age_unit}`, label: 'age' };
-    }
-    return donorMetadata;
+    const age = age_value && age_unit ? `${age_value} ${age_unit}` : undefined;
+    return extractAndLabelMetadata({ sex, race, age }, ['sex', 'race', 'age']);
   }
 
   return {};
