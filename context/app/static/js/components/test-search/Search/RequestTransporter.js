fcf5d3c26470ba11645e9fa84c7bbc08fc6545aa
John Conroy <john-conroy@users.noreply.github.com>
2022-03-30 10:17:19 -0400

    Add request transporter

---
+++ context/app/static/js/components/test-search/Search/RequestTransporter.js
@@ -0,0 +1,18 @@
+// https://www.searchkit.co/docs/reference/searchkit-sdk#network-transport
+class RequestTransporter {
+  constructor(config) {
+    this.config = config;
+  }
+
+  async performRequest(requestBody) {
+    const response = await fetch(this.config.host, {
+      method: 'POST',
+      body: JSON.stringify(requestBody),
+      headers: { 'content-type': 'application/json' },
+    });
+    const json = await response.json();
+    return json;
+  }
+}
+
+export default RequestTransporter;
