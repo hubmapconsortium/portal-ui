d0ec881a4e9f845c3e3ef76bf62df20c34fc287b
John Conroy <john-conroy@users.noreply.github.com>
2022-04-13 15:19:15 -0400

    Add metadata field entities to webpack alias

--- context/build-utils/alias.js
+++ context/build-utils/alias.js
@@ -4,6 +4,7 @@ const { resolve } = require('path');
 const alias = {
   'metadata-field-descriptions$': resolve('./ingest-validation-tools/docs/field-descriptions.yaml'),
   'metadata-field-types$': resolve('./ingest-validation-tools/docs/field-types.yaml'),
+  'metadata-field-entities$': resolve('./ingest-validation-tools/docs/field-entities.yaml'),
   js: resolve(__dirname, '../app/static/js/'),
   assets: resolve(__dirname, '../app/static/assets/'),
   'shared-styles': resolve(__dirname, '../app/static/js/shared-styles/'),
