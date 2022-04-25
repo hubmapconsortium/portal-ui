98287ca3b431ce154af32581c6da8fa7e110401e
John Conroy <john-conroy@users.noreply.github.com>
2022-04-25 16:23:54 -0400

    Add configure search table to configure search

--- context/app/static/js/components/entity-search/sidebar/ConfigureSearch/ConfigureSearch.jsx
+++ context/app/static/js/components/entity-search/sidebar/ConfigureSearch/ConfigureSearch.jsx
@@ -2,9 +2,16 @@ import React, { useState } from 'react';
 import Button from '@material-ui/core/Button';
 
 import DialogModal from 'js/shared-styles/DialogModal';
+import ConfigureSearchTable from 'js/components/entity-search/sidebar/ConfigureSearchTable';
+import { useStore } from 'js/components/entity-search/SearchWrapper/store';
+import { useSelectedFields } from './hooks';
+
+import { Flex } from './style';
 
 function ConfigureSearch() {
   const [dialogIsOpen, setDialogIsOpen] = useState(false);
+  const { selectedFields, handleToggleCheckbox } = useSelectedFields();
+  const { setFields } = useStore();
 
   return (
     <>
@@ -16,6 +23,18 @@ function ConfigureSearch() {
         secondaryText="Add additional terms as a filter or as a column in the search results table."
         isOpen={dialogIsOpen}
         handleClose={() => setDialogIsOpen(false)}
+        maxWidth="md"
+        content={
+          <Flex>
+            <ConfigureSearchTable selectedFields={selectedFields} handleToggleCheckbox={handleToggleCheckbox} />
+          </Flex>
+        }
+        actions={
+          <>
+            <Button onClick={() => setDialogIsOpen(false)}>Cancel</Button>
+            <Button onClick={() => setFields(selectedFields)}>Save</Button>
+          </>
+        }
       />
     </>
   );
