8daf9c1b41ff024afd3e774401e26d4300b4bf38
John Conroy <john-conroy@users.noreply.github.com>
2022-05-02 13:59:14 -0400

    Add util to create data type to fields map

---
+++ context/app/static/js/components/entity-search/sidebar/DatasetConfigureSearchTable/utils.spec.js
@@ -0,0 +1,14 @@
+import { invertKeyToArrayMap } from './utils';
+
+test('should invert map', () => {
+  const initialMap = {
+    ablation_frequency_value: ['3D Imaging Mass Cytometry', 'Imaging Mass Cytometry'],
+    acquisition_id: ['3D Imaging Mass Cytometry', 'Imaging Mass Cytometry', 'Multiplex Ion Beam Imaging'],
+  };
+
+  expect(invertKeyToArrayMap(initialMap)).toEqual({
+    '3D Imaging Mass Cytometry': ['ablation_frequency_value', 'acquisition_id'],
+    'Imaging Mass Cytometry': ['ablation_frequency_value', 'acquisition_id'],
+    'Multiplex Ion Beam Imaging': ['acquisition_id'],
+  });
+});
