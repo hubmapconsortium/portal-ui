adbe0af4e048c7f6ff4393598cb28e8541669820
John Conroy <john-conroy@users.noreply.github.com>
2022-05-03 11:37:29 -0400

    Create data types select checkboxes

--- context/app/static/js/components/entity-search/sidebar/DataTypesSelect/DataTypesSelect.jsx
+++ context/app/static/js/components/entity-search/sidebar/DataTypesSelect/DataTypesSelect.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
-import Checkbox from '@material-ui/core/Checkbox';
 import FormGroup from '@material-ui/core/FormGroup';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
 import FormControl from '@material-ui/core/FormControl';
 
+import DataTypesSelectCheckboxes from 'js/components/entity-search/sidebar/DataTypesSelectCheckboxes';
 import { StyledPaper } from './style';
 
 function DataTypesSelect({ dataTypesToFieldsMap, handleToggleDataType, groups, handleToggleGroup }) {
@@ -11,22 +10,8 @@ function DataTypesSelect({ dataTypesToFieldsMap, handleToggleDataType, groups, h
     <StyledPaper>
       <FormControl component="fieldset">
         <FormGroup aria-label="data-types">
-          {Object.keys(groups).map((group) => (
-            <FormControlLabel
-              value={group}
-              control={<Checkbox color="primary" size="small" onClick={(event) => handleToggleGroup(event, group)} />}
-              label={group}
-            />
-          ))}
-          {Object.keys(dataTypesToFieldsMap).map((dataType) => (
-            <FormControlLabel
-              value={dataType}
-              control={
-                <Checkbox color="primary" size="small" onClick={(event) => handleToggleDataType(event, dataType)} />
-              }
-              label={dataType}
-            />
-          ))}
+          <DataTypesSelectCheckboxes values={Object.keys(groups)} eventHandler={handleToggleGroup} />
+          <DataTypesSelectCheckboxes values={Object.keys(dataTypesToFieldsMap)} eventHandler={handleToggleDataType} />
         </FormGroup>
       </FormControl>
     </StyledPaper>
