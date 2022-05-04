d22d3b578e3bb88d9b633fdc29ccb52898a50e47
John Conroy <john-conroy@users.noreply.github.com>
2022-05-04 14:10:29 -0400

    Add method to sort field configs

--- context/app/static/js/components/entity-search/sidebar/ConfigureSearchTable/utils.js
+++ context/app/static/js/components/entity-search/sidebar/ConfigureSearchTable/utils.js
@@ -6,4 +6,23 @@ function getMetadataFieldsSortedByEntityTypeThenFieldName(fields) {
     .map(([fieldName, fieldEntityType]) => ({ fieldName, fieldEntityType }));
 }
 
-export { getMetadataFieldsSortedByEntityTypeThenFieldName };
+function sortFieldsByConfigureGroup(fields) {
+  return Object.entries(fields).sort(
+    (
+      [, { configureGroup: configureGroupA, label: labelA }],
+      [, { configureGroup: configureGroupB, label: labelB }],
+    ) => {
+      // put 'General' configure group at top
+      if (configureGroupA === 'General' && configureGroupB !== 'General') {
+        return -1;
+      }
+      if (configureGroupA !== 'General' && configureGroupB === 'General') {
+        return 1;
+      }
+
+      return configureGroupA.localeCompare(configureGroupB) || labelA.localeCompare(labelB);
+    },
+  );
+}
+
+export { getMetadataFieldsSortedByEntityTypeThenFieldName, sortFieldsByConfigureGroup };
