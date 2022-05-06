f581ce7f6995823533dd990a4b0d7b3d99b1938b
John Conroy <john-conroy@users.noreply.github.com>
2022-05-06 12:21:35 -0400

    Add special case to handle accessing source sample in dataset documents

---
+++ context/app/static/js/components/entity-search/ResultsTable/utils.js
@@ -0,0 +1,15 @@
+import get from 'lodash/get';
+
+import { paths } from 'js/components/entity-search/SearchWrapper/metadataDocumentPaths';
+
+function getFieldFromHitFields(hitFields, identifier) {
+  const datasetSamplePath = paths.dataset.sample;
+  if (identifier.startsWith(datasetSamplePath)) {
+    // Unlike origin_sample, source_sample is an array and cannot be accessed with lodash/get
+    return hitFields?.source_sample?.[0].metadata[identifier.replace(`${datasetSamplePath}.`, '')];
+  }
+
+  return get(hitFields, identifier);
+}
+
+export { getFieldFromHitFields };
