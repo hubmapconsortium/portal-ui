c8eb9fcb9de1f57fe0fbd641f3c9081adece433b
mccalluc <mccalluc@users.noreply.github.com>
2022-05-10 13:38:44 -0400

    Factor out MatomoProviderWrapper

--- context/app/static/js/components/Providers.jsx
+++ context/app/static/js/components/Providers.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
-import { MatomoProvider, createInstance } from '@datapunt/matomo-tracker-react';
 
 import PropTypes from 'prop-types';
 import { MuiThemeProvider, StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
@@ -9,42 +8,11 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import GlobalStyles from 'js/components/globalStyles';
 import theme from '../theme';
 import GlobalFonts from '../fonts';
+import MatomoProviderWrapper from './MatomoProviderWrapper';
 
 const generateClassName = createGenerateClassName({
   disableGlobal: true,
   seed: 'portal',
-});
-
-// siteIds should correspond to the list at:
-// https://hubmapconsortium.matomo.cloud/index.php?module=SitesManager
-
-// eslint-disable-next-line no-restricted-globals
-const { host } = location;
-let siteId = 3;
-if (host === 'portal.hubmapconsortium.org') {
-  siteId = 1;
-} else if (host === 'localhost:5001') {
-  siteId = 2;
-}
-
-const instance = createInstance({
-  urlBase: 'https://hubmapconsortium.matomo.cloud/',
-  siteId,
-  // userId: 'UID76903202', // optional, default value: `undefined`.
-  // trackerUrl: 'https://LINK.TO.DOMAIN/tracking.php', // optional, default value: `${urlBase}matomo.php`
-  // srcUrl: 'https://LINK.TO.DOMAIN/tracking.js', // optional, default value: `${urlBase}matomo.js`
-  // disabled: false, // optional, false by default. Makes all tracking calls no-ops if set to true.
-  // heartBeat: { // optional, enabled by default
-  //   active: true, // optional, default value: true
-  //   seconds: 10 // optional, default value: `15
-  // },
-  // linkTracking: false, // optional, default value: true
-  // configurations: { // optional, default value: {}
-  //   // any valid matomo configuration, all below are optional
-  //   disableCookies: true,
-  //   setSecureCookie: true,
-  //   setRequestMethod: 'POST'
-  // }
 });
 
 const AppContext = React.createContext({});
@@ -53,7 +21,7 @@ function Providers(props) {
   const { endpoints, groupsToken, children } = props;
   // injectFirst ensures styled-components takes priority over mui for styling
   return (
-    <MatomoProvider value={instance}>
+    <MatomoProviderWrapper>
       <StylesProvider generateClassName={generateClassName} injectFirst>
         <GlobalFonts />
         <MuiThemeProvider theme={theme}>
@@ -66,7 +34,7 @@ function Providers(props) {
           </ThemeProvider>
         </MuiThemeProvider>
       </StylesProvider>
-    </MatomoProvider>
+    </MatomoProviderWrapper>
   );
 }
 
