055d945e46c57accb5efdb26cf82cc643cd7d51d
mccalluc <mccalluc@users.noreply.github.com>
2022-05-16 17:44:26 -0400

    fix user email in header

--- context/app/static/js/components/Header/HeaderContent/HeaderContent.jsx
+++ context/app/static/js/components/Header/HeaderContent/HeaderContent.jsx
@@ -16,7 +16,7 @@ import { HubmapLogo, Spacer, HeaderButton, FlexNoWrap } from './style';
 function HeaderContent({ anchorRef }) {
   const theme = useTheme();
   const shouldDisplayMenu = !useMediaQuery(theme.breakpoints.up('md'));
-  const { isAuthenticated, user_email } = useContext(AppContext);
+  const { isAuthenticated, userEmail } = useContext(AppContext);
   return (
     <>
       {shouldDisplayMenu && <Menu anchorRef={anchorRef} />}
@@ -53,7 +53,7 @@ function HeaderContent({ anchorRef }) {
         </>
       )}
       {shouldDisplayMenu && <Spacer />}
-      <LoginButton isAuthenticated={isAuthenticated} user_email={user_email} />
+      <LoginButton isAuthenticated={isAuthenticated} userEmail={userEmail} />
     </>
   );
 }
