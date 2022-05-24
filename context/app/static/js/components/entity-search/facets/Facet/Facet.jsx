9ec3b16fb4c2346b7bc7e0235d6794769a277099
John Conroy <john-conroy@users.noreply.github.com>
2022-05-24 11:49:15 -0400

    Fix var casing

--- context/app/static/js/components/entity-search/facets/Facet/Facet.jsx
+++ context/app/static/js/components/entity-search/facets/Facet/Facet.jsx
@@ -4,13 +4,13 @@ import FacetAccordion from 'js/components/entity-search/facets/FacetAccordion';
 import RangeSliderFacet from 'js/components/entity-search/searchkit-modifications/RangeSliderFacet';
 import SelectFacet from 'js/components/entity-search/facets/select/SelectFacet';
 
-const facetTypetoComponentMap = {
+const facetTypeToComponentMap = {
   RangeFacet: RangeSliderFacet,
   RefinementSelectFacet: SelectFacet,
 };
 
 function Facet({ facet }) {
-  const FacetComponent = facetTypetoComponentMap[facet.type];
+  const FacetComponent = facetTypeToComponentMap[facet.type];
 
   return (
     <FacetAccordion identifier={facet.identifier} label={facet.label}>
