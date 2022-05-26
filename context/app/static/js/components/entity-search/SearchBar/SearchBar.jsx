1d0f0121fdb4b862ee753571a7f97d5d8163699c
John Conroy <john-conroy@users.noreply.github.com>
2022-05-26 12:55:21 -0400

    Add text field id for accessibility

--- context/app/static/js/components/entity-search/SearchBar/SearchBar.jsx
+++ context/app/static/js/components/entity-search/SearchBar/SearchBar.jsx
@@ -2,10 +2,13 @@ import React from 'react';
 import { useSearchkit, useSearchkitQueryValue } from '@searchkit/client';
 import InputAdornment from '@material-ui/core/InputAdornment';
 import SearchRoundedIcon from '@material-ui/icons/SearchRounded';
-
 import TextField from '@material-ui/core/TextField';
 
+import { useStore } from 'js/components/entity-search/SearchWrapper/store';
+
 function SearchBar() {
+  const { entityType } = useStore();
+
   const [query, setQuery] = useSearchkitQueryValue();
   const api = useSearchkit();
   return (
@@ -16,8 +19,10 @@ function SearchBar() {
         api.search();
         event.preventDefault();
       }}
+      role="search"
     >
       <TextField
+        id={`${entityType}-search`}
         variant="outlined"
         onChange={(event) => setQuery(event.target.value)}
         value={query}
