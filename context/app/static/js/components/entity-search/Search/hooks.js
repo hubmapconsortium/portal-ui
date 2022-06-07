81469b92bf31671b0f14d55707d11714a6da6031
John Conroy <john-conroy@users.noreply.github.com>
2022-06-07 11:59:46 -0400

    Update hook to fetch all results uuids with searchkit query body

--- context/app/static/js/components/entity-search/Search/hooks.js
+++ context/app/static/js/components/entity-search/Search/hooks.js
@@ -1,7 +1,14 @@
-import { useState, useCallback } from 'react';
+import { useState, useCallback, useContext } from 'react';
+import { AppContext } from 'js/components/Providers';
 
-function useQueryBody() {
-  const [queryBody, setQueryBody] = useState(undefined);
+import useDeepCompareEffect from 'use-deep-compare-effect';
+
+import { fetchSearchData } from 'js/hooks/useSearchData';
+
+function useAllResultsUUIDs() {
+  const [queryBody, setQueryBody] = useState({});
+  const [allResultsUUIDs, setAllResultsUUIDS] = useState([]);
+  const { elasticsearchEndpoint, groupsToken } = useContext(AppContext);
 
   const setQueryBodyAndReturnBody = useCallback(
     (body) => {
@@ -11,7 +18,21 @@ function useQueryBody() {
     [setQueryBody],
   );
 
-  return { queryBody, setQueryBodyAndReturnBody };
+  useDeepCompareEffect(() => {
+    async function getAndSetAllUUIDs() {
+      const { query, post_filter } = queryBody;
+      const allResults = await fetchSearchData(
+        { query, post_filter, _source: false, size: 10000 },
+        elasticsearchEndpoint,
+        groupsToken,
+      );
+      // eslint-disable-next-line no-underscore-dangle
+      setAllResultsUUIDS(allResults.hits.hits.map((hit) => hit._id));
+    }
+    getAndSetAllUUIDs();
+  }, [queryBody]);
+
+  return { allResultsUUIDs, setQueryBodyAndReturnBody };
 }
 
-export { useQueryBody };
+export { useAllResultsUUIDs };
