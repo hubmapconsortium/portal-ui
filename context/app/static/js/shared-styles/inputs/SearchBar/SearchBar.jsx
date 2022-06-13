658e73feeeeea3b12e421a6d8ed299e72da0baac
John Conroy <john-conroy@users.noreply.github.com>
2022-06-13 19:41:07 -0400

    Pull reusable SearchBar to shared styles

---
+++ context/app/static/js/shared-styles/inputs/SearchBar/SearchBar.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import SearchRoundedIcon from '@material-ui/icons/SearchRounded';
+import TextField from '@material-ui/core/TextField';
+
+function SearchBar({ onChange, ...rest }) {
+  return (
+    <TextField
+      variant="outlined"
+      size="small"
+      margin="none"
+      InputProps={{
+        startAdornment: (
+          <InputAdornment position="start">
+            <SearchRoundedIcon color="primary" />
+          </InputAdornment>
+        ),
+      }}
+      {...rest}
+    />
+  );
+}
+
+export default SearchBar;
