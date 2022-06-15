a1fa1a1f60bdf2f182f527dfcd635bbb63ef0c74
John Conroy <62477388+john-conroy@users.noreply.github.com>
2022-06-15 16:55:21 -0400

    John conroy/configure search no filters (#2691)
    * Use controlled checkboxes for configure search datat types and set initial value
    
    * Define filtered fields outside of return statement
    
    * Show alert when there are no fields in configure search table
    
    * Ensure dialog modal is at 100% height despite content size
    
    * Change alert language
    
    * Add changelog
    
    * Separate ConfigureSearchTable into two components
    
    * Fix reducer bug by making shallow copy
    
    * Organize destructures per code review
    
    Co-authored-by: John Conroy <john-conroy@users.noreply.github.com>

--- context/app/static/js/components/entity-search/sidebar/ConfigureSearch/style.js
+++ context/app/static/js/components/entity-search/sidebar/ConfigureSearch/style.js
@@ -1,7 +1,12 @@
 import styled from 'styled-components';
+import Paper from '@material-ui/core/Paper';
 
 const Flex = styled.div`
   display: flex;
 `;
 
-export { Flex };
+const StyledPaper = styled(Paper)`
+  height: 100%;
+`;
+
+export { Flex, StyledPaper };
--- context/app/static/js/components/entity-search/sidebar/ConfigureSearchTable/ConfigureSearchTable.jsx
+++ context/app/static/js/components/entity-search/sidebar/ConfigureSearchTable/ConfigureSearchTable.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Checkbox from '@material-ui/core/Checkbox';
 import Paper from '@material-ui/core/Paper';
 import Table from '@material-ui/core/Table';
@@ -9,67 +9,55 @@ import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 
 import { useStore } from 'js/components/entity-search/SearchWrapper/store';
-import { filterFieldConfigs, getFieldEntriesSortedByConfigureGroup } from './utils';
-import { FlexGrow, StyledSearchBar } from './style';
 
 function ConfigureSearchTable({
   selectedFields,
   selectedFacets,
   handleToggleField,
   handleToggleFacet,
-  availableFields,
+  filteredFields,
 }) {
   const { numericFacetsProps } = useStore();
-  const [searchBarFieldName, setSearchBarFieldName] = useState('');
-
-  const handleChange = (event) => {
-    setSearchBarFieldName(event.target.value);
-  };
 
   return (
-    <FlexGrow>
-      <StyledSearchBar fullWidth value={searchBarFieldName} onChange={handleChange} />
-      <TableContainer component={Paper}>
-        <Table>
-          <TableHead>
-            <TableRow>
-              <TableCell />
-              <TableCell>Add Facet</TableCell>
-              <TableCell>Add Column</TableCell>
+    <TableContainer component={Paper}>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell />
+            <TableCell>Add Facet</TableCell>
+            <TableCell>Add Column</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {filteredFields.map(([fieldName, fieldConfig]) => (
+            <TableRow key={fieldName}>
+              <TableCell>{fieldConfig.label}</TableCell>
+              {['string', 'boolean'].includes(fieldConfig.type) || numericFacetsProps?.[fieldName] ? (
+                <TableCell>
+                  <Checkbox
+                    checked={fieldName in selectedFacets}
+                    size="small"
+                    color="primary"
+                    onChange={(event) => handleToggleFacet(event, fieldConfig)}
+                  />
+                </TableCell>
+              ) : (
+                <TableCell />
+              )}
+              <TableCell>
+                <Checkbox
+                  checked={fieldName in selectedFields}
+                  size="small"
+                  color="primary"
+                  onChange={(event) => handleToggleField(event, fieldConfig)}
+                />
+              </TableCell>
             </TableRow>
-          </TableHead>
-          <TableBody>
-            {filterFieldConfigs(getFieldEntriesSortedByConfigureGroup(availableFields), searchBarFieldName).map(
-              ([fieldName, fieldConfig]) => (
-                <TableRow key={fieldName}>
-                  <TableCell>{fieldConfig.label}</TableCell>
-                  {['string', 'boolean'].includes(fieldConfig.type) || numericFacetsProps?.[fieldName] ? (
-                    <TableCell>
-                      <Checkbox
-                        checked={fieldName in selectedFacets}
-                        size="small"
-                        color="primary"
-                        onChange={(event) => handleToggleFacet(event, fieldConfig)}
-                      />
-                    </TableCell>
-                  ) : (
-                    <TableCell />
-                  )}
-                  <TableCell>
-                    <Checkbox
-                      checked={fieldName in selectedFields}
-                      size="small"
-                      color="primary"
-                      onChange={(event) => handleToggleField(event, fieldConfig)}
-                    />
-                  </TableCell>
-                </TableRow>
-              ),
-            )}
-          </TableBody>
-        </Table>
-      </TableContainer>
-    </FlexGrow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
   );
 }
 
