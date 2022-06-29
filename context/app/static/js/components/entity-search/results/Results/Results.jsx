5cdbfa131a5112e81e5d50b94994beb36e240c23
John Conroy <john-conroy@users.noreply.github.com>
2022-06-29 11:06:03 -0400

    Move results table and tiles into a single component

---
+++ context/app/static/js/components/entity-search/results/Results/Results.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import ResultsTable from 'js/components/entity-search/ResultsTable';
+import Pagination from 'js/components/entity-search/results/Pagination';
+import { useStore } from 'js/components/entity-search/SearchWrapper/store';
+import ResultsTiles from 'js/components/entity-search/results/ResultsTiles/';
+import { ResultsLayout } from './style';
+
+function Results({ results }) {
+  const { view } = useStore();
+
+  return (
+    <ResultsLayout>
+      {results?.hits && (
+        <>
+          {view === 'table' && <ResultsTable hits={results.hits} />}
+          {view === 'tile' && <ResultsTiles hits={results.hits} />}
+
+          <Pagination pageHits={results.hits.page} />
+        </>
+      )}
+    </ResultsLayout>
+  );
+}
+
+export default Results;
