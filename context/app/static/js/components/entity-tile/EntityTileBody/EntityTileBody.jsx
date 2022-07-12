88394e69d08697cfb6458a89e1e2bafb937f20d8
John Conroy <john-conroy@users.noreply.github.com>
2022-07-12 14:52:24 -0400

    Remove icon from Entity Tile Body

--- context/app/static/js/components/entity-tile/EntityTileBody/EntityTileBody.jsx
+++ context/app/static/js/components/entity-tile/EntityTileBody/EntityTileBody.jsx
@@ -2,54 +2,38 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Typography from '@material-ui/core/Typography';
 
-import {
-  StyledDatasetIcon,
-  StyledSampleIcon,
-  StyledDonorIcon,
-  Flex,
-  TruncatedTypography,
-  StyledDivider,
-  StyledDiv,
-} from './style';
+import { Flex, TruncatedTypography, StyledDivider, StyledDiv } from './style';
 
 function EntityTileBody(props) {
   const { entity_type, id, entityData, invertColors } = props;
-  const icons = {
-    Donor: <StyledDonorIcon />,
-    Sample: <StyledSampleIcon />,
-    Dataset: <StyledDatasetIcon />,
-    Support: <StyledDatasetIcon />,
-  };
+
   return (
-    <Flex>
-      {icons[entity_type] || <StyledDatasetIcon />}
-      <StyledDiv>
-        <Typography component="h4" variant="h6">
-          {id}
-        </Typography>
-        {'origin_sample' in entityData && (
-          <TruncatedTypography variant="body2">{entityData.origin_sample.mapped_organ}</TruncatedTypography>
-        )}
-        {'mapped_specimen_type' in entityData && (
-          <TruncatedTypography variant="body2">{entityData.mapped_specimen_type}</TruncatedTypography>
-        )}
-        {'mapped_data_types' in entityData && (
-          <TruncatedTypography variant="body2">{entityData.mapped_data_types.join(', ')}</TruncatedTypography>
-        )}
-        {entity_type === 'Donor' && 'mapped_metadata' in entityData && (
-          <>
-            <Flex>
-              <Typography variant="body2">{entityData.mapped_metadata.sex}</Typography>
-              <StyledDivider flexItem orientation="vertical" $invertColors={invertColors} />
-              <Typography variant="body2">
-                {entityData.mapped_metadata.age_value} {entityData.mapped_metadata.age_unit}
-              </Typography>
-            </Flex>
-            <TruncatedTypography variant="body2">{entityData.mapped_metadata.race.join(', ')}</TruncatedTypography>
-          </>
-        )}
-      </StyledDiv>
-    </Flex>
+    <StyledDiv>
+      <Typography component="h4" variant="h6">
+        {id}
+      </Typography>
+      {'origin_sample' in entityData && (
+        <TruncatedTypography variant="body2">{entityData.origin_sample.mapped_organ}</TruncatedTypography>
+      )}
+      {'mapped_specimen_type' in entityData && (
+        <TruncatedTypography variant="body2">{entityData.mapped_specimen_type}</TruncatedTypography>
+      )}
+      {'mapped_data_types' in entityData && (
+        <TruncatedTypography variant="body2">{entityData.mapped_data_types.join(', ')}</TruncatedTypography>
+      )}
+      {entity_type === 'Donor' && 'mapped_metadata' in entityData && (
+        <>
+          <Flex>
+            <Typography variant="body2">{entityData.mapped_metadata.sex}</Typography>
+            <StyledDivider flexItem orientation="vertical" $invertColors={invertColors} />
+            <Typography variant="body2">
+              {entityData.mapped_metadata.age_value} {entityData.mapped_metadata.age_unit}
+            </Typography>
+          </Flex>
+          <TruncatedTypography variant="body2">{entityData.mapped_metadata.race.join(', ')}</TruncatedTypography>
+        </>
+      )}
+    </StyledDiv>
   );
 }
 
--- context/app/static/js/components/entity-tile/EntityTileBody/style.js
+++ context/app/static/js/components/entity-tile/EntityTileBody/style.js
@@ -1,27 +1,6 @@
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
 import Typography from '@material-ui/core/Typography';
 import Divider from '@material-ui/core/Divider';
-
-import { DatasetIcon, SampleIcon, DonorIcon } from 'js/shared-styles/icons';
-
-const iconStyle = css`
-  font-size: 1.3rem;
-  height: 25px;
-  margin-right: ${(props) => props.theme.spacing(1)}px;
-  color: ${(props) => (props.$invertColors ? props.theme.palette.white.main : props.theme.palette.primary.main)};
-`;
-
-const StyledDatasetIcon = styled(DatasetIcon)`
-  ${iconStyle};
-`;
-
-const StyledSampleIcon = styled(SampleIcon)`
-  ${iconStyle};
-`;
-
-const StyledDonorIcon = styled(DonorIcon)`
-  ${iconStyle};
-`;
 
 const Flex = styled.div`
   display: flex;
@@ -41,7 +20,7 @@ const StyledDivider = styled(Divider)`
 `;
 
 const StyledDiv = styled.div`
-  min-width: 0;
+  width: 100%;
 `;
 
-export { StyledDatasetIcon, StyledSampleIcon, StyledDonorIcon, Flex, TruncatedTypography, StyledDivider, StyledDiv };
+export { Flex, TruncatedTypography, StyledDivider, StyledDiv };
