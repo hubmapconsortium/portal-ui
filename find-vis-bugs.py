9e2e38f82d1defe5ef004afe65835bef288decd9
mccalluc <mccalluc@users.noreply.github.com>
2022-08-01 14:57:37 -0400

    closer to reproducing error

--- find-vis-bugs.py
+++ find-vis-bugs.py
@@ -6,9 +6,12 @@ import requests
 from csv import DictReader, excel_tab
 from io import StringIO
 
-from hubmap_commons.type_client import TypeClient
+from flask import Flask
 
-from portal_visualization.builder_factory import get_view_config_builder
+from context.app.api.client import ApiClient
+
+
+client = ApiClient()
 
 
 def main():
@@ -19,35 +22,29 @@ def main():
         default='https://portal.hubmapconsortium.org',
         help='Portal instance to download TSV and JSON from')
     args = parser.parse_args()
+    portal_url = args.url
 
-    tsv_url = f'{args.url}/metadata/v0/datasets.tsv'
-    tsv = requests.get(tsv_url).text
-    datasets = list(DictReader(StringIO(tsv), dialect=excel_tab))[1:]
-    uuids = [dataset['uuid'] for dataset in datasets]
-    errors = {}
-    for uuid in uuids[:10]:
-        dataset_url = f'{args.url}/browse/dataset/{uuid}.json'
-        dataset = requests.get(dataset_url).json()
-        error = get_error(dataset)
-        if error:
-            errors[uuid] = error
-    print(errors)
-
-
-def get_assay(name):
-    types_service_url = 'https://search.api.hubmapconsortium.org'
-    type_client = TypeClient(types_service_url)
-    return type_client.getAssayType(name)
-
-
-def get_error(dataset):
-    Builder = get_view_config_builder(entity=dataset, get_assay=get_assay)
-    builder = Builder(dataset, None, 'https://assets.hubmapconsortium.org')
-    warn(f'Using: {builder.__class__.__name__}')
-    try:
-        builder.get_conf_cells()
-    except Exception as e:
-        return e
+    app = Flask(__name__)
+    app.config.from_mapping({
+        'TYPE_SERVICE_ENDPOINT': 'https://search.api.hubmapconsortium.org',
+        'ASSETS_ENDPOINT': 'https://assets.hubmapconsortium.org',
+    })
+    with app.app_context():
+        tsv_url = f'{portal_url}/metadata/v0/datasets.tsv'
+        tsv = requests.get(tsv_url).text
+        datasets = list(DictReader(StringIO(tsv), dialect=excel_tab))[1:]
+        uuids = [dataset['uuid'] for dataset in datasets]
+        errors = {}
+        for uuid in uuids:
+            if uuid != 'c6a254b2dc2ed46b002500ade163a7cc':
+                continue
+            dataset_url = f'{portal_url}/browse/dataset/{uuid}.json'
+            dataset = requests.get(dataset_url).json()
+            try:
+                client.get_vitessce_conf_cells_and_lifted_uuid(dataset, wrap_error=False)
+            except Exception as e:
+                errors[uuid] = e
+        print(errors)
 
 
 def warn(s):
