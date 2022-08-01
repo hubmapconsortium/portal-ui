ada0f9675d2a24ca7df00c4569293e1341b75635
John Conroy <john-conroy@users.noreply.github.com>
2022-08-01 15:51:53 -0400

    Use mui collapse instead of custom spring animation

--- context/app/static/js/shared-styles/tables/ExpandableRow/ExpandableRow.jsx
+++ context/app/static/js/shared-styles/tables/ExpandableRow/ExpandableRow.jsx
@@ -1,18 +1,14 @@
-import React, { useRef } from 'react';
-
-import { animated } from 'react-spring';
+import React from 'react';
+import Collapse from '@material-ui/core/Collapse';
 
 import ExpandableRowCell from 'js/shared-styles/tables/ExpandableRowCell';
 import { SecondaryBackgroundTooltip } from 'js/shared-styles/tooltips';
-import { useExpandSpring } from 'js/hooks/useExpand';
 import ClickableRow from 'js/shared-styles/tables/ClickableRow';
 import { Provider, createStore, useStore } from './store';
 import { ExpandedRow, ExpandedCell, StyledExpandCollapseIcon } from './style';
 
 function ExpandableRowChild({ children, numCells, disabled, expandedContent, disabledTooltipTitle }) {
   const { isExpanded, toggleIsExpanded } = useStore();
-  const heightRef = useRef(null);
-  const styles = useExpandSpring(heightRef, 0, isExpanded);
 
   return (
     <>
@@ -28,12 +24,11 @@ function ExpandableRowChild({ children, numCells, disabled, expandedContent, dis
       </ClickableRow>
       <ExpandedRow $isExpanded={isExpanded}>
         <ExpandedCell colSpan={numCells} $isExpanded={isExpanded}>
-          <animated.div style={styles}>
+          <Collapse in={isExpanded} timeout="auto">
             {React.cloneElement(expandedContent, {
-              heightRef,
               isExpanded,
             })}
-          </animated.div>
+          </Collapse>
         </ExpandedCell>
       </ExpandedRow>
     </>
