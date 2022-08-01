6ab91e66b00fa309d378eaa97674f5af891cbaa5
John Conroy <john-conroy@users.noreply.github.com>
2022-08-01 15:18:56 -0400

    Add prop types to ClickableRow

--- context/app/static/js/shared-styles/tables/ClickableRow/ClickableRow.jsx
+++ context/app/static/js/shared-styles/tables/ClickableRow/ClickableRow.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { StyledRow } from './style';
 
 function ClickableRow({ onClick, disabled, children, label, ...rest }) {
@@ -15,5 +16,16 @@ function ClickableRow({ onClick, disabled, children, label, ...rest }) {
     </StyledRow>
   );
 }
+
+ClickableRow.propTypes = {
+  onClick: PropTypes.func.isRequired,
+  children: PropTypes.node.isRequired,
+  label: PropTypes.string.isRequired,
+  disabled: PropTypes.bool,
+};
+
+ClickableRow.defaultProps = {
+  disabled: false,
+};
 
 export default ClickableRow;
