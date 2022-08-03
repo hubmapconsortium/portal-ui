95841930b3889aaf30ab341eea0e32c859f069a2
mccalluc <mccalluc@users.noreply.github.com>
2022-08-03 16:17:19 -0400

    only link if active

--- context/app/static/js/components/workspaces/JobDetails.jsx
+++ context/app/static/js/components/workspaces/JobDetails.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 
 import { LightBlueLink } from 'js/shared-styles/Links';
 
+const ACTIVE = 'Active';
+
 function mapStatus(status) {
   return (
     {
       pending: 'Activating',
-      running: 'Active',
+      running: ACTIVE,
     }[status] || 'Inactive'
   );
 }
@@ -15,6 +17,9 @@ function JobDetails({ job }) {
   const status = mapStatus(job.status);
   const { url_domain, url_path } = job.job_details.current_job_details.connection_details;
 
+  if (status !== ACTIVE) {
+    return `Status: ${status}`;
+  }
   return <LightBlueLink href={`${url_domain}${url_path}`}>Status: {status}</LightBlueLink>;
 }
 
