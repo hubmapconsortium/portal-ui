728b1f1f6ab115e06e55c29a304ca77b8b9a99fe
mccalluc <mccalluc@users.noreply.github.com>
2022-08-04 09:02:02 -0400

    Add tests

--- context/app/static/js/components/workspaces/utils.spec.js
+++ context/app/static/js/components/workspaces/utils.spec.js
@@ -1,4 +1,4 @@
-import { mergeJobsIntoWorkspaces } from './utils';
+import { mergeJobsIntoWorkspaces, condenseJobs } from './utils';
 
 test('it should merge jobs into workspaces', () => {
   const workspaces = [{ id: 1, other_ws_info: true }];
@@ -17,4 +17,58 @@ test('it should merge jobs into workspaces', () => {
       ],
     },
   ]);
+});
+
+test('it should pick one active job if available', () => {
+  const jobs = [
+    {
+      status: 'pending',
+    },
+    {
+      status: 'running',
+      job_details: {
+        current_job_details: { connection_details: { url_domain: 'http://example.com/', url_path: 'this' } },
+      },
+    },
+    {
+      status: 'running',
+      job_details: {
+        current_job_details: { connection_details: { url_domain: 'http://example.com/', url_path: 'not-this' } },
+      },
+    },
+    {
+      status: 'other',
+    },
+  ];
+  const job = condenseJobs(jobs);
+  expect(job).toEqual({ status: 'Active', url: 'http://example.com/this' });
+});
+
+test('it should pick an activating job if no active jobs are available', () => {
+  const jobs = [
+    {
+      status: 'other',
+    },
+    {
+      status: 'pending',
+    },
+  ];
+  const job = condenseJobs(jobs);
+  expect(job).toEqual({ status: 'Activating' });
+});
+
+test('it should map unknown status codes', () => {
+  const jobs = [
+    {
+      status: 'other',
+    },
+  ];
+  const job = condenseJobs(jobs);
+  expect(job).toEqual({ status: 'Inactive' });
+});
+
+test('it should return null for an empty list', () => {
+  const jobs = [];
+  const job = condenseJobs(jobs);
+  expect(job).toEqual(null);
 });
