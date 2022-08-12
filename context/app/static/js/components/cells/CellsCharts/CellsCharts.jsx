509316bd4a51e507e6f759005f63429c5819f9a3
John Conroy <john-conroy@users.noreply.github.com>
2022-08-12 12:54:11 -0400

    Move everything in cells charts return inside of conditional

--- context/app/static/js/components/cells/CellsCharts/CellsCharts.jsx
+++ context/app/static/js/components/cells/CellsCharts/CellsCharts.jsx
@@ -19,33 +19,31 @@ function CellsCharts({ uuid, cellVariableName, minExpression, queryType, isExpan
   const clusterData = 'clusterData' in cellsData ? cellsData.clusterData : {};
 
   return (
-    <PaddedDiv>
-      {(isLoading || Object.keys(cellsData).length > 0) && (
-        <>
-          <div>
-            <ChartWrapper>
-              <ChartLoader isLoading={isLoading}>
-                <CellExpressionHistogram queryType={queryType} expressionData={expressionData} />
-              </ChartLoader>
-            </ChartWrapper>
-            <ChartWrapper>
-              <ChartLoader isLoading={isLoading}>
-                <DatasetClusterChart uuid={uuid} results={clusterData} />
-              </ChartLoader>
-            </ChartWrapper>
-          </div>
-          <StyledTypography>
-            {diagnosticInfo ? (
-              `${diagnosticInfo.timeWaiting.toFixed(2)} seconds to receive an API response for ${
-                diagnosticInfo.numCells
-              } cells.`
-            ) : (
-              <Skeleton />
-            )}
-          </StyledTypography>
-        </>
-      )}
-    </PaddedDiv>
+    (isLoading || Object.keys(cellsData).length > 0) && (
+      <PaddedDiv>
+        <div>
+          <ChartWrapper>
+            <ChartLoader isLoading={isLoading}>
+              <CellExpressionHistogram queryType={queryType} expressionData={expressionData} />
+            </ChartLoader>
+          </ChartWrapper>
+          <ChartWrapper>
+            <ChartLoader isLoading={isLoading}>
+              <DatasetClusterChart uuid={uuid} results={clusterData} />
+            </ChartLoader>
+          </ChartWrapper>
+        </div>
+        <StyledTypography>
+          {diagnosticInfo ? (
+            `${diagnosticInfo.timeWaiting.toFixed(2)} seconds to receive an API response for ${
+              diagnosticInfo.numCells
+            } cells.`
+          ) : (
+            <Skeleton />
+          )}
+        </StyledTypography>
+      </PaddedDiv>
+    )
   );
 }
 
