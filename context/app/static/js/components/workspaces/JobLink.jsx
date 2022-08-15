8d3f66d7fa4ed39c5349698100e085889002cdbb
mccalluc <mccalluc@users.noreply.github.com>
2022-08-15 16:03:08 -0400

    setinterval

--- context/app/static/js/components/workspaces/JobLink.jsx
+++ context/app/static/js/components/workspaces/JobLink.jsx
@@ -11,9 +11,6 @@ function JobLink({ workspace, job, children }) {
   function createHandleStart(workspaceId) {
     async function handleStart() {
       startJob({ workspaceId, workspacesEndpoint, workspacesToken });
-      // TODO: Open new tab
-      // eslint-disable-next-line no-alert
-      alert('TODO: Open a new tab that will poll until the job is started.');
     }
     return handleStart;
   }
--- context/app/static/js/pages/WorkspacePleaseWait/WorkspacePleaseWait.jsx
+++ context/app/static/js/pages/WorkspacePleaseWait/WorkspacePleaseWait.jsx
@@ -1,32 +1,28 @@
-import React, { useContext, useState, useEffect } from 'react';
+import React, { useContext } from 'react';
 
 import { AppContext } from 'js/components/Providers';
 
 function WorkspacePleaseWait({ id }) {
   const { workspacesEndpoint, workspacesToken } = useContext(AppContext);
 
-  const [job, setJob] = useState();
-
-  useEffect(() => {
-    async function fetchJob() {
-      const response = await fetch(`${workspacesEndpoint}/jobs/${id}`, {
-        headers: {
-          'Content-Type': 'application/json',
-          'UWS-Authorization': `Token ${workspacesToken}`,
-        },
-      });
-      const responseJson = await response.json();
-      const { url_path, url_domain } = responseJson.data.jobs[0].job_details.current_job_details.connection_details;
-      setJob(`${url_domain}${url_path}`);
+  async function loadIfUp() {
+    const response = await fetch(`${workspacesEndpoint}/jobs/${id}`, {
+      headers: {
+        'Content-Type': 'application/json',
+        'UWS-Authorization': `Token ${workspacesToken}`,
+      },
+    });
+    const responseJson = await response.json();
+    const { connection_details } = responseJson.data.jobs[0].job_details.current_job_details;
+    if (connection_details) {
+      const { url_path, url_domain } = connection_details;
+      document.location = `${url_domain}${url_path}`;
     }
-    fetchJob();
-  }, [workspacesEndpoint, id, workspacesToken]);
+  }
 
-  return (
-    <>
-      Please wait... <pre>{JSON.stringify(job, null, 2)}</pre>
-    </>
-  );
+  setInterval(loadIfUp, 1000);
+
+  return <>Please wait...</>;
 }
 
 export default WorkspacePleaseWait;
