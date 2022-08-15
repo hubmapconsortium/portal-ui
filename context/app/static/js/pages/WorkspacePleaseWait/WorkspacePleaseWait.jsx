78907cc1a271803ef8865404b5ed51f3f658486e
mccalluc <mccalluc@users.noreply.github.com>
2022-08-15 16:10:25 -0400

    Show status

--- context/app/static/js/pages/WorkspacePleaseWait/WorkspacePleaseWait.jsx
+++ context/app/static/js/pages/WorkspacePleaseWait/WorkspacePleaseWait.jsx
@@ -1,9 +1,11 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 
 import { AppContext } from 'js/components/Providers';
 
 function WorkspacePleaseWait({ id }) {
   const { workspacesEndpoint, workspacesToken } = useContext(AppContext);
+
+  const [status, setStatus] = useState();
 
   async function loadIfUp() {
     const response = await fetch(`${workspacesEndpoint}/jobs/${id}`, {
@@ -13,7 +15,9 @@ function WorkspacePleaseWait({ id }) {
       },
     });
     const responseJson = await response.json();
-    const { connection_details } = responseJson.data.jobs[0].job_details.current_job_details;
+    const { current_job_details } = responseJson.data.jobs[0].job_details;
+    setStatus(current_job_details.message);
+    const { connection_details } = current_job_details;
     if (connection_details) {
       const { url_path, url_domain } = connection_details;
       document.location = `${url_domain}${url_path}`;
@@ -22,7 +26,7 @@ function WorkspacePleaseWait({ id }) {
 
   setInterval(loadIfUp, 1000);
 
-  return <>Please wait...</>;
+  return <>Please wait... {status}</>;
 }
 
 export default WorkspacePleaseWait;
