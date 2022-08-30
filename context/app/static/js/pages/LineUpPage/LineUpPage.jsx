83ba9b7414ebe075e93a7b0f91be02f093520948
mccalluc <mccalluc@users.noreply.github.com>
2022-08-30 11:17:37 -0400

    linting: add space

--- context/app/static/js/pages/LineUpPage/LineUpPage.jsx
+++ context/app/static/js/pages/LineUpPage/LineUpPage.jsx
@@ -13,7 +13,7 @@ import metadataFieldTypes from 'metadata-field-types';
 
 function LineUpPage({ entities }) {
   const cleanEntities = entities.map((entity) =>
-    Object.fromEntries(Object.entries(entity).filter(([,v]) => v !== null)),
+    Object.fromEntries(Object.entries(entity).filter(([, value]) => value !== null)),
   );
   const firstRow = cleanEntities[0];
   const notEnumFields = new Set([
