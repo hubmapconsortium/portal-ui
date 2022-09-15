f038e3348b1872bece6b8378f49f602f1998d7c8
Chuck McCallum <mccalluc@users.noreply.github.com>
2022-09-15 13:19:35 -0400

    use find
    Co-authored-by: John Conroy <62477388+john-conroy@users.noreply.github.com>

--- context/app/static/js/components/workspaces/utils.js
+++ context/app/static/js/components/workspaces/utils.js
@@ -70,11 +70,7 @@ async function startJob({ workspaceId, workspacesEndpoint, workspacesToken, setM
 function getNotebookPath(workspace) {
   // TODO: Replace with current_workspace_details, when available.
   const { files } = workspace.workspace_details.request_workspace_details;
-  const nbFiles = files.filter(({ name }) => name.endsWith('.ipynb'));
-  if (nbFiles.length !== 1) {
-    return '';
-  }
-  return nbFiles[0].name;
+ const files.find({name} => name.endsWith('.ipynb'))?.name || '';
 }
 
 function mergeJobsIntoWorkspaces(jobs, workspaces) {
