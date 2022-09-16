ab06a10eaabe6ba2062ed6d386ee36cc149fab60
mccalluc <mccalluc@users.noreply.github.com>
2022-09-16 14:34:32 -0400

    get tests passing

--- context/app/static/js/components/workspaces/utils.spec.js
+++ context/app/static/js/components/workspaces/utils.spec.js
@@ -2,7 +2,7 @@ import { mergeJobsIntoWorkspaces, condenseJobs } from './utils';
 
 test('it should merge jobs into workspaces', () => {
   const workspaces = [{ id: 1, other_ws_info: true, status: 'active' }];
-  const jobs = [{ id: 42, workspace_id: 1, other_job_info: true }];
+  const jobs = [{ id: 42, workspace_id: 1, other_job_info: true, status: 'running' }];
   const mergedWorkspaces = mergeJobsIntoWorkspaces(jobs, workspaces);
   expect(mergedWorkspaces).toEqual([
     {
@@ -14,6 +14,7 @@ test('it should merge jobs into workspaces', () => {
           id: 42,
           workspace_id: 1,
           other_job_info: true,
+          status: 'running',
         },
       ],
     },
