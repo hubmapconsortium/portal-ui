1455792b0eca5e1155cc303d3fca223b665d148f
mccalluc <mccalluc@users.noreply.github.com>
2022-09-21 13:10:38 -0400

    Prod will log to matomo sites 1 and 4

---
+++ CHANGELOG-ga-history-migration.md
@@ -0,0 +1,2 @@
+- Log production to two matomo sites: This should be merged and deployed to prod shortly after GA migration completes, to minimize gap.
+  (In the future, when we've confirmed that the migration looks good, we'll turn off logging for the first matomo site.)
--- context/app/static/js/helpers/trackers.js
+++ context/app/static/js/helpers/trackers.js
@@ -2,7 +2,7 @@ import MatomoTracker from '@datapunt/matomo-tracker-js';
 import ReactGA from 'react-ga';
 import { readCookie } from 'js/helpers/functions';
 
-function getSiteId(location) {
+function getSiteIds(location) {
   const { host } = location;
 
   // siteIds should correspond to the list at:
@@ -10,11 +10,11 @@ function getSiteId(location) {
 
   switch (host) {
     case 'portal.hubmapconsortium.org':
-      return 1;
+      return [1, 4]; // TODO: Remove "1" when we can confirm successful GA migration.
     case 'localhost:5001':
-      return 2;
+      return [2];
     default:
-      return 3;
+      return [3];
   }
 }
 
@@ -23,44 +23,53 @@ function getUserType() {
   return readCookie('last_login') ? 'internal' : 'external';
 }
 
-const tracker = new MatomoTracker({
-  urlBase: 'https://hubmap.matomo.cloud/',
-  // eslint-disable-next-line no-restricted-globals
-  siteId: getSiteId(location),
-  // userId: 'UID76903202', // optional, default value: `undefined`.
-  // trackerUrl: 'https://hubmap.matomo.cloud/tracking.php', // optional, default value: `${urlBase}matomo.php`
-  // srcUrl: 'https://hubmap.matomo.cloud/tracking.js', // optional, default value: `${urlBase}matomo.js`
-  disabled: process.env.NODE_ENV === 'test', // Tracking calls should be no-ops during tests.
-  // heartBeat: { // optional, enabled by default
-  //   active: true, // optional, default value: true
-  //   seconds: 10 // optional, default value: `15
-  // },
-  // linkTracking: false, // optional, default value: true
-  // configurations: { // optional, default value: {}
-  //   // any valid matomo configuration, all below are optional
-  //   disableCookies: true,
-  //   setSecureCookie: true,
-  //   setRequestMethod: 'POST'
-  // }
-  configurations: { setCustomDimension: [1 /* user_type */, getUserType()] },
-});
+// eslint-disable-next-line no-restricted-globals
+const trackers = getSiteIds(location).map(
+  (id) =>
+    new MatomoTracker({
+      urlBase: 'https://hubmap.matomo.cloud/',
+      siteId: id,
+      // userId: 'UID76903202', // optional, default value: `undefined`.
+      // trackerUrl: 'https://hubmap.matomo.cloud/tracking.php', // optional, default value: `${urlBase}matomo.php`
+      // srcUrl: 'https://hubmap.matomo.cloud/tracking.js', // optional, default value: `${urlBase}matomo.js`
+      disabled: process.env.NODE_ENV === 'test', // Tracking calls should be no-ops during tests.
+      // heartBeat: { // optional, enabled by default
+      //   active: true, // optional, default value: true
+      //   seconds: 10 // optional, default value: `15
+      // },
+      // linkTracking: false, // optional, default value: true
+      // configurations: { // optional, default value: {}
+      //   // any valid matomo configuration, all below are optional
+      //   disableCookies: true,
+      //   setSecureCookie: true,
+      //   setRequestMethod: 'POST'
+      // }
+      configurations: { setCustomDimension: [1 /* user_type */, getUserType()] },
+    }),
+);
 
 ReactGA.initialize('UA-133341631-3', { testMode: process.env.NODE_ENV === 'test' });
 
 function trackPageView(path) {
-  tracker.trackPageView({ href: path });
+  trackers.forEach((tracker) => {
+    tracker.trackPageView({ href: path });
+  });
   ReactGA.pageview(path);
 }
 
 function trackEvent(event) {
-  tracker.trackEvent(event);
+  trackers.forEach((tracker) => {
+    tracker.trackEvent(event);
+  });
   ReactGA.event(event);
 }
 
 function trackLink(href, type) {
-  tracker.trackLink({
-    href,
-    linkType: type || 'link',
+  trackers.forEach((tracker) => {
+    tracker.trackLink({
+      href,
+      linkType: type || 'link',
+    });
   });
   ReactGA.event({
     category: type || 'Outbound Link',
@@ -71,8 +80,10 @@ function trackLink(href, type) {
 }
 
 function trackSiteSearch(keyword) {
-  tracker.trackSiteSearch({
-    keyword,
+  trackers.forEach((tracker) => {
+    tracker.trackSiteSearch({
+      keyword,
+    });
   });
   /*
   We currently call both trackEvent and trackSiteSearch:
