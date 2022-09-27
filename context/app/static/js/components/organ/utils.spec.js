ed7b7f9420f4a69cfa87477ef7a422b15be8d961
mccalluc <mccalluc@users.noreply.github.com>
2022-09-27 12:40:01 -0400

    update test

--- context/app/static/js/components/organ/utils.spec.js
+++ context/app/static/js/components/organ/utils.spec.js
@@ -1,12 +1,23 @@
 import { getSearchURL } from './utils';
 
-test('should construct search url', () => {
-  const searchURL = getSearchURL('Whatever', ['Elbow (Left)', 'Elbow (Right)']);
+test('construct search url w/o assay', () => {
+  const searchURL = getSearchURL({ entityType: 'Whatever', organTerms: ['Elbow (Left)', 'Elbow (Right)'] });
   const [path, query] = searchURL.split('?');
   expect(path).toEqual('/search');
   expect(decodeURI(query).split('&')).toEqual([
     'entity_type[0]=Whatever',
     'origin_sample.mapped_organ[0]=Elbow+(Left)',
     'origin_sample.mapped_organ[1]=Elbow+(Right)',
+  ]);
+});
+
+test('construct search url w/ assay', () => {
+  const searchURL = getSearchURL({ entityType: 'Whatever', organTerms: ['Nose'], assay: ['something FISH'] });
+  const [path, query] = searchURL.split('?');
+  expect(path).toEqual('/search');
+  expect(decodeURI(query).split('&')).toEqual([
+    'entity_type[0]=Whatever',
+    'origin_sample.mapped_organ[0]=Nose',
+    'mapped_data_types[0]=something+FISH',
   ]);
 });
