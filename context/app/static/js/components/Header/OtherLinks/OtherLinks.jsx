1f4f5c357a3653f4dacf855a98d25f99f41a7671
mccalluc <mccalluc@users.noreply.github.com>
2022-10-03 15:33:35 -0400

    add menu link

--- context/app/static/js/components/Header/OtherLinks/OtherLinks.jsx
+++ context/app/static/js/components/Header/OtherLinks/OtherLinks.jsx
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
 
+import { AppContext } from 'js/components/Providers';
 import DropdownLink from '../DropdownLink';
 
 function OtherLinks({ isIndented }) {
+  const { groupsToken } = useContext(AppContext);
+  const isLoggedIn = Boolean(groupsToken);
+
   return (
     <>
       <DropdownLink href="/collections" isIndented={isIndented}>
@@ -12,6 +16,11 @@ function OtherLinks({ isIndented }) {
       <DropdownLink href="/organ" isIndented={isIndented}>
         Organs
       </DropdownLink>
+      {isLoggedIn && (
+        <DropdownLink href="/cells" isIndented={isIndented}>
+          Molecular Data Queries
+        </DropdownLink>
+      )}
       {/* TODO: Add publications here, when ready. */}
     </>
   );
