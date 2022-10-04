0f2cee288e586a8dd3bf21e7a2b12b44abca8573
John Conroy <john-conroy@users.noreply.github.com>
2022-10-04 15:40:08 -0400

    Use available fields for donor sample configure search

--- context/app/static/js/components/entity-search/sidebar/DonorSampleConfigureSearchTable/hooks.js
+++
@@ -1,9 +0,0 @@
-import { useMetadataFieldConfigs } from 'js/components/entity-search/sidebar/ConfigureSearchTable/hooks';
-import { useStore } from 'js/components/entity-search/SearchWrapper/store';
-
-function useDonorSampleMetadatFieldConfigs() {
-  const { initialFields, initialFacets } = useStore();
-  return useMetadataFieldConfigs({ ...initialFields, ...initialFacets });
-}
-
-export { useDonorSampleMetadatFieldConfigs };
