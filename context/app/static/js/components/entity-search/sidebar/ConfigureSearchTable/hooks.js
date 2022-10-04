11225d376b2cf9a1b5939a53ea088730608d51e1
John Conroy <john-conroy@users.noreply.github.com>
2022-10-04 15:40:46 -0400

    Remove unused useMetadataFieldConfigs

--- context/app/static/js/components/entity-search/sidebar/ConfigureSearchTable/hooks.js
+++ context/app/static/js/components/entity-search/sidebar/ConfigureSearchTable/hooks.js
@@ -1,34 +1,5 @@
 import { useRef } from 'react';
-import produce from 'immer';
 import useResizeObserver from 'use-resize-observer/polyfilled';
-
-import metadataFieldtoEntityMap from 'metadata-field-entities';
-import { useStore } from 'js/components/entity-search/SearchWrapper/store';
-import { createField } from 'js/components/entity-search/SearchWrapper/utils';
-
-const relatedEntityTypesMap = {
-  donor: ['donor'],
-  sample: ['donor', 'sample'],
-  dataset: ['donor', 'sample'],
-};
-
-function useMetadataFieldConfigs(initialFieldConfigs) {
-  const { entityType } = useStore();
-  return Object.entries(metadataFieldtoEntityMap).reduce(
-    (acc, [fieldName, fieldEntityType]) => {
-      if (relatedEntityTypesMap[entityType].includes(fieldEntityType)) {
-        return produce(acc, (draft) => {
-          return {
-            ...draft,
-            ...createField({ fieldName, entityType }),
-          };
-        });
-      }
-      return acc;
-    },
-    { ...initialFieldConfigs },
-  );
-}
 
 function useTableHeadHeight() {
   const tableHeadRef = useRef(null);
@@ -37,4 +8,4 @@ function useTableHeadHeight() {
   return { tableHeadRef, tableHeadHeight };
 }
 
-export { useMetadataFieldConfigs, useTableHeadHeight };
+export { useTableHeadHeight };
