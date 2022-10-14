8b3e9bdf2741718fbdc531b685b27351a885f1fe
mccalluc <mccalluc@users.noreply.github.com>
2022-10-14 15:16:04 -0400

    minimal structure

--- etc/dev/schema-utils/validate_entities.py
+++ etc/dev/schema-utils/validate_entities.py
@@ -6,7 +6,8 @@ from pathlib import Path
 import json
 
 from jsonschema.validators import Draft7Validator
-from yaml import safe_load
+from jsonschema import RefResolver
+
 
 def main():
     parser = argparse.ArgumentParser()
@@ -16,17 +17,32 @@ def main():
         type=Path)
     parser.add_argument(
         '--schema',
-        default=Path(__file__).parent / 'schema/index.yaml',
+        default=Path(__file__).parent / 'schema/index.json',
         type=Path)
     args = parser.parse_args()
 
-    schema = safe_load(args.schema.read_text())
-    validator = Draft7Validator(schema)
+    schema = json.loads(args.schema.read_text())
+    validator = Draft7Validator(
+        schema=schema,
+        resolver=RefResolver(
+            base_uri=f"{(Path(__file__).parent / 'schema').as_uri()}/",
+            referrer=schema,
+        ))
 
     i = 0
     for entity_path in args.doc_dir.iterdir():
         entity = json.loads(entity_path.read_text())
-        validator.validate(entity)
+        errors = [
+            {
+                'message': e.message,
+                # 'absolute_schema_path': _as_path_string(e.absolute_schema_path),
+                # 'absolute_path': _as_path_string(e.absolute_path)
+            } for e in validator.iter_errors(entity)
+        ]
+        if errors:
+            print()
+            print(errors)
+            break
         print('.', end='', flush=True)
         i += 1
     print(f'\nValidated {i}')
