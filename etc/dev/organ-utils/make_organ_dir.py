601d4781998a9aaf87d95182acf409e07c045489
Bruce W. Herr II <bherr@indiana.edu>
2022-12-13 12:45:49 -0500

    Update organ info with latest HRA Release 4 (v1.3) Data and Code

--- etc/dev/organ-utils/make_organ_dir.py
+++ etc/dev/organ-utils/make_organ_dir.py
@@ -76,22 +76,23 @@ def main():
         # and https://github.com/hubmapconsortium/portal-ui/issues/2943 are resolved.
 
         # Why FMA? Resolve paired organs:
-        'FMA_7214', 'FMA_7213',
-        'FMA_24977', 'FMA_24978',
+        'fma7214', 'fma7213', 
+        'fma24977', 'fma24978',
+        'fma57991', 'fma57987',
+        'fma323951',
 
         # Just resolve paired organs:
         'UBERON_0004549', 'UBERON_0004548',
         'UBERON_0001222', 'UBERON_0001223',
         'UBERON_0004539', 'UBERON_0004538',
         'UBERON_0001303', 'UBERON_0001302',
-        '0001911',
 
         # Other data soures are higher or lower level:
         'UBERON_0002509', 'UBERON_0000079', 'UBERON_0001004', 'UBERON_0001465'
     }
     unexpected_unmatched = unmatched - expected_unmatched
     if unexpected_unmatched:
-        as_string = ", ".join(unexpected_unmatched)
+        as_string = ", ".join(sorted(unexpected_unmatched))
         raise Exception(f'Unexpected unmatched IDs: {as_string}')
 
     organs = [
