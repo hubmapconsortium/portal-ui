9808ce84b6e2c310b604888be723231f2b93eacb
John Conroy <john-conroy@users.noreply.github.com>
2023-01-24 15:57:12 -0500

    Add cells store

---
+++ context/app/static/js/components/cells/store.js
@@ -0,0 +1,51 @@
+import create from 'zustand';
+import { queryTypes } from 'js/components/cells/queryTypes';
+
+const types = {
+  setResults: 'SET_RESULTS',
+  setIsLoading: 'SET_IS_LOADING',
+  setMinExpressionLog: 'SET_MIN_EXPRESSION_LOG',
+  setMinCellPercentage: 'SET_MIN_CELL_PERCENTAGE',
+  setCellVariableNames: 'SET_CELL_VARIABLE_NAMES',
+  setQueryType: 'SET_QUERY_TYPE',
+};
+
+const reducer = (state, { type, payload }) => {
+  switch (type) {
+    case types.setResults:
+      return { results: payload };
+    case types.setIsLoading:
+      return { isLoading: payload };
+    case types.setMinExpressionLog:
+      return { minExpressionLog: payload };
+    case types.setMinCellPercentage:
+      return { minCellPercentage: payload };
+    case types.setCellVariableNames:
+      return { cellVariableNames: payload };
+    case types.setQueryType:
+      return { queryType: payload };
+    default:
+      return state;
+  }
+};
+
+const useStore = create((set, get) => ({
+  results: [],
+  isLoading: true,
+  minExpressionLog: 1,
+  minCellPercentage: 10,
+  cellVariableNames: [],
+  queryType: queryTypes.gene.value,
+  dispatch: (args) => set((state) => reducer(state, args)),
+  setResults: (results) => get().dispatch({ type: types.setResults, payload: results }),
+  setIsLoading: (isLoading) => get().dispatch({ type: types.setIsLoading, payload: isLoading }),
+  setMinExpressionLog: (minExpressionLog) =>
+    get().dispatch({ type: types.setMinExpressionLog, payload: minExpressionLog }),
+  setMinCellPercentage: (minCellPercentage) =>
+    get().dispatch({ type: types.setMinCellPercentage, payload: minCellPercentage }),
+  setCellVariableNames: (cellVariableNames) =>
+    get().dispatch({ type: types.setCellVariableNames, payload: cellVariableNames }),
+  setQueryType: (queryType) => get().dispatch({ type: types.setQueryType, payload: queryType }),
+}));
+
+export { useStore, reducer, types };
