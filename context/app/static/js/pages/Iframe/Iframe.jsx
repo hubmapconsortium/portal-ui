7da85c2a9919d46d40f5ca330dac3ddb53c36988
John Conroy <john-conroy@users.noreply.github.com>
2023-02-06 16:01:30 -0500

    Pass organ counts prop to entity counts iframe

--- context/app/static/js/pages/Iframe/Iframe.jsx
+++ context/app/static/js/pages/Iframe/Iframe.jsx
@@ -5,14 +5,14 @@ import Providers from 'js/components/Providers';
 const EntityCounts = lazy(() => import('js/components/home/EntityCounts'));
 const HuBMAPDatasetsChart = lazy(() => import('js/components/home/HuBMAPDatasetsChart'));
 
-function Switch() {
+function Switch({ organs_count }) {
   const { pathname } = window.location;
   const params = new URLSearchParams(window.location.search);
   const iri = params.get('iri');
 
   switch (pathname) {
     case '/iframe/entity-counts':
-      return <EntityCounts />;
+      return <EntityCounts organsCount={organs_count} />;
     case '/iframe/assay-barchart':
       return <HuBMAPDatasetsChart />;
     case '/iframe/organ':
@@ -28,14 +28,14 @@ function Switch() {
   }
 }
 
-function Iframe({ flaskData }) {
+function Iframe({ flaskData: { endpoints, organs_count } }) {
   return (
-    <Providers endpoints={flaskData.endpoints}>
+    <Providers endpoints={endpoints}>
       <base target="_parent" />
       {/* Set base to make sure links load in the parent of the iframe. */}
       <Suspense fallback={null}>
         {/* Required by lazy(), but we don't really need a please-wait. */}
-        <Switch />
+        <Switch organs_count={organs_count} />
       </Suspense>
     </Providers>
   );
