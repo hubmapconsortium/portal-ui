9fc618be7766a5b5c5c1dcf7ba77856635729019
John Conroy <john-conroy@users.noreply.github.com>
2023-02-07 14:03:13 -0500

    Add store provider to cells

--- context/app/static/js/pages/Cells/Cells.jsx
+++ context/app/static/js/pages/Cells/Cells.jsx
@@ -8,6 +8,7 @@ import QuerySelect from 'js/components/cells/QuerySelect';
 import CellsResults from 'js/components/cells/CellsResults';
 import CellsTutorial from 'js/components/cells/tutorial/CellsTutorial';
 import { withTutorialProvider } from 'js/shared-styles/tutorials/TutorialProvider';
+import { AccordionStepsProvider } from 'js/shared-styles/accordions/AccordionSteps/provider';
 
 function Cells() {
   // Refs for the step actions used to traverse forwards in the tutorial.
@@ -17,8 +18,26 @@ function Cells() {
   // Refs for the accordion steps used to traverse backwards in the tutorial.
   const queryTypeStepRef = useRef(null);
   const parametersStepRef = useRef(null);
+
+  const steps = [
+    {
+      heading: '1. Query Type',
+      content: <QuerySelect setParametersButtonRef={setParametersButtonRef} />,
+      ref: queryTypeStepRef,
+    },
+    {
+      heading: '2. Parameters',
+      content: <DatasetsSelectedByExpression runQueryButtonRef={runQueryButtonRef} />,
+      ref: parametersStepRef,
+    },
+    {
+      heading: '3. Results',
+      content: <CellsResults />,
+    },
+  ];
+
   return (
-    <>
+    <AccordionStepsProvider stepsLength={steps.length}>
       <CellsTutorial
         setParametersButtonRef={setParametersButtonRef}
         runQueryButtonRef={runQueryButtonRef}
@@ -32,27 +51,8 @@ function Cells() {
         Refine datasets to discover genomic and proteomic information including expression distribution and cluster
         membership. To begin your search, select a query type and the relevant parameters.
       </SectionPaper>
-      <AccordionSteps
-        id="cells-steps"
-        steps={[
-          {
-            heading: '1. Query Type',
-            content: <QuerySelect setParametersButtonRef={setParametersButtonRef} />,
-            ref: queryTypeStepRef,
-          },
-          {
-            heading: '2. Parameters',
-            content: <DatasetsSelectedByExpression runQueryButtonRef={runQueryButtonRef} />,
-            ref: parametersStepRef,
-          },
-          {
-            heading: '3. Results',
-            content: <CellsResults />,
-          },
-        ]}
-        isFirstStepOpen
-      />
-    </>
+      <AccordionSteps id="cells-steps" steps={steps} isFirstStepOpen />
+    </AccordionStepsProvider>
   );
 }
 
