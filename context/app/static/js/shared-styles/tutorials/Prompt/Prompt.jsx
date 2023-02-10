2c85e7df9e8b8df102eb75ad18bbfe047ad994d3
John Conroy <john-conroy@users.noreply.github.com>
2023-02-10 13:49:30 -0500

    Use tutorial store in prompt

--- context/app/static/js/shared-styles/tutorials/Prompt/Prompt.jsx
+++ context/app/static/js/shared-styles/tutorials/Prompt/Prompt.jsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
 import PropTypes from 'prop-types';
+
+import { useStore } from 'js/shared-styles/tutorials/TutorialProvider/store';
 
 import {
   CenteredDiv,
@@ -13,27 +15,32 @@ import {
   StyledButton,
 } from './style';
 
-function Prompt({ headerText, descriptionText, buttonText, buttonOnClick, buttonIsDisabled, closeOnClick }) {
+function Prompt({ headerText, descriptionText, buttonText, buttonIsDisabled }) {
+  const [isOpen, setIsOpen] = useState(true);
+  const { runTutorial } = useStore();
+
   return (
-    <StyledPaper>
-      <CenteredDiv>
-        <Flex>
-          <StyledInfoIcon />
-          <Typography variant="subtitle1" color="textPrimary">
-            {headerText}
-          </Typography>
-        </Flex>
-        <StyledTypography>{descriptionText}</StyledTypography>
-        <StyledButton variant="contained" onClick={buttonOnClick} disabled={buttonIsDisabled}>
-          {buttonText}
-        </StyledButton>
-      </CenteredDiv>
-      <div>
-        <IconButton aria-label="close" onClick={closeOnClick}>
-          <StyledCloseIcon />
-        </IconButton>
-      </div>
-    </StyledPaper>
+    isOpen && (
+      <StyledPaper>
+        <CenteredDiv>
+          <Flex>
+            <StyledInfoIcon />
+            <Typography variant="subtitle1" color="textPrimary">
+              {headerText}
+            </Typography>
+          </Flex>
+          <StyledTypography>{descriptionText}</StyledTypography>
+          <StyledButton variant="contained" onClick={runTutorial} disabled={buttonIsDisabled}>
+            {buttonText}
+          </StyledButton>
+        </CenteredDiv>
+        <div>
+          <IconButton aria-label="close" onClick={() => setIsOpen(false)}>
+            <StyledCloseIcon />
+          </IconButton>
+        </div>
+      </StyledPaper>
+    )
   );
 }
 
@@ -41,9 +48,7 @@ Prompt.propTypes = {
   headerText: PropTypes.string.isRequired,
   descriptionText: PropTypes.string.isRequired,
   buttonText: PropTypes.string.isRequired,
-  buttonOnClick: PropTypes.func.isRequired,
   buttonIsDisabled: PropTypes.bool.isRequired,
-  closeOnClick: PropTypes.func.isRequired,
 };
 
 export default Prompt;
