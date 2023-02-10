1b93e96f66a028547bfb94471090a06b00540d48
John Conroy <john-conroy@users.noreply.github.com>
2023-02-10 16:21:48 -0500

    Fix typo in test var

--- context/app/static/js/shared-styles/accordions/StepAccordion/StepAccordion.spec.js
+++ context/app/static/js/shared-styles/accordions/StepAccordion/StepAccordion.spec.js
@@ -7,7 +7,7 @@ import { AccordionStepsProvider } from '../AccordionSteps/provider';
 
 test('should show success icon when step completed text is provided', async () => {
   render(
-    <AccordionStepsProvider stepsLength={1} initialState={{ completeStepsText: { 0: 'Completed Step 0' } }}>
+    <AccordionStepsProvider stepsLength={1} initialState={{ completedStepsText: { 0: 'Completed Step 0' } }}>
       <StepAccordion index={0} summaryHeading="Step 0" content={<div />} />
     </AccordionStepsProvider>,
   );
