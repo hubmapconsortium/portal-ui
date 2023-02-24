eca342015b3c46a8c9888892bb03f7b75f7bb1c6
John Conroy <john-conroy@users.noreply.github.com>
2023-02-24 17:42:58 -0500

    Use panel list for collections panel list

--- context/app/static/js/components/CollectionsPanelList/CollectionsPanelList.jsx
+++ context/app/static/js/components/CollectionsPanelList/CollectionsPanelList.jsx
@@ -1,23 +1,18 @@
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
 
-import { PanelScrollBox } from 'js/shared-styles/panels';
-import Panel from 'js/shared-styles/panels/Panel';
+import PanelList from 'js/shared-styles/panels/PanelList';
 
 function CollectionsPanelList({ collectionsData }) {
-  return (
-    <PanelScrollBox>
-      {collectionsData.map(({ _source }) => (
-        <Panel
-          key={_source.uuid}
-          href={`/browse/collection/${_source.uuid}`}
-          title={_source.title}
-          secondaryText={_source.hubmap_id}
-          rightText={<Typography variant="caption">{`${_source.datasets.length} Datasets`}</Typography>}
-        />
-      ))}
-    </PanelScrollBox>
-  );
+  const panelsProps = collectionsData.map(({ _source }) => ({
+    key: _source.uuid,
+    href: `/browse/collection/${_source.uuid}`,
+    title: _source.title,
+    secondaryText: _source.hubmap_id,
+    rightText: <Typography variant="caption">{`${_source.datasets.length} Datasets`}</Typography>,
+  }));
+
+  return <PanelList panelsProps={panelsProps} />;
 }
 
 export default CollectionsPanelList;
