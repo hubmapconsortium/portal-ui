801ccf654f32b4c9aecf4ae903b4d8c7a6dac2f4
John Conroy <john-conroy@users.noreply.github.com>
2023-02-27 10:58:11 -0500

    Use panel list landing page in collections

--- context/app/static/js/pages/Collections/Collections.jsx
+++ context/app/static/js/pages/Collections/Collections.jsx
@@ -1,29 +1,31 @@
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
 
-import CollectionsPanelList from 'js/components/CollectionsPanelList';
 import { useSearchHits } from 'js/hooks/useSearchData';
 import { getAllCollectionsQuery } from 'js/helpers/queries';
-import { PageWrapper, StyledDescription } from './style';
+import PanelListLandingPage from 'js/shared-styles/panels/PanelListLandingPage';
+
+const description =
+  'Collections of HuBMAP datasets represent data from related experiments—such as assays performed on the same organ—or data that has been grouped for other reasons. In the future, it will be possible to reference collections through Document Object Identifiers (DOIs).';
 
 function Collections() {
   const { searchHits: collectionsData } = useSearchHits(getAllCollectionsQuery);
 
+  const panelsProps = collectionsData.map(({ _source }) => ({
+    key: _source.uuid,
+    href: `/browse/collection/${_source.uuid}`,
+    title: _source.title,
+    secondaryText: _source.hubmap_id,
+    rightText: <Typography variant="caption">{`${_source.datasets.length} Datasets`}</Typography>,
+  }));
+
   return (
-    <PageWrapper>
-      <Typography variant="h2" component="h1">
-        Collections
-      </Typography>
-      <Typography variant="subtitle1" color="primary">
-        {collectionsData.length > 0 && `${collectionsData.length} Collections`}
-      </Typography>
-      <StyledDescription>
-        Collections of HuBMAP datasets represent data from related experiments—such as assays performed on the same
-        organ—or data that has been grouped for other reasons. In the future, it will be possible to reference
-        collections through Document Object Identifiers (DOIs).
-      </StyledDescription>
-      {collectionsData.length > 0 && <CollectionsPanelList collectionsData={collectionsData} />}
-    </PageWrapper>
+    <PanelListLandingPage
+      title="Collections"
+      subtitle={collectionsData.length > 0 && `${collectionsData.length} Collections`}
+      description={description}
+      panelsProps={panelsProps}
+    />
   );
 }
 
