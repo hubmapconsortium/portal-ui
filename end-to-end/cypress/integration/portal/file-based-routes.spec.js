00385fef051643db5b9b8b412aeb8e9e07721072
John Conroy <john-conroy@users.noreply.github.com>
2023-03-02 11:39:27 -0500

    Add missing closing brackets

--- end-to-end/cypress/integration/portal/file-based-routes.spec.js
+++ end-to-end/cypress/integration/portal/file-based-routes.spec.js
@@ -1,22 +1,23 @@
-describe('file-based-routes', () => {
-  context('macbook-size', () => {
+describe("file-based-routes", () => {
+  context("macbook-size", () => {
     beforeEach(() => {
-      cy.viewport('macbook-15')
-    })
-    it('has working preview pages', () => {
-      cy.visit('/');
-      cy.contains('Resources').click();
-      cy.contains('Cell Type Annotations').click();
-      cy.contains('HuBMAP Data Portal Previews demonstrate functionality');
-      cy.contains('Rahul Satija');
+      cy.viewport("macbook-15");
+    });
+    it("has working preview pages", () => {
+      cy.visit("/");
+      cy.contains("Resources").click();
+      cy.contains("Cell Type Annotations").click();
+      cy.contains("HuBMAP Data Portal Previews demonstrate functionality");
+      cy.contains("Rahul Satija");
       // Vitessce loads, pulling data from the network.
       // Are incidental network requests ok, as long as tests don't depend on them?
     });
-    it('has links to docs pages', () => {
-      cy.visit('/');
-      cy.contains('Resources').click();
-      cy.contains('FAQ');
-      cy.contains('About');
-      cy.contains('Technical');
+    it("has links to docs pages", () => {
+      cy.visit("/");
+      cy.contains("Resources").click();
+      cy.contains("FAQ");
+      cy.contains("About");
+      cy.contains("Technical");
     });
+  });
 });
