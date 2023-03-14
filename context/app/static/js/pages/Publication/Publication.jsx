d868761fad5975e290994f9cf7d6569e5b337a9b
John Conroy <john-conroy@users.noreply.github.com>
2023-03-14 14:15:12 -0400

    Add browse route for publication

--- context/app/static/js/pages/Publication/Publication.jsx
+++ context/app/static/js/pages/Publication/Publication.jsx
@@ -1,51 +1,7 @@
 import React from 'react';
-import Typography from '@material-ui/core/Typography';
 
-import Markdown from 'js/components/Markdown';
-import VisualizationWrapper from 'js/components/detailPage/visualization/VisualizationWrapper';
-import SectionHeader from 'js/shared-styles/sections/SectionHeader';
-import EmailIconLink from 'js/shared-styles/Links/iconLinks/EmailIconLink';
-import OutboundLink from 'js/shared-styles/Links/OutboundLink';
-
-import { StyledPaper } from './style';
-
-function Publication({ metadata, markdown }) {
-  const { vitessce_conf, title, authors, manuscript, abstract } = metadata;
-  const { journal, url } = manuscript;
-
-  return (
-    <>
-      <Typography variant="subtitle1">Publication</Typography>
-      <SectionHeader variant="h1" component="h1">
-        {title}
-      </SectionHeader>
-      <StyledPaper>
-        <Typography variant="h4" component="h2">
-          Abstract
-        </Typography>
-        {abstract}
-        <Typography variant="h4" component="h2">
-          Manuscript
-        </Typography>
-        <b>{journal}</b>: <OutboundLink href={url}>{url}</OutboundLink>
-        <Typography variant="h4" component="h2">
-          Authors
-        </Typography>
-        {authors.long}
-        <Typography variant="h4" component="h2">
-          Contact
-        </Typography>
-        <b>Corresponding Author:</b>{' '}
-        {authors.corresponding.map((author) => (
-          <span key={author.name}>
-            {author.name} - <EmailIconLink email={`${author.email}`}>{author.email}</EmailIconLink>
-          </span>
-        ))}
-      </StyledPaper>
-      {Boolean(vitessce_conf) && <VisualizationWrapper vitData={vitessce_conf} />}
-      <Markdown markdown={markdown} />
-    </>
-  );
+function Publication() {
+  return <>Publication</>;
 }
 
 export default Publication;
