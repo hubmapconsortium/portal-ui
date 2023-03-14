2e2776014cba1832dad5365f001317b2564f8ea4
John Conroy <john-conroy@users.noreply.github.com>
2023-03-14 12:27:42 -0400

    Add hook to separate publications by status

---
+++ context/app/static/js/pages/Publications/hooks.js
@@ -0,0 +1,36 @@
+import { useSearchHits } from 'js/hooks/useSearchData';
+import { buildPublicationPanelProp } from './utils';
+
+const getAllPublicationsQuery = {
+  post_filter: { term: { 'entity_type.keyword': 'Publication' } },
+  size: 10000,
+};
+
+const publicationStatusMap = {
+  true: 'published',
+  false: 'preprint',
+};
+
+function usePublications() {
+  const { searchHits: publications } = useSearchHits(getAllPublicationsQuery);
+
+  const publicationsPanelsPropsSeparatedByStatus = publications.reduce(
+    (acc, publication) => {
+      const {
+        _source: { publication_status },
+      } = publication;
+
+      if (!publication_status) {
+        return acc;
+      }
+
+      acc[publicationStatusMap[publication_status]].push(buildPublicationPanelProp(publication));
+      return acc;
+    },
+    { published: [], preprint: [] },
+  );
+
+  return { publicationsPanelsPropsSeparatedByStatus, publicationsCount: publications.length };
+}
+
+export { usePublications };
