92ce1f9aa908407fa8f345d49b6e9ffc05ef8f92
John Conroy <john-conroy@users.noreply.github.com>
2023-03-25 16:47:01 -0400

    Add label and description for publication collections

--- context/app/static/js/components/publications/PublicationCollections/PublicationCollections.jsx
+++ context/app/static/js/components/publications/PublicationCollections/PublicationCollections.jsx
@@ -2,10 +2,22 @@ import React from 'react';
 
 import { buildCollectionsPanelsProps } from 'js/pages/Collections/utils';
 import PanelList from 'js/shared-styles/panels/PanelList';
+import LabelledSectionText from 'js/shared-styles/sections/LabelledSectionText';
+
+import { StyledSectionPaper } from './styles';
 
 function PublicationCollections({ collectionsData }) {
   const panelsProps = buildCollectionsPanelsProps(collectionsData);
-  return <PanelList panelsProps={panelsProps} />;
+  return (
+    <>
+      <StyledSectionPaper>
+        <LabelledSectionText label="Collections">
+          Data for this publication is available on the associated HuBMAP Data Collection page(s).
+        </LabelledSectionText>
+      </StyledSectionPaper>
+      <PanelList panelsProps={panelsProps} />;
+    </>
+  );
 }
 
 export default PublicationCollections;
