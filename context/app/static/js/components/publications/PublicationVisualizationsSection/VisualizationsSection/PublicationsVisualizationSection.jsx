74fc1057b5216e585f8c11d1edd4ee3d3b0e068b
John Conroy <john-conroy@users.noreply.github.com>
2023-03-27 11:21:48 -0400

    Update vignette accordion title

--- context/app/static/js/components/publications/PublicationVisualizationsSection/VisualizationsSection/PublicationsVisualizationSection.jsx
+++ context/app/static/js/components/publications/PublicationVisualizationsSection/VisualizationsSection/PublicationsVisualizationSection.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Accordion from '@material-ui/core/Accordion';
 import AccordionSummary from '@material-ui/core/AccordionSummary';
 import AccordionDetails from '@material-ui/core/AccordionDetails';
+import { Typography } from '@material-ui/core';
 
 import PublicationVignette from 'js/components/publications/PublicationVignette';
 
@@ -9,7 +10,9 @@ function PublicationsVisualizationSection({ vignette_data, uuid }) {
   return Object.entries(vignette_data).map(([k, v], i) => {
     return (
       <Accordion key={k} defaultExpanded={i === 0} TransitionProps={{ unmountOnExit: i !== 0 }}>
-        <AccordionSummary>{k}</AccordionSummary>
+        <AccordionSummary>
+          <Typography variant="subtitle1">{`Vignette ${i + 1}: ${v.name}`}</Typography>
+        </AccordionSummary>
         <AccordionDetails>
           <PublicationVignette vignette={v} uuid={uuid} vignetteDirName={k} />;
         </AccordionDetails>
