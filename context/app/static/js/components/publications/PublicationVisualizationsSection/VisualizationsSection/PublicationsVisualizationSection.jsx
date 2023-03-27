d661c4b8623b60741d52cea96b6a9397ea9878a0
John Conroy <john-conroy@users.noreply.github.com>
2023-03-27 11:01:58 -0400

    Loop over vignette data

--- context/app/static/js/components/publications/PublicationVisualizationsSection/VisualizationsSection/PublicationsVisualizationSection.jsx
+++ context/app/static/js/components/publications/PublicationVisualizationsSection/VisualizationsSection/PublicationsVisualizationSection.jsx
@@ -1,52 +1,11 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React from 'react';
 
-import { AppContext } from 'js/components/Providers';
-import VisualizationWrapper from 'js/components/detailPage/visualization/VisualizationWrapper';
-import { fillUrls } from './utils';
-
-async function fetchVitessceConf(assetsEndpoint, uuid, filePath, groupsToken) {
-  const urlHandler = (url, isZarr) => {
-    return `${url.replace('{{ base_url }}', `${assetsEndpoint}/${uuid}/data`)}${isZarr ? '' : `?token=${groupsToken}`}`;
-  };
-
-  const requestInitHandler = () => {
-    return {
-      headers: { Authorization: `Bearer ${groupsToken}` },
-    };
-  };
-  const response = await fetch(`${assetsEndpoint}/${uuid}/vignettes/vignette_01/${filePath}?token=${groupsToken}`, {
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  });
-
-  if (!response.ok) {
-    console.error('Assets API failed', response);
-    return undefined;
-  }
-  const conf = await response.json();
-  return fillUrls(conf, urlHandler, requestInitHandler);
-}
+import PublicationVignette from 'js/components/publications/PublicationVignette';
 
 function PublicationsVisualizationSection({ vignette_data, uuid }) {
-  const { assetsEndpoint, groupsToken } = useContext(AppContext);
-  const p = vignette_data.vignette_01.figures[0].file;
-
-  const [vitessceConfs, setVitessceConfs] = useState(undefined);
-
-  useEffect(() => {
-    async function getAndSetVitessceConf() {
-      const figuresConfs = await Promise.all(
-        vignette_data.vignette_01.figures.map((figure) => {
-          return fetchVitessceConf(assetsEndpoint, uuid, figure.file, groupsToken);
-        }),
-      );
-      setVitessceConfs(figuresConfs);
-    }
-    getAndSetVitessceConf();
-  }, [assetsEndpoint, groupsToken, p, uuid, vignette_data.vignette_01.figures]);
-
-  return vitessceConfs ? <VisualizationWrapper vitData={vitessceConfs[0]} uuid={uuid} hasNotebook={false} /> : null;
+  return Object.entries(vignette_data).map(([k, v]) => {
+    return <PublicationVignette vignette={v} uuid={uuid} key={k} vignetteDirName={k} />;
+  });
 }
 
 export default PublicationsVisualizationSection;
