5d54318a9f543cd09f73ba467be1fec5b623fb72
John Conroy <john-conroy@users.noreply.github.com>
2023-03-27 13:03:15 -0400

    Add primary color accordion summary

---
+++ context/app/static/js/shared-styles/accordions/PrimaryColorAccordionSummary/style.js
@@ -0,0 +1,17 @@
+import styled, { css } from 'styled-components';
+
+import AccordionSummary from '@material-ui/core/ExpansionPanelSummary';
+
+const PrimaryColorAccordionSummary = styled(AccordionSummary)`
+  ${(props) =>
+    props.$isExpanded &&
+    css`
+      background-color: ${props.$isExpanded ? props.theme.palette.primary.main : '#E0E0E0'};
+
+      div > * {
+        color: ${props.$isExpanded ? '#fff' : props.theme.palette.text.primary};
+      }
+    `}
+`;
+
+export { PrimaryColorAccordionSummary };
