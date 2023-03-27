e2bd4346876eeca5715c48087fbc6dc0731949c1
John Conroy <john-conroy@users.noreply.github.com>
2023-03-27 13:14:14 -0400

    Use styled component instead of inline styles

--- context/app/static/js/components/publications/PublicationVisualizationsSection/VisualizationsSection/PublicationsVisualizationSection.jsx
+++ context/app/static/js/components/publications/PublicationVisualizationsSection/VisualizationsSection/PublicationsVisualizationSection.jsx
@@ -1,11 +1,11 @@
 import React, { useState } from 'react';
 import Accordion from '@material-ui/core/ExpansionPanel';
-import AccordionDetails from '@material-ui/core/AccordionDetails';
 import Typography from '@material-ui/core/Typography';
 import ArrowDropUpRoundedIcon from '@material-ui/icons/ArrowDropUpRounded';
 
 import PublicationVignette from 'js/components/publications/PublicationVignette';
 import PrimaryColorAccordionSummary from 'js/shared-styles/accordions/PrimaryColorAccordionSummary';
+import { StyledAccordionDetails } from './style';
 
 function PublicationsVisualizationSection({ vignette_data, uuid }) {
   const [expandedIndex, setExpandedIndex] = useState(0);
@@ -25,9 +25,9 @@ function PublicationsVisualizationSection({ vignette_data, uuid }) {
         <PrimaryColorAccordionSummary $isExpanded={i === expandedIndex} expandIcon={<ArrowDropUpRoundedIcon />}>
           <Typography variant="subtitle1">{`Vignette ${i + 1}: ${v.name}`}</Typography>
         </PrimaryColorAccordionSummary>
-        <AccordionDetails style={{ 'flex-direction': 'column' }}>
+        <StyledAccordionDetails>
           <PublicationVignette vignette={v} uuid={uuid} vignetteDirName={k} />;
-        </AccordionDetails>
+        </StyledAccordionDetails>
       </Accordion>
     );
   });
---
+++ context/app/static/js/components/publications/PublicationVisualizationsSection/VisualizationsSection/style.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+import AccordionDetails from '@material-ui/core/AccordionDetails';
+
+const StyledAccordionDetails = styled(AccordionDetails)`
+  flex-direction: column;
+`;
+
+export { StyledAccordionDetails };
