08cbdda0f60f5522c507f0609755d1d3e6a02a77
John Conroy <john-conroy@users.noreply.github.com>
2023-03-27 10:36:39 -0400

    Rename state variable

--- context/app/static/js/components/publications/PublicationVisualizationsSection/VisualizationsSection/PublicationsVisualizationSection.jsx
+++ context/app/static/js/components/publications/PublicationVisualizationsSection/VisualizationsSection/PublicationsVisualizationSection.jsx
@@ -4,8 +4,17 @@ import { AppContext } from 'js/components/Providers';
 import VisualizationWrapper from 'js/components/detailPage/visualization/VisualizationWrapper';
 import { fillUrls } from './utils';
 
-async function fetchVitessceConf(assetsEndpoint, uuid, p, groupsToken) {
-  const response = await fetch(`${assetsEndpoint}/${uuid}/vignettes/vignette_01/${p}?token=${groupsToken}`, {
+async function fetchVitessceConf(assetsEndpoint, uuid, filePath, groupsToken) {
+  const urlHandler = (url, isZarr) => {
+    return `${url.replace('{{ base_url }}', `${assetsEndpoint}/${uuid}/data`)}${isZarr ? '' : `?token=${groupsToken}`}`;
+  };
+
+  const requestInitHandler = () => {
+    return {
+      headers: { Authorization: `Bearer ${groupsToken}` },
+    };
+  };
+  const response = await fetch(`${assetsEndpoint}/${uuid}/vignettes/vignette_01/${filePath}?token=${groupsToken}`, {
     headers: {
       'Content-Type': 'application/json',
     },
@@ -15,40 +24,29 @@ async function fetchVitessceConf(assetsEndpoint, uuid, p, groupsToken) {
     console.error('Assets API failed', response);
     return undefined;
   }
-  const results = await response.json();
-  return results;
+  const conf = await response.json();
+  return fillUrls(conf, urlHandler, requestInitHandler);
 }
 
 function PublicationsVisualizationSection({ vignette_data, uuid }) {
   const { assetsEndpoint, groupsToken } = useContext(AppContext);
   const p = vignette_data.vignette_01.figures[0].file;
 
-  const [v, setV] = useState(undefined);
+  const [vitessceConfs, setVitessceConfs] = useState(undefined);
 
   useEffect(() => {
     async function getAndSetVitessceConf() {
-      const results = await fetchVitessceConf(assetsEndpoint, uuid, p, groupsToken);
-
-      const urlHandler = (url, isZarr) => {
-        return `${url.replace('{{ base_url }}', `${assetsEndpoint}/${uuid}/data`)}${
-          isZarr ? '' : `?token=${groupsToken}`
-        }`;
-      };
-
-      const requestInitHandler = () => {
-        return {
-          headers: { Authorization: `Bearer ${groupsToken}` },
-        };
-      };
-
-      const filledConf = fillUrls(results, urlHandler, requestInitHandler);
-      setV(filledConf);
-      // setV(JSON.parse(JSON.stringify(results).replaceAll('{{ base_url }}', `${assetsEndpoint}/${uuid}/data`)));
+      const figuresConfs = await Promise.all(
+        vignette_data.vignette_01.figures.map((figure) => {
+          return fetchVitessceConf(assetsEndpoint, uuid, figure.file, groupsToken);
+        }),
+      );
+      setVitessceConfs(figuresConfs);
     }
     getAndSetVitessceConf();
-  }, [assetsEndpoint, groupsToken, p, uuid]);
+  }, [assetsEndpoint, groupsToken, p, uuid, vignette_data.vignette_01.figures]);
 
-  return v ? <VisualizationWrapper vitData={v} uuid={uuid} hasNotebook={false} /> : null;
+  return vitessceConfs ? <VisualizationWrapper vitData={vitessceConfs[0]} uuid={uuid} hasNotebook={false} /> : null;
 }
 
 export default PublicationsVisualizationSection;
