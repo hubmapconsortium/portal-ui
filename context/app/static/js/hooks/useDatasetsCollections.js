39904bcda422894a63161a28092efebb4e65ca48
John Conroy <62477388+john-conroy@users.noreply.github.com>
2023-04-19 11:45:21 -0400

    John conroy/use es for dataset collections (#3085)
    * Get collections which contain dataset from es
    
    * Add changelog
    
    ---------
    
    Co-authored-by: John Conroy <john-conroy@users.noreply.github.com>

--- context/app/static/js/hooks/useDatasetsCollections.js
+++ context/app/static/js/hooks/useDatasetsCollections.js
@@ -1,17 +1,21 @@
+import { useMemo } from 'react';
 import { useSearchHits } from 'js/hooks/useSearchData';
 import { getAllCollectionsQuery } from 'js/helpers/queries';
 
-function getCollectionsWhichContainDatasets(datasetsUUIDSet, collections) {
-  return collections.filter((collection) => {
-    // eslint-disable-next-line no-underscore-dangle
-    return collection._source?.datasets.some((dataset) => datasetsUUIDSet.has(dataset.uuid));
-  });
-}
-
 function useDatasetsCollections(datasetUUIDs) {
-  const { searchHits: collections } = useSearchHits(getAllCollectionsQuery);
-  const datasetUUIDSet = new Set(datasetUUIDs);
-  return getCollectionsWhichContainDatasets(datasetUUIDSet, collections);
+  const collectionsWithDatasetQuery = useMemo(() => {
+    return {
+      ...getAllCollectionsQuery,
+      query: {
+        terms: {
+          'datasets.uuid': datasetUUIDs,
+        },
+      },
+    };
+  }, [datasetUUIDs]);
+
+  const { searchHits: collections } = useSearchHits(collectionsWithDatasetQuery);
+  return collections;
 }
 
 export { useDatasetsCollections };
