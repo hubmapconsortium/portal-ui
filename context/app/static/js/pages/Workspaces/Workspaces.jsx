3a51da340361f4e6ab3e0a0e5153bb4a2ee301b1
John Conroy <john-conroy@users.noreply.github.com>
2023-04-20 22:45:10 -0400

    Use icon page title in workspaces

--- context/app/static/js/pages/Workspaces/Workspaces.jsx
+++ context/app/static/js/pages/Workspaces/Workspaces.jsx
@@ -1,24 +1,18 @@
 import React, { useContext } from 'react';
 
-import HeaderIcon from 'js/shared-styles/icons/HeaderIcon';
 import { ReactComponent as WorkspacesIcon } from 'assets/svg/workspaces.svg';
 import { AppContext } from 'js/components/Providers';
-import SectionHeader from 'js/shared-styles/sections/SectionHeader';
 import Description from 'js/shared-styles/sections/Description';
 import { LightBlueLink } from 'js/shared-styles/Links';
 import WorkspacesAuthenticated from 'js/components/workspaces/WorkspacesAuthenticated';
 
-import { FlexContainer } from './style';
+import IconPageTitle from 'js/shared-styles/pages/IconPageTitle';
 
 function Workspaces() {
   const { isAuthenticated, isWorkspacesUser } = useContext(AppContext);
   return (
     <>
-      <SectionHeader variant="h1" component="h1">
-        <FlexContainer>
-          <HeaderIcon component={WorkspacesIcon} /> My Workspaces
-        </FlexContainer>
-      </SectionHeader>
+      <IconPageTitle icon={WorkspacesIcon}>My Workspaces</IconPageTitle>
       {!(isAuthenticated && isWorkspacesUser) ? (
         <Description padding="20px">
           The workspaces feature is only available if logged in. <LightBlueLink href="/login">Log in</LightBlueLink> to
