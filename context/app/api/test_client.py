470793d6f0be1cea064bbc5c52e2a8c14fe277cc
John Conroy <62477388+john-conroy@users.noreply.github.com>
2023-04-27 14:48:25 -0400

    John conroy/handle hubmap request redirect [HMP-60] (#3073)
    * Initial handle s3 redirect on flask side
    
    * Use decorator to share request error handling
    
    * Remove decorator and simplify
    
    * Update file request
    
    * Add test for redirect
    
    * Add required status code to mocks
    
    * Add params to fix kw args errors
    
    * Add changelog
    
    * Add comment
    
    * Update comment
    
    * Fix pep8
    
    * Shorten comment for pep8
    
    * Update comment
    
    ---------
    
    Co-authored-by: John Conroy <john-conroy@users.noreply.github.com>

---
+++ context/app/api/test_client.py
@@ -0,0 +1,48 @@
+import json
+
+from .client import ApiClient
+
+
+mock_es = {
+    'hits': {
+        'total': {'value': 1},
+        'hits': [{'_source': {
+            'uuid': 'ABC123',
+                  'hubmap_id': 'HMB123.XYZ',
+                  'mapped_metadata': {
+                      'age_unit': ['eons'],
+                    'age_value': [42]
+                  }
+                  }}]
+    }
+}
+
+
+def mock_post_303(path, **kwargs):
+    class MockResponse():
+        def __init__(self):
+            self.status_code = 303
+            self.content = 's3-bucket-url'
+
+        def raise_for_status(self):
+            pass
+    return MockResponse()
+
+
+def mock_get_s3_json_file(path, **kwargs):
+    class MockResponse():
+        def __init__(self):
+            self.status_code = 200
+            self.content = json.dumps(mock_es)
+
+        def raise_for_status(self):
+            pass
+    return MockResponse()
+
+
+def test_s3_redirect(mocker):
+    mocker.patch('requests.post', side_effect=mock_post_303)
+    mocker.patch('requests.get', side_effect=mock_get_s3_json_file)
+    api_client = ApiClient()
+    response = api_client._request('search-api-url', body_json={'query': {}})
+    assert response == mock_es
