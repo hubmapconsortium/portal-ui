d78d6c7bae6b456582a3846f9d2b3e125d6edff8
Nikolay Akhmetov <nick.akhmetov@gmail.com>
2023-05-25 10:01:23 -0400

    Set default formatter in vscode workspace settings, rerun eslint formatting on each page

--- context/app/static/js/pages/Publication/Publication.jsx
+++ context/app/static/js/pages/Publication/Publication.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
-import { getCombinedDatasetStatus, getSectionOrder } from 'js/components/detailPage/utils';
 import ContributorsTable from 'js/components/detailPage/ContributorsTable/ContributorsTable';
-import PublicationsDataSection from 'js/components/publications/PublicationsDataSection';
-import PublicationsVisualizationSection from 'js/components/publications/PublicationVisualizationsSection/';
-import PublicationSummary from 'js/components/publications/PublicationSummary';
-import ProvSection from 'js/components/detailPage/provenance/ProvSection';
 import DetailLayout from 'js/components/detailPage/DetailLayout';
+import ProvSection from 'js/components/detailPage/provenance/ProvSection';
+import { getCombinedDatasetStatus, getSectionOrder } from 'js/components/detailPage/utils';
+import PublicationSummary from 'js/components/publications/PublicationSummary';
+import PublicationsVisualizationSection from 'js/components/publications/PublicationVisualizationsSection/';
+import PublicationsDataSection from 'js/components/publications/PublicationsDataSection';
 import useEntityStore from 'js/stores/useEntityStore';
 
 const entityStoreSelector = (state) => state.setAssayMetadata;
