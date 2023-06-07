25898907d7fcc5991b8c32cd9a3eae9427bdd5a6
Kevin Yoo <kevinyoo@Kevins-MacBook-Pro.local>
2023-06-07 00:42:44 -0700

    fixed deconstruction error

--- context/app/static/js/components/detailPage/Attribution/Attribution.jsx
+++ context/app/static/js/components/detailPage/Attribution/Attribution.jsx
@@ -5,7 +5,7 @@ import EmailIconLink from 'js/shared-styles/Links/iconLinks/EmailIconLink';
 import { FlexPaper } from './style';
 import SectionItem from '../SectionItem';
 
-function Attribution(group_name, created_by_user_displayname, created_by_user_email) {
+function Attribution({ group_name, created_by_user_displayname, created_by_user_email }) {
   return (
     <DetailPageSection id="attribution">
       <SectionHeader>Attribution</SectionHeader>
