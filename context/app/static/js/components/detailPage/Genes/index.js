fbf41101c689d4dc10993129e78903fc67a9ced1
Kevin Yoo <kevinyoo@Kevins-MacBook-Pro.local>
2023-06-07 21:20:02 -0700

    fix build testing error index.js in Genes

---
+++ context/app/static/js/components/detailPage/Genes/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Summary from './Summary';
+import SummaryBody from './SummaryBody';
+
+const Genes = () => {
+  return (
+    <>
+      <Summary />
+      <SummaryBody />
+    </>
+  );
+};
+export default Genes;
