39d8eb2b5d8c75e76003bff2112d01c1f7cddf07
Kevin Yoo <kevinyoo@Kevins-MacBook-Pro.local>
2023-06-07 22:12:31 -0700

    move AppContext to Context file

--- context/app/static/js/components/Contexts.jsx
+++ context/app/static/js/components/Contexts.jsx
@@ -1,8 +1,9 @@
 import React, { useContext } from 'react';
 
 const FlaskDataContext = React.createContext({});
+const AppContext = React.createContext({});
 
 FlaskDataContext.displayName = 'FlaskDataContext';
 
-export { FlaskDataContext };
+export { FlaskDataContext, AppContext };
 export const useFlaskDataContext = () => useContext(FlaskDataContext);
