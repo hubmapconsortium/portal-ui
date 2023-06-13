a7490fe2978e4d59b604adb920d12fe44910c2f2
Nikolay Akhmetov <nick.akhmetov@gmail.com>
2023-06-13 16:33:35 -0400

    fetch cell types list

--- context/app/routes_cell_types.py
+++ context/app/routes_cell_types.py
@@ -1,14 +1,18 @@
 from itertools import islice, groupby
-import time
 
-from flask import render_template, current_app, request
+from flask import render_template, current_app, request, jsonify
 
 from hubmap_api_py_client import Client
 from hubmap_api_py_client.errors import ClientError
+import requests
 
 from .utils import get_default_flask_data, make_blueprint, get_organs
 
 blueprint = make_blueprint(__name__)
+
+
+def _get_client(app):
+    return Client(app.config['XMODALITY_ENDPOINT'] + '/api/')
 
 
 @blueprint.route('/cell-types')
@@ -19,3 +23,13 @@ def cell_types_ui():
         title='Cell Types',
         flask_data={**get_default_flask_data(), 'organs': organs}
     )
+
+
+@blueprint.route('/cell-types/list.json')
+def cell_types_list():
+    celltype_token = requests.post(
+        'https://cells.api.hubmapconsortium.org/api/celltype/', {}).json()['results'][0]['query_handle']
+    print(celltype_token)
+    celltype_list = [result['grouping_name'] for result in requests.post('https://cells.api.hubmapconsortium.org/api/celltypeevaluation/', {
+        'key': celltype_token, 'set_type': 'cell_type', 'limit': 500}).json()['results']]
+    return jsonify(celltype_list)
