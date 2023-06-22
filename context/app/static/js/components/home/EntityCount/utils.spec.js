d0a3457cffe46bf25d04ab109f613c27e7be672b
kevin <kevinyooky@gmail.com>
2023-06-22 12:23:42 -0700

    test case addition for utils test file

--- context/app/static/js/components/home/EntityCount/utils.spec.js
+++ context/app/static/js/components/home/EntityCount/utils.spec.js
@@ -2,7 +2,7 @@ import { formatCount } from './utils';
 
 test('formatCount function should return correct format', () => {
   expect(formatCount(10500)).toEqual('10.5k+');
+  expect(formatCount(99999)).toEqual('99.9k+');
   expect(formatCount(9999)).toEqual(9999);
-  expect(formatCount(1000)).toEqual(1000);
   expect(formatCount(1234.56)).toEqual(1234.56);
 });
