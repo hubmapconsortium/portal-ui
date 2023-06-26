aa14983f3919b52f2b94ce21ad631965c4329624
kevin <kevinyooky@gmail.com>
2023-06-25 19:12:50 -0700

    remove login button

--- context/app/static/js/components/detailPage/BulkDataTransfer/NotLoggedIn/ProtectedData/DbGaP.jsx
+++ context/app/static/js/components/detailPage/BulkDataTransfer/NotLoggedIn/ProtectedData/DbGaP.jsx
@@ -1,31 +1,24 @@
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
-import BlockIcon from '@material-ui/icons/Block';
+import CheckCircleIcon from '@material-ui/icons/CheckCircle';
 import InfoIcon from '@material-ui/icons/Info';
 
 import { DetailSectionPaper } from 'js/shared-styles/surfaces';
-import { StyledTypography, LoginButton } from './style';
+import { StyledTypography } from './style';
 
 function GlobusAccess() {
   return (
     <DetailSectionPaper>
       <StyledTypography variant="h5">
-        Non-Consortium Members:
-        <BlockIcon color="error" />
+        Non-Consortium Members: Database of Genotypes and Phenotypes (dbGaP)
+        <CheckCircleIcon style={{ color: '#6c8938' }} />
         <InfoIcon />
       </StyledTypography>
       <Typography variant="body2">
-        Please{' '}
-        <a href="https://app.globus.org" target="_blank" rel="noopener noreferrer">
-          log in
-        </a>{' '}
-        for Globus access or email
-        <a href="mailto:help@hubmapconsortium.org"> help@hubmapconsortium.org </a>
-        with the dataset ID about the files you are trying to access.
+        This dataset contains protected-access human sequence data. If you are not a Consortium meber, you must access
+        these data through dbGaP if available. dbGaP authentication is required for downloading through these links.
+        View <a href="/">documentation</a> on how to attain dbGaP access.
       </Typography>
-      <LoginButton href="https://app.globus.org" target="_blank" variant="contained" color="primary">
-        Member Login
-      </LoginButton>
     </DetailSectionPaper>
   );
 }
