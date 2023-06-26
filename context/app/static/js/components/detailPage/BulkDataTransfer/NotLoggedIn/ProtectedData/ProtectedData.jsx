7e21d77fdae253d779af339993141b242b68f0c4
kevin <kevinyooky@gmail.com>
2023-06-25 17:39:55 -0700

    Gobus access message

--- context/app/static/js/components/detailPage/BulkDataTransfer/NotLoggedIn/ProtectedData/ProtectedData.jsx
+++ context/app/static/js/components/detailPage/BulkDataTransfer/NotLoggedIn/ProtectedData/ProtectedData.jsx
@@ -7,8 +7,12 @@ import { StyledTypography } from './style';
 function ProtectedData() {
   return (
     <DetailSectionPaper>
-      <StyledTypography variant="h6">Bulk Data Transfer</StyledTypography>
-      <Typography variant="body2">This data is protected and requires you to be logged in to access it.</Typography>
+      <StyledTypography variant="h5">HuBMAP Consortium Members: Globus Access</StyledTypography>
+      <Typography variant="body2">
+        Please log in for Globus access or email
+        <a href="mailto:help@hubmapconsortium.org"> help@hubmapconsortium.org </a>
+        with the dataset ID about the files you are trying to access.
+      </Typography>
     </DetailSectionPaper>
   );
 }
