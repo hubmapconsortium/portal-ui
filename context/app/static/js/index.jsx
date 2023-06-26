0ee4ea495c350ff241795a4e4f2087cb001bc1dd
Nikolay Akhmetov <nick.akhmetov@gmail.com>
2023-06-26 17:05:17 -0400

    WIP - adding typescript, updating eslint config

--- context/app/static/js/index.jsx
+++ context/app/static/js/index.jsx
@@ -12,9 +12,6 @@ import { setJsonLD } from './schema.org';
 //   console.warn('Schema validation errors', validation_errors);
 // }
 
-// Define global values for eslint https://eslint.org/docs/latest/use/configure/language-options#specifying-globals
-// Since they're propagated into a context from here, we don't need to define them in every file/in the main config
-/* global flaskData, groupsToken, isAuthenticated, userEmail, workspacesToken, userGroups */
 ReactDOM.render(
   window.location.pathname.startsWith('/iframe/') ? (
     <Iframe flaskData={flaskData} />
