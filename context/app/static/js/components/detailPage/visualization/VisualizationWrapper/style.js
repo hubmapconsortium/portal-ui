294629eedce9dffefbc7b326ad1aca9a02c39cfe
Nikolay Akhmetov <nick.akhmetov@gmail.com>
2023-06-27 10:39:18 -0400

    Add error boundary around visualization component

--- context/app/static/js/components/detailPage/visualization/VisualizationWrapper/style.js
+++ context/app/static/js/components/detailPage/visualization/VisualizationWrapper/style.js
@@ -1,11 +1,23 @@
+import { Paper } from '@material-ui/core';
 import styled from 'styled-components';
+
 import { vitessceFixedHeight } from '../Visualization/style';
 
+const height = `height: ${vitessceFixedHeight}px;`;
+
 const VisualizationBackground = styled.div`
-  height: ${vitessceFixedHeight}px;
+  ${height}
   display: flex;
   align-items: center;
   justify-content: center;
 `;
 
-export { VisualizationBackground };
+const VisualizationErrorBoundaryBackground = styled(Paper)`
+  ${height}
+  white-space: pre-line;
+  overflow-y: auto;
+  padding: 16px;
+  font-zie: 16px;
+`;
+
+export { VisualizationBackground, VisualizationErrorBoundaryBackground };
