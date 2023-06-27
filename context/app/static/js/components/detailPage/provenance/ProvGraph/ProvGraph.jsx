a939c7d60ffdad32ad34f3b1f389574143a37823
Nikolay Akhmetov <nick.akhmetov@gmail.com>
2023-06-27 17:10:13 -0400

    Fix `react/no-unstable-nested-components` with `DetailPanel` getting defined inside of `ProvGraph`

--- context/app/static/js/components/detailPage/provenance/ProvGraph/ProvGraph.jsx
+++ context/app/static/js/components/detailPage/provenance/ProvGraph/ProvGraph.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useCallback } from 'react';
 import PropTypes from 'prop-types';
 
 import SectionItem from 'js/components/detailPage/SectionItem';
@@ -10,6 +10,53 @@ import { StyledPaper, Flex, StyledTypography } from './style';
 import '@hms-dbmi-bgm/react-workflow-viz/dist/react-workflow-viz.min.css';
 
 const provenanceStoreSelector = (state) => state.setUUID;
+
+function DetailPanel({ node, timeKey, uuid, typeKey, idKey, getNameForActivity, getNameForEntity }) {
+  const { prov } = node.meta;
+
+  const typeEl =
+    typeKey in prov ? (
+      <SectionItem label="Type">{prov[typeKey]}</SectionItem>
+    ) : (
+      <SectionItem label="Type">{prov['prov:type']}</SectionItem>
+    );
+  const entityTypes = ['Donor', 'Sample', 'Dataset', 'Support'];
+  const idEl =
+    typeKey in prov && entityTypes.includes(prov[typeKey]) ? (
+      <SectionItem label="ID" ml>
+        <LightBlueLink href={`/browse/${prov[typeKey].toLowerCase()}/${prov['hubmap:uuid']}`}>
+          {prov[idKey]}
+        </LightBlueLink>
+      </SectionItem>
+    ) : null;
+  const createdEl =
+    timeKey in prov ? (
+      <SectionItem label="Created" ml>
+        {prov[timeKey]}
+      </SectionItem>
+    ) : null;
+  const actionsEl =
+    typeKey in prov && entityTypes.includes(prov[typeKey]) ? (
+      <SectionItem ml>
+        <ShowDerivedEntitiesButton
+          id={prov[idKey]}
+          getNameForActivity={getNameForActivity}
+          getNameForEntity={getNameForEntity}
+        />
+      </SectionItem>
+    ) : null;
+  return (
+    <StyledPaper>
+      <Flex>
+        {typeEl}
+        {idEl}
+        {createdEl}
+        {actionsEl}
+      </Flex>
+      {uuid === node.meta.prov['hubmap:uuid'] && <StyledTypography>* Indicates Current Entity Node</StyledTypography>}
+    </StyledPaper>
+  );
+}
 
 function ProvGraph({ provData, entity_type, uuid }) {
   const isOld = 'ex' in provData.prefix;
@@ -23,69 +70,33 @@ function ProvGraph({ provData, entity_type, uuid }) {
     setUUID(uuid);
   }, [setUUID, uuid]);
 
-  function getNameForActivity(id, prov) {
-    const activity = prov.activity[id];
-    return `${activity['hubmap:creation_action']} - ${activity[idKey]}`;
-  }
+  const getNameForActivity = useCallback(
+    function getNameForActivity(id, prov) {
+      const activity = prov.activity[id];
+      return `${activity['hubmap:creation_action']} - ${activity[idKey]}`;
+    },
+    [idKey],
+  );
 
-  function getNameForEntity(id, prov) {
-    const entity = prov.entity[id];
-    // NOTE: The initial entity node was not included in the sample data;
-    // Fallback to ID, if needed. https://github.com/hubmapconsortium/prov-vis/issues/15
-    return entity ? `${entity[typeKey]} - ${entity[idKey]}` : id;
-  }
+  const getNameForEntity = useCallback(
+    function getNameForEntity(id, prov) {
+      const entity = prov.entity[id];
+      // NOTE: The initial entity node was not included in the sample data;
+      // Fallback to ID, if needed. https://github.com/hubmapconsortium/prov-vis/issues/15
+      return entity ? `${entity[typeKey]} - ${entity[idKey]}` : id;
+    },
+    [idKey, typeKey],
+  );
 
-  function renderDetailPane(node) {
-    function DetailPanel() {
-      const { prov } = node.meta;
-
-      const typeEl =
-        typeKey in prov ? (
-          <SectionItem label="Type">{prov[typeKey]}</SectionItem>
-        ) : (
-          <SectionItem label="Type">{prov['prov:type']}</SectionItem>
-        );
-      const entityTypes = ['Donor', 'Sample', 'Dataset', 'Support'];
-      const idEl =
-        typeKey in prov && entityTypes.includes(prov[typeKey]) ? (
-          <SectionItem label="ID" ml>
-            <LightBlueLink href={`/browse/${prov[typeKey].toLowerCase()}/${prov['hubmap:uuid']}`}>
-              {prov[idKey]}
-            </LightBlueLink>
-          </SectionItem>
-        ) : null;
-      const createdEl =
-        timeKey in prov ? (
-          <SectionItem label="Created" ml>
-            {prov[timeKey]}
-          </SectionItem>
-        ) : null;
-      const actionsEl =
-        typeKey in prov && entityTypes.includes(prov[typeKey]) ? (
-          <SectionItem ml>
-            <ShowDerivedEntitiesButton
-              id={prov[idKey]}
-              getNameForActivity={getNameForActivity}
-              getNameForEntity={getNameForEntity}
-            />
-          </SectionItem>
-        ) : null;
-      return (
-        <StyledPaper>
-          <Flex>
-            {typeEl}
-            {idEl}
-            {createdEl}
-            {actionsEl}
-          </Flex>
-          {uuid === node.meta.prov['hubmap:uuid'] && (
-            <StyledTypography>* Indicates Current Entity Node</StyledTypography>
-          )}
-        </StyledPaper>
-      );
-    }
-    return node?.meta?.prov && <DetailPanel />;
-  }
+  const renderDetailPane = useCallback(
+    function renderDetailPane(node) {
+      if (node?.meta?.prov) {
+        return <DetailPanel node={node} timeKey={timeKey} uuid={uuid} idKey={idKey} typeKey={typeKey} />;
+      }
+      return null;
+    },
+    [idKey, timeKey, typeKey, uuid],
+  );
 
   return (
     <ProvVis
