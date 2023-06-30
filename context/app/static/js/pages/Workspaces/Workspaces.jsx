d3f92ea7074f3588e40f2104fbb1ff0feffdbae7
kevinyooky <45410262+kevinyooky@users.noreply.github.com>
2023-06-30 10:31:42 -0700

    Kevin yoo/hmp 156 update text for access permissions for workspaces (#3144)
    * text update
    
    * if else logic for text rendering
    
    * update text
    
    * changelog
    
    * changelog
    
    * text change
    
    * code refactor

--- context/app/static/js/pages/Workspaces/Workspaces.jsx
+++ context/app/static/js/pages/Workspaces/Workspaces.jsx
@@ -1,25 +1,44 @@
 import React from 'react';
 
+import EmailIconLink from 'js/shared-styles/Links/iconLinks/EmailIconLink';
 import { useAppContext } from 'js/components/Contexts';
 import WorkspacesTitle from 'js/components/workspaces/WorkspacesTitle';
 import { LightBlueLink } from 'js/shared-styles/Links';
 import WorkspacesAuthenticated from 'js/components/workspaces/WorkspacesAuthenticated';
-
 import { StyledDescription } from './style';
 
-function Workspaces() {
+function WorkspacesContent() {
   const { isAuthenticated, isWorkspacesUser } = useAppContext();
+
+  if (!isAuthenticated) {
+    return (
+      <StyledDescription>
+        The workspaces feature is only available if you are logged in and part of the allowed Globus group.{' '}
+        <LightBlueLink href="/login">Log in</LightBlueLink> to view saved workspaces or to begin a new workspace.
+      </StyledDescription>
+    );
+  }
+
+  if (!isWorkspacesUser) {
+    return (
+      <StyledDescription>
+        You must be a member of the allowed Globus group to access this feature. Email{' '}
+        <EmailIconLink variant="body2" email="help@hubmapconsortium.org">
+          help@hubmapconsortium.org
+        </EmailIconLink>{' '}
+        to gain access.
+      </StyledDescription>
+    );
+  }
+
+  return <WorkspacesAuthenticated />;
+}
+
+function Workspaces() {
   return (
     <>
       <WorkspacesTitle />
-      {!(isAuthenticated && isWorkspacesUser) ? (
-        <StyledDescription>
-          The workspaces feature is only available if logged in. <LightBlueLink href="/login">Log in</LightBlueLink> to
-          view saved workspaces or to begin a new workspace.
-        </StyledDescription>
-      ) : (
-        <WorkspacesAuthenticated />
-      )}
+      <WorkspacesContent />
     </>
   );
 }
