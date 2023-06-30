02a4743766c2035a503482f327b5cfd47fe2c0f0
Nikolay Akhmetov <nick.akhmetov@gmail.com>
2023-06-30 11:01:44 -0400

    remove unnecessary import

--- context/app/static/js/components/detailPage/provenance/ProvGraph/ProvGraph.jsx
+++ context/app/static/js/components/detailPage/provenance/ProvGraph/ProvGraph.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useCallback } from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 
 import SectionItem from 'js/components/detailPage/SectionItem';
