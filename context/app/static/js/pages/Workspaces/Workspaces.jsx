4b93d4a3facab44327f4d000fdc4b665a60ef46f
kevin <kevinyooky@gmail.com>
2023-06-30 09:23:47 -0700

    code refactor

--- context/app/static/js/pages/Workspaces/Workspaces.jsx
+++ context/app/static/js/pages/Workspaces/Workspaces.jsx
@@ -7,40 +7,38 @@ import { LightBlueLink } from 'js/shared-styles/Links';
 import WorkspacesAuthenticated from 'js/components/workspaces/WorkspacesAuthenticated';
 import { StyledDescription } from './style';
 
-function Workspaces() {
+function WorkspacesContent() {
   const { isAuthenticated, isWorkspacesUser } = useAppContext();
 
   if (!isAuthenticated) {
     return (
-      <>
-        <WorkspacesTitle />
-        <StyledDescription>
-          The workspaces feature is only available if you are logged in and part of the allowed Globus group.{' '}
-          <LightBlueLink href="/login">Log in</LightBlueLink> to view saved workspaces or to begin a new workspace.
-        </StyledDescription>
-      </>
+      <StyledDescription>
+        The workspaces feature is only available if you are logged in and part of the allowed Globus group.{' '}
+        <LightBlueLink href="/login">Log in</LightBlueLink> to view saved workspaces or to begin a new workspace.
+      </StyledDescription>
     );
   }
 
   if (!isWorkspacesUser) {
     return (
-      <>
-        <WorkspacesTitle />
-        <StyledDescription>
-          You must be a member of the allowed Globus group to access this feature. Email{' '}
-          <EmailIconLink variant="body2" email="help@hubmapconsortium.org">
-            help@hubmapconsortium.org
-          </EmailIconLink>{' '}
-          to gain access.
-        </StyledDescription>
-      </>
+      <StyledDescription>
+        You must be a member of the allowed Globus group to access this feature. Email{' '}
+        <EmailIconLink variant="body2" email="help@hubmapconsortium.org">
+          help@hubmapconsortium.org
+        </EmailIconLink>{' '}
+        to gain access.
+      </StyledDescription>
     );
   }
 
+  return <WorkspacesAuthenticated />;
+}
+
+function Workspaces() {
   return (
     <>
       <WorkspacesTitle />
-      <WorkspacesAuthenticated />
+      <WorkspacesContent />
     </>
   );
 }
