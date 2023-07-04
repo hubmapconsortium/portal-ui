6b8b6d59ec1ec2bc2260e2ad36db2ec3d9497546
kevin <kevinyooky@gmail.com>
2023-07-04 09:20:55 -0700

    tooltip

--- context/app/static/js/components/detailPage/BulkDataTransfer/NotLoggedIn/ProtectedData/DbGaP.jsx
+++ context/app/static/js/components/detailPage/BulkDataTransfer/NotLoggedIn/ProtectedData/DbGaP.jsx
@@ -4,6 +4,7 @@ import CheckCircleIcon from '@material-ui/icons/CheckCircle';
 import InfoIcon from '@material-ui/icons/Info';
 
 import { DetailSectionPaper } from 'js/shared-styles/surfaces';
+import { SecondaryBackgroundTooltip } from 'js/shared-styles/tooltips';
 import { Header } from './style';
 
 function GlobusAccess() {
@@ -12,7 +13,9 @@ function GlobusAccess() {
       <Header variant="h5">
         Non-Consortium Members: Database of Genotypes and Phenotypes (dbGaP)
         <CheckCircleIcon style={{ color: '#6c8938' }} />
-        <InfoIcon />
+        <SecondaryBackgroundTooltip title="The database of Genotypes and Phenotypes archive and distribute data and results from studies that have investigated the interaction of genotype and phenotype in humans">
+          <InfoIcon />
+        </SecondaryBackgroundTooltip>
       </Header>
       <Typography variant="body2">
         This dataset contains protected-access human sequence data. If you are not a Consortium meber, you must access
