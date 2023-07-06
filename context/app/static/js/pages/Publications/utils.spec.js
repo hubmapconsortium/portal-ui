22467af0a80fb5e37860703a531bd94521056efa
John Conroy <john-conroy@users.noreply.github.com>
2023-07-06 14:30:38 -0400

    Update test

--- context/app/static/js/pages/Publications/utils.spec.js
+++ context/app/static/js/pages/Publications/utils.spec.js
@@ -15,7 +15,10 @@ describe('countPublicationAggs', () => {
 
   test('should ignore counts for unexpected buckets', () => {
     expect(countPublicationAggs([...aggsBuckets, { doc_count: 20, key_as_string: 'geodude' }])).toEqual({
-      statuses: { true: { category: 'Peer Reviewed', count: 10 }, false: { category: 'Preprint', count: 5 } },
+      statuses: {
+        true: { category: 'Peer Reviewed', id: 'peer-reviewed', count: 10 },
+        false: { category: 'Preprint', id: 'preprint', count: 5 },
+      },
       publicationsCount: 15,
     });
   });
