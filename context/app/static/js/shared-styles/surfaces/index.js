9b40b9bb28ea85b2b947d21c753257d8135864b6
kevin <kevinyooky@gmail.com>
2023-07-05 20:46:49 -0700

    apply theme spacing

--- context/app/static/js/shared-styles/surfaces/index.js
+++ context/app/static/js/shared-styles/surfaces/index.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 import Paper from '@material-ui/core/Paper';
 
 const DetailSectionPaper = styled(Paper)`
-  padding: 16px;
+  padding: ${(props) => props.theme.spacing(2)}px;
 `;
 
 export { DetailSectionPaper };
