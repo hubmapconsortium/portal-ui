a3e0203b222aea9e63afcf01401d84da73bfbdc7
kevin <kevinyooky@gmail.com>
2023-07-06 14:12:37 -0700

    refactor Links shared style

--- context/app/static/js/shared-styles/Links/index.js
+++ context/app/static/js/shared-styles/Links/index.js
@@ -1,3 +1,20 @@
-import { StyledOpenInNewRoundedIcon, LightBlueLink } from './style';
+import React from 'react';
+import Link from '@material-ui/core/Link';
+import OpenInNewRoundedIcon from '@material-ui/icons/OpenInNewRounded';
+import styled from 'styled-components';
+
+function LinkWithoutUnderline(props) {
+  return <Link {...props} underline="none" />;
+}
+
+const LightBlueLink = styled(LinkWithoutUnderline)`
+  color: ${(props) => props.theme.palette.info.main};
+`;
+
+const StyledOpenInNewRoundedIcon = styled(OpenInNewRoundedIcon)`
+  font-size: 1.1rem;
+  vertical-align: text-bottom;
+  margin-left: ${(props) => props.theme.spacing(0.5)}px;
+`;
 
 export { StyledOpenInNewRoundedIcon, LightBlueLink };
