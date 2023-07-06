c92448dcf21fad7d1650b678ee99807964d941b4
kevin <kevinyooky@gmail.com>
2023-07-06 14:41:53 -0700

    Links component

--- context/app/static/js/components/detailPage/BulkDataTransfer/NotLoggedIn/ProtectedData/ProtectedData.jsx
+++ context/app/static/js/components/detailPage/BulkDataTransfer/NotLoggedIn/ProtectedData/ProtectedData.jsx
@@ -1,62 +1,15 @@
 import React from 'react';
-import Typography from '@material-ui/core/Typography';
-import Paper from '@material-ui/core/Paper';
-
-import { useFlaskDataContext } from 'js/components/Contexts';
-import OutboundIconLink from 'js/shared-styles/Links/iconLinks/OutboundIconLink';
-import { SecondaryBackgroundTooltip } from 'js/shared-styles/tooltips';
-import { InfoIcon } from 'js/shared-styles/icons';
-import { StyledContainer, LinkContainer, StyledLink } from './style';
+import { StyledContainer } from './style';
 import GlobusAccess from './GlobusAccess';
 import DbGaP from './DbGaP';
+import Links from '../../Links';
 
 function ProtectedData() {
-  const {
-    entity: { dbgap_study_url, dbgap_sra_experiment_url },
-  } = useFlaskDataContext();
-
-  const linksContentArray = [
-    {
-      title: 'dbGaP Study',
-      url: dbgap_study_url,
-      toolTip: '',
-      description: 'Navigate to the "Bioproject" or "Sequencing Read Archive" links to access the datasets.',
-      outBoundLink: '',
-    },
-    {
-      title: 'SRA Experiment',
-      url: dbgap_sra_experiment_url,
-      toolTip:
-        'SRA data, available through multiple cloud providers and NCBI servers, is the largest publicly available repository of high throughput sequencing data.',
-      description: 'Select the "Run" link on the page to download the dataset information.',
-      outBoundLink: 'https://www.ncbi.nlm.nih.gov/sra/docs/',
-    },
-  ];
-
-  const filteredLinksContentArray = linksContentArray.filter((link) => link.url);
-
   return (
     <StyledContainer>
       <GlobusAccess />
       <DbGaP />
-      <Paper>
-        {filteredLinksContentArray.map((link) => (
-          <LinkContainer>
-            <StyledLink variant="body1">
-              <OutboundIconLink href={link.url}>{link.title}</OutboundIconLink>
-              {link.toolTip && (
-                <SecondaryBackgroundTooltip title={link.toolTip}>
-                  <InfoIcon />
-                </SecondaryBackgroundTooltip>
-              )}
-            </StyledLink>
-            <Typography variant="body2">{link.description}</Typography>
-            {link.outBoundLink && (
-              <OutboundIconLink href={link.outBoundLink}> Here is additional documentation.</OutboundIconLink>
-            )}
-          </LinkContainer>
-        ))}
-      </Paper>
+      <Links />
     </StyledContainer>
   );
 }
