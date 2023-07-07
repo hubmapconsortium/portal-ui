93a6d2fecc65fb1333f1e3bcf1f8c06af71f3b01
Nikolay Akhmetov <nick.akhmetov@gmail.com>
2023-07-07 12:20:10 -0400

    Try waiting before searching for entity header?

--- end-to-end/cypress/e2e/portal/publication-page.cy.js
+++ end-to-end/cypress/e2e/portal/publication-page.cy.js
@@ -149,6 +149,7 @@ describe("Publication page", () => {
     it("has a visible Entity Header when the user scrolls down the page", () => {
       cy.findByTestId("entity-header").should("not.exist");
       cy.scrollTo("bottom");
+      cy.wait(50);
       cy.findByTestId("entity-header")
         .should("be.visible")
         .and("contain", title);
