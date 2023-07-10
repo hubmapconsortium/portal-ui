94b91f8ce5fa26c3b4531713b239c23793ef3020
Nikolay Akhmetov <nick.akhmetov@gmail.com>
2023-07-10 17:41:49 -0400

    Explanatory comments

--- context/app/static/js/components/publications/PublicationVignette/hooks.ts
+++ context/app/static/js/components/publications/PublicationVignette/hooks.ts
@@ -3,8 +3,9 @@ import { useAppContext } from 'js/components/Contexts';
 import { fillUrls } from './utils';
 import { PublicationVignette } from '../types';
 
+// Fetcher function that lets SWR fetch multiple urls at once
 const multiFetcher = (...urls: string[]) => {
-  const f = (url: string) => fetch(url).then((r) => r.json());
+  const f = (url: string) => fetch(url).then((response) => response.json());
   return Promise.all(urls.map((url) => f(url)));
 };
 
@@ -16,24 +17,25 @@ type PublicationVignetteConfsInput = {
 
 export function usePublicationVignetteConfs({ uuid, vignetteDirName, vignette }: PublicationVignetteConfsInput) {
   const { assetsEndpoint, groupsToken } = useAppContext();
+  // Extract file paths from the vignette object to form the urls to fetch for this vignette
   const urls = vignette.figures.map(
     ({ file }) => `${assetsEndpoint}/${uuid}/vignettes/${vignetteDirName}/${file}?token=${groupsToken}`,
   );
-  const urlHandler = (url: string, isZarr: boolean) => {
-    return `${url.replace('{{ base_url }}', `${assetsEndpoint}/${uuid}/data`)}${isZarr ? '' : `?token=${groupsToken}`}`;
-  };
-
-  const requestInitHandler = () => {
-    return {
-      headers: { Authorization: `Bearer ${groupsToken}` },
-    };
-  };
   const { data } = useSWR(urls, multiFetcher, {
     revalidateOnFocus: false,
     revalidateOnReconnect: false,
   });
 
   if (data) {
+    const urlHandler = (url: string, isZarr: boolean) => {
+      return `${url.replace('{{ base_url }}', `${assetsEndpoint}/${uuid}/data`)}${isZarr ? '' : `?token=${groupsToken}`}`;
+    };
+  
+    const requestInitHandler = () => {
+      return {
+        headers: { Authorization: `Bearer ${groupsToken}` },
+      };
+    };
     // Formats the vitessce config data to replace the {{ base_url }} placeholder with the actual url.
     // TODO: Fix this any; I couldn't figure out how to import the appropriate type from Vitessce.
     const formattedData: any[] = data.map(d => fillUrls(d, urlHandler, requestInitHandler));
