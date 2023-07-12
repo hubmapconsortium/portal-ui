4d3cdfca13baef8605e209c2f382b87dc8b4b668
John Conroy <john-conroy@users.noreply.github.com>
2023-07-12 14:12:11 -0400

    Separate useDatasetsCollections query

--- context/app/static/js/hooks/useDatasetsCollections.js
+++ context/app/static/js/hooks/useDatasetsCollections.js
@@ -1,23 +1,21 @@
-import { useMemo } from 'react';
 import { useSearchHits } from 'js/hooks/useSearchData';
 import { getAllCollectionsQuery } from 'js/helpers/queries';
 
-function useDatasetsCollections(datasetUUIDs) {
-  const datasetUUIDsString = JSON.stringify(datasetUUIDs.sort((a, b) => a - b));
-  const collectionsWithDatasetQuery = useMemo(
-    () => ({
-      ...getAllCollectionsQuery,
-      query: {
-        terms: {
-          'datasets.uuid': JSON.parse(datasetUUIDsString),
-        },
+function buildCollectionsWithDatasetQuery(datasetUUIDs) {
+  return {
+    ...getAllCollectionsQuery,
+    query: {
+      terms: {
+        'datasets.uuid': datasetUUIDs,
       },
-    }),
-    [datasetUUIDsString],
-  );
+    },
+  };
+}
 
-  const { searchHits: collections } = useSearchHits(collectionsWithDatasetQuery);
+function useDatasetsCollections(datasetUUIDs) {
+  const query = buildCollectionsWithDatasetQuery(datasetUUIDs);
+  const { searchHits: collections } = useSearchHits(query);
   return collections;
 }
 
-export { useDatasetsCollections };
+export { useDatasetsCollections, buildCollectionsWithDatasetQuery };
