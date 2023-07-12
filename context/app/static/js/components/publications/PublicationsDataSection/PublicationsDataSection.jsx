bb60f1ce4b0512d55f1aa2a8f3026155b22a9ea4
John Conroy <john-conroy@users.noreply.github.com>
2023-07-12 14:24:30 -0400

    Only show collection for associated collection if defined

--- context/app/static/js/components/publications/PublicationsDataSection/PublicationsDataSection.jsx
+++ context/app/static/js/components/publications/PublicationsDataSection/PublicationsDataSection.jsx
@@ -2,14 +2,20 @@ import React from 'react';
 
 import PublicationRelatedEntities from 'js/components/publications/PublicationRelatedEntities';
 import PublicationCollections from 'js/components/publications/PublicationCollections';
-import { useDatasetsCollections } from 'js/hooks/useDatasetsCollections';
+import { buildCollectionsWithDatasetQuery } from 'js/hooks/useDatasetsCollections';
+import { useSearchHits } from 'js/hooks/useSearchData';
+import { getIDsQuery } from 'js/helpers/queries';
 
-function PublicationsDataSection({ datasetUUIDs, uuid }) {
-  const collectionsData = useDatasetsCollections(datasetUUIDs);
+function PublicationsDataSection({ datasetUUIDs, uuid, associated_collection, isCollectionPublication }) {
+  const query = isCollectionPublication
+    ? { query: getIDsQuery(associated_collection) }
+    : buildCollectionsWithDatasetQuery(datasetUUIDs);
+
+  const { searchHits: collectionsData } = useSearchHits(query);
 
   return (
     <>
-      <PublicationRelatedEntities uuid={uuid} />
+      {!isCollectionPublication && <PublicationRelatedEntities uuid={uuid} />}
       {Boolean(collectionsData) && <PublicationCollections collectionsData={collectionsData} />}
     </>
   );
