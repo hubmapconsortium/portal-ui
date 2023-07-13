9ae48c1b3e84a2513e891c00dea9c667e45cf1d7
John Conroy <john-conroy@users.noreply.github.com>
2023-07-13 15:58:20 -0400

    Scroll graph halfway after graph has rendered

--- context/app/static/js/components/detailPage/provenance/NodeElement/NodeElement.jsx
+++ context/app/static/js/components/detailPage/provenance/NodeElement/NodeElement.jsx
@@ -1,18 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Typography from '@material-ui/core/Typography';
 
 import useProvenanceStore from 'js/stores/useProvenanceStore';
 import { SecondaryBackgroundTooltip } from 'js/shared-styles/tooltips';
 import { AsteriskWrapper } from './style';
 
-const provenanceStoreUUIDSelector = (state) => state.uuid;
+const uuidSelector = (state) => state.uuid;
+const setHasRenderedSelector = (state) => state.setHasRendered;
 
 function NodeElement({ node, title, columnWidth }) {
   const style = node.nodeType === 'input' || node.nodeType === 'output' ? { width: columnWidth || 100 } : null;
 
   const displayTitle = title || node.title || node.name;
 
-  const uuid = useProvenanceStore(provenanceStoreUUIDSelector);
+  const uuid = useProvenanceStore(uuidSelector);
+  const setHasRendered = useProvenanceStore(setHasRenderedSelector);
+
+  useEffect(() => {
+    setHasRendered();
+  }, [setHasRendered]);
 
   return (
     <SecondaryBackgroundTooltip title={displayTitle}>
