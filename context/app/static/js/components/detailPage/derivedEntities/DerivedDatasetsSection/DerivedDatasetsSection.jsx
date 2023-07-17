5a763a289320eb9e993c5f665c1b3427266024e4
kevin <kevinyooky@gmail.com>
2023-07-17 12:54:15 -0700

    fix data on search page link

--- context/app/static/js/components/detailPage/derivedEntities/DerivedDatasetsSection/DerivedDatasetsSection.jsx
+++ context/app/static/js/components/detailPage/derivedEntities/DerivedDatasetsSection/DerivedDatasetsSection.jsx
@@ -1,16 +1,11 @@
 import React, { useState } from 'react';
 
-import { useFlaskDataContext } from 'js/components/Contexts';
 import DerivedEntitiesSectionWrapper from 'js/components/detailPage/related-entities/RelatedEntitiesSectionWrapper';
 import RelatedEntitiesTabs from 'js/components/detailPage/related-entities/RelatedEntitiesTabs';
 import DerivedEntitiesSectionHeader from 'js/components/detailPage/related-entities/RelatedEntitiesSectionHeader';
 import { useDerivedDatasetsSection } from './hooks';
 
-function DerivedDatasetsSection() {
-  const {
-    entity: { uuid, entity_type },
-  } = useFlaskDataContext();
-
+function DerivedDatasetsSection({ uuid, entityType }) {
   const [openIndex, setOpenIndex] = useState(0);
   const { entities, isLoading } = useDerivedDatasetsSection(uuid);
 
@@ -22,7 +17,7 @@ function DerivedDatasetsSection() {
         <DerivedEntitiesSectionHeader
           header="Derived Datasets"
           uuid={uuid}
-          searchPageHref={`/search?ancestor_ids[0]=${uuid}&entity_type[0]=${entities[openIndex].entity_type}`}
+          searchPageHref={`/search?ancestor_ids[0]=${uuid}&entity_type[0]=${entities[openIndex].entityType}`}
         />
       }
     >
@@ -32,7 +27,7 @@ function DerivedDatasetsSection() {
         setOpenIndex={setOpenIndex}
         ariaLabel="Derived Datasets Tab"
         renderWarningMessage={(tableEntityType) =>
-          `No derived ${tableEntityType.toLowerCase()}s for this ${entity_type.toLowerCase()}.`
+          `No derived ${tableEntityType.toLowerCase()}s for this ${entityType.toLowerCase()}.`
         }
       />
     </DerivedEntitiesSectionWrapper>
