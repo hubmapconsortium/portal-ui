fa63e71ba0f50d047ec9f7542319c7f2261616ae
kevin <kevinyooky@gmail.com>
2023-07-17 18:45:50 -0700

    fix render funciton error

--- context/app/static/js/components/detailPage/Attribution/Attribution.spec.js
+++ context/app/static/js/components/detailPage/Attribution/Attribution.spec.js
@@ -5,9 +5,17 @@ import Attribution from './Attribution';
 
 test('text displays properly', () => {
   render(
-    <FlaskDataContext>
-      <Attribution />,
-    </FlaskDataContext>,
+    <FlaskDataContext.Provider
+      value={{
+        entity: {
+          group_name: 'Test Group',
+          created_by_user_displayname: 'Test User',
+          created_by_user_email: 'test@test.com',
+        },
+      }}
+    >
+      <Attribution />
+    </FlaskDataContext.Provider>,
   );
   const textToTest = ['Attribution', 'Group', 'Fake TMC', 'Registered by', 'Fake Name', 'fake@fake.com'];
   textToTest.forEach((text) => expect(screen.getByText(text)).toBeInTheDocument());
