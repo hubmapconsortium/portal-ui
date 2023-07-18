d1233a8fdc6826be52531170a4b98f19004c9919
kevin <kevinyooky@gmail.com>
2023-07-17 18:17:34 -0700

    cleanup

--- context/app/static/js/components/detailPage/Attribution/Attribution.spec.js
+++ context/app/static/js/components/detailPage/Attribution/Attribution.spec.js
@@ -3,16 +3,7 @@ import { render, screen } from 'test-utils/functions';
 import Attribution from './Attribution';
 
 test('text displays properly', () => {
-  const group_name = 'Fake TMC';
-  const created_by_user_displayname = 'Fake Name';
-  const created_by_user_email = 'fake@fake.com';
-  render(
-    <Attribution
-      group_name={group_name}
-      created_by_user_displayname={created_by_user_displayname}
-      created_by_user_email={created_by_user_email}
-    />,
-  );
+  render(<Attribution />);
   const textToTest = ['Attribution', 'Group', 'Fake TMC', 'Registered by', 'Fake Name', 'fake@fake.com'];
   textToTest.forEach((text) => expect(screen.getByText(text)).toBeInTheDocument());
 });
