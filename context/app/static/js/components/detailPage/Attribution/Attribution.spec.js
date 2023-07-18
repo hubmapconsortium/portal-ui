33e292729a7ab22b6d1361571313770f7aeba607
kevin <kevinyooky@gmail.com>
2023-07-18 14:50:00 -0700

    attribution test file

--- context/app/static/js/components/detailPage/Attribution/Attribution.spec.js
+++ context/app/static/js/components/detailPage/Attribution/Attribution.spec.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import { render, screen } from 'test-utils/functions';
+
 import { FlaskDataContext } from 'js/components/Contexts';
 import Attribution from './Attribution';
 
 test('text displays properly', () => {
+  const flaskDataContext = {
+    entity: {
+      group_name: 'Fake TMC',
+      created_by_user_displayname: 'Fake Name',
+      created_by_user_email: 'fake@fake.com',
+    },
+  };
+
   render(
-    <FlaskDataContext.Provider
-      value={{
-        entity: {
-          group_name: 'Test Group',
-          created_by_user_displayname: 'Test User',
-          created_by_user_email: 'test@test.com',
-        },
-      }}
-    >
+    <FlaskDataContext.Provider value={flaskDataContext}>
       <Attribution />
     </FlaskDataContext.Provider>,
   );
