cebc435ceb0fa1cfee9139dcd5066a168fd559cf
kevin <kevinyooky@gmail.com>
2023-07-17 18:34:11 -0700

    fix build error test

--- context/app/static/js/components/detailPage/Attribution/Attribution.spec.js
+++ context/app/static/js/components/detailPage/Attribution/Attribution.spec.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import { render, screen } from 'test-utils/functions';
+import { FlaskDataContext } from 'js/components/Contexts';
 import Attribution from './Attribution';
 
 test('text displays properly', () => {
-  render(<Attribution />);
+  render(
+    <FlaskDataContext>
+      <Attribution />,
+    </FlaskDataContext>,
+  );
   const textToTest = ['Attribution', 'Group', 'Fake TMC', 'Registered by', 'Fake Name', 'fake@fake.com'];
   textToTest.forEach((text) => expect(screen.getByText(text)).toBeInTheDocument());
 });
