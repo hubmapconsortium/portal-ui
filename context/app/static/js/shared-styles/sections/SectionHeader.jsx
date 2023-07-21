38e72404908ff266458c2523930e3cdc2b9e5e79
kevin <kevinyooky@gmail.com>
2023-07-21 12:30:59 -0700

    refactor sectionHeader

--- context/app/static/js/shared-styles/sections/SectionHeader.jsx
+++ context/app/static/js/shared-styles/sections/SectionHeader.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import Typography from '@material-ui/core/Typography';
 
 import { SecondaryBackgroundTooltip } from 'js/shared-styles/tooltips';
@@ -7,11 +7,20 @@ import { StyledInfoIcon } from './LabelledSectionText/style';
 
 const StyledTypography = styled(Typography)`
   margin-bottom: ${(props) => props.theme.spacing(1)}px;
+  ${(props) =>
+    props.$hasTooltip &&
+    css`
+      display: flex;
+      align-items: center;
+      svg {
+        margin-left: ${props.theme.spacing(0.5)}px;
+      }
+    `};
 `;
 
 function SectionHeader({ children, iconTooltipText, ...rest }) {
   return (
-    <StyledTypography variant="h4" component="h2" {...rest}>
+    <StyledTypography variant="h4" component="h2" {...rest} $hasTooltip={Boolean(iconTooltipText)}>
       {children}
       {iconTooltipText && (
         <SecondaryBackgroundTooltip title={iconTooltipText}>
