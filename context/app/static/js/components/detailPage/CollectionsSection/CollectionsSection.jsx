5e844b228c57652f05dbc327b7cb60678625df6d
kevin <kevinyooky@gmail.com>
2023-07-21 00:03:28 -0700

    add icon and tooltip for collecitons

--- context/app/static/js/components/detailPage/CollectionsSection/CollectionsSection.jsx
+++ context/app/static/js/components/detailPage/CollectionsSection/CollectionsSection.jsx
@@ -1,16 +1,23 @@
 import React from 'react';
+import InfoIcon from '@material-ui/icons/Info';
 
-import SectionHeader from 'js/shared-styles/sections/SectionHeader';
+import { SecondaryBackgroundTooltip } from 'js/shared-styles/tooltips';
 import { DetailPageSection } from 'js/components/detailPage/style';
 import { buildCollectionsPanelsProps } from 'js/pages/Collections/utils';
 
 import PanelList from 'js/shared-styles/panels/PanelList';
+import { StyledSectionHeader } from './style';
 
 function CollectionsSection({ collectionsData }) {
   const panelsProps = buildCollectionsPanelsProps(collectionsData);
   return (
     <DetailPageSection id="collections">
-      <SectionHeader>Collections</SectionHeader>
+      <StyledSectionHeader>
+        Collections
+        <SecondaryBackgroundTooltip title="List of collections that contain this dataset.">
+          <InfoIcon fontSize="small" color="primary" />
+        </SecondaryBackgroundTooltip>
+      </StyledSectionHeader>
       <PanelList panelsProps={panelsProps} />
     </DetailPageSection>
   );
