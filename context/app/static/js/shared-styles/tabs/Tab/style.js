6a8982bcf7d15ed7b13bb120a118998f0c03ef6a
kevin <kevinyooky@gmail.com>
2023-07-24 21:25:07 -0700

    undo tab

--- context/app/static/js/shared-styles/tabs/Tab/style.js
+++ context/app/static/js/shared-styles/tabs/Tab/style.js
@@ -3,14 +3,6 @@ import Tab from '@material-ui/core/Tab';
 
 const StyledTab = styled(Tab)`
   min-height: 45px;
-
-  span {
-    flex-direction: row;
-  }
-
-  svg {
-    margin-bottom: 0 !important;
-  }
 `;
 
 export { StyledTab };
