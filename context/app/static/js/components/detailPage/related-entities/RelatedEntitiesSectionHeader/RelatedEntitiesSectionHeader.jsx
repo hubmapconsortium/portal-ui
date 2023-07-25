bb72ab9052045e1b2ab0c267c2eae221b7c723ac
kevin <kevinyooky@gmail.com>
2023-07-24 23:23:36 -0700

    remove unused

--- context/app/static/js/components/detailPage/related-entities/RelatedEntitiesSectionHeader/RelatedEntitiesSectionHeader.jsx
+++ context/app/static/js/components/detailPage/related-entities/RelatedEntitiesSectionHeader/RelatedEntitiesSectionHeader.jsx
@@ -2,16 +2,23 @@ import React from 'react';
 import Button from '@material-ui/core/Button';
 
 import SectionHeader from 'js/shared-styles/sections/SectionHeader';
+import { useFlaskDataContext } from 'js/components/Contexts';
 import { SpacedSectionButtonRow } from 'js/shared-styles/sections/SectionButtonRow';
 
-function RelatedEntitiesSectionHeader({ header, searchPageHref, iconTooltipText }) {
+function RelatedEntitiesSectionHeader({ header, searchPageHref }) {
+  const {
+    entity: { entity_type },
+  } = useFlaskDataContext();
+
+  const tooltipTexts = {
+    Donor: 'Samples and datasets derived from this donor.',
+    Sample: 'Datasets derived from this sample.',
+    Dataset: 'These datasets include those that have additional processing, such as visualizations.',
+  };
+
   return (
     <SpacedSectionButtonRow
-      leftText={
-        <div>
-          <SectionHeader iconTooltipText={iconTooltipText}>{header}</SectionHeader>
-        </div>
-      }
+      leftText={<SectionHeader iconTooltipText={tooltipTexts[entity_type]}>{header}</SectionHeader>}
       buttons={
         <Button
           variant="contained"
