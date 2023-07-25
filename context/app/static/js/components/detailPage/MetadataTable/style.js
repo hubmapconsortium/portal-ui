26e49079d41d81a9e42f784eba79bea3d23c72da
kevin <kevinyooky@gmail.com>
2023-07-24 19:07:06 -0700

    metadata

--- context/app/static/js/components/detailPage/MetadataTable/style.js
+++ context/app/static/js/components/detailPage/MetadataTable/style.js
@@ -2,7 +2,6 @@ import styled from 'styled-components';
 import GetAppIcon from '@material-ui/icons/GetAppRounded';
 
 import { WhiteBackgroundIconButton } from 'js/shared-styles/buttons';
-import SectionHeader from 'js/shared-styles/sections/SectionHeader';
 
 const DownloadIcon = styled(GetAppIcon)`
   font-size: 25px;
@@ -17,12 +16,4 @@ const StyledWhiteBackgroundIconButton = styled(WhiteBackgroundIconButton)`
   margin-bottom: ${(props) => props.theme.spacing(0.5)}px;
 `;
 
-const StyledSectionHeader = styled(SectionHeader)`
-  display: flex;
-  align-items: center;
-  svg {
-    margin-left: ${(props) => props.theme.spacing(0.5)}px;
-  }
-`;
-
-export { DownloadIcon, Flex, StyledWhiteBackgroundIconButton, StyledSectionHeader };
+export { DownloadIcon, Flex, StyledWhiteBackgroundIconButton };
