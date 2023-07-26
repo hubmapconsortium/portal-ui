5409bf3a0d67a1e7ef5bcd637e3dbff2a0342754
John Conroy <john-conroy@users.noreply.github.com>
2023-07-26 11:12:43 -0400

    Update swr fetcher to throw error with unexpected status codes

--- context/app/static/js/helpers/swr.ts
+++ context/app/static/js/helpers/swr.ts
@@ -1,16 +1,34 @@
+type fetchOptionsType = {
+  urls: string;
+  requestInit: RequestInit;
+  expectedStatusCodes: number[];
+};
+
 /**
  * SWR fetcher which accepts an array of URLs and returns the responses as JSON
  * A custom requestInit object can be passed to fetch as well.
  * @example // without requestInit
- * const { data } = useSWR(urls, multiFetcher);
+ * const { data } = useSWR({ urls, multiFetcher });
  * @example // with requestInit
- * const { data } = useSWR({ urls, token }, ({ urls, token }) => multiFetcher(urls, { headers: { Authorization: `Bearer ${token}` } })
- * @param urls - Array of URLs to fetch
- * @param requestInit - Optional RequestInit object to pass to fetch
+ * const { data } = useSWR({ urls, token }, ({ urls, token }) => multiFetcher({ urls, { headers: { Authorization: `Bearer ${token}` } } })
+ * @param fetchOptions
+ * @param fetchOptions.urls - Array of URLs to fetch
+ * @param fetchOptions.requestInit - Optional RequestInit object to pass to fetch
+ * @param fetchOptions.expectedStatusCodes - Optional array of status codes which reflect a succesful request
  */
 
-export async function multiFetcher<T>(urls: string[], requestInit: RequestInit = {}) {
-  const f = (url: string) => fetch(url, requestInit).then((response) => response.json());
+export async function multiFetcher<T>({ urls, requestInit = {}, expectedStatusCodes = [200] }: fetchOptionsType) {
+  const f = (url: string) =>
+    fetch(url, requestInit).then((response) => {
+      if (!expectedStatusCodes.includes(response.status)) {
+        const error = new Error(`The request to ${urls.join(', ')} failed.`);
+        error.info = response.json();
+        error.status = response.status;
+        throw error;
+      }
+      return response.json();
+    });
+
   if (urls.length === 0) {
     return Promise.resolve([] as T[]);
   }
@@ -21,12 +39,14 @@ export async function multiFetcher<T>(urls: string[], requestInit: RequestInit =
  * SWR fetcher which accepts a single URL and returns the response as JSON.
  * A custom requestInit object can be passed to fetch as well.
  * @example // without requestInit
- * const { data } = useSWR(urls, multiFetcher);
+ * const { data } = useSWR({ urls, multiFetcher });
  * @example // with requestInit
- * const { data } = useSWR({ urls, token }, ({ urls, token }) => multiFetcher(urls, { headers: { Authorization: `Bearer ${token}` } })
- * @param urls - Array of URLs to fetch
- * @param requestInit - Optional RequestInit object to pass to fetch
+ * const { data } = useSWR({ urls, token }, ({ urls, token }) => multiFetcher({ urls, { headers: { Authorization: `Bearer ${token}` } } })
+ * @param fetchOptions
+ * @param fetchOptions.urls - Array of URLs to fetch
+ * @param fetchOptions.requestInit - Optional RequestInit object to pass to fetch
+ * @param fetchOptions.expectedStatusCodes - Optional array of status codes which reflect a succesful request
  */
-export async function fetcher<T>(url: string, requestInit: RequestInit = {}) {
-  return multiFetcher([url], requestInit).then((data) => data[0]) as Promise<T>;
+export async function fetcher<T>({ url, requestInit = {}, expectedStatusCodes = [200] }: fetchOptionsType) {
+  return multiFetcher({ urls: [url], requestInit, expectedStatusCodes }).then((data) => data[0]) as Promise<T>;
 }
