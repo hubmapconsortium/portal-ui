df7cc3194bc7f6bcbba205c993272b88c244ce56
John Conroy <john-conroy@users.noreply.github.com>
2023-07-26 11:13:59 -0400

    Handle error from protocols api

--- context/app/static/js/components/detailPage/Protocol/Protocol.jsx
+++ context/app/static/js/components/detailPage/Protocol/Protocol.jsx
@@ -3,31 +3,43 @@ import PropTypes from 'prop-types';
 import Divider from '@material-ui/core/Divider';
 
 import OutboundIconLink from 'js/shared-styles/Links/iconLinks/OutboundIconLink';
-import EmailIconLink from 'js/shared-styles/Links/iconLinks/EmailIconLink';
 import useProtocolData, { useFormattedProtocolUrls } from 'js/hooks/useProtocolData';
 import SectionHeader from 'js/shared-styles/sections/SectionHeader';
+import HelpLink from 'js/shared-styles/Links/HelpLink';
 import { DetailPageSection } from 'js/components/detailPage/style';
 import { StyledPaper } from './style';
 import SectionItem from '../SectionItem';
 
+function ProtocolMessage({ precedingText }) {
+  return (
+    <SectionItem>
+      <div>
+        {precedingText}
+        <HelpLink /> about this issue and mention the HuBMAP ID.
+      </div>
+    </SectionItem>
+  );
+}
+
 function ProtocolLink({ url, index }) {
-  const { loading, data, error } = useProtocolData(url);
-  if (error || loading || !data) {
+  const { isLoading, data, error } = useProtocolData(url);
+
+  if (error || isLoading || !data) {
     if (index !== 0) {
       // Only show loading message for first protocol link
       return null;
     }
-    // Extra `div` wrapper is necessary to prevent the email icon link from taking up the full width and breaking text
-    return (
-      <SectionItem>
-        <div>
-          Protocols are loading. If protocols take a significant time to load, please contact{' '}
-          <EmailIconLink email="help@hubmapconsortium.org">help@hubmapconsortium.org</EmailIconLink> about this issue
-          and mention the HuBMAP ID.
-        </div>
-      </SectionItem>
-    );
+
+    if (isLoading && !error) {
+      // Extra `div` wrapper is necessary to prevent the email icon link from taking up the full width and breaking text
+      return (
+        <ProtocolMessage precedingText="Protocols are loading. If protocols take a significant time to load, please contact " />
+      );
+    }
+
+    return <ProtocolMessage precedingText="Failed to retrieve protocols. Please contact " />;
   }
+
   return (
     <SectionItem label={data?.payload?.title}>
       <OutboundIconLink href={data?.payload?.url}>{data?.payload?.url}</OutboundIconLink>
