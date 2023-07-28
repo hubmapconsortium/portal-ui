3366c57712b9047f3be8ff7269c287f16994d6d6
kevin <kevinyooky@gmail.com>
2023-07-28 14:58:42 -0700

    undo title change

--- context/app/static/js/components/home/Title/Title.jsx
+++ context/app/static/js/components/home/Title/Title.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
 
-import PageTitle from 'js/shared-styles/pages/PageTitle';
 import { StyledDiv } from './style';
 
 function Title() {
   return (
     <StyledDiv>
-      <PageTitle>Human BioMolecular Atlas Program</PageTitle>
+      <Typography variant="h2" component="h1">
+        Human BioMolecular Atlas Program
+      </Typography>
       <Typography variant="h4" component="h2" color="secondary">
         An open, global atlas of the human body at the cellular level
       </Typography>
