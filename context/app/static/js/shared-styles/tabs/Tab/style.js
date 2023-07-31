b0bde6416a95413602daa5eacc72d6bc760d6e6e
Nikolay Akhmetov <nick.akhmetov@gmail.com>
2023-07-31 12:48:40 -0400

    Fix tab colors

--- context/app/static/js/shared-styles/tabs/Tab/style.js
+++ context/app/static/js/shared-styles/tabs/Tab/style.js
@@ -1,8 +1,17 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import Tab from '@mui/material/Tab';
 
 const StyledTab = styled(Tab)`
   min-height: 45px;
+  ${({ theme: { palette } }) => css`
+    color: ${palette.white.main};
+    &:hover {
+      filter: ${palette.white.hover};
+    }
+    &.Mui-selected {
+      color: ${palette.white.main};
+    }
+  `}
 `;
 
 export { StyledTab };
