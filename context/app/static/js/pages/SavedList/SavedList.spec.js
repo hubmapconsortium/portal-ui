c8855161d7fd68da85726e6cc6fa2c8b042e56fd
kevin <kevinyooky@gmail.com>
2023-08-02 09:39:05 -0700

    change import for render from testing library

--- context/app/static/js/pages/SavedList/SavedList.spec.js
+++ context/app/static/js/pages/SavedList/SavedList.spec.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render } from 'test-utils/functions';
 import useSavedEntitiesStore from 'js/stores/useSavedEntitiesStore';
 import SavedList from './SavedList';
 
