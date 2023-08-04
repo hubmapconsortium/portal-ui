975171ab9653be96ba6cbf88ac1658b83e1a5fcf
kevin <kevinyooky@gmail.com>
2023-08-04 11:00:25 -0700

    change display to block

--- context/app/static/js/components/detailPage/BulkDataTransfer/NoAccess.jsx
+++ context/app/static/js/components/detailPage/BulkDataTransfer/NoAccess.jsx
@@ -22,7 +22,7 @@ function NoAccess({ status, children, title, tooltip }) {
           </SecondaryBackgroundTooltip>
         </Header>
       )}
-      <NoAccessContainer displayValue={status === 'exclamation' ? 'display' : 'flex'}>
+      <NoAccessContainer displayValue={status === 'exclamation' ? 'block' : 'flex'}>
         {status === 'warning' && <IconContainer>{statusIcons[status]}</IconContainer>}
         {children}
       </NoAccessContainer>
