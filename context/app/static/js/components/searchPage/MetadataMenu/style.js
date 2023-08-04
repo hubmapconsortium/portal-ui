21f0b16b3a919cc5ed74dfcff4656662e661fc1a
Nikolay Akhmetov <nick.akhmetov@gmail.com>
2023-08-04 12:24:23 -0400

    NickAkhmetov/HMP-323 Remove underline on Visualize link (#3216)

--- context/app/static/js/components/searchPage/MetadataMenu/style.js
+++ context/app/static/js/components/searchPage/MetadataMenu/style.js
@@ -1,5 +1,4 @@
 import styled from 'styled-components';
-import Link from '@mui/material/Link';
 
 import { InfoIcon } from 'js/shared-styles/icons';
 import DropdownMenuButton from 'js/shared-styles/dropdowns/DropdownMenuButton';
@@ -9,15 +8,9 @@ const StyledDropdownMenuButton = styled(DropdownMenuButton)`
   height: 100%;
 `;
 
-const StyledLink = styled(Link)`
-  &:hover {
-    text-decoration: none;
-  }
-`;
-
 const StyledInfoIcon = styled(InfoIcon)`
   font-size: 1rem;
   margin-left: ${(props) => props.theme.spacing(0.5)};
 `;
 
-export { StyledDropdownMenuButton, StyledLink, StyledInfoIcon };
+export { StyledDropdownMenuButton, StyledInfoIcon };
