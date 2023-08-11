ee5f84a733474d6209d3058a80c0d451fe548b00
Nikolay Akhmetov <nick.akhmetov@gmail.com>
2023-08-11 13:17:23 -0400

    avoid `React.` since we're not using next.js

--- context/app/static/js/helpers/context/create-context.tsx
+++ context/app/static/js/helpers/context/create-context.tsx
@@ -1,12 +1,12 @@
-import { createContext as createContextNative } from 'react';
+import { createContext as createContextNative, type Context } from 'react';
 
 /**
  * Overloads for the `createContext` function to ease the creation of new contexts.
  * Allows for the instantiation of typed contexts without having to specify the default values.
  * @param displayName The display name to apply to the context
  */
-export function createContext<T>(displayName: string): React.Context<T | undefined>;
-export function createContext<T>(displayName: string, defaultValue: T): React.Context<T>;
+export function createContext<T>(displayName: string): Context<T | undefined>;
+export function createContext<T>(displayName: string, defaultValue: T): Context<T>;
 export function createContext<T>(displayName: string, defaultValue?: T) {
   const context =
     defaultValue !== undefined ? createContextNative<T>(defaultValue) : createContextNative<T | undefined>(undefined);
