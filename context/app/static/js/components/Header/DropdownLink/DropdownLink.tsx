ccea0781bea869c8fafb2b26b14ed88e7c3d34e5
John Conroy <62477388+john-conroy@users.noreply.github.com>
2023-08-29 13:01:28 -0400

    John conroy/avr link HMP-362 (#3237)
    * Add LinkGroups component
    
    * Use link groups in atlas links and add avr
    
    * Use link groups in other links
    
    * Fix type exports
    
    * Update groups structure to provide key for fragment
    
    * Use link groups in resource links
    
    * Consolidate static links
    
    * Add changelog
    
    * Implement Nick's type suggestions
    
    * Remove underline from links by default
    
    * Use menu item prop for divider between link groups
    
    * Update dropdown link with ts and styles
    
    * Remove unused prop
    
    ---------
    
    Co-authored-by: John Conroy <john-conroy@users.noreply.github.com>

---
+++ context/app/static/js/components/Header/DropdownLink/DropdownLink.tsx
@@ -0,0 +1,24 @@
+import React, { PropsWithChildren } from 'react';
+import MenuItem, { MenuItemProps } from '@mui/material/MenuItem';
+import Link from '@mui/material/Link';
+
+type DropdownLinkProps = {
+  href: string;
+  isIndented: boolean;
+  rest?: MenuItemProps;
+};
+
+function DropdownLink({ href, isIndented, children, ...rest }: PropsWithChildren<DropdownLinkProps>) {
+  return (
+    <MenuItem
+      {...rest}
+      sx={({ spacing, palette }) => ({
+        color: palette.primary.main,
+        paddingLeft: isIndented ? spacing(5) : spacing(2),
+      })}
+    >
+      <Link href={href}>{children}</Link>
+    </MenuItem>
+  );
+}
+export default DropdownLink;
