c3a476af08859a1ba8e530dd3cfe02375e24f5ba
Nikolay Akhmetov <nick.akhmetov@gmail.com>
2023-08-30 17:06:24 -0400

    convert JS styled component to TS to test intellisense

---
+++ context/app/static/js/components/detailPage/provenance/ProvTableDerivedLink/style.ts
@@ -0,0 +1,19 @@
+import { styled } from '@mui/material/styles';
+
+const LinkButton = styled('a')(({ theme }) => ({
+  color: theme.palette.white.main,
+  borderRadius: theme.spacing(0.5),
+  backgroundColor: theme.palette.primary.main,
+  ...theme.typography.subtitle2,
+  marginTop: theme.spacing(1),
+  width: '175px',
+  boxSizing: 'content-box',
+  padding: theme.spacing(1, 4),
+  textAlign: 'center',
+  '&:hover': {
+    boxShadow: theme.shadows[8],
+    filter: theme.palette.primary.hover,
+  },
+}));
+
+export { LinkButton };
