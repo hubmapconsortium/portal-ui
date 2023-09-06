303b04b415a96f85f1fa6970ce31a0e432d4babc
Nikolay Akhmetov <nick.akhmetov@gmail.com>
2023-09-06 11:51:30 -0400

    Fix `rest` props

--- context/app/static/js/components/Header/DropdownLink/DropdownLink.tsx
+++ context/app/static/js/components/Header/DropdownLink/DropdownLink.tsx
@@ -2,10 +2,9 @@ import React, { PropsWithChildren } from 'react';
 import MenuItem, { MenuItemProps } from '@mui/material/MenuItem';
 import Link from '@mui/material/Link';
 
-interface DropdownLinkProps {
+interface DropdownLinkProps extends MenuItemProps {
   href: string;
   isIndented: boolean;
-  rest?: MenuItemProps;
 }
 
 function DropdownLink({ href, isIndented, children, ...rest }: PropsWithChildren<DropdownLinkProps>) {
