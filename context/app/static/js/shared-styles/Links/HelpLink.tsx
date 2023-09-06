1d9e4d421316ea6debfa00d3980fa4ac2caeb1b7
Nikolay Akhmetov <nick.akhmetov@gmail.com>
2023-09-06 16:43:55 -0400

    Revise helplink to point to contact us form

--- context/app/static/js/shared-styles/Links/HelpLink.tsx
+++ context/app/static/js/shared-styles/Links/HelpLink.tsx
@@ -1,15 +1,35 @@
-import React, { ComponentProps, PropsWithChildren } from 'react';
-import EmailIconLink from './iconLinks/EmailIconLink';
+import React, { ComponentProps, MouseEvent, PropsWithChildren } from 'react';
+import IconLink from './iconLinks/IconLink';
+import { trackLink } from 'js/helpers/trackers';
+import { EmailIcon } from '../icons';
 
-type HelpLinkProps = PropsWithChildren<ComponentProps<typeof EmailIconLink>>;
+type HelpLinkProps = PropsWithChildren<Omit<ComponentProps<typeof IconLink>, 'isOutbound' | 'href'>> & {
+  iconFontSize?: string;
+};
 
-const helpEmail = 'help@hubmapconsortium.org';
+const href = 'https://hubmapconsortium.org/contact-form/';
+const defaultText = 'contact us';
 
-function HelpLink({ children, ...props }: HelpLinkProps) {
+function getHelpEvent() {
+  const href = window.location.href;
+  trackLink(href, 'help_form');
+}
+
+function HelpLink({ iconFontSize, children, ...props }: HelpLinkProps) {
   return (
-    <EmailIconLink {...props} email={helpEmail}>
-      {children ?? helpEmail}
-    </EmailIconLink>
+    <IconLink 
+      href={href} 
+      isOutbound={true} 
+      iconOnLeft={false}
+      // @ts-ignore
+      icon={<EmailIcon $fontSize={iconFontSize} />} 
+      {...props}>
+      {children ?? defaultText}
+      onClick={(e: MouseEvent) => {
+        getHelpEvent(); 
+        props?.onClick(e)
+      }} 
+    </IconLink>
   );
 }
 
