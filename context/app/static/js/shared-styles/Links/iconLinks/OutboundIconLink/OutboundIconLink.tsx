a3a49ae8a4870d82c27e5d2442c5708b01cd44e4
Nikolay Akhmetov <nick.akhmetov@gmail.com>
2023-09-11 16:41:31 -0400

    `prettier` fixes

--- context/app/static/js/shared-styles/Links/iconLinks/OutboundIconLink/OutboundIconLink.tsx
+++ context/app/static/js/shared-styles/Links/iconLinks/OutboundIconLink/OutboundIconLink.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import IconLink, {IconLinkProps} from 'js/shared-styles/Links/iconLinks/IconLink';
+import IconLink, { IconLinkProps } from 'js/shared-styles/Links/iconLinks/IconLink';
 import { ExternalLinkIcon } from 'js/shared-styles/icons';
 
 type OutboundIconLinkProps = Omit<IconLinkProps, 'isOutbound' | 'icon'>;
 
 function OutboundIconLink(props: OutboundIconLinkProps) {
-  return <IconLink {...props} isOutbound icon={<ExternalLinkIcon />}  />;
+  return <IconLink {...props} isOutbound icon={<ExternalLinkIcon />} />;
 }
 
 export default OutboundIconLink;
