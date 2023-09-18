26babc6206d91b979ae627a14733a9d389ccbd4e
Nikolay Akhmetov <nick.akhmetov@gmail.com>
2023-09-18 18:28:13 -0400

    NickAkhmetov/HMP-407/HMP-409 Upgrade `jest` and `msw`, remove `marked` in favor of `react-markdown` (#3264) (#3263)
    * Upgrade `jest` and `msw`
    
    * changelog + lint msw
    
    * Nickakhmetov/HMP-409 Remove `marked` in favor of `react-markdown` (#3264)
    
    * Remove `marked` in favor of `react-markdown`, fix alert alignment
    
    * changelog

--- context/app/static/js/components/style.js
+++ context/app/static/js/components/style.js
@@ -7,6 +7,8 @@ const StyledAlert = styled(Alert)`
   width: ${(props) => props.theme.breakpoints.values.lg}px;
   margin-top: ${(props) => props.theme.spacing(3)};
   z-index: 0; // Does not display on preview pages without this; Not sure sure why not.
+  display: flex;
+  align-items: center;
 `;
 
 const FlexContainer = styled(Container)`
