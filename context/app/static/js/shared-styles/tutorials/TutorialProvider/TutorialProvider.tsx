1cabebd65649302328fb1295ce4f46dfdde2fcb6
Nikolay Akhmetov <nick.akhmetov@gmail.com>
2023-09-25 10:23:50 -0400

    lint fixes

--- context/app/static/js/shared-styles/tutorials/TutorialProvider/TutorialProvider.tsx
+++ context/app/static/js/shared-styles/tutorials/TutorialProvider/TutorialProvider.tsx
@@ -1,9 +1,9 @@
 import React, { ComponentProps, ComponentType, PropsWithChildren, useRef } from 'react';
 
-import { createContext, useContext, } from 'js/helpers/context';
+import { createContext, useContext } from 'js/helpers/context';
 import { useStore } from 'zustand';
 
-import { Selector, createStore} from './store';
+import { Selector, createStore } from './store';
 
 type TutorialContextType = ReturnType<typeof createStore>;
 
@@ -16,20 +16,23 @@ interface TutorialProviderProps extends PropsWithChildren {
 export default function TutorialProvider({ children, tutorial_key }: TutorialProviderProps) {
   const store = useRef(createStore(tutorial_key));
 
-  return <TutorialContext.Provider value={store.current}>{children}</TutorialContext.Provider>
+  return <TutorialContext.Provider value={store.current}>{children}</TutorialContext.Provider>;
 }
 
 export function withTutorialProvider<P extends React.JSX.IntrinsicAttributes>(
-  Component: ComponentType<P>, 
-  tutorial_key: string) {
-  return (props: ComponentProps<typeof Component>) => (
-    <TutorialProvider tutorial_key={tutorial_key}>
-      <Component {...props} />
-    </TutorialProvider>
-  );
+  Component: ComponentType<P>,
+  tutorial_key: string,
+) {
+  return function ComponentWithTutorialProvider(props: ComponentProps<typeof Component>) {
+    return (
+      <TutorialProvider tutorial_key={tutorial_key}>
+        <Component {...props} />
+      </TutorialProvider>
+    );
+  };
 }
 
 export const useTutorial = (selector: Selector) => {
   const store = useContext(TutorialContext);
   return useStore(store, selector);
-}
+};
