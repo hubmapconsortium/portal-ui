c0543cea4c2cfd349aa49b30ae6ba5161f00b9cd
John Conroy <john-conroy@users.noreply.github.com>
2023-10-05 21:40:55 -0400

    Add hook to launch workspace from templates

--- context/app/static/js/components/workspaces/NewWorkspaceDialog/hooks.ts
+++ context/app/static/js/components/workspaces/NewWorkspaceDialog/hooks.ts
@@ -1,8 +1,21 @@
+import { useCallback } from 'react';
 import useSWR from 'swr';
 import type { SWRResponse } from 'swr';
 
 import { useAppContext } from 'js/components/Contexts';
-import { fetcher } from 'js/helpers/swr';
+import { fetcher, multiFetcher } from 'js/helpers/swr';
+import { startJob } from '../utils';
+
+interface WorkspaceAPIResponse {
+  message: string;
+  success: true | false;
+  data: unkown;
+}
+
+interface UserTemplatesTypes {
+  templatesURL: string;
+  f?: typeof fetcher | typeof multiFetcher;
+}
 
 interface TemplateTypes {
   title: string;
@@ -12,19 +25,41 @@ interface TemplateTypes {
   template_format: string;
 }
 
-interface TemplatesResponse {
-  message: string;
-  success: boolean;
+interface TemplatesResponse extends WorkspaceAPIResponse {
   data: Record<string, TemplateTypes>;
 }
 
-function useTemplatesAPI(templatesURL) {
+interface CreateTemplateNotebooksTypes {
+  templateKeys: string[];
+  uuids: string[];
+  workspaceName: string;
+}
+
+interface CreateTemplateData {
+  template: string;
+}
+
+interface CreateTemplatesResponse extends WorkspaceAPIResponse {
+  data: CreateTemplateData;
+}
+
+interface CreateWorkspaceData {
+  workspace: {
+    id: number;
+  };
+}
+
+interface CreateWorkspaceResponse extends WorkspaceAPIResponse {
+  data: CreateWorkspaceData;
+}
+
+function useUserTemplatesAPI({ templatesURL, f = fetcher }: UserTemplatesTypes) {
   const { groupsToken } = useAppContext();
 
   const result = useSWR(
     [templatesURL, groupsToken],
     ([url, token]: string[]) =>
-      fetcher({
+      f({
         url,
         requestInit: {
           headers: { Authorization: `Bearer ${token}` },
@@ -40,19 +75,74 @@ function useWorkspaceTemplates(tags: string[] = []) {
   const queryParams = tags.map((tag, i) => `${i === 0 ? '' : '&'}tags=${encodeURIComponent(tag)}`).join('');
 
   const url = `https://user-templates-api.dev.hubmapconsortium.org/templates/jupyter_lab/?${queryParams}`;
-  const result: SWRResponse<TemplatesResponse> = useTemplatesAPI(url);
+  const result: SWRResponse<TemplatesResponse> = useUserTemplatesAPI({ templatesURL: url });
 
   const templates = result?.data?.data ?? {};
   return { templates };
 }
 
-function useWorkspaceTemplateTags() {
-  const result: SWRResponse<TemplatesResponse> = useTemplatesAPI(
-    'https://user-templates-api.dev.hubmapconsortium.org/tags/',
+function useTemplateNotebooks() {
+  const { groupsToken, workspacesToken, workspacesEndpoint } = useAppContext();
+
+  const createTemplateNotebooks = useCallback(
+    async ({ workspaceName, templateKeys, uuids }: CreateTemplateNotebooksTypes) => {
+      const urls = templateKeys.map(
+        (key) => `https://user-templates-api.dev.hubmapconsortium.org/templates/jupyter_lab/${key}`,
+      );
+      const createdTemplates = await multiFetcher<CreateTemplatesResponse>({
+        urls,
+        requestInit: {
+          method: 'POST',
+          body: JSON.stringify({ uuids }),
+          headers: { Authorization: `Bearer ${groupsToken}` },
+        },
+      });
+
+      const s = await fetcher<CreateWorkspaceResponse>({
+        url: 'https://workspaces.api.hubmapconsortium.org/workspaces/',
+        requestInit: {
+          method: 'POST',
+          body: JSON.stringify({
+            name: workspaceName,
+            description: workspaceName,
+            workspace_details: {
+              globus_groups_token: groupsToken,
+              files: templateKeys.map((templateKey, i) => ({
+                name: `${templateKey}.ipynb`,
+                content: createdTemplates[i]?.data?.template,
+              })),
+              symlinks: uuids.map((uuid) => ({
+                name: `datasets/${uuid}`,
+                dataset_uuid: uuid,
+              })),
+            },
+          }),
+          headers: { 'UWS-Authorization': `Token ${workspacesToken}` },
+        },
+      });
+      const workspaceId = s?.data?.workspace?.id;
+
+      if (workspaceId) {
+        await startJob({ workspaceId, workspacesEndpoint, workspacesToken });
+        window.open(
+          `/workspaces/${workspaceId}?notebook_path=${encodeURIComponent(`${templateKeys[0]}.ipynb`)}`,
+          '_blank',
+        );
+      }
+    },
+    [groupsToken, workspacesToken, workspacesEndpoint],
   );
+
+  return createTemplateNotebooks;
+}
+
+function useWorkspaceTemplateTags() {
+  const result: SWRResponse<TemplatesResponse> = useUserTemplatesAPI({
+    templatesURL: 'https://user-templates-api.dev.hubmapconsortium.org/tags/',
+  });
 
   const tags = result?.data?.data ?? {};
   return { tags };
 }
 
-export { useWorkspaceTemplates, useWorkspaceTemplateTags, TemplateTypes };
+export { useWorkspaceTemplates, useWorkspaceTemplateTags, TemplateTypes, useTemplateNotebooks };
