e28138c363d4eab410b6d8ba9c8d56283d4c112c
Nikolay Akhmetov <nick.akhmetov@gmail.com>
2023-10-18 16:50:33 -0400

    track duration of workspace startup

--- context/app/static/js/pages/WorkspacePleaseWait/hooks.js
+++ context/app/static/js/pages/WorkspacePleaseWait/hooks.js
@@ -1,13 +1,15 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 
 import { useAppContext } from 'js/components/Contexts';
 import { locationIfJobRunning } from 'js/components/workspaces/utils';
 import useInterval from 'js/hooks/useInterval';
+import { trackEvent } from 'js/helpers/trackers';
 
 function useWorkspacesPleaseWait(workspaceId) {
   const [message, setMessage] = useState();
   const [dead, setDead] = useState();
   const { workspacesEndpoint, workspacesToken } = useAppContext();
+  const loadingStartTime = useRef(new Date().getTime());
 
   async function setLocationOrRetry() {
     if (dead) {
@@ -21,6 +23,12 @@ function useWorkspacesPleaseWait(workspaceId) {
       workspacesToken,
     });
     if (jobLocation) {
+      const loadingEndTime = new Date().getTime();
+      const loadingDuration = loadingEndTime - loadingStartTime.current;
+      trackEvent({
+        category: 'Workspace Loading',
+        value: loadingDuration,
+      });
       const [urlBase, urlQuery] = jobLocation.split('?');
       const workspacePath = new URLSearchParams(document.location.search).get('notebook_path');
       const jupyterUrl = `${urlBase}/tree/${workspacePath}?${urlQuery}`;
