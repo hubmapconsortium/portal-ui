7fd92aec3782081f54e05b0b22407c15e2c990e8
John Conroy <john-conroy@users.noreply.github.com>
2023-10-23 16:26:38 -0400

    Reduce source to needed fields

--- context/app/static/js/pages/Collections/hooks.js
+++ context/app/static/js/pages/Collections/hooks.js
@@ -2,8 +2,10 @@ import { useSearchHits } from 'js/hooks/useSearchData';
 import { getAllCollectionsQuery } from 'js/helpers/queries';
 import { buildCollectionsPanelsProps } from './utils';
 
+const query = { ...getAllCollectionsQuery, _source: ['uuid', 'title', 'hubmap_id', 'datasets.hubmap_id'] };
+
 function useCollections() {
-  const { searchHits: collectionsData } = useSearchHits(getAllCollectionsQuery);
+  const { searchHits: collectionsData } = useSearchHits(query);
   return buildCollectionsPanelsProps(collectionsData);
 }
 
