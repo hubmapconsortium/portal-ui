1b42144640c682d9dd7286e8a8ca9e555d93b8d0
John Conroy <john-conroy@users.noreply.github.com>
2023-10-23 12:31:37 -0400

    Conform selectable chip components

--- context/app/static/js/shared-styles/chips/SelectableChip/SelectableChip.tsx
+++ context/app/static/js/shared-styles/chips/SelectableChip/SelectableChip.tsx
@@ -2,20 +2,27 @@ import React from 'react';
 import Chip, { ChipProps } from '@mui/material/Chip';
 import CheckRoundedIcon from '@mui/icons-material/CheckRounded';
 
-interface SelectableChipProps extends ChipProps {
+type SelectableChipProps = Pick<ChipProps<'button'>, 'label' | 'onClick' | 'disabled'> & {
   isSelected: boolean;
-}
+};
 
-function SelectableChip({ isSelected, ...rest }: SelectableChipProps) {
-  const selectedProps = isSelected
-    ? {
-        icon: <CheckRoundedIcon />,
-        color: 'primary',
-      }
-    : {
-        variant: 'outlined',
-      };
-  return <Chip {...selectedProps} {...rest} />;
+function SelectableChip({ label, onClick, disabled, isSelected, ...rest }: SelectableChipProps) {
+  return (
+    <Chip
+      label={label}
+      clickable
+      onClick={onClick}
+      component="button"
+      disabled={disabled}
+      color={isSelected ? 'primary' : undefined}
+      icon={isSelected ? <CheckRoundedIcon /> : undefined}
+      variant={isSelected ? 'filled' : 'outlined'}
+      sx={{
+        px: isSelected ? 0 : 1.5,
+      }}
+      {...rest}
+    />
+  );
 }
 
 export default SelectableChip;
