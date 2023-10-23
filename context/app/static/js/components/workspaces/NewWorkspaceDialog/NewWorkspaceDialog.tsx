11e2768fdbd6ea333828a20a34cd8fd70793d307
John Conroy <john-conroy@users.noreply.github.com>
2023-10-23 14:31:40 -0400

    Refactor workspace dialog text

--- context/app/static/js/components/workspaces/NewWorkspaceDialog/NewWorkspaceDialog.tsx
+++ context/app/static/js/components/workspaces/NewWorkspaceDialog/NewWorkspaceDialog.tsx
@@ -1,4 +1,4 @@
-import React, { useState, PropsWithChildren, useCallback } from 'react';
+import React, { useState, PropsWithChildren, useCallback, ReactElement } from 'react';
 import { UseFormReturn, FieldErrors } from 'react-hook-form';
 import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
@@ -21,6 +21,49 @@ import TemplateGrid from '../TemplateGrid';
 import { useWorkspaceTemplates, useWorkspaceTemplateTags } from './hooks';
 import { CreateWorkspaceFormTypes } from './useCreateWorkspaceForm';
 import { CreateTemplateNotebooksTypes } from '../types';
+
+function ContactPrompt() {
+  return (
+    <>
+      If you have ideas about additional templates to include in the future, please <ContactUsLink /> .
+    </>
+  );
+}
+
+const text = {
+  overview: {
+    title: 'Launch New Workspace',
+    description: [
+      'Workspaces are currently Jupyter Notebooks that allows you to perform operations on HuBMAP data.',
+      'Three steps are shown for launching a workspace, but the only required field to launch a workspace is “Step 2: Configure Workspace”. “Step 1: Edit Datasets Selection” is only required if there are issues with any of the datasets selected, which will be indicated by an error banner.',
+    ],
+  },
+  datasets: {
+    title: 'Edit Datasets Selection',
+    description: [
+      'To remove a dataset, select the dataset and press the delete button. If all datasets are removed, an empty workspace will be launched.',
+      'To add more datasets to a workspace, you must navigate to the dataset search page, select datasets of interests and follow steps to launch a workspace from the search page. As a reminder, once you navigate away from this page, all selected datasets will be lost so take note of any HuBMAP IDs of interest, or copy IDs to your clipboard by selecting datasets in the table below and pressing the copy button. You can also save datasets to the “My Lists” feature.',
+    ],
+  },
+  configure: { title: 'Configure Workspace' },
+  templates: {
+    title: 'Select Templates',
+    description: [
+      'Templates can be selected for your workspace for potential workflows revolving around data assays, visualization, QA or other purposes. Multiple templates can be selected. If you are unsure of which templates to launch, the “Select All” button selects all templates.',
+      <ContactPrompt />,
+    ],
+  },
+};
+
+function Description({ blocks }: { blocks: (string | ReactElement)[] }) {
+  return (
+    <Stack gap={2} sx={{ p: 2 }} component={Paper} direction="column">
+      {blocks.map((block) => (
+        <Typography>{block}</Typography>
+      ))}
+    </Stack>
+  );
+}
 
 interface TagTypes extends ChipProps {
   option: string;
@@ -78,45 +121,23 @@ function NewWorkspaceDialog({
       open={dialogIsOpen}
       onClose={handleClose}
       scroll="paper"
-      aria-labelledby="scroll-dialog-title"
-      aria-describedby="scroll-dialog-description"
+      aria-labelledby="create-workspace-dialog-title"
       maxWidth="lg"
     >
       <Box mb={2}>
-        <DialogTitle id="scroll-dialog-title" variant="h3">
-          Launch New Workspace
+        <DialogTitle id="create-workspace-dialog-title" variant="h3">
+          {text.overview.title}
         </DialogTitle>
         <Box sx={{ px: 3 }}>
-          <Paper sx={{ p: 2 }}>
-            <Typography sx={{ mb: 2 }}>
-              Workspaces are currently Jupyter Notebooks that allows you to perform operations on HuBMAP data.
-            </Typography>
-            <Typography>
-              Three steps are shown for launching a workspace, but the only required field to launch a workspace is
-              “Step 2: Configure Workspace”. “Step 1: Edit Datasets Selection” is only required if there are issues with
-              any of the datasets selected, which will be indicated by an error banner.
-            </Typography>
-          </Paper>
+          <Description blocks={text.overview.description} />
         </Box>
       </Box>
       <DialogContent dividers>
-        <Step title="Edit Datasets Selection" index={0}>
+        <Step title={text.datasets.title} index={0}>
           {children}
-          <Paper sx={{ p: 2 }}>
-            <Typography sx={{ mb: 2 }}>
-              To remove a dataset, select the dataset and press the delete button. If all datasets are removed, an empty
-              workspace will be launched.
-            </Typography>
-            <Typography>
-              To add more datasets to a workspace, you must navigate to the dataset search page, select datasets of
-              interests and follow steps to launch a workspace from the search page. As a reminder, once you navigate
-              away from this page, all selected datasets will be lost so take note of any HuBMAP IDs of interest, or
-              copy IDs to your clipboard by selecting datasets in the table below and pressing the copy button. You can
-              also save datasets to the “My Lists” feature.
-            </Typography>
-          </Paper>
+          <Description blocks={text.datasets.description} />
         </Step>
-        <Step title="Configure Workspace" isRequired index={1}>
+        <Step title={text.configure.title} isRequired index={1}>
           <Box
             id="create-workspace-form"
             component="form"
@@ -140,18 +161,11 @@ function NewWorkspaceDialog({
             />
           </Box>
         </Step>
-        <Step title="Select Templates" index={2}>
-          <Paper sx={{ p: 2, mb: 2 }}>
-            <Typography sx={{ mb: 2 }}>
-              Templates can be selected for your workspace for potential workflows revolving around data assays,
-              visualization, QA or other purposes. Multiple templates can be selected. If you are unsure of which
-              templates to launch, the “Select All” button selects all templates.
-            </Typography>
-            <Typography>
-              If you have ideas about additional templates to include in the future, please <ContactUsLink /> .
-            </Typography>
-          </Paper>
-          <Typography variant="subtitle1">Filter workspace templates by tags</Typography>
+        <Step title={text.templates.title} index={2}>
+          <Description blocks={text.templates.description} />
+          <Typography sx={{ mt: 2 }} variant="subtitle1">
+            Filter workspace templates by tags
+          </Typography>
           <Stack spacing={1}>
             <MultiAutocomplete<string>
               value={selectedTags}
@@ -192,7 +206,7 @@ function NewWorkspaceDialog({
           form="create-workspace-form"
           disabled={Object.keys(errors).length > 0 || errorMessages.length > 0}
         >
-          Launch
+          Launch Workspace
         </Button>
       </DialogActions>
     </Dialog>
