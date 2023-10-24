5b1c398114bdbbb616a15c9842e60d8fdb204dd7
John Conroy <john-conroy@users.noreply.github.com>
2023-10-24 17:16:03 -0400

    Add clause to only show collections with doi to public

--- context/app/static/js/pages/Collections/hooks.ts
+++ context/app/static/js/pages/Collections/hooks.ts
@@ -1,14 +1,19 @@
 import { useSearchHits } from 'js/hooks/useSearchData';
 import { getAllCollectionsQuery } from 'js/helpers/queries';
+import { useAppContext } from 'js/components/Contexts';
 import { buildCollectionsPanelsProps } from './utils';
 import { CollectionSearchHits } from './types';
 
-const sourceFields = ['uuid', 'title', 'hubmap_id', 'datasets.hubmap_id'];
+const query = { ...getAllCollectionsQuery, _source: ['uuid', 'title', 'hubmap_id', 'datasets.hubmap_id'] };
 
-const query = { ...getAllCollectionsQuery, _source: sourceFields };
+const mustHaveDOIClause = { query: { exists: { field: 'doi_url' } } };
 
 function useCollections() {
-  const { searchHits: collectionsData } = useSearchHits(query) as CollectionSearchHits;
+  const { isHubmapUser } = useAppContext();
+
+  const { searchHits: collectionsData } = useSearchHits(
+    isHubmapUser ? query : { ...query, ...mustHaveDOIClause },
+  ) as CollectionSearchHits;
   return buildCollectionsPanelsProps(collectionsData);
 }
 
