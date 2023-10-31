b1ae28ac10bdb7802b0426e5940b26a275f52572
Nikolay Akhmetov <nick.akhmetov@gmail.com>
2023-10-31 12:27:34 -0400

    fix title alignment

--- context/app/static/js/shared-styles/DialogModal/style.ts
+++ context/app/static/js/shared-styles/DialogModal/style.ts
@@ -4,13 +4,13 @@ import DialogTitle from '@mui/material/DialogTitle';
 
 const StyledDialogTitle = styled(DialogTitle)(({ theme }) => ({
   marginBottom: theme.spacing(1),
-  padding: theme.spacing(2, 3, 0, 2),
-}));
+  padding: theme.spacing(2, 3, 0, 3),
+})) as typeof DialogTitle;
 
 const StyledDivider = styled(Divider)(({ theme }) => ({
   backgroundColor: theme.palette.primary.main,
   height: '1.5px',
   mt: theme.spacing(1),
-}));
+})) as typeof Divider;
 
 export { StyledDivider, StyledDialogTitle };
