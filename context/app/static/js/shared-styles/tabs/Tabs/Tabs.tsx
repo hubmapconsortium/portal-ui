4f7dc73d7aa59a4a0b7fea97cfda9a3e7d4aaf20
John Conroy <john-conroy@users.noreply.github.com>
2023-10-31 12:14:45 -0400

    Update tabs component

---
+++ context/app/static/js/shared-styles/tabs/Tabs/Tabs.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import MUITabs, { TabsProps } from '@mui/material/Tabs';
+
+function Tabs({ children, ...props }: TabsProps) {
+  return (
+    <MUITabs
+      variant="fullWidth"
+      TabIndicatorProps={{ sx: ({ spacing }) => ({ backgroundColor: 'action.activeTab', height: spacing(0.5) }) }}
+      sx={{
+        color: 'primaryContainer.contrastText',
+        backgroundColor: 'primaryContainer.main',
+        borderRadius: '4px 4px 0px 0px',
+      }}
+      {...props}
+    >
+      {children}
+    </MUITabs>
+  );
+}
+
+export default Tabs;
