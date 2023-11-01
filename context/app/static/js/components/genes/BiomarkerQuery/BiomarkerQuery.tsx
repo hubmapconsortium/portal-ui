1bf8b35b737b524fa8fa7f09085720afd9b2d52b
Nikolay Akhmetov <nick.akhmetov@gmail.com>
2023-11-01 15:33:20 -0400

    start

---
+++ context/app/static/js/components/genes/BiomarkerQuery/BiomarkerQuery.tsx
@@ -0,0 +1,24 @@
+import CellsResults from 'js/components/cells/CellsResults';
+import DatasetsSelectedByExpression from 'js/components/cells/DatasetsSelectedByExpression';
+import AccordionSteps from 'js/shared-styles/accordions/AccordionSteps';
+import { AccordionStepsProvider } from 'js/shared-styles/accordions/AccordionSteps/store';
+import React from 'react';
+
+function BiomarkerQuery() {
+  const runQueryButtonRef = React.useRef<HTMLButtonElement>(null);
+  const steps = [
+    {
+      heading: '1. Parameters',
+      content: <DatasetsSelectedByExpression runQueryButtonRef={runQueryButtonRef} />,
+    },
+    {
+      heading: '2. Results',
+      content: <CellsResults />,
+    },
+  ];
+  return (
+    <AccordionStepsProvider stepsLength={steps.length}>
+      <AccordionSteps id="cells-steps" steps={steps} isFirstStepOpen />
+    </AccordionStepsProvider>
+  );
+}
