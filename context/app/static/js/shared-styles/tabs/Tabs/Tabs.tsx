c5c53046b1241892ccb907cf77685d958b5813e4
John Conroy <john-conroy@users.noreply.github.com>
2023-11-08 17:04:12 -0500

    Refactor tabs

--- context/app/static/js/shared-styles/tabs/Tabs/Tabs.tsx
+++ context/app/static/js/shared-styles/tabs/Tabs/Tabs.tsx
@@ -1,22 +1,22 @@
 import React from 'react';
 import MUITabs, { TabsProps } from '@mui/material/Tabs';
+import { styled } from '@mui/material/styles';
 
-function Tabs({ children, ...props }: TabsProps) {
-  return (
-    <MUITabs
-      variant="fullWidth"
-      TabIndicatorProps={{ sx: ({ spacing }) => ({ backgroundColor: 'action.activeTab', height: spacing(0.5) }) }}
-      sx={{
-        color: 'primaryContainer.contrastText',
-        backgroundColor: 'primaryContainer.main',
-        borderRadius: '4px 4px 0px 0px',
-        flex: 'none',
-      }}
-      {...props}
-    >
-      {children}
-    </MUITabs>
-  );
-}
-
+const Tabs = styled((props: TabsProps) => <MUITabs variant="fullWidth" {...props} />)(
+  ({
+    theme: {
+      palette: { primaryContainer, action },
+      spacing,
+    },
+  }) => ({
+    color: primaryContainer.contrastText,
+    backgroundColor: primaryContainer.main,
+    borderRadius: spacing(0.5, 0.5, 0, 0),
+    flex: 'none',
+    '& .MuiTabs-indicator': {
+      backgroundColor: action.activeTab,
+      height: spacing(0.5),
+    },
+  }),
+);
 export default Tabs;
