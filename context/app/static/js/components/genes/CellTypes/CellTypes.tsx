9c9567646a1e2717332216f1fe07fbf7691aa705
Nikolay Akhmetov <nick.akhmetov@gmail.com>
2023-11-13 12:31:16 -0500

    start basic cell tyeps table

--- context/app/static/js/components/genes/CellTypes/CellTypes.tsx
+++ context/app/static/js/components/genes/CellTypes/CellTypes.tsx
@@ -1,16 +1,52 @@
 import React from 'react';
+
+import Table from '@mui/material/Table';
+import TableHead from '@mui/material/TableHead';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableRow from '@mui/material/TableRow';
+import LinearProgress from '@mui/material/LinearProgress';
+
 import { DetailPageSection } from 'js/components/detailPage/style';
 import SectionHeader from 'js/shared-styles/sections/SectionHeader';
 
-import EntityTable from 'js/shared-styles/tables/EntitiesTable/EntityTable';
 import { cellTypes } from '../constants';
 import { useGeneDetails } from '../hooks';
 
+const columnLabels = ['Cell Types', 'Description', 'Organs'];
+
 export default function CellTypes() {
   const { data, isLoading } = useGeneDetails();
+
   return (
     <DetailPageSection id={cellTypes.id}>
       <SectionHeader iconTooltipText={cellTypes.tooltip}>{cellTypes.title}</SectionHeader>
+      <Table>
+        <TableHead>
+          <TableRow>
+            {columnLabels.map((label) => (
+              <TableCell key={label}>{label}</TableCell>
+            ))}
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {isLoading ? (
+            <TableRow>
+              <TableCell colSpan={columnLabels.length}>
+                <LinearProgress />
+              </TableCell>
+            </TableRow>
+          ) : (
+            data?.cell_types.map((cellType) => (
+              <TableRow key={cellType.id}>
+                <TableCell>{cellType.name}</TableCell>
+                <TableCell>{cellType.definition}</TableCell>
+                <TableCell>{cellType.organs.join(', ')}</TableCell>
+              </TableRow>
+            ))
+          )}
+        </TableBody>
+      </Table>
     </DetailPageSection>
   );
 }
