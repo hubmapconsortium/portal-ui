33588c29a7c87849de3bdaf7a3df67765ecc46e5
Nikolay Akhmetov <nick.akhmetov@gmail.com>
2023-11-13 13:34:34 -0500

    wip

--- context/app/static/js/components/genes/CellTypes/CellTypes.tsx
+++ context/app/static/js/components/genes/CellTypes/CellTypes.tsx
@@ -6,14 +6,33 @@ import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
 import TableRow from '@mui/material/TableRow';
 import LinearProgress from '@mui/material/LinearProgress';
+import Paper from '@mui/material/Paper';
+import Button from '@mui/material/Button';
 
 import { DetailPageSection } from 'js/components/detailPage/style';
 import SectionHeader from 'js/shared-styles/sections/SectionHeader';
 
+import LoadingTableRows from 'js/shared-styles/tables/LoadingTableRows';
+import { TableContainer } from '@mui/material';
+import { entityIconMap } from 'js/shared-styles/icons/entityIconMap';
+import { StyledTableContainer } from 'js/shared-styles/tables';
 import { cellTypes } from '../constants';
 import { useGeneDetails } from '../hooks';
 
 const columnLabels = ['Cell Types', 'Description', 'Organs'];
+
+function TableSkeleton() {
+  return (
+    <>
+      <TableRow>
+        <TableCell colSpan={columnLabels.length}>
+          <LinearProgress />
+        </TableCell>
+      </TableRow>
+      <LoadingTableRows numberOfRows={3} numberOfCols={columnLabels.length} />
+    </>
+  );
+}
 
 export default function CellTypes() {
   const { data, isLoading } = useGeneDetails();
@@ -21,32 +40,35 @@ export default function CellTypes() {
   return (
     <DetailPageSection id={cellTypes.id}>
       <SectionHeader iconTooltipText={cellTypes.tooltip}>{cellTypes.title}</SectionHeader>
-      <Table>
-        <TableHead>
-          <TableRow>
-            {columnLabels.map((label) => (
-              <TableCell key={label}>{label}</TableCell>
-            ))}
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {isLoading ? (
+      <StyledTableContainer component={Paper}>
+        <Table stickyHeader>
+          <TableHead>
             <TableRow>
-              <TableCell colSpan={columnLabels.length}>
-                <LinearProgress />
-              </TableCell>
+              {columnLabels.map((label) => (
+                <TableCell key={label}>{label}</TableCell>
+              ))}
             </TableRow>
-          ) : (
-            data?.cell_types.map((cellType) => (
-              <TableRow key={cellType.id}>
-                <TableCell>{cellType.name}</TableCell>
-                <TableCell>{cellType.definition}</TableCell>
-                <TableCell>{cellType.organs.join(', ')}</TableCell>
-              </TableRow>
-            ))
-          )}
-        </TableBody>
-      </Table>
+          </TableHead>
+          <TableBody>
+            {isLoading ? (
+              <TableSkeleton />
+            ) : (
+              data?.cell_types.map((cellType) => (
+                <TableRow key={cellType.id}>
+                  <TableCell>{cellType.name}</TableCell>
+                  <TableCell>{cellType.definition}</TableCell>
+                  <TableCell>{cellType.organs.map((o) => o.name).join(',\u00A0') || <>&mdash;</>}</TableCell>
+                  {/* <TableCell>
+                    <Button startIcon={<entityIconMap.Dataset />} variant="outlined" sx={{ borderRadius: '4px' }}>
+                      View&nbsp;Datasets
+                    </Button>
+                  </TableCell> */}
+                </TableRow>
+              ))
+            )}
+          </TableBody>
+        </Table>
+      </StyledTableContainer>
     </DetailPageSection>
   );
 }
