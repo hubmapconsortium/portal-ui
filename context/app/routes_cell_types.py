c9be17c84905f6858997ef824b005e3f40f38c08
Nikolay Akhmetov <nick.akhmetov@gmail.com>
2023-11-14 15:51:41 -0500

    explanatory comment for hardcode

--- context/app/routes_cell_types.py
+++ context/app/routes_cell_types.py
@@ -1,11 +1,15 @@
 from flask import json
 from hubmap_api_py_client import Client
 
-from .utils import make_blueprint, get_client
+from .utils import make_blueprint
 
 
 def get_cells_client():
-    # TODO: This should be in config
+    # TODO: Since the `cell_type` lookup does not currently exist
+    # outside of the test environment, this is currently hardcoded.
+    # Once the `cell_type` lookup is available in the production
+    # environment, these routes should be moved to `routes_cells`,
+    # and this file should be deleted/removed from main.py.
     client = Client('https://cells.test.hubmapconsortium.org/api/')
     return client
 
