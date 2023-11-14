8519bcbb5a79957b3bc926d61f0aa556f0a35045
Nikolay Akhmetov <nick.akhmetov@gmail.com>
2023-11-14 16:21:31 -0500

    switch to base64 compress/decompress

--- context/app/static/js/helpers/compress.ts
+++ context/app/static/js/helpers/compress.ts
@@ -2,10 +2,10 @@ import LZString from 'lz-string';
 
 export function compressStringList(stringList: string[]): string {
   const joinedList = stringList.join(',');
-  return LZString.compressToEncodedURIComponent(joinedList);
+  return LZString.compressToBase64(joinedList);
 }
 
 export function decompressStringList(uuidListStr: string): string[] {
-  const decompressedStringList = LZString.decompressFromEncodedURIComponent(uuidListStr);
+  const decompressedStringList = LZString.decompressFromBase64(uuidListStr);
   return decompressedStringList.split(',');
 }
