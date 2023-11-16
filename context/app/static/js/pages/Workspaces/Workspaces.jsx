2930f4c98987cd02052aaccb1640b8b1e5d19841
John Conroy <62477388+john-conroy@users.noreply.github.com>
2023-11-16 16:05:59 -0500

    Update context/app/static/js/pages/Workspaces/Workspaces.jsx
    Co-authored-by: Nikolay Akhmetov <nick.akhmetov@gmail.com>

--- context/app/static/js/pages/Workspaces/Workspaces.jsx
+++ context/app/static/js/pages/Workspaces/Workspaces.jsx
@@ -13,7 +13,7 @@ function Workspaces() {
     <>
       <WorkspacesTitle />
       <WorkspacesAuthGuard>
-      <WorkspacesAuthenticated />;
+        <WorkspacesAuthenticated />
       </WorkspacesAuthGuard>
     </>
   );
