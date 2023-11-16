35b269d283bb42761fd58e96f411740f1a1dace7
John Conroy <62477388+john-conroy@users.noreply.github.com>
2023-11-16 15:00:45 -0500

    Update context/app/static/js/components/workspaces/hooks.ts
    Co-authored-by: Nikolay Akhmetov <nick.akhmetov@gmail.com>

--- context/app/static/js/components/workspaces/hooks.ts
+++ context/app/static/js/components/workspaces/hooks.ts
@@ -178,18 +178,17 @@ export function useCreateAndLaunchWorkspace() {
 function getWorkspaceTimeLeft(workspace: MergedWorkspace) {
   return findBestJob(workspace?.jobs ?? [])?.job_details?.current_job_details?.time_left;
 }
+type FoundPair = [MergedWorkspace, number];
 
 function useSessionWarning(workspaces: MergedWorkspace[]) {
-  const matchedWorkspace = workspaces.find((workspace) => getWorkspaceTimeLeft(workspace));
-
-  if (!matchedWorkspace) {
-    return false;
-  }
-
-  const timeLeft = getWorkspaceTimeLeft(matchedWorkspace);
-
-  if (!timeLeft) {
-    return false;
+  const [matchedWorkspace, timeLeft] = workspaces.reduce<FoundPair | undefined>((acc, ws) => {
+  	if (acc) return acc; // Avoids extra calls to `getWorkspaceTimeLeft` after workspace has been found since we can't break a .reduce() 
+    const time = getWorkspaceTimeLeft(ws);
+    return time ? [ws, time]: acc; // If a time is found, return the workspace with time, otherwise continue iteration
+  }, undefined);
+  
+  if (!matchedWorkspace || !timeLeft) {
+  	return false;
   }
 
   return `${matchedWorkspace.name} is currently running. You have ${Math.floor(
