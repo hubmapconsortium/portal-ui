761533c2d1dc4986351a5d3cedcebf0e638af066
Nikolay Akhmetov <nick.akhmetov@gmail.com>
2023-11-17 14:50:22 -0500

    convert `alert` to TSX

--- context/app/static/js/shared-styles/alerts/Alert.tsx
+++ context/app/static/js/shared-styles/alerts/Alert.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import styled from 'styled-components';
-import Alert from '@mui/material/Alert';
+import { styled } from '@mui/material/styles';
+import Alert, { AlertProps } from '@mui/material/Alert';
 import ErrorRoundedIcon from '@mui/icons-material/ErrorRounded';
 import InfoRoundedIcon from '@mui/icons-material/InfoRounded';
 import CheckCircleRoundedIcon from '@mui/icons-material/CheckCircleRounded';
 import WarningRoundedIcon from '@mui/icons-material/WarningRounded';
 
-function OutlinedAlert(props) {
+function OutlinedAlert(props: AlertProps) {
   return (
     <Alert
       {...props}
@@ -21,11 +21,15 @@ function OutlinedAlert(props) {
   );
 }
 
-const StyledAlert = styled(OutlinedAlert)`
-  :not(svg) {
-    color: ${(props) => props.theme.palette.text.primary};
-  }
-  margin-bottom: ${(props) => props.$marginBottom || 0}px;
-`;
+interface StyledAlertProps extends AlertProps {
+  $marginBottom?: number;
+}
+
+const StyledAlert = styled(OutlinedAlert)<StyledAlertProps>(({ theme, $marginBottom }) => ({
+  ':not(svg)': {
+    color: theme.palette.text.primary,
+  },
+  marginBottom: $marginBottom ?? 0,
+}));
 
 export { StyledAlert as Alert };
