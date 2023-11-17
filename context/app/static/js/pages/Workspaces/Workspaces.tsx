3120dd8a24197b66b846e077cf4b7349d1f1f10c
Nikolay Akhmetov <nick.akhmetov@gmail.com>
2023-11-17 13:37:19 -0500

    swap workspaces page to .tsx

--- context/app/static/js/pages/Workspaces/Workspaces.tsx
+++ context/app/static/js/pages/Workspaces/Workspaces.tsx
@@ -1,11 +1,7 @@
 import React from 'react';
 
-import { useAppContext } from 'js/components/Contexts';
 import WorkspacesTitle from 'js/components/workspaces/WorkspacesTitle';
-import { InternalLink } from 'js/shared-styles/Links';
 import WorkspacesAuthenticated from 'js/components/workspaces/WorkspacesAuthenticated';
-import ContactUsLink from 'js/shared-styles/Links/ContactUsLink';
-import { StyledDescription } from './style';
 import WorkspacesAuthGuard from 'js/components/workspaces/WorkspacesAuthGuard';
 
 function Workspaces() {
