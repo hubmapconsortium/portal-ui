a7533f0eb4279bdf011d3ddd205d74d0d809203c
John Conroy <john-conroy@users.noreply.github.com>
2023-11-27 12:48:00 -0500

    Create basic workspace name dialog

---
+++ context/app/static/js/components/workspaces/EditWorkspaceNameDialog/EditWorkspaceNameDialog.tsx
@@ -0,0 +1,81 @@
+import React, { useCallback } from 'react';
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
+import Box from '@mui/material/Box';
+import LoadingButton from '@mui/lab/LoadingButton';
+
+import DialogModal from 'js/shared-styles/DialogModal';
+import { useEditWorkspaceNameStore } from './store';
+
+import { useEditWorkspaceForm, EditWorkspaceFormTypes } from './hooks';
+import WorkspaceField from '../WorkspaceField/WorkspaceField';
+
+function EditWorkspaceNameDialog() {
+  const { isOpen, close, workspace } = useEditWorkspaceNameStore();
+  const workspaceName = workspace?.name ?? '';
+  const workspaceId = workspace?.id ?? 0;
+
+  const { onSubmit, control, handleSubmit, isSubmitting, errors } = useEditWorkspaceForm({
+    defaultName: workspaceName,
+    workspaceId,
+  });
+
+  const submit = useCallback(
+    async ({ 'workspace-name': wsName }: EditWorkspaceFormTypes) => {
+      await onSubmit({
+        workspaceName: wsName,
+      });
+    },
+    [onSubmit],
+  );
+
+  return (
+    <DialogModal
+      title="Edit Workspace Name"
+      content={
+        <Box
+          id="edit-workspace-form"
+          component="form"
+          sx={{
+            display: 'grid',
+            gap: 2,
+            marginTop: 1,
+          }}
+          // eslint-disable-next-line @typescript-eslint/no-misused-promises
+          onSubmit={handleSubmit(submit)}
+        >
+          <WorkspaceField
+            control={control}
+            name="workspace-name"
+            label="Name"
+            placeholder="Like “Spleen-Related Data” or “ATAC-seq Visualizations”"
+            autoFocus
+            onKeyDown={(e: React.KeyboardEvent<HTMLInputElement>) => {
+              e.stopPropagation();
+            }}
+          />
+        </Box>
+      }
+      isOpen={isOpen}
+      handleClose={close}
+      actions={
+        <Stack direction="row" spacing={2} alignItems="end">
+          <Button type="button" onClick={close}>
+            Cancel
+          </Button>
+          <LoadingButton
+            loading={isSubmitting}
+            type="submit"
+            form="edit-workspace-form"
+            disabled={Object.keys(errors).length > 0}
+          >
+            Save
+          </LoadingButton>
+        </Stack>
+      }
+      withCloseButton
+    />
+  );
+}
+
+export default EditWorkspaceNameDialog;
---
+++ context/app/static/js/components/workspaces/EditWorkspaceNameDialog/hooks.ts
@@ -0,0 +1,79 @@
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { z } from 'zod';
+
+import { useEditWorkspaceNameStore } from './store';
+import { useHandleUpdateWorkspace } from '../hooks';
+
+interface EditWorkspaceFormTypes {
+  'workspace-name': string;
+}
+
+interface UseEditWorkspaceNameFormTypes {
+  defaultName: string;
+  workspaceId: number;
+}
+
+interface SubmitEditWorkspaceNameTypes {
+  workspaceName: string;
+}
+
+function withCustomMessage(message: string): z.ZodErrorMap {
+  return function tooSmallErrorMap(issue, ctx) {
+    if (issue.code === z.ZodIssueCode.too_small) {
+      return { message };
+    }
+    return { message: ctx.defaultError };
+  };
+}
+
+const schema = z
+  .object({
+    'workspace-name': z
+      .string({ errorMap: withCustomMessage('A workspace name is required. Please enter a workspace name.') })
+      .min(1)
+      .max(150),
+  })
+  .partial()
+  .required({ 'workspace-name': true });
+
+function useEditWorkspaceForm({ defaultName, workspaceId }: UseEditWorkspaceNameFormTypes) {
+  const { handleUpdateWorkspace } = useHandleUpdateWorkspace({ workspaceId });
+  const { close } = useEditWorkspaceNameStore();
+
+  const {
+    handleSubmit,
+    control,
+    reset,
+    formState: { errors, isSubmitting, isSubmitSuccessful },
+  } = useForm({
+    defaultValues: {
+      'workspace-name': defaultName,
+    },
+    mode: 'onChange',
+    resolver: zodResolver(schema),
+  });
+
+  function handleClose() {
+    close();
+    reset();
+  }
+
+  async function onSubmit({ workspaceName }: SubmitEditWorkspaceNameTypes) {
+    if (isSubmitting || isSubmitSuccessful) return;
+    await handleUpdateWorkspace({ name: workspaceName });
+    reset();
+    handleClose();
+  }
+
+  return {
+    handleClose,
+    handleSubmit,
+    control,
+    errors,
+    onSubmit,
+    isSubmitting: isSubmitting || isSubmitSuccessful,
+  };
+}
+
+export { useEditWorkspaceForm, type EditWorkspaceFormTypes };
