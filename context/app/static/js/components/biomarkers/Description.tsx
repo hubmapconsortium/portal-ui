e6e133de06769901facf6c5b26b6907d1d0a8b2a
Nikolay Akhmetov <nick.akhmetov@gmail.com>
2023-12-01 16:46:31 -0500

    update description text

--- context/app/static/js/components/biomarkers/Description.tsx
+++ context/app/static/js/components/biomarkers/Description.tsx
@@ -3,14 +3,28 @@ import Stack from '@mui/material/Stack';
 import { InternalLink } from 'js/shared-styles/Links';
 import React from 'react';
 
+// Revert to this description when protein information is available
+
+// const fullDescription = (
+//   <div>
+//     Gene and protein information are available in HuBMAP data. To look up information about a specific gene or protein,
+//     use the search bar below. Launch the <InternalLink href="/cells">Molecular Query</InternalLink> to find datasets for
+//     a specific biomarker.
+//   </div>
+// );
+
+const currentDescription = (
+  <div>
+    Gene information is currently available in HuBMAP data. To look up information about a specific gene, use the search
+    bar below. Protein information is upcoming. Launch the <InternalLink href="/cells">Molecular Query</InternalLink> to
+    find datasets for a specific biomarker.
+  </div>
+);
+
 export default function BiomarkersLandingPageDescription() {
   return (
     <Stack direction="column" spacing={1}>
-      <div>
-        Gene and protein information are available in HuBMAP data. To look up information about a specific gene or
-        protein, use the search bar below. Launch the <InternalLink href="/cells">Molecular Query</InternalLink> to find
-        datasets for a specific biomarker.
-      </div>
+      {currentDescription}
       <Button variant="contained" color="primary" href="/cells" sx={{ width: 'max-content' }}>
         Launch Molecular Query (BETA)
       </Button>
