04dc5082b42d794ee1bf2adaaf4c6a68c1b33e0d
John Conroy <john-conroy@users.noreply.github.com>
2024-01-09 11:16:23 -0500

    Add internal link react component

--- context/app/static/js/shared-styles/Links/InternalLink.tsx
+++ context/app/static/js/shared-styles/Links/InternalLink.tsx
@@ -1,8 +1,13 @@
-import Link from '@mui/material/Link';
+import React from 'react';
+import Link, { LinkProps } from '@mui/material/Link';
 import { styled } from '@mui/material/styles';
 
-const InternalLink = styled(Link)(({ theme }) => ({
+const StyledLink = styled(Link)(({ theme }) => ({
   color: theme.palette.common.link,
 })) as typeof Link;
+
+function InternalLink(props: LinkProps) {
+  return <StyledLink {...props} />;
+}
 
 export default InternalLink;
