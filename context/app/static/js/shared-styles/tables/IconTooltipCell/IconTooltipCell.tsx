ed59512a75f031e38278c37033d235b9614df9b6
Nikolay Akhmetov <nick.akhmetov@gmail.com>
2024-03-06 14:12:19 -0500

    convert to TS

--- context/app/static/js/shared-styles/tables/IconTooltipCell/IconTooltipCell.tsx
+++ context/app/static/js/shared-styles/tables/IconTooltipCell/IconTooltipCell.tsx
@@ -1,35 +1,29 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import TableCell from '@mui/material/TableCell';
+import TableCell, { TableCellProps } from '@mui/material/TableCell';
 
 import { SecondaryBackgroundTooltip } from 'js/shared-styles/tooltips';
 import { InfoIcon } from 'js/shared-styles/icons';
 import { Flex, StyledSvgIcon } from './style';
 
-function IconTooltipCell({ children, tooltipTitle, icon, ...rest }) {
+interface IconTooltipCellProps {
+  children: React.ReactNode;
+  tooltipTitle?: string;
+  icon?: React.ComponentType;
+}
+
+function IconTooltipCell({ children, tooltipTitle, icon, ...rest }: IconTooltipCellProps & TableCellProps) {
   return (
     <TableCell {...rest}>
       <Flex>
         {children}
         {tooltipTitle && (
           <SecondaryBackgroundTooltip title={tooltipTitle} placement="bottom-start">
-            <StyledSvgIcon component={icon || InfoIcon} color="primary" />
+            <StyledSvgIcon component={icon ?? InfoIcon} color="primary" />
           </SecondaryBackgroundTooltip>
         )}
       </Flex>
     </TableCell>
   );
 }
-
-IconTooltipCell.propTypes = {
-  children: PropTypes.node.isRequired,
-  tooltipTitle: PropTypes.string,
-  icon: PropTypes.element,
-};
-
-IconTooltipCell.defaultProps = {
-  tooltipTitle: undefined,
-  icon: undefined,
-};
 
 export default IconTooltipCell;
