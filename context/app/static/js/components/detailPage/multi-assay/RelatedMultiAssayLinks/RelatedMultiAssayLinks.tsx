28d251efa66275967fc288d598a3d167dba99010
John Conroy <john-conroy@users.noreply.github.com>
2024-03-07 20:38:43 -0500

    Differentiate current dataset in multi assay relationship

--- context/app/static/js/components/detailPage/multi-assay/RelatedMultiAssayLinks/RelatedMultiAssayLinks.tsx
+++ context/app/static/js/components/detailPage/multi-assay/RelatedMultiAssayLinks/RelatedMultiAssayLinks.tsx
@@ -1,11 +1,14 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import React from 'react';
+import Box from '@mui/material/Box';
 import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
 
 import { InternalLink } from 'js/shared-styles/Links';
 import LabelledSectionText from 'js/shared-styles/sections/LabelledSectionText';
-import useRelatedMultiAssayDatasets from '../useRelatedMultiAssayDatasets';
+import { SecondaryBackgroundTooltip } from 'js/shared-styles/tooltips';
+import { useFlaskDataContext } from 'js/components/Contexts';
+import useRelatedMultiAssayDatasets, { MultiAssayEntity } from '../useRelatedMultiAssayDatasets';
 
 const text = {
   component: {
@@ -31,7 +34,31 @@ type Entries<T> = {
   [K in keyof T]: [K, T[K]];
 }[keyof T][];
 
+interface MultiAssayLinkProps {
+  dataset: MultiAssayEntity;
+}
+function MultiAssayLink({ dataset: { assay_display_name, uuid, hubmap_id } }: MultiAssayLinkProps) {
+  return (
+    <Typography>
+      {assay_display_name}: <InternalLink href={`/browse/dataset/${uuid}`}>{hubmap_id}</InternalLink>
+    </Typography>
+  );
+}
+
+function CurrentMultiAssayLink({ dataset }: MultiAssayLinkProps) {
+  return (
+    <SecondaryBackgroundTooltip title={text.current.tooltip}>
+      <Box sx={(theme) => ({ borderLeft: `2px solid ${theme.palette.success.main}`, pl: 0.5 })}>
+        <MultiAssayLink dataset={dataset} />
+      </Box>
+    </SecondaryBackgroundTooltip>
+  );
+}
+
 function RelatedMultiAssayLinks() {
+  const {
+    entity: { uuid },
+  } = useFlaskDataContext();
   const { datasets } = useRelatedMultiAssayDatasets();
 
   const entries = Object.entries(datasets) as Entries<typeof datasets>;
@@ -43,12 +70,13 @@ function RelatedMultiAssayLinks() {
     return (
       <LabelledSectionText label={text?.[key]?.label} key={key} iconTooltipText={text?.[key]?.tooltip}>
         <Stack>
-          {v.map((dataset) => (
-            <Typography key={dataset.uuid}>
-              {dataset.assay_display_name}:{' '}
-              <InternalLink href={`/browse/dataset/${dataset.uuid}`}>{dataset.hubmap_id}</InternalLink>
-            </Typography>
-          ))}
+          {v.map((dataset) =>
+            dataset.uuid === uuid ? (
+              <CurrentMultiAssayLink dataset={dataset} key={dataset.uuid} />
+            ) : (
+              <MultiAssayLink dataset={dataset} key={dataset.uuid} />
+            ),
+          )}
         </Stack>
       </LabelledSectionText>
     );
