fb10667a8ed149f3147625cb61864d9a5103f616
L Choy <3586093+lchoy@users.noreply.github.com>
2024-03-22 17:31:15 -0400

    Set platform environment variable. (#3405)
    Docker will automatically select an image variant which matches your OS and architecture. To instruct docker to pass the --platform flag to build our Docker image for an x86/amd64 platform, we set the environment variable 'DOCKER_DEFAULT_PLATFORM' in the build script.

--- etc/build/build.sh
+++ etc/build/build.sh
@@ -10,7 +10,8 @@ IMAGE_NAME=$1
 
 NODE_V=$(perl -pne 's/v//' .nvmrc )
 PYTHON_MINOR_V=$(perl -pne 's/\.\d+$//' .python-version)
-echo "Building $IMAGE_NAME with Node $NODE_V and Python $PYTHON_MINOR_V..."
+export DOCKER_DEFAULT_PLATFORM=linux/amd64
+echo "Building $IMAGE_NAME with Node $NODE_V and Python $PYTHON_MINOR_V using platform $DOCKER_DEFAULT_PLATFORM..."
 
 # Docker BuildKit supports parallelizing multi-stage builds.
 # https://medium.com/@tonistiigi/advanced-multi-stage-build-patterns-6f741b852fae
