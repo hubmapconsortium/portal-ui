7fb38745527ef1b87081362f4ffe79666c71deef
Nikolay Akhmetov <nick.akhmetov@gmail.com>
2024-04-23 16:54:32 -0400

    update description

--- context/app/static/js/pages/Cells/Cells.jsx
+++ context/app/static/js/pages/Cells/Cells.jsx
@@ -1,5 +1,6 @@
 import React, { useRef } from 'react';
 import Typography from '@mui/material/Typography';
+import Stack from '@mui/material/Stack';
 
 import AccordionSteps from 'js/shared-styles/accordions/AccordionSteps';
 import PageTitle from 'js/shared-styles/pages/PageTitle';
@@ -8,9 +9,9 @@ import SectionPaper from 'js/shared-styles/sections/SectionPaper';
 import QuerySelect from 'js/components/cells/QuerySelect';
 import CellsResults from 'js/components/cells/CellsResults';
 import CellsTutorial from 'js/components/cells/tutorial/CellsTutorial';
-import OutboundLink from 'js/shared-styles/Links/OutboundLink';
 import { withTutorialProvider } from 'js/shared-styles/tutorials/TutorialProvider';
 import { AccordionStepsProvider } from 'js/shared-styles/accordions/AccordionSteps/store';
+import ContactUsLink from 'js/shared-styles/Links/ContactUsLink';
 
 function Cells() {
   // Refs for the step actions used to traverse forwards in the tutorial.
@@ -36,21 +37,24 @@ function Cells() {
     <AccordionStepsProvider stepsLength={steps.length}>
       <CellsTutorial setParametersButtonRef={setParametersButtonRef} runQueryButtonRef={runQueryButtonRef} />
       <PageTitle color="primary">Datasets: Molecular Data Queries (BETA)</PageTitle>
-      <SectionPaper>
+      <Stack as={SectionPaper} direction="column" spacing={2}>
         <Typography>
           Retrieve datasets based on the abundance of transcriptomic, epigenomic, and proteomic biomarkers across cells.
+        </Typography>
+        <Typography>
           For example, you can retrieve a list of datasets where normalized transcript abundance for the UMOD
           (uromodulin) gene is above a user-defined cut off, e.g., 1, in at least 5% of all cells. This query will
-          return a list of kidney datasets. To begin your search, select a query type (&quot;Gene&quot; for
-          transcriptomic and epigenomic measurements, &quot;Protein&quot; for proteomic measurements) and set the
-          desired parameters. The nature of the cut off values is modality dependent, e.g., RPKM for transcriptomics.
-          Molecular data queries are in beta testing and there is a list of{' '}
-          <OutboundLink href="https://github.com/hubmapconsortium/portal-ui/issues?q=is%3Aopen+is%3Aissue+label%3A%22feature%3A+cells%22">
-            known issues
-          </OutboundLink>
-          .
+          return a list of kidney datasets.{' '}
         </Typography>
-      </SectionPaper>
+        <Typography>
+          To begin your search, select a query type (&quot;Gene&quot; for transcriptomic and epigenomic measurements,
+          &quot;Protein&quot; for proteomic measurements, &quot;Cell Type&quot; for cell type distribution) and set the
+          desired parameters.
+        </Typography>
+        <Typography>
+          <ContactUsLink>Contact us</ContactUsLink> with any questions and issues that may arise during your experience.
+        </Typography>
+      </Stack>
       <AccordionSteps id="cells-steps" steps={steps} />
     </AccordionStepsProvider>
   );
