384812dad353cc396acff3be9ec3f12ff1ccfe46
Nikolay Akhmetov <nick.akhmetov@gmail.com>
2024-05-31 11:09:52 -0400

    Hide data product generation section

--- context/app/static/js/components/detailPage/files/DataProducts/PipelineInfo.tsx
+++ context/app/static/js/components/detailPage/files/DataProducts/PipelineInfo.tsx
@@ -4,13 +4,5 @@ import { usePipelineInfo } from './hooks';
 
 export function PipelineInfo() {
   const { origin, name } = usePipelineInfo();
-  return (
-    // onClick={undefined} is a workaround
-    // for the .jsx definition of OutboundLink not specifying
-    // that the `onClick` prop is optional.
-    // Once we convert the related files to .ts (HMP-361) we can remove this
-    <OutboundLink onClick={undefined} href={origin}>
-      {name}
-    </OutboundLink>
-  );
+  return <OutboundLink href={origin}>{name}</OutboundLink>;
 }
