c8b1aa678191b8b03cb391ed2d8d462f3e9b4534
Nikolay Akhmetov <nick.akhmetov@gmail.com>
2024-06-12 12:28:17 -0400

    reset timeline scroll position on tab change

--- context/app/static/js/components/home/Hero/HeroTimeline.tsx
+++ context/app/static/js/components/home/Hero/HeroTimeline.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 
 import Timeline from 'js/shared-styles/Timeline';
 
 import Typography from '@mui/material/Typography';
 
 import Box from '@mui/material/Box';
+import { useIsDesktop } from 'js/hooks/media-queries';
 import { HOME_TIMELINE_ITEMS } from './const';
 import { HeroPanelContainer } from './styles';
 import { useHeroTabContext } from './HeroTabsContext';
@@ -14,9 +15,19 @@ interface HeroTimelineProps {
 }
 
 export default function HeroTimeline({ index }: HeroTimelineProps) {
-  const { activeTab } = useHeroTabContext();
+  const { activeTab, setActiveTab } = useHeroTabContext();
+  const ref = useRef<HTMLDivElement>(null);
+  const isDesktop = useIsDesktop();
+  const onFocus = () => setActiveTab(index);
+
+  // Reset scroll position when switching tabs
+  useEffect(() => {
+    if (isDesktop && ref.current && activeTab === index) {
+      ref.current.scrollTop = 0;
+    }
+  }, [activeTab, index, isDesktop]);
   return (
-    <HeroPanelContainer $index={index} $activeSlide={activeTab}>
+    <HeroPanelContainer ref={ref} onFocus={onFocus} $index={index} $activeSlide={activeTab}>
       <Typography variant="h4" px={2} py={1} zIndex={2} bgcolor="#FBEEEB" position="sticky" top={0}>
         Latest Changes
       </Typography>
