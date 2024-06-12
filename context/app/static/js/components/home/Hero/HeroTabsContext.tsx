b6e79c64635d6b2129ad19ed05d8e5d633d2f342
Nikolay Akhmetov <nick.akhmetov@gmail.com>
2024-06-12 12:48:00 -0400

    lint fix

--- context/app/static/js/components/home/Hero/HeroTabsContext.tsx
+++ context/app/static/js/components/home/Hero/HeroTabsContext.tsx
@@ -1,5 +1,5 @@
-import { createContext, useContext } from "js/helpers/context";
-import React, { PropsWithChildren, useState } from "react";
+import { createContext, useContext } from 'js/helpers/context';
+import React, { PropsWithChildren } from 'react';
 
 interface HeroTabContextProps {
   activeTab: number;
@@ -9,9 +9,6 @@ interface HeroTabContextProps {
 export const HeroTabContext = createContext<HeroTabContextProps>('HeroTabContext');
 export const useHeroTabContext = () => useContext(HeroTabContext);
 export function HeroTabContextProvider({ children, activeTab, setActiveTab }: PropsWithChildren<HeroTabContextProps>) {
-  return (
-    <HeroTabContext.Provider value={{ activeTab, setActiveTab }}>
-      {children}
-    </HeroTabContext.Provider>
-  );
+  // eslint-disable-next-line react/jsx-no-constructed-context-values -- No need to wrap this in memo
+  return <HeroTabContext.Provider value={{ activeTab, setActiveTab }}>{children}</HeroTabContext.Provider>;
 }
