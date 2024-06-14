5e869efdf8b98dc75bd6931f1c98e37146a9f346
Nikolay Akhmetov <nick.akhmetov@gmail.com>
2024-06-14 11:44:24 -0400

    convert to ts

--- context/app/static/js/components/Header/HubmapLogo.tsx
+++ context/app/static/js/components/Header/HubmapLogo.tsx
@@ -1,13 +1,13 @@
-import styled from 'styled-components';
+import { styled } from '@mui/material/styles';
 
 // This import doesn't work at test-time, so pull into a separate file,
 // so it doesn't cause problems for other styled components.
 import Logo from 'assets/svg/hubmap-logo.svg';
 
-const HubmapLogo = styled(Logo)`
-  margin-right: 10px;
-  fill: ${(props) => props.theme.palette.white.main};
-  height: 20px;
-`;
+const HubmapLogo = styled(Logo)(({ theme }) => ({
+  fill: theme.palette.white.main,
+  height: 20,
+  marginRight: 10,
+}));
 
 export default HubmapLogo;
