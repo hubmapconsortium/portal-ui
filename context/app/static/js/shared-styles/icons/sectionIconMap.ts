245d9780a74d3de96d89ca8dd068b7fb30cc20dd
John Conroy <john-conroy@users.noreply.github.com>
2024-07-11 15:31:08 -0400

    Add icons map

---
+++ context/app/static/js/shared-styles/icons/sectionIconMap.ts
@@ -0,0 +1,25 @@
+import {
+  AnalysisDetailsIcon,
+  AttributionIcon,
+  BulkDataIcon,
+  CollectionIcon,
+  FileIcon,
+  MetadataIcon,
+  ProcessedDataIcon,
+  ProvenanceIcon,
+  SummaryIcon,
+  VisualizationIcon,
+} from './icons';
+
+export const sectionIconMap = {
+  summary: SummaryIcon,
+  metadata: MetadataIcon,
+  processed: ProcessedDataIcon,
+  visualization: VisualizationIcon,
+  files: FileIcon,
+  analysis: AnalysisDetailsIcon,
+  data: BulkDataIcon,
+  provenance: ProvenanceIcon,
+  collections: CollectionIcon,
+  attribution: AttributionIcon,
+};
