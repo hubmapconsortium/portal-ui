f271e6fd2cbc7a6b95c027c6717a118de4c373a6
Austen Money <austen.money@gmail.com>
2024-07-15 12:51:54 -0400

    remove formatting

--- context/app/static/js/components/detailPage/entityHeader/EntityHeader/EntityHeader.tsx
+++ context/app/static/js/components/detailPage/entityHeader/EntityHeader/EntityHeader.tsx
@@ -9,12 +9,10 @@ import EntityHeaderContent from '../EntityHeaderContent';
 
 const AnimatedPaper = animated(StyledPaper);
 
-const entityStoreSelector = (state: EntityStore) => {
-  return {
-    assayMetadata: state.assayMetadata,
-    summaryComponentObserver: state.summaryComponentObserver,
-  };
-};
+const entityStoreSelector = (state: EntityStore) => ({
+  assayMetadata: state.assayMetadata,
+  summaryComponentObserver: state.summaryComponentObserver,
+});
 
 const visualizationSelector = (state: VisualizationStore) => ({
   vizIsFullscreen: state.vizIsFullscreen,
