f5030af3755be30846dbd494a7360decf85aec26
Austen Money <austen.money@gmail.com>
2024-07-22 10:46:32 -0400

    Austenem/cat 774 fix workspaces text (#3475)
    * update login alert to update text for workspaces only
    
    * remove template string
    
    * add changelog
    
    * update CHANGELOG-fix-workspace-text.md
    
    Co-authored-by: John Conroy <62477388+john-conroy@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: John Conroy <62477388+john-conroy@users.noreply.github.com>

--- context/app/static/js/shared-styles/alerts/LoginAlert.tsx
+++ context/app/static/js/shared-styles/alerts/LoginAlert.tsx
@@ -9,7 +9,10 @@ interface LoginAlertProps {
 export default function LoginAlert({ featureName }: LoginAlertProps) {
   return (
     <Alert severity="info" action={<Button href="/login">Log in</Button>}>
-      You must be logged in to access {featureName}. Access to {featureName} is restricted to HuBMAP members at present.
+      You must be logged in to access {featureName}.
+      {featureName === 'workspaces'
+        ? ' At present, access to workspaces is restricted to HuBMAP members and invited community members.'
+        : ` Access to ${featureName} is restricted to HuBMAP members at present.`}
     </Alert>
   );
 }
