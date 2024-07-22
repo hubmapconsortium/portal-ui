fe06a28c6defb04b1e3898444c6739c7b5b2b137
John Conroy <john-conroy@users.noreply.github.com>
2024-07-22 13:54:12 -0400

    Update organ page to use layout

--- context/app/static/js/pages/Organ/Organ.tsx
+++ context/app/static/js/pages/Organ/Organ.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import Typography from '@mui/material/Typography';
 
-import TableOfContents from 'js/shared-styles/sections/TableOfContents';
-
 import Azimuth from 'js/components/organ/Azimuth';
 import Assays from 'js/components/organ/Assays';
 import Description from 'js/components/organ/Description';
@@ -10,10 +8,8 @@ import HumanReferenceAtlas from 'js/components/organ/HumanReferenceAtlas';
 import Samples from 'js/components/organ/Samples';
 import DatasetsBarChart from 'js/components/organ/OrganDatasetsChart';
 import Section from 'js/shared-styles/sections/Section';
-import { getSections } from 'js/shared-styles/sections/TableOfContents/utils';
-
 import { OrganFile } from 'js/components/organ/types';
-import { FlexRow, Content } from './style';
+import DetailLayout from 'js/components/detailPage/DetailLayout';
 
 interface OrganProps {
   organ: OrganFile;
@@ -36,48 +32,43 @@ function Organ({ organ }: OrganProps) {
     [samplesId]: shouldDisplaySearch,
   };
 
-  const items = getSections(shouldDisplaySection);
-
   return (
-    <FlexRow>
-      <TableOfContents items={items} />
-      <Content>
-        <Typography variant="subtitle1" component="h1" color="primary">
-          Organ
-        </Typography>
-        <Typography variant="h1" component="h2">
-          {organ.name}
-        </Typography>
-        {shouldDisplaySection[summaryId] && (
-          <Section id={summaryId}>
-            <Description uberonIri={organ.uberon} uberonShort={organ.uberon_short} asctbId={organ.asctb}>
-              {organ.description}
-            </Description>
-          </Section>
-        )}
-        {shouldDisplaySection[hraId] && (
-          <Section id={hraId}>
-            <HumanReferenceAtlas uberonIri={organ.uberon} />
-          </Section>
-        )}
-        {shouldDisplaySection[referenceId] && (
-          <Section id={referenceId}>
-            <Azimuth config={organ.azimuth} />
-          </Section>
-        )}
-        {shouldDisplaySection[assaysId] && (
-          <Section id={assaysId}>
-            <Assays organTerms={organ.search} />
-            <DatasetsBarChart search={organ.search} />
-          </Section>
-        )}
-        {shouldDisplaySection[samplesId] && (
-          <Section id={samplesId}>
-            <Samples organTerms={organ.search} />
-          </Section>
-        )}
-      </Content>
-    </FlexRow>
+    <DetailLayout sections={shouldDisplaySection}>
+      <Typography variant="subtitle1" component="h1" color="primary">
+        Organ
+      </Typography>
+      <Typography variant="h1" component="h2">
+        {organ.name}
+      </Typography>
+      {shouldDisplaySection[summaryId] && (
+        <Section id={summaryId}>
+          <Description uberonIri={organ.uberon} uberonShort={organ.uberon_short} asctbId={organ.asctb}>
+            {organ.description}
+          </Description>
+        </Section>
+      )}
+      {shouldDisplaySection[hraId] && (
+        <Section id={hraId}>
+          <HumanReferenceAtlas uberonIri={organ.uberon} />
+        </Section>
+      )}
+      {shouldDisplaySection[referenceId] && (
+        <Section id={referenceId}>
+          <Azimuth config={organ.azimuth} />
+        </Section>
+      )}
+      {shouldDisplaySection[assaysId] && (
+        <Section id={assaysId}>
+          <Assays organTerms={organ.search} />
+          <DatasetsBarChart search={organ.search} />
+        </Section>
+      )}
+      {shouldDisplaySection[samplesId] && (
+        <Section id={samplesId}>
+          <Samples organTerms={organ.search} />
+        </Section>
+      )}
+    </DetailLayout>
   );
 }
 
