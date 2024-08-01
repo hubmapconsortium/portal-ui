c482b7ae973ee9e493aec216c68373c8ea768450
Austen Money <austen.money@gmail.com>
2024-08-01 15:09:42 -0400

    add hive alert

--- context/app/static/js/components/detailPage/Attribution/Attribution.tsx
+++ context/app/static/js/components/detailPage/Attribution/Attribution.tsx
@@ -5,19 +5,51 @@ import { DetailPageSection } from 'js/components/detailPage/style';
 import EmailIconLink from 'js/shared-styles/Links/iconLinks/EmailIconLink';
 import SectionHeader from 'js/shared-styles/sections/SectionHeader';
 
-import { FlexPaper } from './style';
+import Stack from '@mui/material/Stack';
+import Typography from '@mui/material/Typography';
+import { InfoIcon } from 'js/shared-styles/icons';
+import Paper from '@mui/material/Paper';
 import SectionItem from '../SectionItem';
+import { FlexPaper } from './style';
 
 function Attribution() {
   const {
-    entity: { group_name, created_by_user_displayname, created_by_user_email, entity_type },
+    entity: {
+      group_name,
+      created_by_user_displayname,
+      created_by_user_email,
+      entity_type,
+      processing,
+      creation_action,
+    },
   } = useFlaskDataContext();
+
+  const isHiveProcessedDataset =
+    entity_type === 'Dataset' && processing === 'processed' && creation_action === 'Central Process';
+
+  const infoAlert = (
+    <Stack component={Paper} p={2} spacing={2} marginBottom={1.25}>
+      <Stack direction="row" spacing={2}>
+        <InfoIcon color="primary" fontSize="1.5rem" />
+        <Typography>
+          {`The data provided by the ${group_name} Group was centrally processed by HuBMAP. The results of this processing are independent of analyses conducted by the data providers or third parties.`}
+        </Typography>
+      </Stack>
+    </Stack>
+  );
 
   return (
     <DetailPageSection id="attribution">
-      <SectionHeader iconTooltipText={`Information about the group registering this ${entity_type?.toLowerCase()}.`}>
+      <SectionHeader
+        iconTooltipText={
+          isHiveProcessedDataset
+            ? undefined
+            : `Information about the group registering this ${entity_type?.toLowerCase()}.`
+        }
+      >
         Attribution
       </SectionHeader>
+      {isHiveProcessedDataset && infoAlert}
       <FlexPaper>
         <SectionItem label="Group">{group_name}</SectionItem>
         <SectionItem label="Registered by" ml>
