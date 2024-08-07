0ff1a240055b0d2ee28c410ad0de668f314b7750
John Conroy <john-conroy@users.noreply.github.com>
2024-08-07 09:56:52 -0400

    Use position sticky

--- context/app/static/js/components/detailPage/entityHeader/EntityHeader/style.ts
+++ context/app/static/js/components/detailPage/entityHeader/EntityHeader/style.ts
@@ -4,7 +4,7 @@ import Paper from '@mui/material/Paper';
 import { headerHeight } from 'js/components/Header/HeaderAppBar/style';
 
 const StyledPaper = styled(Paper)(({ theme }) => ({
-  position: 'fixed',
+  position: 'sticky',
   width: '100%',
   top: headerHeight,
   zIndex: theme.zIndex.entityHeader,
