70c5c452897b7a75b5029fabef4cde277325596a
Nikolay Akhmetov <nick.akhmetov@gmail.com>
2024-08-19 15:28:40 -0400

    type lint fix

--- context/app/static/js/components/detailPage/files/DataProducts/hooks.ts
+++ context/app/static/js/components/detailPage/files/DataProducts/hooks.ts
@@ -39,7 +39,7 @@ export function processDagList(acc: PipelineInfo, dag: DagProvenanceType) {
     acc.origin = dag.origin;
   }
   if ('name' in dag) {
-    if (nameDenylist.includes(dag.name!)) {
+    if (nameDenylist.includes(dag.name)) {
       acc.name = getGithubRepoName(acc.origin);
     } else {
       acc.name = dag.name!;
