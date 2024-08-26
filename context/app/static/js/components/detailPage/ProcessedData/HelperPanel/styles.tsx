bdcdec4cff556f45516059b16b4d7854e9dad8c9
Nikolay Akhmetov <nick.akhmetov@gmail.com>
2024-08-26 19:58:38 -0400

    CAT-850 Fix outlined button hover incompatibility

--- context/app/static/js/components/detailPage/ProcessedData/HelperPanel/styles.tsx
+++ context/app/static/js/components/detailPage/ProcessedData/HelperPanel/styles.tsx
@@ -1,13 +1,14 @@
-import React from 'react';
+import React, { ForwardedRef, forwardRef } from 'react';
 
 import { styled } from '@mui/material/styles';
 import Button, { ButtonProps } from '@mui/material/Button';
 
-// TODO: Fix hover issue with tooltips - seems to be caused by ref forwarding
-export const HelperPanelButton = styled((props: ButtonProps) => <Button {...props} variant="outlined" />)(
-  ({ theme }) => ({
-    backgroundColor: 'white',
-    borderRadius: theme.spacing(0.5),
-    whiteSpace: 'nowrap',
-  }),
-);
+const OutlinedButton = forwardRef((props: ButtonProps, ref: ForwardedRef<HTMLButtonElement>) => (
+  <Button ref={ref} {...props} variant="outlined" />
+));
+
+export const HelperPanelButton = styled(OutlinedButton)(({ theme }) => ({
+  backgroundColor: 'white',
+  borderRadius: theme.spacing(0.5),
+  whiteSpace: 'nowrap',
+}));
