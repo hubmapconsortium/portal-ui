c4ba297f73f6d2194a2d05aeeb8c0b76c7244d98
Nikolay Akhmetov <nick.akhmetov@gmail.com>
2024-09-05 11:11:48 -0400

    NickAkhmetov/CAT-880 Ensure data product and file URLs use relevant dataset IDs when constructing.

--- context/app/static/js/components/detailPage/files/DataProducts/DataProducts.spec.tsx
+++ context/app/static/js/components/detailPage/files/DataProducts/DataProducts.spec.tsx
@@ -5,8 +5,11 @@ import { render, screen } from 'test-utils/functions';
 
 import { FlaskDataContext } from 'js/components/Contexts';
 import { DetailContext } from 'js/components/detailPage/DetailContext';
+import { ProcessedDatasetInfo } from 'js/pages/Dataset/hooks';
 import DataProducts from './DataProducts';
 import { flaskDataContext } from '../file-fixtures.spec';
+import { ProcessedDatasetContextProvider } from '../../ProcessedData/ProcessedDataset/ProcessedDatasetContext';
+import { ProcessedDatasetDetails } from '../../ProcessedData/ProcessedDataset/hooks';
 
 const files = {
   get all() {
@@ -39,11 +42,17 @@ const flaskDataContextEdited = { ...flaskDataContext, entity: { ...flaskDataCont
 
 function TestDataProducts({ files: passedFiles = files.all }) {
   return (
-    <FlaskDataContext.Provider value={flaskDataContextEdited}>
-      <DetailContext.Provider value={detailContext}>
-        <DataProducts files={passedFiles} />
-      </DetailContext.Provider>
-    </FlaskDataContext.Provider>
+    <ProcessedDatasetContextProvider
+      dataset={{ uuid: 'fakeuuid' } as unknown as ProcessedDatasetDetails}
+      defaultExpanded
+      sectionDataset={{ uuid: 'fakeparentuuid' } as unknown as ProcessedDatasetInfo}
+    >
+      <FlaskDataContext.Provider value={flaskDataContextEdited}>
+        <DetailContext.Provider value={detailContext}>
+          <DataProducts files={passedFiles} />
+        </DetailContext.Provider>
+      </FlaskDataContext.Provider>
+    </ProcessedDatasetContextProvider>
   );
 }
 
