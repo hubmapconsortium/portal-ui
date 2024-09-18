7f31fb3753113f2136ca60957c6a0ad1cd27c19d
John Conroy <john-conroy@users.noreply.github.com>
2024-09-18 11:37:46 -0400

    Update max workspace resource options

--- context/app/static/js/components/workspaces/constants.ts
+++ context/app/static/js/components/workspaces/constants.ts
@@ -23,10 +23,10 @@ export const DEFAULT_GPU_ENABLED = false;
 
 /* Workspace resource limits */
 export const MIN_NUM_CPUS = 1;
-export const MAX_NUM_CPUS = 2;
+export const MAX_NUM_CPUS = 16;
 
 export const MIN_MEMORY_MB = 1024;
-export const MAX_MEMORY_MB = 32768;
+export const MAX_MEMORY_MB = 131072;
 
 export const MIN_TIME_LIMIT_MINUTES = 60;
 export const MAX_TIME_LIMIT_MINUTES = 360;
