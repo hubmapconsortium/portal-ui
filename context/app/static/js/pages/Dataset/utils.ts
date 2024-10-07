01035afce88904c70b3a2b0440262e11c12ad799
Nikolay Akhmetov <nick.akhmetov@gmail.com>
2024-10-07 10:48:44 -0400

    adjust logic to make it easier to extend it to other states

--- context/app/static/js/pages/Dataset/utils.ts
+++ context/app/static/js/pages/Dataset/utils.ts
@@ -1,5 +1,7 @@
 import { ProcessedDatasetInfo } from './hooks';
 
+const potentialDuplicateStates = ['error'];
+
 export function datasetSectionId(
   dataset: Pick<ProcessedDatasetInfo, 'hubmap_id' | 'status'> & { pipeline?: string },
   prefix = '',
@@ -7,7 +9,8 @@ export function datasetSectionId(
   const { pipeline, hubmap_id, status } = dataset;
   const formattedDatasetIdentifier = (pipeline ?? hubmap_id).replace(/\s/g, '');
   const formattedStatus = status.replace(/\s/g, '');
-  const deduplicatedFormattedStatus =
-    formattedStatus.toLowerCase() === 'error' ? `${formattedStatus}-${hubmap_id}` : formattedStatus;
+  const deduplicatedFormattedStatus = potentialDuplicateStates.includes(formattedStatus.toLowerCase())
+    ? `${formattedStatus}-${hubmap_id}`
+    : formattedStatus;
   return `${prefix}-${encodeURIComponent(formattedDatasetIdentifier)}-${encodeURIComponent(deduplicatedFormattedStatus)}`.toLowerCase();
 }
