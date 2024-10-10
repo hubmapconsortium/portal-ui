d649284deb0d12070035384411e936d98b6c4df4
Austen Money <austen.money@gmail.com>
2024-10-10 17:14:53 -0400

    fix header bug

--- context/app/static/js/components/Header/Header/Header.tsx
+++ context/app/static/js/components/Header/Header/Header.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import Box from '@mui/material/Box';
 import EntityHeader from 'js/components/detailPage/entityHeader/EntityHeader';
 import HeaderAppBar from '../HeaderAppBar';
 import HeaderContent from '../HeaderContent';
@@ -9,12 +10,12 @@ function Header() {
   const { shouldDisplayHeader, ...props } = useEntityHeaderVisibility();
 
   return (
-    <>
+    <Box sx={(theme) => ({ position: 'fixed', width: '100%', zIndex: theme.zIndex.header })}>
       <HeaderAppBar {...props}>
         <HeaderContent />
       </HeaderAppBar>
       {shouldDisplayHeader && <EntityHeader />}
-    </>
+    </Box>
   );
 }
 
