308a6ebd7ea05029858c2d61d50fa4585b8021b9
Austen Money <austen.money@gmail.com>
2024-10-21 11:40:14 -0400

    add detail page tests

--- end-to-end/cypress/e2e/portal/all-pages.cy.js
+++ end-to-end/cypress/e2e/portal/all-pages.cy.js
@@ -1,26 +1,33 @@
-// describe("Home page", () => {
-//   context("macbook-size", () => {
-//     beforeEach(() => {
-//       cy.viewport("macbook-15");
-//       cy.visit("/");
-//       });
-//     it("loads", () => {
-//       cy.url().should("include", "/");
-//     });
-//     it("has a title at the top", () => {
-//       cy.findByTestId("home-page-title").contains("Human BioMolecular Atlas Program Data Portal");
-//     });
-//   })
-// });
+describe("Detail page types all load", () => {
+  const searchDetailPages = [
+    "Dataset",
+    "Sample",
+    "Donor",
+  ];
+
+  context("macbook-size", () => {
+    beforeEach(() => {
+      cy.viewport("macbook-15");
+    });
+
+    searchDetailPages.forEach((page) => {
+      it(`loads a ${page} detail page`, () => {
+        cy.visit(`/search?entity_type[0]=${page}`);
+        cy.findAllByTestId("hubmap-id-link").first().click();
+        cy.findByTestId("entity-title").should("exist").and("be.visible");
+      });
+    });
+  });
+});
 
 describe("Landing pages all load", () => {
   const pages = [
     { url: "/biomarkers", testId: "biomarkers-title", name: "Biomarkers" },
     { url: "/collections", testId: "collections-title", name: "Collections" },
-    { url: "/search?entity_type[0]=Dataset", testId: "entity-header", name: "Datasets" },
+    { url: "/search?entity_type[0]=Dataset", testId: "search-header", name: "Datasets" },
     { url: "/dev-search", testId: "dev-search-title", name: "Dev Search" },
     { url: "/diversity", testId: "diversity-title", name: "Donor Diversity" },
-    { url: "/search?entity_type[0]=Donor", testId: "entity-header", name: "Donors" },
+    { url: "/search?entity_type[0]=Donor", testId: "search-header", name: "Donors" },
     { url: "/", testId: "home-page-title", name: "Home" },
     { url: "/my-lists", testId: "my-lists-title", name: "My Lists" },
     { url: "/workspaces", testId: "my-workspaces-title", name: "My Workspaces" },
@@ -28,7 +35,7 @@ describe("Landing pages all load", () => {
     { url: "/organ", testId: "organs-title", name: "Organs" },
     { url: "/profile", testId: "login-alert", name: "Profile" },
     { url: "/publications", testId: "publications-title", name: "Publications" },
-    { url: "/search?entity_type[0]=Sample", testId: "entity-header", name: "Samples" },
+    { url: "/search?entity_type[0]=Sample", testId: "search-header", name: "Samples" },
     { url: "/services", testId: "services-title", name: "Services" },
     { url: "/templates", testId: "templates-title", name: "Templates" },
     { url: "/tutorials", testId: "tutorials-title", name: "Tutorials" },
