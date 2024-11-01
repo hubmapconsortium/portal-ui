cdd6d6953e4f9cc04a8278838050b2a38f845106
Austen Money <austen.money@gmail.com>
2024-11-01 16:06:10 -0400

    integrate with search page

--- context/app/static/js/components/searchPage/SearchBarLayout/SearchBarLayout.jsx
+++ context/app/static/js/components/searchPage/SearchBarLayout/SearchBarLayout.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { SearchBox, SelectedFilters, SortingSelector, ViewSwitcherToggle, SimpleQueryString } from 'searchkit';
 
@@ -7,7 +7,12 @@ import Download from '@mui/icons-material/Download';
 
 import { withAnalyticsCategory } from 'js/components/searchPage/hooks';
 import WorkspacesDropdownMenu from 'js/components/workspaces/WorkspacesDropdownMenu';
+import { useBulkDownloadDialog } from 'js/components/bulkDownload/hooks';
+import { useSelectableTableStore } from 'js/shared-styles/tables/SelectableTableProvider';
+import { fetchSearchData } from 'js/hooks/useSearchData';
+import { useAppContext } from 'js/components/Contexts';
 import { WhiteBackgroundIconTooltipButton } from 'js/shared-styles/buttons';
+import { getIDsQuery } from 'js/helpers/queries';
 import SearchViewSwitch, { DevSearchViewSwitch } from './SearchViewSwitch';
 import MetadataMenu from '../MetadataMenu';
 import TilesSortDropdown from '../TilesSortDropdown';
@@ -15,6 +20,25 @@ import SelectedFilter from '../SelectedFilter';
 import { Flex, CenteredDiv } from './style';
 
 function SearchBarLayout({ type, queryFields, sortOptions, isDevSearch, analyticsCategory }) {
+  const { openDialog } = useBulkDownloadDialog();
+  const { selectedRows } = useSelectableTableStore();
+  const { elasticsearchEndpoint, groupsToken } = useAppContext();
+
+  const [datasets, setDatasets] = useState([]);
+
+  useEffect(() => {
+    const datasetQuery = {
+      query: getIDsQuery([...selectedRows]),
+      _source: ['hubmap_id', 'processing', 'uuid', 'files'],
+    };
+    fetchSearchData(datasetQuery, elasticsearchEndpoint, groupsToken)
+      .then((response) => {
+        const results = response?.hits?.hits || [];
+        setDatasets(results.map((d) => d._source));
+      })
+      .catch((e) => console.error('Error fetching datasets:', e));
+  }, [selectedRows, elasticsearchEndpoint, groupsToken]);
+
   const SwitchComponent = withAnalyticsCategory(
     isDevSearch ? DevSearchViewSwitch : SearchViewSwitch,
     analyticsCategory,
@@ -38,8 +62,11 @@ function SearchBarLayout({ type, queryFields, sortOptions, isDevSearch, analytic
           {!isDevSearch && <MetadataMenu type={type} analyticsCategory={analyticsCategory} />}
           {!isDevSearch && <WorkspacesDropdownMenu type={type} />}
           {!isDevSearch && (
-            // TODO: CLT modal
-            <WhiteBackgroundIconTooltipButton tooltip="Download dataset manifest" onClick={() => {}} sx={{ mr: 1 }}>
+            <WhiteBackgroundIconTooltipButton
+              tooltip="Download dataset manifest"
+              onClick={() => openDialog(datasets)}
+              sx={{ mr: 1 }}
+            >
               <SvgIcon color="primary" component={Download} />
             </WhiteBackgroundIconTooltipButton>
           )}
