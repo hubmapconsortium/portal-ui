81089a78ef54284a0b4eb9ed3caec140f3b773b0
Austen Money <austen.money@gmail.com>
2024-11-25 16:14:04 -0500

    update description link

--- context/app/static/js/components/detailPage/BulkDataTransfer/BulkDataTransferSection.tsx
+++ context/app/static/js/components/detailPage/BulkDataTransfer/BulkDataTransferSection.tsx
@@ -6,12 +6,24 @@ import { CollapsibleDetailPageSection } from 'js/components/detailPage/DetailPag
 import { FilesContextProvider } from 'js/components/detailPage/files/FilesContext';
 import { Tabs, Tab, TabPanel } from 'js/shared-styles/tables/TableTabs';
 import { DetailSectionPaper } from 'js/shared-styles/surfaces';
+import { OutboundLink } from 'js/shared-styles/Links';
 import withShouldDisplay from 'js/helpers/withShouldDisplay';
 import { sectionIconMap } from 'js/shared-styles/icons/sectionIconMap';
 import { SectionDescription } from 'js/shared-styles/sections/SectionDescription';
 import BulkDownloadTextButton from 'js/components/bulkDownload/buttons/BulkDownloadTextButton';
+import { LINKS } from 'js/components/bulkDownload/constants';
 import BulkDataTransferPanels from './BulkDataTransferPanels';
 import { useProcessedDatasetTabs } from '../ProcessedData/ProcessedDataset/hooks';
+
+const sectionDescription = (
+  <Typography>
+    This section explains how to bulk download the raw and processed data for this dataset. Files for individual raw or
+    processed data can be downloaded via Globus or dbGaP from the respective tabs. To download files from multiple
+    Globus directories simultaneously, use the
+    <OutboundLink href={LINKS.documentation}> HuBMAP Command Line Transfer (CLT) Tool</OutboundLink>. Note that
+    processed data has separate download directories in Globus or dbGaP, distinct from the raw data directory.
+  </Typography>
+);
 
 function BulkDataTransfer() {
   const tabs = useProcessedDatasetTabs();
@@ -29,13 +41,7 @@ function BulkDataTransfer() {
       <FilesContextProvider>
         <SectionDescription>
           <Stack spacing={1}>
-            <Typography>
-              This section explains how to bulk download the raw and processed data for this dataset. Files for
-              individual raw or processed data can be downloaded via Globus or dbGaP from the respective tabs. To
-              download files from multiple Globus directories simultaneously, use the HuBMAP Command Line Transfer (CLT)
-              Tool. Note that processed data has separate download directories in Globus or dbGaP, distinct from the raw
-              data directory.
-            </Typography>
+            {sectionDescription}
             <BulkDownloadTextButton uuids={uuids} />
           </Stack>
         </SectionDescription>
