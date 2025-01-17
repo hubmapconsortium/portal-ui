4f8b5e29b395da308d0d58bdc092637ef83acc52
Austen Money <austen.money@gmail.com>
2025-01-17 15:29:30 -0500

    update alerts

---
+++ context/app/static/js/components/savedLists/SavedListsAlerts.tsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import useEntityStore, { EntityStore, savedAlertStatus } from 'js/stores/useEntityStore';
+import { useAppContext } from 'js/components/Contexts';
+import Button from '@mui/material/Button';
+import InternalLink from 'js/shared-styles/Links/InternalLink';
+import { StyledAlert } from './style';
+
+const entityStoreSelector = (state: EntityStore) => ({
+  shouldDisplaySavedOrEditedAlert: state.shouldDisplaySavedOrEditedAlert,
+  setShouldDisplaySavedOrEditedAlert: state.setShouldDisplaySavedOrEditedAlert,
+});
+
+function SavedListsEntityDetailAlerts() {
+  const { shouldDisplaySavedOrEditedAlert, setShouldDisplaySavedOrEditedAlert } = useEntityStore(entityStoreSelector);
+  const { isAuthenticated } = useAppContext();
+
+  const showSavedAlert = shouldDisplaySavedOrEditedAlert === savedAlertStatus;
+
+  if (!shouldDisplaySavedOrEditedAlert) {
+    return null;
+  }
+
+  if (isAuthenticated) {
+    return (
+      <StyledAlert
+        severity="success"
+        onClose={() => setShouldDisplaySavedOrEditedAlert(false)}
+        action={<Button href="/my-lists">My Lists</Button>}
+      >
+        Successfully{' '}
+        {showSavedAlert ? (
+          <>
+            added to <InternalLink href="/my-lists">My Saved Items</InternalLink>
+          </>
+        ) : (
+          'updated lists'
+        )}
+        . Visit your <InternalLink href="/my-lists">lists</InternalLink> to view your saved items.
+      </StyledAlert>
+    );
+  }
+
+  return (
+    <StyledAlert
+      severity="success"
+      onClose={() => setShouldDisplaySavedOrEditedAlert(false)}
+      action={<Button href="/login">Log in</Button>}
+    >
+      Successfully{' '}
+      {showSavedAlert ? (
+        <>
+          saved to <InternalLink href="/my-lists">My Saved Items</InternalLink>
+        </>
+      ) : (
+        'updated lists'
+      )}{' '}
+      locally. <InternalLink href="/login">Log in</InternalLink> to save it to your profile for easy access across
+      devices.
+    </StyledAlert>
+  );
+}
+
+export { SavedListsEntityDetailAlerts };
