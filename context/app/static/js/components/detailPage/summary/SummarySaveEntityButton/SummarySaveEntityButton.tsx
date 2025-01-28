d22059f0469f3a895394909fcd9ca55428b88d39
Austen Money <austen.money@gmail.com>
2025-01-28 17:54:30 -0500

    add save entity button to processed datasets

---
+++ context/app/static/js/components/detailPage/summary/SummarySaveEntityButton/SummarySaveEntityButton.tsx
@@ -0,0 +1,56 @@
+import React, { useState } from 'react';
+import { useAppContext } from 'js/components/Contexts';
+import { useTrackEntityPageEvent } from 'js/components/detailPage/useTrackEntityPageEvent';
+import useSavedLists from 'js/components/savedLists/hooks';
+import { Entity } from 'js/components/types';
+import EditSavedStatusDialog from 'js/components/savedLists/EditSavedStatusDialog';
+import { WhiteRectangularTooltipIconButton } from 'js/shared-styles/buttons/TooltipButton';
+import { EditSavedEntityIcon, SaveEntityIcon } from 'js/shared-styles/icons';
+
+function SaveEntityButton({ uuid }: Pick<Entity, 'uuid'>) {
+  const { handleSaveEntities } = useSavedLists();
+  const trackSave = useTrackEntityPageEvent();
+
+  return (
+    <WhiteRectangularTooltipIconButton
+      onClick={() => {
+        handleSaveEntities({ entityUUIDs: new Set([uuid]) }).catch((error) => {
+          console.error(error);
+        });
+        trackSave({ action: 'Save To List', label: uuid });
+      }}
+      tooltip="Save to list"
+    >
+      <SaveEntityIcon color="primary" />
+    </WhiteRectangularTooltipIconButton>
+  );
+}
+
+function EditSavedEntityButton({ uuid }: Pick<Entity, 'uuid'>) {
+  const [dialogIsOpen, setDialogIsOpen] = useState(false);
+
+  return (
+    <>
+      <WhiteRectangularTooltipIconButton
+        onClick={() => {
+          setDialogIsOpen(true);
+        }}
+        tooltip="Edit saved status"
+      >
+        <EditSavedEntityIcon color="primary" />
+      </WhiteRectangularTooltipIconButton>
+      <EditSavedStatusDialog dialogIsOpen={dialogIsOpen} setDialogIsOpen={setDialogIsOpen} uuid={uuid} />
+    </>
+  );
+}
+
+export default function SummarySaveEntityButton({ uuid }: Pick<Entity, 'uuid'>) {
+  const { savedEntities } = useSavedLists();
+  const { isAuthenticated } = useAppContext();
+
+  if (!isAuthenticated) {
+    return null;
+  }
+
+  return uuid in savedEntities.savedEntities ? <EditSavedEntityButton uuid={uuid} /> : <SaveEntityButton uuid={uuid} />;
+}
