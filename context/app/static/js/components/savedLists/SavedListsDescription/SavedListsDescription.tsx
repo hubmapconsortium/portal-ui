71d431edfade25a2d8f6f8a46a8eb224d4095bd6
Austen Money <austen.money@gmail.com>
2025-01-28 17:27:58 -0500

    update logged out description

--- context/app/static/js/components/savedLists/SavedListsDescription/SavedListsDescription.tsx
+++ context/app/static/js/components/savedLists/SavedListsDescription/SavedListsDescription.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
+import Typography from '@mui/material/Typography';
 
 import { useAppContext } from 'js/components/Contexts';
 import Description from 'js/shared-styles/sections/Description';
-import LoginAlert from 'js/shared-styles/alerts/LoginAlert';
+import { InternalLink } from 'js/shared-styles/Links';
 
 function LocalStorageDescription() {
   const { isAuthenticated } = useAppContext();
@@ -11,7 +15,21 @@ function LocalStorageDescription() {
     return <Description>Lists saved here are stored to your profile and accessible across devices.</Description>;
   }
 
-  return <LoginAlert featureName="lists" />;
+  return (
+    <Description>
+      <Stack spacing={1}>
+        <Typography>
+          <InternalLink href="/login">Log in</InternalLink> to access and manage your saved lists. Any lists previously
+          stored locally will be linked to your profile once you <InternalLink href="/login">log in</InternalLink>.
+        </Typography>
+        <Box>
+          <Button variant="contained" href="/login">
+            Log In
+          </Button>
+        </Box>
+      </Stack>
+    </Description>
+  );
 }
 
 export default LocalStorageDescription;
