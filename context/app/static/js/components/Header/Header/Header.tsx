28fc4d9b27f9f0cb5d6ed453efc920f6d856643c
Austen Money <austen.money@gmail.com>
2025-04-03 10:38:14 -0400

    add banner

--- context/app/static/js/components/Header/Header/Header.tsx
+++ context/app/static/js/components/Header/Header/Header.tsx
@@ -1,16 +1,28 @@
 import React from 'react';
+import Typography from '@mui/material/Typography';
+import Stack from '@mui/material/Stack';
 
 import EntityHeader from 'js/components/detailPage/entityHeader/EntityHeader';
+import { FixedHeightBanner } from 'js/components/Header/HeaderAppBar/style';
+import { InfoIcon } from 'js/shared-styles/icons';
 import HeaderAppBar from '../HeaderAppBar';
 import HeaderContent from '../HeaderContent';
 import { useEntityHeaderVisibility } from './hooks';
 
 function Header() {
-  const { shouldDisplayHeader, ...props } = useEntityHeaderVisibility();
+  const { shouldDisplayHeader, elevation, ...props } = useEntityHeaderVisibility();
 
   return (
     <>
-      <HeaderAppBar {...props}>
+      <FixedHeightBanner>
+        <Stack direction="row" spacing={1} alignItems="center">
+          <InfoIcon fontSize="0.85rem" />
+          <Typography variant="button">
+            This repository is under review for potential modification in compliance with Administration directives.
+          </Typography>
+        </Stack>
+      </FixedHeightBanner>
+      <HeaderAppBar elevation={elevation} {...props}>
         <HeaderContent />
       </HeaderAppBar>
       {shouldDisplayHeader && <EntityHeader />}
