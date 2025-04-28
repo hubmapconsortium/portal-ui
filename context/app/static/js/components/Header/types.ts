ce5721ef356aef4b9dadab2e197dadf67a3fc840
Austen Money <austen.money@gmail.com>
2025-04-28 12:03:38 -0400

    add header button tracking

---
+++ context/app/static/js/components/Header/types.ts
@@ -0,0 +1,7 @@
+export enum NavigationEventCategories {
+  Navigation = 'Header Navigation',
+  Data = 'Header Navigation / Data',
+  Resources = 'Header Navigation / Resources',
+  ToolsAndApps = 'Header Navigation / Tools and Applications',
+  YourProfile = 'Header Navigation / Your Profile',
+}
