61c9769f26d29e6d42cb742acb61ac51b63c4878
Austen Money <austen.money@gmail.com>
2025-04-29 14:40:26 -0400

    restructure landing page intro

--- context/app/static/js/pages/Collections/Collections.tsx
+++ context/app/static/js/pages/Collections/Collections.tsx
@@ -1,24 +1,38 @@
 import React from 'react';
+import Typography from '@mui/material/Typography';
+import Stack from '@mui/material/Stack';
 
-import PanelListLandingPage from 'js/shared-styles/panels/PanelListLandingPage';
 import PanelList from 'js/shared-styles/panels/PanelList';
-
+import Description from 'js/shared-styles/sections/Description';
+import PageTitle from 'js/shared-styles/pages/PageTitle';
 import { useCollections } from './hooks';
 
-const description =
-  'Collections of HuBMAP datasets represent data from related experiments—such as assays performed on the same organ—or data that has been grouped for other reasons. In the future, it will be possible to reference collections through Document Object Identifiers (DOIs).';
+const descriptions = {
+  header:
+    'HuBMAP collections group datasets from related experiments, such as assays performed on the same organ or datasets with shared research relevance. Each collection is assigned a Document Object Identifier (DOI) for citation and reference.',
+  subheader:
+    'Explore collections of HuBMAP datasets. This table can be downloaded in TSV format. Collection pages provide context on why the datasets are grouped and include a list of the associated HuBMAP datasets.',
+};
 
 function Collections() {
   const panelsProps = useCollections();
   return (
-    <PanelListLandingPage
-      title="Collections"
-      subtitle={panelsProps.length > 0 ? `${panelsProps.length} Collections` : undefined}
-      description={description}
-      data-testid="collections-title"
-    >
-      <PanelList panelsProps={panelsProps} />
-    </PanelListLandingPage>
+    <Stack spacing={3} marginBottom={4}>
+      <Stack spacing={1}>
+        <PageTitle>Collections</PageTitle>
+        <Typography variant="subtitle1" color="primary" data-testid="landing-page-subtitle">
+          {panelsProps.length > 0 ? `${panelsProps.length} Collections` : undefined}
+        </Typography>
+        <Description data-testid="landing-page-description">{descriptions.header}</Description>
+      </Stack>
+      <Stack spacing={1}>
+        <Typography variant="h4" marginTop={4}>
+          Collections of HuBMAP Data
+        </Typography>
+        <Description>{descriptions.subheader}</Description>
+        <PanelList panelsProps={panelsProps} />
+      </Stack>
+    </Stack>
   );
 }
 
