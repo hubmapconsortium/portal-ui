4870d1c2b3ee2e200a8bad57b3f66822db007cc8
Austen Money <austen.money@gmail.com>
2025-05-02 10:04:08 -0400

    Austenem/CAT-1245 Minor fixes (#3733)
    * update bulk data transfer panels
    
    * add collections metadata description
    
    * update title subheader
    
    * add changelog
    
    * fix line length

--- context/app/static/js/components/home/Title/Title.tsx
+++ context/app/static/js/components/home/Title/Title.tsx
@@ -8,8 +8,8 @@ function Title() {
   return (
     <StyledDiv>
       <PageTitle data-testid="home-page-title">Human BioMolecular Atlas Program Data Portal</PageTitle>
-      <Typography variant="h4" component="h2" color="secondary">
-        An open platform to discover, visualize and download standardized healthy single-cell tissue data
+      <Typography variant="h5" component="h2" color="secondary">
+        An open platform to discover, visualize and download standardized healthy single-cell and spatial tissue data
       </Typography>
     </StyledDiv>
   );
