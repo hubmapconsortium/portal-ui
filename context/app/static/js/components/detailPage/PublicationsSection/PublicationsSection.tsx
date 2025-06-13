600006b5df45a46b7e0c7a54887e024a1d6b3122
Austen Money <austen.money@gmail.com>
2025-06-13 13:57:19 -0400

    add publications tabs

---
+++ context/app/static/js/components/detailPage/PublicationsSection/PublicationsSection.tsx
@@ -0,0 +1,103 @@
+import React from 'react';
+
+import PanelList from 'js/shared-styles/panels/PanelList';
+import { CollapsibleDetailPageSection } from 'js/components/detailPage/DetailPageSection';
+import { Tabs, Tab, TabPanel } from 'js/shared-styles/tables/TableTabs';
+import { OutlinedAlert } from 'js/shared-styles/alerts/OutlinedAlert.stories';
+import withShouldDisplay from 'js/helpers/withShouldDisplay';
+import { sectionIconMap } from 'js/shared-styles/icons/sectionIconMap';
+import { useTabs } from 'js/shared-styles/tabs';
+import { SectionDescription } from 'js/shared-styles/sections/SectionDescription';
+import { useProcessedDatasets } from 'js/pages/Dataset/hooks';
+import Skeleton from '@mui/material/Skeleton';
+import { PublicationHit } from 'js/components/publications/utils';
+import { useDatasetsPublicationsTabs } from 'js/hooks/useDatasetsPublications';
+
+function buildPublicationsPanelsProps(publications: PublicationHit[]) {
+  return publications.map(({ _source: { uuid, title } }) => ({
+    key: uuid,
+    href: `/browse/collection/${uuid}`,
+    title,
+  }));
+}
+
+function PublicationsPanel({
+  index,
+  value,
+  publications = [],
+}: {
+  index: number;
+  value: number;
+  publications: PublicationHit[];
+}) {
+  const panelsProps = buildPublicationsPanelsProps(publications);
+
+  if (panelsProps.length === 0) {
+    return (
+      <TabPanel value={value} index={index} sx={{ '> .MuiPaper-root': { width: '100%' } }}>
+        <OutlinedAlert severity="info">The raw dataset is not referenced in any existing collections.</OutlinedAlert>
+      </TabPanel>
+    );
+  }
+
+  return (
+    <TabPanel value={value} index={index}>
+      <PanelList panelsProps={panelsProps} key={index} />
+    </TabPanel>
+  );
+}
+
+const publicationsSectionDescription =
+  'Collections may contain references to either raw or processed datasets. If a processed dataset is not included in any collection, there will be no corresponding tabs in the table below.';
+
+function PublicationsSection() {
+  const datasetPublicationsTabs = useDatasetsPublicationsTabs();
+
+  const { isLoading } = useProcessedDatasets();
+
+  const { openTabIndex, handleTabChange, setOpenTabIndex } = useTabs(0);
+
+  if (isLoading) {
+    return (
+      <CollapsibleDetailPageSection id="publications" title="Publications" icon={sectionIconMap.collections}>
+        <SectionDescription>{publicationsSectionDescription}</SectionDescription>
+        <Skeleton variant="rectangular" height={200} />
+      </CollapsibleDetailPageSection>
+    );
+  }
+
+  if (datasetPublicationsTabs.length === 0) {
+    return null;
+  }
+
+  return (
+    <CollapsibleDetailPageSection id="publications" title="Publications" icon={sectionIconMap.publications}>
+      <SectionDescription>{publicationsSectionDescription}</SectionDescription>
+      <Tabs
+        value={openTabIndex}
+        onChange={(e, newValue) => {
+          handleTabChange(e, newValue as number);
+        }}
+        aria-label="Dataset publications"
+      >
+        {datasetPublicationsTabs.map(({ label, uuid, icon: Icon }, index) => (
+          <Tab
+            key={uuid}
+            label={label}
+            isSingleTab={datasetPublicationsTabs.length === 1}
+            index={index}
+            icon={Icon ? <Icon /> : undefined}
+            iconPosition="start"
+            onClick={() => setOpenTabIndex(index)}
+            value={index}
+          />
+        ))}
+      </Tabs>
+      {datasetPublicationsTabs.map(({ uuid, publications }, index) => (
+        <PublicationsPanel key={uuid} index={index} value={openTabIndex} publications={publications} />
+      ))}
+    </CollapsibleDetailPageSection>
+  );
+}
+
+export default withShouldDisplay(PublicationsSection);
---
+++ context/app/static/js/hooks/useDatasetsPublications.ts
@@ -0,0 +1,92 @@
+import { useSearchHits } from 'js/hooks/useSearchData';
+import { SearchRequest } from '@elastic/elasticsearch/lib/api/types';
+import { useMemo } from 'react';
+import { useFlaskDataContext } from 'js/components/Contexts';
+import { useProcessedDatasetTabs } from 'js/components/detailPage/ProcessedData/ProcessedDataset/hooks';
+import { Publication } from 'js/components/types';
+import { PublicationHit } from 'js/components/publications/utils';
+
+export function buildPublicationsWithDatasetQuery(datasetUUIDs: string[]): SearchRequest {
+  return {
+    query: {
+      bool: {
+        filter: [
+          {
+            terms: {
+              'ancestor_ids.keyword': datasetUUIDs,
+            },
+          },
+          {
+            term: {
+              'entity_type.keyword': 'Publication',
+            },
+          },
+        ],
+      },
+    },
+    _source: ['uuid', 'title', 'hubmap_id', 'entity_type', 'ancestor_ids', 'publication_status'],
+  };
+}
+
+function useDatasetsPublications(datasetUUIDs: string[]) {
+  const query = buildPublicationsWithDatasetQuery(datasetUUIDs);
+  const { searchHits: publications } = useSearchHits<Publication>(query);
+
+  return publications;
+}
+
+function useDatasetsPublicationsTabs() {
+  const processedDatasetTabs = useProcessedDatasetTabs();
+  const datasetUUIDs = useMemo(() => processedDatasetTabs.map((tab) => tab.uuid), [processedDatasetTabs]);
+  const publications = useDatasetsPublications(datasetUUIDs);
+
+  const {
+    entity: { uuid: primaryDatasetId },
+  } = useFlaskDataContext();
+
+  const publicationsMap = useMemo(
+    () =>
+      datasetUUIDs.reduce(
+        (acc, datasetUUID) => {
+          const publicationsContainingDataset = publications.filter((pub) => {
+            const { ancestor_ids } = pub._source;
+            if (!ancestor_ids) {
+              return false;
+            }
+            return ancestor_ids.some((id) => id === datasetUUID);
+          });
+          if (publicationsContainingDataset.length > 0 || datasetUUID === primaryDatasetId) {
+            acc[datasetUUID] = publicationsContainingDataset.map((hit) => ({
+              ...hit,
+              _source: {
+                ...hit._source,
+                publication_status: false,
+              },
+            }));
+          }
+          return acc;
+        },
+        { [primaryDatasetId]: [] } as Record<string, PublicationHit[]>,
+      ),
+    [publications, datasetUUIDs, primaryDatasetId],
+  );
+
+  return useMemo(() => {
+    let atLeastOnePublication = false;
+    return processedDatasetTabs
+      .map((processedDatasetTab) => {
+        const { uuid } = processedDatasetTab;
+        const publicationsForDataset = publicationsMap[uuid];
+        if (publicationsForDataset?.length > 0) {
+          atLeastOnePublication = true;
+        }
+        return {
+          ...processedDatasetTab,
+          publications: publicationsForDataset,
+        };
+      })
+      .filter((tab) => tab.publications?.length > 0 || (atLeastOnePublication && tab.uuid === primaryDatasetId));
+  }, [publicationsMap, processedDatasetTabs, primaryDatasetId]);
+}
+
+export { useDatasetsPublications, useDatasetsPublicationsTabs };
