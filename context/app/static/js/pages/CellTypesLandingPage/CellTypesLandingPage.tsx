fcc739202d2ed772ef73aa2e4307edf3ecfabcb3
Nikolay Akhmetov <nick.akhmetov@gmail.com>
2025-06-26 16:53:10 -0400

    add chart

--- context/app/static/js/pages/CellTypesLandingPage/CellTypesLandingPage.tsx
+++ context/app/static/js/pages/CellTypesLandingPage/CellTypesLandingPage.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 
 import PanelListLandingPage from 'js/shared-styles/panels/PanelListLandingPage';
-import { useCellTypeNamesMap, useCellTypeOrgans } from 'js/api/scfind/useCellTypeNames';
+import useCellTypeNames, { useCellTypeNamesMap, useCellTypeOrgans } from 'js/api/scfind/useCellTypeNames';
 import CellTypesSearchProvider from 'js/components/cell-types-landing/CellTypesSearchContext';
 import CellTypesPanelList from 'js/components/cell-types-landing/CellTypesPanelList';
 import CellTypesSearchBar from 'js/components/cell-types-landing/CellTypesSearchBar';
+import MultiOrganCellTypeDistributionChartWithProvider from 'js/components/cell-types/CellTypesDistribution/MultiOrganCellTypesDistributionChart';
 
 export default function CellTypesLandingPage() {
   const cellTypesMap = useCellTypeNamesMap();
+  const { data } = useCellTypeNames();
   const cellTypeOrgans = useCellTypeOrgans();
 
   const uniqueCellTypes = Object.keys(cellTypesMap).length;
@@ -32,6 +34,15 @@ export default function CellTypesLandingPage() {
         }
         data-testid="cell-types-title"
       >
+        {data?.cellTypeNames && cellTypeOrgans && (
+          <div style={{ marginBottom: '1rem' }}>
+            <MultiOrganCellTypeDistributionChartWithProvider
+              cellTypes={data?.cellTypeNames}
+              organs={cellTypeOrgans}
+              hideLegend
+            />
+          </div>
+        )}
         <CellTypesSearchBar />
         <CellTypesPanelList />
       </PanelListLandingPage>
