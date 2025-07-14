22dc57cd713f65093ee38bd021de1e62b823dd9f
Nikolay Akhmetov <nick.akhmetov@gmail.com>
2025-07-14 11:18:26 -0400

    initial

---
+++ context/app/static/js/components/cells/DatasetsOverview/DatasetsOverviewChart.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function DatasetsOverviewChart() {
+  return (
+    <div>
+      <h2>Datasets Overview Chart</h2>
+      {/* Chart implementation will go here */}
+    </div>
+  );
+}
