1fc6b021a2babb80943c650b3c8d958fdafea36b
Nikolay Akhmetov <nick.akhmetov@gmail.com>
2025-08-07 12:40:31 -0400

    Fix appearance of assays chart

--- context/app/static/js/shared-styles/charts/ChartWrapper/ChartWrapper.tsx
+++ context/app/static/js/shared-styles/charts/ChartWrapper/ChartWrapper.tsx
@@ -6,6 +6,7 @@ import Box from '@mui/material/Box';
 import { TitleWrapper } from 'js/shared-styles/charts/style';
 import InfoTextTooltip from 'js/shared-styles/tooltips/InfoTextTooltip';
 import Divider from '@mui/material/Divider';
+import Stack from '@mui/material/Stack';
 import { OrdinalScale } from '../hooks';
 
 interface ChartWrapperProps extends PropsWithChildren {
@@ -70,86 +71,88 @@ function ChartWrapper(
       <Box sx={{ gridArea: 'y-axis', p: yAxisDropdown ? 1 : 0 }}>{yAxisDropdown}</Box>
       <Box sx={{ gridArea: 'chart' }}>{children}</Box>
       <Box sx={{ gridArea: 'x-axis', p: xAxisDropdown ? 1 : 0 }}>{xAxisDropdown}</Box>
-      <Box sx={{ gridArea: 'legend', display: 'flex', flexDirection: 'column', maxHeight: '100%', overflow: 'none' }}>
-        {dropdown && <Box sx={{ marginY: 1, minWidth: 0 }}>{dropdown}</Box>}
-        <Box sx={{ flex: 1, overflowY: 'auto', gridArea: 'legend' }} tabIndex={0}>
-          {colorScale && (
-            <LegendOrdinal scale={colorScale} domain={domain}>
-              {(labels) => (
-                <div style={{ display: 'flex', flexDirection: 'column' }}>
-                  {labels.map((label, idx) => {
-                    const isMultiple = label.text === 'Multiple';
-                    return (
-                      <React.Fragment key={label.text}>
-                        <LegendItem key={`legend-${label.text}`} margin="0 15px 0 0">
-                          <svg width="1em" height="1em" style={{ borderRadius: '4px' }}>
-                            {isMultiple && (
-                              <defs>
-                                <pattern
-                                  id={`pattern-${label.text}`}
-                                  patternUnits="userSpaceOnUse"
-                                  width={domain.length}
-                                  height="2.5"
-                                  patternTransform="rotate(90)"
+      <Box sx={{ gridArea: 'legend', display: 'grid', maxHeight: '100%', overflow: 'none' }}>
+        <Stack direction="column" pl={1}>
+          {dropdown && <Box sx={{ marginY: 1, minWidth: 0 }}>{dropdown}</Box>}
+          <Box sx={{ flex: 1, overflowY: 'auto', gridArea: 'legend', mt: dropdown ? 0 : 2 }} tabIndex={0}>
+            {colorScale && (
+              <LegendOrdinal scale={colorScale} domain={domain}>
+                {(labels) => (
+                  <div style={{ display: 'flex', flexDirection: 'column' }}>
+                    {labels.map((label, idx) => {
+                      const isMultiple = label.text === 'Multiple';
+                      return (
+                        <React.Fragment key={label.text}>
+                          <LegendItem key={`legend-${label.text}`} margin="0 15px 0 0">
+                            <svg width="1em" height="1em" style={{ borderRadius: '4px' }}>
+                              {isMultiple && (
+                                <defs>
+                                  <pattern
+                                    id={`pattern-${label.text}`}
+                                    patternUnits="userSpaceOnUse"
+                                    width={domain.length}
+                                    height="2.5"
+                                    patternTransform="rotate(90)"
+                                  >
+                                    {domain.map((key, index) => (
+                                      <line
+                                        key={key}
+                                        x1={index + 0.5}
+                                        y1="0"
+                                        x2={index + 0.5}
+                                        y2="2.5"
+                                        stroke={colorScale(key)}
+                                        strokeWidth="1"
+                                      />
+                                    ))}
+                                  </pattern>
+                                </defs>
+                              )}
+                              <rect
+                                fill={!isMultiple ? colorScale(label.text) : `url(#pattern-${label.text})`}
+                                width="1em"
+                                height="1em"
+                              />
+                            </svg>
+                            <LegendLabel align="left" margin="0 0 0 4px">
+                              {isMultiple ? (
+                                <InfoTextTooltip tooltipTitle="This data has multiple categories">
+                                  {label.text}
+                                </InfoTextTooltip>
+                              ) : (
+                                label.text
+                              )}
+                              {labelValueMap[label.text] && (
+                                <Typography
+                                  component="span"
+                                  variant="caption"
+                                  color="textSecondary"
+                                  display="inline-block"
+                                  ml={1}
                                 >
-                                  {domain.map((key, index) => (
-                                    <line
-                                      key={key}
-                                      x1={index + 0.5}
-                                      y1="0"
-                                      x2={index + 0.5}
-                                      y2="2.5"
-                                      stroke={colorScale(key)}
-                                      strokeWidth="1"
-                                    />
-                                  ))}
-                                </pattern>
-                              </defs>
-                            )}
-                            <rect
-                              fill={!isMultiple ? colorScale(label.text) : `url(#pattern-${label.text})`}
-                              width="1em"
-                              height="1em"
-                            />
-                          </svg>
-                          <LegendLabel align="left" margin="0 0 0 4px">
-                            {isMultiple ? (
-                              <InfoTextTooltip tooltipTitle="This data has multiple categories">
-                                {label.text}
-                              </InfoTextTooltip>
-                            ) : (
-                              label.text
-                            )}
-                            {labelValueMap[label.text] && (
-                              <Typography
-                                component="span"
-                                variant="caption"
-                                color="textSecondary"
-                                display="inline-block"
-                                ml={1}
-                              >
-                                ({labelValueMap[label.text]})
-                              </Typography>
-                            )}
-                          </LegendLabel>
-                        </LegendItem>
-                        {(isMultiple || (dividersInLegend && idx !== labels.length - 1)) && (
-                          <Divider sx={{ marginY: 1 }} />
-                        )}
-                      </React.Fragment>
-                    );
-                  })}
-                </div>
-              )}
-            </LegendOrdinal>
-          )}
-          {allKeysScale && (
-            /* This is used to prevent content shift when toggling between different sets of data */
-            <Box sx={{ height: 0, speak: 'none', overflow: 'hidden' }}>
-              <LegendOrdinal scale={allKeysScale} domain={allKeysDomain} />
-            </Box>
-          )}
-        </Box>
+                                  ({labelValueMap[label.text]})
+                                </Typography>
+                              )}
+                            </LegendLabel>
+                          </LegendItem>
+                          {(isMultiple || (dividersInLegend && idx !== labels.length - 1)) && (
+                            <Divider sx={{ marginY: 1 }} />
+                          )}
+                        </React.Fragment>
+                      );
+                    })}
+                  </div>
+                )}
+              </LegendOrdinal>
+            )}
+            {allKeysScale && (
+              /* This is used to prevent content shift when toggling between different sets of data */
+              <Box sx={{ height: 0, speak: 'none', overflow: 'hidden' }}>
+                <LegendOrdinal scale={allKeysScale} domain={allKeysDomain} />
+              </Box>
+            )}
+          </Box>
+        </Stack>
       </Box>
       <Box sx={{ gridArea: 'top-controls' }}>{additionalControls}</Box>
     </Box>
