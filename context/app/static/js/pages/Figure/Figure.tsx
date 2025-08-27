0ff45541ba5e67379c82b20994992e4ab27311c6
Austen Money <austen.money@gmail.com>
2025-08-27 12:47:44 -0400

    add figures to new page

--- context/app/static/js/pages/Figure/Figure.tsx
+++ context/app/static/js/pages/Figure/Figure.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
+import FIGURE_1B_1 from 'assets/svg/figure/figure_1b_1.svg';
+import FIGURE_1B_2 from 'assets/svg/figure/figure_1b_2.svg';
+import FIGURE_1C from 'assets/svg/figure/figure_1c.svg';
+import FIGURE_1D from 'assets/svg/figure/figure_1d.svg';
+import FIGURE_1E from 'assets/svg/figure/figure_1e.svg';
 import Stack from '@mui/material/Stack';
 
-function Figure() {
+export default function InlineFigures() {
   return (
-    <Stack spacing={2} direction="column" mb={2}>
-      woah buddy!
+    <Stack>
+      <FIGURE_1B_1 />
+      <FIGURE_1B_2 />
+      <FIGURE_1C />
+      <FIGURE_1D />
+      <FIGURE_1E />
     </Stack>
   );
 }
-
-export default Figure;
