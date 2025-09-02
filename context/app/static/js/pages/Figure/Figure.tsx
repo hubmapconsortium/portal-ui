5f33621a3dd852b8b23d0129c3bd24a030efbcd1
Austen Money <austen.money@gmail.com>
2025-09-02 13:47:30 -0400

    continue updates and add to python CI

--- context/app/static/js/pages/Figure/Figure.tsx
+++ context/app/static/js/pages/Figure/Figure.tsx
@@ -2,14 +2,14 @@ import React, { useEffect, useState } from 'react';
 import FIGURE_1B_1 from 'assets/svg/figure/figure_1b_1.svg';
 import FIGURE_1B_2 from 'assets/svg/figure/figure_1b_2.svg';
 import FIGURE_1C from 'assets/svg/figure/figure_1c.svg';
-import FIGURE_1D from 'assets/svg/figure/figure_1d.svg';
+import FIGURE_1D_1 from 'assets/svg/figure/figure_1d_1.svg';
+import FIGURE_1D_2 from 'assets/svg/figure/figure_1d_2.svg';
 import FIGURE_1E from 'assets/svg/figure/figure_1e.svg';
-import Grid from '@mui/material/Grid2';
 import Stack from '@mui/material/Stack';
-import Box from '@mui/material/Box';
+import Box, { BoxProps } from '@mui/material/Box';
 
 // Fetch + inject figure 1a
-function HtmlEmbed({ path }: { path: string }) {
+function HtmlEmbed({ path, sx, ...boxProps }: { path: string } & BoxProps) {
   const [html, setHtml] = useState<string>('');
   useEffect(() => {
     let mounted = true;
@@ -26,33 +26,54 @@ function HtmlEmbed({ path }: { path: string }) {
     };
   }, [path]);
 
-  return <Box sx={{ width: '100%' }} dangerouslySetInnerHTML={{ __html: html }} />;
+  return <Box {...boxProps} sx={{ width: '100%', ...(sx ?? {}) }} dangerouslySetInnerHTML={{ __html: html }} />;
 }
 
 export default function InlineFigures() {
   const base = '/static/assets/svg/figure';
 
   return (
-    <Grid sx={{ bgcolor: 'background.paper' }} spacing={1} container>
-      {/* Left column: 1/3 */}
-      <Grid size={{ xs: 12, md: 4.5 }}>
-        <Stack spacing={2}>
-          <HtmlEmbed path={`${base}/figure_1a.html`} />
-          <FIGURE_1C style={{ width: '100%', height: 'auto', display: 'block' }} />
-        </Stack>
-      </Grid>
+    <Stack direction="row" spacing={2} sx={{ bgcolor: 'background.paper' }}>
+      {/* Left column: a, b, c */}
+      <Stack sx={{ flex: 0.8, minWidth: 0 }} spacing={2}>
+        {/* a */}
+        <Box sx={{ flex: 1, minWidth: 0 }}>
+          <HtmlEmbed path={`${base}/figure_1a.html`} sx={{ height: '100%' }} />
+        </Box>
 
-      {/* Right column: 2/3 */}
-      <Grid size={{ xs: 12, md: 7.5 }}>
-        <Stack>
-          <Stack direction="row">
+        {/* b (stacked b2 over b1) */}
+        <Stack sx={{ flex: 1, minWidth: 0 }} spacing={2}>
+          <Box sx={{ flex: 1, minWidth: 0 }}>
             <FIGURE_1B_2 style={{ width: '100%', height: 'auto', display: 'block' }} />
+          </Box>
+          <Box sx={{ flex: 1, minWidth: 0 }}>
             <FIGURE_1B_1 style={{ width: '100%', height: 'auto', display: 'block' }} />
-          </Stack>
-          <FIGURE_1D style={{ width: '100%', height: 'auto', display: 'block' }} />
-          <FIGURE_1E style={{ width: '100%', height: 'auto', display: 'block' }} />
+          </Box>
         </Stack>
-      </Grid>
-    </Grid>
+
+        {/* c */}
+        <Box sx={{ flex: 1, minWidth: 0 }}>
+          <FIGURE_1C style={{ width: '100%', height: 'auto', display: 'block' }} />
+        </Box>
+      </Stack>
+
+      {/* Right column: d, e */}
+      <Stack sx={{ flex: 1, minWidth: 0 }} spacing={2}>
+        {/* d */}
+        <Stack spacing={2} sx={{ flex: 1, minWidth: 0 }}>
+          <Box sx={{ flex: 1, minWidth: 0 }}>
+            <FIGURE_1D_1 style={{ width: '100%', height: 'auto', display: 'block' }} />
+          </Box>
+          <Box sx={{ flex: 1, minWidth: 0 }}>
+            <FIGURE_1D_2 style={{ width: '100%', height: 'auto', display: 'block' }} />
+          </Box>
+        </Stack>
+
+        {/* e */}
+        <Box sx={{ flex: 1, minWidth: 0 }}>
+          <FIGURE_1E style={{ width: '100%', height: 'auto', display: 'block' }} />
+        </Box>
+      </Stack>
+    </Stack>
   );
 }
