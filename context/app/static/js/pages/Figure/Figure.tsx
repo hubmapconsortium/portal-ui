d4210188f06fedd8edfc3cafe3d8ee52139cbf0b
Austen Money <austen.money@gmail.com>
2025-09-04 12:59:19 -0400

    rename figures, continue iterating

--- context/app/static/js/pages/Figure/Figure.tsx
+++ context/app/static/js/pages/Figure/Figure.tsx
@@ -1,17 +1,16 @@
-import React, { useEffect, useState } from 'react';
-import FIGURE_1B_1 from 'assets/svg/figure/figure_1b_1.svg';
-import FIGURE_1B_2 from 'assets/svg/figure/figure_1b_2.svg';
+import React from 'react';
+import FIGURE_1B from 'assets/svg/figure/figure_1b.svg';
 import FIGURE_1C from 'assets/svg/figure/figure_1c.svg';
-import FIGURE_1D_1 from 'assets/svg/figure/figure_1d_1.svg';
-import FIGURE_1D_2 from 'assets/svg/figure/figure_1d_2.svg';
-import FIGURE_1E from 'assets/svg/figure/figure_1e.svg';
+import FIGURE_1D from 'assets/svg/figure/figure_1d.svg';
+import FIGURE_1EFG from 'assets/svg/figure/figure_1efg.svg';
+import FIGURE_1H from 'assets/svg/figure/figure_1h.svg';
+import FIGURE_1I from 'assets/svg/figure/figure_1i.svg';
 import Stack from '@mui/material/Stack';
 import Box, { BoxProps } from '@mui/material/Box';
 
-// Fetch + inject figure 1a
 function HtmlEmbed({ path, sx, ...boxProps }: { path: string } & BoxProps) {
-  const [html, setHtml] = useState<string>('');
-  useEffect(() => {
+  const [html, setHtml] = React.useState<string>('');
+  React.useEffect(() => {
     let mounted = true;
     fetch(path)
       .then((r) => r.text())
@@ -34,41 +33,38 @@ export default function InlineFigures() {
 
   return (
     <Stack sx={{ bgcolor: 'background.paper' }}>
-      {/* Row 1: a and b (b is stacked b2 over b1) */}
+      {/* Row 1: a and b */}
       <Stack direction="row" spacing={2} sx={{ flex: 1, minWidth: 0 }}>
-        {/* a */}
-        <Box sx={{ flex: 0.4, minWidth: 0 }}>
+        <Box sx={{ flex: 0.7, minWidth: 0, pt: 1 }}>
           <HtmlEmbed path={`${base}/figure_1a.html`} sx={{ height: '100%' }} />
         </Box>
-
-        {/* b */}
-        <Stack sx={{ flex: 1, minWidth: 0 }} spacing={2} direction="row">
+        <Stack sx={{ flex: 1, minWidth: 0 }} spacing={1} direction="row">
           <Box sx={{ flex: 1, minWidth: 0 }}>
-            <FIGURE_1B_2 style={{ width: '100%', height: 'auto', display: 'block' }} />
+            <FIGURE_1B style={{ width: '100%', height: 'auto', display: 'block' }} />
           </Box>
-          <Box sx={{ flex: 1, minWidth: 0 }}>
-            <FIGURE_1B_1 style={{ width: '100%', height: 'auto', display: 'block' }} />
+          <Box sx={{ flex: 1, minWidth: 0, display: 'flex', alignItems: 'flex-start' }}>
+            <FIGURE_1C style={{ width: '100%', height: 'auto', display: 'block' }} />
           </Box>
         </Stack>
       </Stack>
 
       {/* Row 2: d_2 and c */}
-      <Stack direction="row" spacing={2} sx={{ flex: 1, minWidth: 0 }}>
+      <Stack direction="row" alignItems="flex-start" sx={{ flex: 1, minWidth: 0, mt: -3 }}>
         <Box sx={{ flex: 1, minWidth: 0 }}>
-          <FIGURE_1D_2 style={{ width: '100%', height: 'auto', display: 'block' }} />
+          <FIGURE_1D style={{ width: '100%', height: 'auto', display: 'block' }} />
         </Box>
-        <Box sx={{ flex: 0.8, minWidth: 0 }}>
-          <FIGURE_1C style={{ width: '100%', height: 'auto', display: 'block' }} />
+        <Box sx={{ flex: 0.85, minWidth: 0, mt: 5 }}>
+          <FIGURE_1EFG style={{ width: '100%', height: 'auto', display: 'block' }} />
         </Box>
       </Stack>
 
       {/* Row 3: d_1 and e */}
-      <Stack direction="row" sx={{ flex: 1, minWidth: 0 }}>
-        <Box sx={{ flex: 0.78, minWidth: 0 }}>
-          <FIGURE_1D_1 style={{ width: '100%', height: 'auto', display: 'block' }} />
+      <Stack direction="row" sx={{ flex: 1, minWidth: 0, mt: 2 }} spacing={1}>
+        <Box sx={{ flex: 0.75, minWidth: 0, pt: 1.8 }}>
+          <FIGURE_1H style={{ width: '100%', height: 'auto', display: 'block' }} />
         </Box>
         <Box sx={{ flex: 1, minWidth: 0 }}>
-          <FIGURE_1E style={{ width: '100%', height: 'auto', display: 'block' }} />
+          <FIGURE_1I style={{ width: '100%', height: 'auto', display: 'block' }} />
         </Box>
       </Stack>
     </Stack>
