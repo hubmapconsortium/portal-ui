5bb0ece6443ff025ac7217ca6212cf45e1f64e5e
Nikolay Akhmetov <nick.akhmetov@gmail.com>
2025-09-19 11:34:32 -0400

    CAT-1394 add breadcrumb links from detail pages to landing pages

--- context/app/static/js/components/detailPage/summary/SummaryTitle/SummaryTitle.tsx
+++ context/app/static/js/components/detailPage/summary/SummaryTitle/SummaryTitle.tsx
@@ -5,7 +5,7 @@ import { useInView } from 'react-intersection-observer';
 
 import useEntityStore, { EntityStore } from 'js/stores/useEntityStore';
 import InfoTooltipIcon from 'js/shared-styles/icons/TooltipIcon';
-import { entityIconMap } from 'js/shared-styles/icons/entityIconMap';
+import { AllEntityTypes, entityIconMap } from 'js/shared-styles/icons/entityIconMap';
 
 const entityStoreSelector = (state: EntityStore) => state.setSummaryComponentObserver;
 
@@ -13,6 +13,20 @@ interface SummaryTitleProps extends PropsWithChildren {
   iconTooltipText?: string;
   entityIcon?: keyof typeof entityIconMap;
 }
+
+const titleLinks: Record<AllEntityTypes, string | undefined> = {
+  Donor: '/search/donors',
+  Sample: '/search/samples',
+  Dataset: '/search/datasets',
+  Support: '/search/datasets',
+  Publication: '/publications',
+  Collection: '/collections',
+  Workspace: '/workspaces',
+  WorkspaceTemplate: '/workspaces',
+  CellType: '/cell-types',
+  Gene: '/genes',
+  VerifiedUser: undefined,
+};
 
 function SummaryTitle({ children, iconTooltipText, entityIcon }: SummaryTitleProps) {
   const setSummaryComponentObserver = useEntityStore(entityStoreSelector);
@@ -31,10 +45,13 @@ function SummaryTitle({ children, iconTooltipText, entityIcon }: SummaryTitlePro
     }
   }, [setSummaryComponentObserver, entry, inView]);
 
+  const href = entityIcon ? titleLinks[entityIcon] : undefined;
+  const component = href ? 'a' : 'div';
+
   return (
     <Stack direction="row" alignItems="center" gap={1}>
       {Icon && <Icon color="primary" />}
-      <Typography variant="subtitle1" color="primary" ref={ref}>
+      <Typography variant="subtitle1" color="primary" ref={ref} href={href} component={component}>
         {children}
       </Typography>
       <InfoTooltipIcon iconTooltipText={iconTooltipText} />
