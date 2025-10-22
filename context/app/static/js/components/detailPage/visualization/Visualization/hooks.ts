b78cd54f4014865fd0171822e94bf03eab8a1974
Nikolay Akhmetov <nick.akhmetov@gmail.com>
2025-10-22 13:26:57 -0400

    Fix marker gene handling for error cases (occurs on dev environment)

--- context/app/static/js/components/detailPage/visualization/Visualization/hooks.ts
+++ context/app/static/js/components/detailPage/visualization/Visualization/hooks.ts
@@ -11,13 +11,13 @@ const FEATURE_SELECTION = 'featureSelection';
 const OBS_COLOR_ENCODING = 'obsColorEncoding';
 
 function handleMarkerGene(vData: object, markerGene?: string) {
-  if ('layout' in vData && vData.layout && markerGene) {
+  if ('layout' in vData && vData.layout && markerGene && 'name' in vData && vData.name !== 'Error') {
     const vc = VitessceConfig.fromJSON(vData);
     const [featureSelection, obsColorEncoding] = vc.addCoordination(FEATURE_SELECTION, OBS_COLOR_ENCODING);
     // @ts-expect-error VitessceConfig's layout property is not properly typed, so treat this section as JS
 
     // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-return
-    vc.config.layout.forEach((v) => v.useCoordination(featureSelection, obsColorEncoding));
+    [...vc.config.layout].filter(Boolean).forEach((v) => v.useCoordination(featureSelection, obsColorEncoding));
     featureSelection.setValue([markerGene]);
     obsColorEncoding.setValue('geneSelection');
     return vc.toJSON();
