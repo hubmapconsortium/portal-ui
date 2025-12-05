17fb70bda163777faa9307bfa9922b86cc3fff9f
Nikolay Akhmetov <nick.akhmetov@gmail.com>
2025-12-05 15:51:14 -0500

    Revert "Fix empty IDs query breaking permission checks"
    This reverts commit 6fb4a5ba41c23d7d3fc7ecde21f288d3d1fba821.

--- context/app/static/js/hooks/useHubmapIds.ts
+++ context/app/static/js/hooks/useHubmapIds.ts
@@ -9,10 +9,7 @@ function useHubmapIds(uuids: string[]) {
     _source: ['hubmap_id'],
     size: 10000,
   };
-
-  const { searchHits, isLoading } = useSearchHits<Dataset>(datasetQuery, {
-    shouldFetch: uuids.length > 0,
-  });
+  const { searchHits, isLoading } = useSearchHits<Dataset>(datasetQuery);
   const hubmapIds = searchHits.map(({ _source }) => _source.hubmap_id);
 
   return {
