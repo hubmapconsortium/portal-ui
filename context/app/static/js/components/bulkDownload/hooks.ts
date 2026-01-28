c095f7b755d23d5efe6144317172dc5cf4b39a72
Nikolay Akhmetov <nick.akhmetov@gmail.com>
2026-01-28 14:26:20 -0500

    Improve bulk download UX by preselecting by default

--- context/app/static/js/components/bulkDownload/hooks.ts
+++ context/app/static/js/components/bulkDownload/hooks.ts
@@ -57,7 +57,7 @@ function useBulkDownloadForm() {
 
 function useBulkDownloadDialog(deselectRows?: (uuids: string[]) => void) {
   const { isOpen, uuids, open, close, setUuids, setDownloadSuccess } = useBulkDownloadStore();
-  const { control, handleSubmit, errors, reset, trigger } = useBulkDownloadForm();
+  const { control, handleSubmit, errors, reset, trigger, setValue } = useBulkDownloadForm();
   const { toastErrorDownloadFile, toastSuccessDownloadFile } = useBulkDownloadToasts();
 
   // Fetch datasets for the selected uuids
@@ -100,6 +100,20 @@ function useBulkDownloadDialog(deselectRows?: (uuids: string[]) => void) {
     deselectRows: removeUuidsOrRows,
     restrictedDatasetsErrorMessage,
   });
+
+  // Set all download options as selected by default when options are loaded and no restricted IDs are present
+  useEffect(() => {
+    if (downloadOptions.length > 0 && isOpen && restrictedDatasetsFields.restrictedHubmapIds.length === 0) {
+      setValue(
+        'bulkDownloadOptions',
+        downloadOptions.map((option) => option.key),
+      );
+      // Re-trigger validation after setting default values
+      trigger('bulkDownloadOptions').catch((e) => {
+        console.error(e);
+      });
+    }
+  }, [downloadOptions, isOpen, restrictedDatasetsFields.restrictedHubmapIds.length, setValue, trigger]);
 
   const downloadMetadata = useCallback(
     (datasetsToDownload: BulkDownloadDataset[]) => {
