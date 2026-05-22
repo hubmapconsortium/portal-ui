cad0a996d78196ef2f7122e69e8de62418cf3081
Nikolay Akhmetov <nick.akhmetov@gmail.com>
2026-05-22 16:17:35 -0400

    Fix top level nav appearing below full screen yac

--- context/app/static/js/components/detailPage/visualization/Visualization/style.ts
+++ context/app/static/js/components/detailPage/visualization/Visualization/style.ts
@@ -40,7 +40,7 @@ const ExpandableDiv = styled('div')<ExpandableDivProps>(
     top: $isExpanded ? `${totalHeightOffset}px` : 'auto',
     left: $isExpanded ? '0' : 'auto',
     position: $isExpanded ? 'fixed' : 'relative',
-    zIndex: $isExpanded ? theme.zIndex.modal : 'auto',
+    zIndex: $isExpanded ? 800 : 'auto',
     height: $isExpanded ? `calc(100vh - ${totalHeightOffset}px)` : `${$nonExpandedHeight}px`,
     backgroundColor: $theme === 'dark' ? '#333333' : theme.palette.white.main,
     width: '100%',
