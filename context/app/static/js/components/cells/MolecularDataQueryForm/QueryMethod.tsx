ffae10e68f57b03146fec07cecff919842c7408e
Nikolay Akhmetov <nick.akhmetov@gmail.com>
2026-09-10 13:54:51 -0400

    simplify no longer necessary multi-case constants

--- context/app/static/js/components/cells/MolecularDataQueryForm/QueryMethod.tsx
+++ context/app/static/js/components/cells/MolecularDataQueryForm/QueryMethod.tsx
@@ -7,41 +7,28 @@ import { useMolecularDataQueryFormState } from './hooks';
 import { FormFieldContainer, FormFieldSubtitle } from './FormField';
 import { useMolecularDataQueryFormTracking } from './MolecularDataQueryFormTrackingProvider';
 
-const queryMethods = {
-  gene: [
-    { value: 'scFind', label: 'RNAseq (gene expression)', disabled: false },
-    { value: 'scFindATAC', label: 'ATACseq (DNA accessibility)', disabled: false },
-  ],
-  'cell-type': [
-    { value: 'scFind', label: 'RNAseq (gene expression)', disabled: false },
-    { value: 'scFindATAC', label: 'ATACseq (DNA accessibility)', disabled: false },
-  ],
-};
+const queryMethods = [
+  { value: 'scFind', label: 'RNAseq (gene expression)', disabled: false },
+  { value: 'scFindATAC', label: 'ATACseq (DNA accessibility)', disabled: false },
+];
 
 const scFindLink = <OutboundIconLink href="https://doi.org/10.1038/s41592-021-01076-9">scFind</OutboundIconLink>;
 
-const description = {
-  gene: <>Choose the query method to retrieve data. {scFindLink} supports RNAseq and ATACseq analyses.</>,
-  'cell-type': <>Choose the query method to retrieve data. {scFindLink} supports RNAseq and ATACseq data.</>,
-};
+const description = (
+  <>
+    Choose between RNAseq (gene expression) and ATACseq (DNA accessibility) experiments to retrieve data. Methodology
+    Source: {scFindLink}.
+  </>
+);
 
 export default function QueryMethod() {
   const { watch, register } = useMolecularDataQueryFormState();
 
-  const queryType = watch('queryType');
-
   const { track } = useMolecularDataQueryFormTracking();
-
-  const currentQueryMethods = queryMethods[queryType];
-  const currentDescription = description[queryType];
-
-  if (!currentQueryMethods || !currentDescription) {
-    return null;
-  }
 
   return (
     <FormFieldContainer title="Query Method">
-      <FormFieldSubtitle>{currentDescription}</FormFieldSubtitle>
+      <FormFieldSubtitle>{description}</FormFieldSubtitle>
       <TextField
         id="query-method-select"
         label="Query Method"
@@ -65,7 +52,7 @@ export default function QueryMethod() {
           },
         }}
       >
-        {currentQueryMethods.map((method) => (
+        {queryMethods.map((method) => (
           <MenuItem value={method.value} key={method.value} disabled={method.disabled}>
             {method.label} {method.disabled && '(coming soon)'}
           </MenuItem>
